1a8cc054539505f7b4759a29e1acf009e3b950b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:43 +0000

    Add package pyleniumio/1.8.1/pyleniumio-1.8.1.tar.gz

---
+++ packages/pyleniumio/pyleniumio-1.8.1.tar.gz/pyleniumio-1.8.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pyleniumio',
+    version='1.8.1',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/ElSnoMan/pyleniumio',
+    license='MIT',
+    author='Carlos Kidman',
+    author_email='carlos@qap.dev',
+    description='The best of Selenium and Cypress in a single Python Package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'selenium', 'pytest', 'pytest-xdist', 'pydantic', 'faker', 'requests', 'webdriver-manager', 'click'
+    ],
+    data_files=[('', ['pylenium/scripts/pylenium.json', 'pylenium/scripts/drag_and_drop.js'])],
+    entry_points='''
+        [console_scripts]
+        pylenium=pylenium.scripts.cli:cli
+    '''
+)
