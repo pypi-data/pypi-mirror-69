94e72fa995705ac6d1b5a6f862048ecc519e08fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:05 +0000

    Add package testpip-install/0.1/testpip-install-0.1.tar.gz

---
+++ packages/testpip-install/testpip-install-0.1.tar.gz/testpip-install-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='testpip-install',
+      version='0.1',
+      description='test pip install how to use',
+      url='http://github.com/tongling/clinicaltrial',
+      author='Ling',
+      author_email='aguan.zhang@megatronix.co',
+      license='MIT',
+      packages=['testpip'],
+      zip_safe=False)
---
+++ packages/testpip-install/testpip-install-0.1.tar.gz/testpip-install-0.1/testpip/test_install.py
@@ -0,0 +1,3 @@
+def tohex(a):
+    print("1234566")
+    return hex(a)
