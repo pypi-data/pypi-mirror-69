2e93640c00169d4dce5794f7afa374023d761ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:45 +0000

    Add package ddc/0.0.4/ddc-0.0.4.tar.gz

---
+++ packages/ddc/ddc-0.0.4.tar.gz/ddc-0.0.4/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+import sys
+from codecs import open
+from setuptools import setup
+from ddc import info
+
+if sys.version_info < (3, 6, 0):
+    print("Python 3.6+ is required")
+    exit(1)
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+DEPENDENCIES = ["requests>=2.19.1"]
+
+setup(
+    name=info.__package_name__,
+    version=info.__version__,
+    scripts=['bin/ddc'],
+    description="Devision Developers Cli",
+    long_description="",
+    url="https://github.com/devision-io/ddc",
+    author="Devision",
+    author_email="info@devision.io",
+    license="Apache 2.0",
+    zip_safe=False,
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    install_requires=DEPENDENCIES,
+    python_requires=">=3.6",
+    packages=["ddc"],
+    package_data={"": ["LICENSE"]},
+    platforms='Posix; MacOS X; Windows',
+    include_package_data=True,
+)
