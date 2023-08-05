fef50012b9fd34c4d06c647e8365a644e8259af0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:32 +0000

    Add package crack/0.2.1/crack-0.2.1.tar.gz

---
+++ packages/crack/crack-0.2.1.tar.gz/crack-0.2.1/crack/run.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# @author: leesoar
+# @email: secure@tom.com
+# @email2: employ@aliyun.com
+
+import argparse
+
+from crack import __version__
+
+parser = argparse.ArgumentParser(description=f"Crack everything.", prog="crack", add_help=False)
+parser.add_argument('-v', '--version', action='version', version=__version__, help='Get version of anole')
+parser.add_argument('-h', '--help', action='help', help='Show help message')
+parser.add_argument('-c', '--code', type=str, help="Curl code to parse")
+
+c = parser.parse_args().code
+
+
+def run():
+    return "Powered by leesoar.com"
