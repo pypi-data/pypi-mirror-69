2e9bc72e904675d4019958a2d7f93beb8e152ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:14 +0000

    Add package Conger/0.2.2/Conger-0.2.2.tar.gz

---
+++ packages/Conger/Conger-0.2.2.tar.gz/Conger-0.2.2/conger/__init__.py
@@ -0,0 +1,37 @@
+from .components import *
+import eel
+
+def get_input_value(serial: str):
+    return eel.get_input_text(serial)()
+
+
+def set_input_value(serial: str, text: str):
+    return eel.set_input_text(serial, text)
+
+
+def set_text(serial: str, text: str):
+    eel.set_p_text(serial, text)
+
+
+def set_image_src(serial: str, src: str):
+    eel.set_image_src(serial, src)
+
+
+def set_background(serial: str, color: str):
+    eel.set_background(serial, color)
+
+
+def set_font_color(serial: str, color: str):
+    eel.set_text_color(serial, color)
+
+
+def init(component: BaseComponent):
+    html = component.html()
+    with open('main.html', 'w') as f:
+        f.write(html)
+    print(html)
+    eel.init('')
+
+
+def start():
+    eel.start('main.html')
---
+++ packages/Conger/Conger-0.2.2.tar.gz/Conger-0.2.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='Conger',
+        version='0.2.2',
+        packages=['conger'],
+        url='https://github.com/guo40020/Conger',
+        license='MIT',
+        author='Kelly',
+        author_email='',
+        description='',
+        install_requires=['eel', 'pexpect'],
+)
