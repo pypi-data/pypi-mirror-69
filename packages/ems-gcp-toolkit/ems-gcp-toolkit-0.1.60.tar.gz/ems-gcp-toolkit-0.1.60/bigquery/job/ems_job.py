f3891256b5aa5dff3c269e775f68e899b63c0279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:31 +0000

    Add package ems-gcp-toolkit/0.1.60/ems-gcp-toolkit-0.1.60.tar.gz

---
+++ packages/ems-gcp-toolkit/ems-gcp-toolkit-0.1.60.tar.gz/ems-gcp-toolkit-0.1.60/bigquery/job/ems_job.py
@@ -0,0 +1,31 @@
+from abc import ABC
+from typing import Union
+
+from bigquery.job.ems_job_state import EmsJobState
+
+
+class EmsJob(ABC):
+
+    def __init__(self,
+                 job_id: str,
+                 state: EmsJobState,
+                 error_result: Union[dict, None]):
+        self.__job_id = job_id
+        self.__state = state
+        self.__error_result = error_result
+
+    @property
+    def state(self) -> EmsJobState:
+        return self.__state
+
+    @property
+    def job_id(self) -> str:
+        return self.__job_id
+
+    @property
+    def is_failed(self) -> bool:
+        return self.__error_result is not None
+
+    @property
+    def error_result(self) -> Union[dict, None]:
+        return self.__error_result
