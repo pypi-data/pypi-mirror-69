5923ad5b7ad11af2bf3464af60d6cb15090a814d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:03 +0000

    Add package waterch-tasker/0.0.4.202005220217/waterch-tasker-0.0.4.202005220217.tar.gz

---
+++ packages/waterch-tasker/waterch-tasker-0.0.4.202005220217.tar.gz/waterch-tasker-0.0.4.202005220217/src/waterch/tasker/__init__.py
@@ -0,0 +1,17 @@
+__all__ = [
+    'Profile',
+    'Definition',
+    'Return',
+    'storage',
+    'mixin',
+    'value',
+    'include',
+    'launch',
+    'utils',
+    'tasks'
+]
+
+from waterch.tasker import storage, mixin, utils, tasks
+from waterch.tasker.launcher import launch
+from waterch.tasker.mixin import value, include
+from waterch.tasker.typedef import Profile, Definition, Return
---
+++ packages/waterch-tasker/waterch-tasker-0.0.4.202005220217.tar.gz/waterch-tasker-0.0.4.202005220217/src/waterch/tasker/tasks/__init__.py
@@ -0,0 +1,7 @@
+__all__ = [
+    'Task',
+    'utils'
+]
+
+from waterch.tasker.tasks import utils
+from waterch.tasker.tasks.base import Task
