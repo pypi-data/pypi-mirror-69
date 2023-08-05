eb821624c652d3f5c8b377c898d757b457ad5a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:01 +0000

    Add package MovieKit/1.3/MovieKit-1.3.tar.gz

---
+++ packages/MovieKit/MovieKit-1.3.tar.gz/MovieKit-1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='MovieKit',
+      version='1.3',
+      description='A python package to store movies you\'ve watched',
+      packages=['MovieKit'],
+      author='Maximilian Müller',
+      license='MIT',
+      download_url='https://github.com/muellermax/MovieKit/archive/1.2.tar.gz',
+      url='https://github.com/muellermax/',
+      keywords=['movie', 'MovieKit', 'diary', 'moviediary', 'statistics'],
+      install_requires=['pandas', 'numpy', 'matplotlib', 'seaborn'],
+      zip_safe=False)
