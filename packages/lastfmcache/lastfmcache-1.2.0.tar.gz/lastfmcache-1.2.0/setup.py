b01f3868e02ace255d8f342b5aec800c725782ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package lastfmcache/1.2.0/lastfmcache-1.2.0.tar.gz

---
+++ packages/lastfmcache/lastfmcache-1.2.0.tar.gz/lastfmcache-1.2.0/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+    name = 'lastfmcache',
+    packages = ['lastfmcache'],
+    version = '1.2.0',
+    description = 'Python interface to the Last.FM API/website with caching support',
+    url = 'https://github.com/spiritualized/lastfmcache',
+    download_url = 'https://github.com/spiritualized/lastfmcache/archive/v1.2.0.tar.gz',
+    keywords = ['lastfm', 'python', 'cache', 'api'],
+    install_requires = [
+                    'bs4',
+                    'requests',
+                    'html5lib',
+                    'pylast',
+                    'pymysql',
+                    'sqlalchemy',
+                    'sqlalchemy-utils'
+                ],
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
