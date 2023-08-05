eb0897319fb29faa710e64acd458d20c21683e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:56 +0000

    Add package tccli/3.0.181.1/tccli-3.0.181.1.tar.gz

---
+++ packages/tccli/tccli-3.0.181.1.tar.gz/tccli-3.0.181.1/tccli/services/taf/v20200210/help.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+DESC = "taf-2020-02-10"
+INFO = {
+  "EnhanceTaDegree": {
+    "params": [
+      {
+        "name": "BspData",
+        "desc": "业务数据"
+      }
+    ],
+    "desc": "EnhanceTaDegree"
+  },
+  "RecognizeTargetAudience": {
+    "params": [
+      {
+        "name": "BspData",
+        "desc": "业务数据"
+      }
+    ],
+    "desc": "流量反欺诈-流量验准"
+  },
+  "DetectAccountActivity": {
+    "params": [
+      {
+        "name": "BusinessSecurityData",
+        "desc": "业务入参"
+      }
+    ],
+    "desc": "DetectAccountActivity"
+  },
+  "RecognizeCustomizedAudience": {
+    "params": [
+      {
+        "name": "BspData",
+        "desc": "业务入参"
+      }
+    ],
+    "desc": "流量反欺诈-流量验准定制版"
+  },
+  "DetectFraudKOL": {
+    "params": [
+      {
+        "name": "BspData",
+        "desc": "业务数据"
+      }
+    ],
+    "desc": "DetectFraudKOL"
+  },
+  "RecognizePreciseTargetAudience": {
+    "params": [
+      {
+        "name": "BspData",
+        "desc": "业务数据"
+      }
+    ],
+    "desc": "RecognizePreciseTargetAudience"
+  },
+  "SendTrafficSecuritySmsMessage": {
+    "params": [
+      {
+        "name": "BspData",
+        "desc": "业务入参"
+      }
+    ],
+    "desc": "SendTrafficSecuritySmsMessage"
+  }
+}
