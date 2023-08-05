ec565ab5c83a23a6b8df5c62a8826d71ab482320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:21 +0000

    Add package django-helpful/1.5.5/django-helpful-1.5.5.tar.gz

---
+++ packages/django-helpful/django-helpful-1.5.5.tar.gz/django-helpful-1.5.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+# python setup.py register
+# python setup.py sdist upload
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
+
+setup(
+	name="django-helpful",
+	version="1.5.5",
+	description="Django helpful things",
+	long_description=open('README.rst').read(),
+	author="Stanislav Baltrunas",
+	author_email="stanislav@baltrunas.ru",
+	license="BSD",
+	url="https://github.com/Baltrunas/django-helpful",
+	packages=find_packages(exclude=["tests.*", "tests"]),
+	package_data={
+		'templates' :['*'],
+		'static' :['*'],
+		'locale': ['*'],
+		'docs': ['*'],
+	},
+	include_package_data=True,
+	install_requires=[],
+	python_requires='>=2.6, <4',
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Environment :: Web Environment",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: BSD License",
+		"Operating System :: OS Independent",
+		"Programming Language :: Python",
+		"Framework :: Django",
+	],
+	zip_safe=False,
+)
