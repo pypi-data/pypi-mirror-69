08a3f901f30f397b7cd4824bab3c47d8b8b59bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:13 +0000

    Add package athena-run/0.1.27.dev0/athena_run-0.1.27.dev0-py3-none-any.whl

---
+++ packages/athena-run/athena_run-0.1.27.dev0-py3-none-any.whl/athena/bootstrap/sitecustomize.py
@@ -0,0 +1,19 @@
+"""
+Bootstrapping code that is run when using the `athena-run` Python entrypoint
+Add all monkey-patching that needs to run by default here
+"""
+from __future__ import print_function
+
+import logging; log = logging.getLogger(__name__)
+
+try:
+    from athena.patch import patch_all
+    patch_all()  # noqa
+
+    from athena.patch.statsd_config import DEFAULT_STATSD_CLIENT as stats
+    _main_timer = stats.timer("main")
+    setattr(stats, '_main_timer', _main_timer)
+    _main_timer.start()
+except Exception as e:
+    log.exception(e)
+    log.warning("errors configuring Athena tracing")
