c7819ee5fbb52d37556a639d98862dfcc6358487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:35 +0000

    Add package udp/0.0.6/udp-0.0.6-py3-none-any.whl

---
+++ packages/udp/udp-0.0.6-py3-none-any.whl/udp/peer.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+#
+#   UDP: User Datagram Protocol
+#
+#                                Written in 2020 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+"""
+    Peer
+    ~~~~
+
+    Network Node
+"""
+
+import threading
+import time
+from abc import ABC, abstractmethod
+from typing import Union, Optional
+
+from .data import uint32_to_bytes
+from .protocol import Package
+from .protocol import Command, CommandRespond
+from .protocol import Message, MessageRespond, MessageFragment
+from .task import Departure, Arrival, Pool
+from .hub import HubListener
+
+
+class PeerDelegate(ABC):
+
+    @abstractmethod
+    def send_data(self, data: bytes, destination: tuple, source: Union[tuple, int]=None) -> int:
+        """
+        Send data to destination address
+
+        :param data:        data package to send
+        :param destination: remote address
+        :param source:      local address or port number
+        :return: -1 on error
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def received_command(self, cmd: bytes, source: tuple, destination: tuple) -> bool:
+        """
+        Received command data from source address
+
+        :param cmd:         command data (package body) received
+        :param source:      remote address
+        :param destination: local address
+        :return: False on error
+        """
+        raise NotImplemented
+
+    @abstractmethod
+    def received_message(self, msg: bytes, source: tuple, destination: tuple) -> bool:
+        """
+        Received message data from source address
+
+        :param msg:         message data (package body) received
+        :param source:      remote address
+        :param destination: local address
+        :return: False on error
+        """
+        raise NotImplemented
+
+
+class Peer(threading.Thread, HubListener):
+
+    def __init__(self):
+        super().__init__()
+        self.delegate: PeerDelegate = None
+        self.running = True
+        self.__pool = Pool()
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        while self.running:
+            # first, process all arrivals
+            done = self.__clean_arrivals()
+            # second, get one departure task
+            task = self.__pool.get_departure()
+            if task is None:
+                # if no departure task, remove expired fragments
+                self.__pool.discard_fragments()
+                if done == 0:
+                    # all jobs done, have a rest. ^_^
+                    time.sleep(0.1)
+            else:
+                # redo this departure task
+                self.__send(task=task)
+
+    def __clean_arrivals(self) -> int:
+        """
+        Process the received packages in waiting list
+
+        :return: False on no data
+        """
+        total = self.__pool.arrivals_count()
+        done = 0
+        while done < total:
+            task = self.__pool.get_arrival()
+            if task is None:
+                # no data now
+                break
+            self.__handle(task=task)
+            done += 1
+        return done
+
+    def __handle(self, task: Arrival):
+        pack = Package.parse(data=task.payload)
+        assert pack is not None, 'package error: %s' % task.payload
+        head = pack.head
+        body = pack.body
+        data_type = head.data_type
+        if data_type == CommandRespond or data_type == MessageRespond:
+            self.__pool.del_departure(response=pack)
+            return None
+        elif data_type == Command:
+            ack = self.delegate.received_command(cmd=body, source=task.source, destination=task.destination)
+        elif data_type == Message:
+            ack = self.delegate.received_message(msg=body, source=task.source, destination=task.destination)
+        else:
+            assert data_type == MessageFragment, 'data type error: %s' % data_type
+            # assemble fragments
+            msg = self.__pool.add_fragment(fragment=pack)
+            if msg is None:
+                ack = True
+            else:
+                # all fragments received
+                ack = self.delegate.received_message(msg=msg, source=task.source, destination=task.destination)
+        # respond to the sender
+        if ack:
+            self.__respond(pack=pack, remote=task.source, local=task.destination)
+
+    def __respond(self, pack: Package, remote: tuple, local: tuple):
+        head = pack.head
+        data_type = head.data_type
+        if data_type == Command:
+            data_type = CommandRespond
+            body = b''
+        elif data_type == Message:
+            data_type = MessageRespond
+            body = b''
+        elif data_type == MessageFragment:
+            data_type = MessageRespond
+            body = uint32_to_bytes(head.pages) + uint32_to_bytes(head.offset)
+        else:
+            raise TypeError('data type error: %s' % data_type)
+        sn = head.trans_id
+        response = Package.new(data_type=data_type, sn=sn, body=body)
+        # send response directly, don't add this task to waiting list
+        res = self.delegate.send_data(data=response.data, destination=remote, source=local)
+        if res < 0:
+            raise IOError('failed to respond %s: %s' % (data_type, remote))
+
+    #
+    #   Sending
+    #
+    def __send(self, task: Departure) -> Optional[Departure]:
+        # treat the task as a bundle of packages
+        if isinstance(task.payload, Package):
+            pack = [task.payload]
+        else:
+            assert isinstance(task.payload, list), 'pack error: %s' % task.payload
+            pack = task.payload
+        for item in pack:
+            res = self.delegate.send_data(data=item.data, destination=task.destination, source=task.source)
+            if res < 0:
+                raise IOError('failed to resend task (%d packages) to: %s' % (len(pack), task.destination))
+        if task.max_retries > 0:
+            task.last_time = time.time()
+            task.max_retries -= 1
+            self.__pool.add_departure(task=task)
+            return task
+
+    def send_command(self, data: bytes, destination: tuple, source: Union[tuple, int]=None):
+        pack = Package.new(data_type=Command, body=data)
+        task = Departure(payload=pack, destination=destination, source=source)
+        self.__send(task=task)
+
+    def send_message(self, data: bytes, destination: tuple, source: Union[tuple, int]=None):
+        pack = Package.new(data_type=Message, body=data)
+        # check body length
+        if len(data) > Package.MAX_BODY_LEN:
+            pack = Package.split(package=pack)
+        task = Departure(payload=pack, destination=destination, source=source)
+        self.__send(task=task)
+
+    #
+    #   HubListener
+    #
+    def received(self, data: bytes, source: tuple, destination: tuple) -> Optional[bytes]:
+        task = Arrival(payload=data, source=source, destination=destination)
+        self.__pool.add_arrival(task=task)
+        return None
---
+++ packages/udp/udp-0.0.6-py3-none-any.whl/udp/task.py
@@ -0,0 +1,288 @@
+# -*- coding: utf-8 -*-
+#
+#   UDP: User Datagram Protocol
+#
+#                                Written in 2020 by Moky <albert.moky@gmail.com>
+#
+# ==============================================================================
+# MIT License
+#
+# Copyright (c) 2019 Albert Moky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
+
+"""
+    Peer
+    ~~~~
+
+    Network Node
+"""
+
+import threading
+import time
+from typing import Union, Optional
+
+from .data import bytes_to_int
+from .protocol import Package
+from .protocol import Command, CommandRespond
+from .protocol import Message, MessageRespond, MessageFragment
+
+
+class Departure:
+    """
+        Package(s) to sent out (waiting response)
+    """
+
+    EXPIRES = 60  # 1 minute
+
+    def __init__(self, payload: Union[Package, list], destination: tuple, source: Union[tuple, int]=0):
+        super().__init__()
+        self.payload = payload
+        self.destination = destination
+        self.source = source
+        self.max_retries = 5
+        self.last_time = 0
+
+    @property
+    def is_expired(self) -> bool:
+        return (self.last_time + self.EXPIRES) < time.time()
+
+
+class Arrival:
+    """
+        Data package received (waiting process)
+    """
+
+    def __init__(self, payload: bytes, source: tuple, destination: tuple):
+        super().__init__()
+        self.payload = payload
+        self.source = source
+        self.destination = destination
+
+
+class Assemble:
+    """
+        Message fragments received (waiting assemble)
+    """
+
+    EXPIRES = 60  # 1 minutes
+
+    def __init__(self, fragment: Package):
+        super().__init__()
+        self.__fragments = [fragment]
+        self.pages = fragment.head.pages
+        assert self.pages > 1, 'fragment pages error: %s' % fragment.head
+        self.last_time = time.time()
+
+    @property
+    def fragments(self) -> list:
+        return self.__fragments
+
+    @property
+    def is_expired(self) -> bool:
+        count = len(self.__fragments)
+        assert count > 0, 'fragments error'
+        return (self.last_time + self.EXPIRES * self.pages) < time.time()
+
+    @property
+    def is_completed(self) -> bool:
+        return len(self.__fragments) == self.pages
+
+    def insert(self, fragment: Package) -> bool:
+        count = len(self.__fragments)
+        assert count > 0, 'fragments error'
+        head = fragment.head
+        offset = head.offset
+        assert head.data_type == MessageFragment, 'data type error: %s' % head
+        assert head.trans_id == self.__fragments[0].head.trans_id, 'transaction ID not match: %s' % head
+        assert head.pages == self.pages, 'pages error: %s' % head
+        assert head.pages > offset, 'offset error: %s' % head
+        index = count
+        while index > 0:
+            index -= 1
+            item = self.__fragments[index]
+            if offset < item.head.offset:
+                continue
+            elif offset == item.head.offset:
+                # assert False, 'duplicated: %s' % head
+                return False
+            self.__fragments.insert(index, fragment)
+            self.last_time = time.time()
+            return True
+
+
+class Pool:
+
+    def __init__(self):
+        super().__init__()
+        # waiting list for responding
+        self.__departures = []
+        self.__departures_lock = threading.Lock()
+        # waiting list for processing
+        self.__arrivals = []
+        self.__arrivals_lock = threading.Lock()
+        # waiting list for assembling
+        self.__fragments = {}  # TransactionID -> Assemble
+        self.__fragments_lock = threading.Lock()
+
+    #
+    #   Departures
+    #
+    def departures_count(self) -> int:
+        with self.__departures_lock:
+            return len(self.__departures)
+
+    def get_departure(self) -> Optional[Departure]:
+        with self.__departures_lock:
+            if len(self.__departures) > 0:
+                # check last sent time
+                task = self.__departures[0]
+                if task.is_expired:
+                    return self.__departures.pop(0)
+
+    def add_departure(self, task: Departure) -> Departure:
+        with self.__departures_lock:
+            self.__departures.append(task)
+        return task
+
+    def del_departure(self, response: Package) -> int:
+        count = 0
+        head = response.head
+        body = response.body
+        body_len = len(body)
+        data_type = head.data_type
+        trans_id = head.trans_id
+        if data_type == CommandRespond:
+            assert body_len == 0, 'CommandRespond should has no body'
+            with self.__departures_lock:
+                pos = len(self.__departures)
+                while pos > 0:
+                    pos -= 1
+                    pack = self.__departures[pos].payload
+                    if not isinstance(pack, Package):
+                        continue
+                    head = pack.head
+                    if head.trans_id != trans_id:
+                        continue
+                    assert head.data_type == Command, 'task payload not a Command: %s' % pack
+                    # Got it!
+                    self.__departures.pop(pos)
+                    count += 1
+        elif data_type == MessageRespond:
+            if body_len == 8:
+                # Message Fragment Response
+                pages = bytes_to_int(body[:4])
+                offset = bytes_to_int(body[4:])
+                assert pages > 1 and pages > offset, 'pages error: %d, %d' % (pages, offset)
+                with self.__departures_lock:
+                    pos = len(self.__departures)
+                    while pos > 0:
+                        pos -= 1
+                        packages = self.__departures[pos].payload
+                        if not isinstance(packages, list):
+                            continue
+                        index = len(packages)
+                        while index > 0:
+                            index -= 1
+                            pack = packages[index]
+                            head = pack.head
+                            if head.trans_id != trans_id:
+                                break
+                            assert head.data_type == MessageFragment, 'data type should be a Fragment: %s' % pack
+                            assert head.pages == pages, 'pages not match: %d, %s' % (pages, head)
+                            if head.offset == offset:
+                                # Got it!
+                                packages.pop(index)
+                                count += 1
+                        if len(packages) == 0:
+                            # all fragment sent, remove this task
+                            self.__departures.pop(pos)
+            else:
+                assert body_len == 0, 'MessageRespond body error: %s' % body
+                with self.__departures_lock:
+                    pos = len(self.__departures)
+                    while pos > 0:
+                        pos -= 1
+                        pack = self.__departures[pos].payload
+                        if not isinstance(pack, Package):
+                            continue
+                        head = pack.head
+                        if head.trans_id != trans_id:
+                            continue
+                        assert head.data_type == Message, 'task payload not a Message: %s' % pack
+                        # Got it!
+                        self.__departures.pop(pos)
+                        count += 1
+        else:
+            assert False, 'data type should be a Respond: %s' % response
+        return count
+
+    #
+    #   Arrivals
+    #
+    def arrivals_count(self) -> int:
+        with self.__arrivals_lock:
+            return len(self.__arrivals)
+
+    def get_arrival(self) -> Optional[Arrival]:
+        with self.__arrivals_lock:
+            if len(self.__arrivals) > 0:
+                return self.__arrivals.pop(0)
+
+    def add_arrival(self, task: Arrival) -> Arrival:
+        with self.__arrivals_lock:
+            self.__arrivals.append(task)
+        return task
+
+    #
+    #   Fragments Assembling
+    #
+    def add_fragment(self, fragment: Package) -> Optional[bytes]:
+        data = None
+        with self.__fragments_lock:
+            trans_id = fragment.head.trans_id
+            assemble = self.__fragments.get(trans_id)
+            if assemble is None:
+                self.__fragments[trans_id] = Assemble(fragment=fragment)
+            elif assemble.insert(fragment=fragment) and assemble.is_completed:
+                data = Package.join(packages=assemble.fragments)
+                self.__fragments.pop(trans_id)
+        return data
+
+    def discard_fragments(self) -> int:
+        """
+        Remove all expired fragments
+
+        :return:
+        """
+        count = 0
+        with self.__fragments_lock:
+            keys = list(self.__fragments.keys())
+            for trans_id in keys:
+                assemble = self.__fragments.get(trans_id)
+                if assemble is None:
+                    # error
+                    self.__fragments.pop(trans_id)
+                    count += 1
+                elif assemble.is_expired:
+                    # remove expired fragments
+                    self.__fragments.pop(trans_id)
+                    count += 1
+        return count
