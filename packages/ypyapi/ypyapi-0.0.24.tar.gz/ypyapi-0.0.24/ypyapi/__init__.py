e9c57574b14184914cdd74c9080dcf79c905334a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:28 +0000

    Add package ypyapi/0.0.24/ypyapi-0.0.24.tar.gz

---
+++ packages/ypyapi/ypyapi-0.0.24.tar.gz/ypyapi-0.0.24/ypyapi/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*- 
+# @Time : 2020/5/18 13:51
+# @Author : yangpingyan@gmail.com
+from .chrome_api import *
+from .thread_api import *
+
+__VERSION__ = '0.0.24'
+print(f"ypyapi version is: {__VERSION__}")
