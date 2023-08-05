a7a3b65137517d32e506591e35968c28fff80500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:26 +0000

    Add package lume/0.2.14/lume-0.2.14-py3-none-any.whl

---
+++ packages/lume/lume-0.2.14-py3-none-any.whl/lume/config/install_config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import List, Optional
+from lume.config.check_list_or_str_item import check_list_or_str_item
+
+
+@dataclass
+class InstallConfig:
+    run: List[str]
+    cwd: Optional[str] = None
+
+    @staticmethod
+    def from_dict(kdict):
+        run = check_list_or_str_item(kdict, "run")
+
+        return InstallConfig(run=run if run else [], cwd=kdict.get("cwd"))
