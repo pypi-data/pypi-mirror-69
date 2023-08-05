a92f314119f0d52f565e4676fc0ddedcd0efd239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:22 +0000

    Add package hendrics/5.0b1/hendrics-5.0b1.tar.gz

---
+++ packages/hendrics/hendrics-5.0b1.tar.gz/hendrics-5.0b1/docs/api/hendrics.test.rst
@@ -0,0 +1,8 @@
+
+
+test
+=============
+
+.. currentmodule:: hendrics
+
+.. autofunction:: test
---
+++ packages/hendrics/hendrics-5.0b1.tar.gz/hendrics-5.0b1/hendrics/version.py
@@ -0,0 +1,4 @@
+# Note that we need to fall back to the hard-coded version if either
+# setuptools_scm can't be imported or setuptools_scm can't determine the
+# version, so we catch the generic 'Exception'.
+version = '5.0b1'
