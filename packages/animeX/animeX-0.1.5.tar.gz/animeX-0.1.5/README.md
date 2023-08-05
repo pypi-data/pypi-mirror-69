0b987ef0982054f6fdd4f96890f19ccbe02f9ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:45 +0000

    Add package animeX/0.1.5/animeX-0.1.5.tar.gz

---
+++ packages/animeX/animeX-0.1.5.tar.gz/animeX-0.1.5/README.md
@@ -0,0 +1,2 @@
+# animeX-pack
+A lightweight Python library (and command-line utility) for downloading anime.
---
+++ packages/animeX/animeX-0.1.5.tar.gz/animeX-0.1.5/animeX/__init__.py
@@ -0,0 +1,5 @@
+__title__ = 'animeX'
+__version__ = '0.1.0'
+__author__ = 'Samuel Abada'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2020 Abada Samuel Oghenero'
