1a4d41d35b1c6938e10eb0fb2155b3a48f83dab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:12 +0000

    Add package sense-django/0.2.22/sense-django-0.2.22.tar.gz

---
+++ packages/sense-django/sense-django-0.2.22.tar.gz/sense-django-0.2.22/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+requirements = [
+    "sense_core>=0.1.4",
+    "numpy>=1.15",
+    "pypinyin>=0.33.1",
+]
+
+setup(
+    name='sense-django',
+    version='0.2.22',
+    packages=[
+            "sense_django",
+    ],
+    license='BSD License',  # example license
+    description='sense django',
+    install_requires=requirements,
+    long_description=__doc__,
+    url='',
+    author='kafka0102',
+    author_email='yujianjia@sensedeal.ai',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
