192e3900efed5189630b1b75fc3a5dd64c549668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:57 +0000

    Add package gdk/0.1/gdk-0.1.tar.gz

---
+++ packages/gdk/gdk-0.1.tar.gz/gdk-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gdk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gdk'],
+      author = 'Dinesh Kumar Gnanasekaran',
+      author_email = 'dinesh.gna111@gmail.com',
+      zip_safe=False)
