83db53c031d4e7b286a3ae0ce015be91668d463f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:06 +0000

    Add package sphinx-scylladb-theme/0.1.2/sphinx_scylladb_theme-0.1.2-py3-none-any.whl

---
+++ packages/sphinx-scylladb-theme/sphinx_scylladb_theme-0.1.2-py3-none-any.whl/sphinx_scylladb_theme/__init__.py
@@ -0,0 +1,11 @@
+from os import path
+from .extensions import panel_box, topic_box
+
+def setup(app):
+    """Setup theme and custom extensions."""
+    app.add_html_theme('sphinx_scylladb_theme', path.abspath(path.dirname(__file__)))
+
+    panel_box.setup(app)
+    topic_box.setup(app)
+
+__version__ = '0.1.2'
