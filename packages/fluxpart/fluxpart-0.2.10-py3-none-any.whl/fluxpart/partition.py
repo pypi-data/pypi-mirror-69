31d76eecd61a90f51ae1cbb6ecc30e2d7d798334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:12 +0000

    Add package fluxpart/0.2.10/fluxpart-0.2.10-py3-none-any.whl

---
+++ packages/fluxpart/fluxpart-0.2.10-py3-none-any.whl/fluxpart/partition.py
@@ -0,0 +1,422 @@
+"""TODO:"""
+import math
+from types import SimpleNamespace
+
+import numpy as np
+
+import fluxpart.util as util
+from .containers import FVSPSolution, MassFluxes, RootSoln, WQCData
+
+
+class Error(Exception):
+    pass
+
+
+class FVSError(Error):
+    def __init__(self, message):
+        self.message = message
+
+
+def fvspart_progressive(w, q, c, wue, adjust_fluxes=True):
+    """FVS flux partitioning using high frequency eddy covariance data.
+
+    If a valid partitioning solution is not found for the passed series
+    data, low-frequency (large-scale) components are progressively
+    removed from the data until either a valid solution is found or
+    the series decomposition is exhausted.
+
+    Parameters
+    ----------
+    w,q,c : array_like
+        1D high frequency time series for vertical wind speed `w` (m/s),
+        water vapor `q` (kg/m^3), and CO2 `c` (kg/m^3).
+    wue : float, `wue` < 0
+        leaf-level water use efficiency, kg CO2 / kg H2O.
+    adjust_fluxes : bool, optional
+        Indicates whether the obtained partitioned fluxes should be
+        adjusted so that the totals match the original data. Default is
+        True.
+
+    Returns
+    -------
+    :class:`~fluxpart.containers.FVSPSolution`
+
+    Notes
+    -----
+    If a valid partitioning is not found, the returned `numersoln` and
+    `wqc_data` correspond to the final iteration attempted.
+
+    """
+    max_decomp_lvl = int(np.log2(w.size))
+    wq_tot = np.cov((w, q))[0, 1]
+    wc_tot = np.cov((w, c))[0, 1]
+
+    # The loop progressively filters the data until a physically valid
+    # partitioning is found or the loop/filter is exhausted. The first
+    # iteration of progressive_lowcut removes only the mean value
+    # (q'=q-<q>, etc.), so the first iteration uses the "unfiltered"
+    # deviations.
+
+    for cnt, lowcut_wqc in enumerate(_progressive_lowcut(w, q, c)):
+        wave_lvl = (max_decomp_lvl - cnt, max_decomp_lvl)
+
+        fluxes, fvsp = fvspart_series(*lowcut_wqc, wue)
+        if cnt == 0:
+            if fvsp.fvsp_mssg:
+                mssg_for_unfiltered_data = fvsp.fvsp_mssg
+            else:
+                mssg_for_unfiltered_data = "NA"
+
+        if fvsp.rootsoln.valid_root:
+            if adjust_fluxes:
+                fluxes = _adjust_fluxes(fluxes, wue, wq_tot, wc_tot)
+                fvsp.valid_partition, fvsp.mssg = _isvalid_partition(fluxes)
+            if fvsp.valid_partition:
+                break
+
+    fvsp.wave_lvl = wave_lvl
+    if not fvsp.rootsoln.valid_root:
+        fvsp.valid_partition = False
+        fvsp.mssg = mssg_for_unfiltered_data
+    if not fvsp.valid_partition:
+        fluxes = MassFluxes()
+        fvsp.mssg = mssg_for_unfiltered_data
+    return fluxes, fvsp
+
+
+def fvspart_series(w, q, c, wue, wipe_if_invalid=False):
+    """FVS partition q and c fluxes using high frequency eddy cov data.
+
+    Parameters
+    ----------
+    w,q,c : array_like
+        1D high frequency time series for vertical wind speed `w` (m/s),
+        water vapor `q` (kg/m^3), and CO2 `c` (kg/m^3).
+    wue : float, `wue` < 0
+        leaf-level water use efficiency, kg CO2 / kg H2O.
+    wipe_if_invalid : boolean
+        If True, return default (nan) values for all mass fluxes if any
+        calculated fluxes violate directional requirements. Default is
+        False.
+
+    Returns
+    -------
+    :class:`~fluxpart.containers.FVSPSolution`,
+
+    """
+    cov = np.cov([w, q, c])
+    wqc_data = WQCData(
+        wq=cov[0, 1],
+        wc=cov[0, 2],
+        var_q=cov[1, 1],
+        var_c=cov[2, 2],
+        corr_qc=cov[1, 2] / math.sqrt(cov[1, 1] * cov[2, 2]),
+    )
+    return fvspart_interval(wqc_data, wue)
+
+
+def fvspart_interval(wqc_data, wue, wipe_if_invalid=False):
+    """Partition H2O and CO2 fluxes using interval averaged data values.
+
+    Parameters
+    ----------
+    wqc_data : :class:`~fluxpart.containers.WQCData`
+    wue : float, kg CO2 / kg H2O
+        Leaf-level water use efficiency, `wue` < 0
+    wipe_if_invalid : boolean
+        If True, return default (nan) values for all mass fluxes if any
+        calculated fluxes violate directional requirements. Default is
+        False.
+
+    Returns
+    -------
+    :class:`~fluxpart.containers.FVSPSolution`
+
+    """
+    try:
+        rootsoln = findroot(wqc_data, wue)
+    except FVSError as e:
+        fvsp = FVSPSolution(
+            wqc_data=wqc_data,
+            valid_partition=False,
+            fvsp_mssg=e.args[0],
+            rootsoln=RootSoln(valid_root=False),
+        )
+        return MassFluxes(), fvsp
+
+    if not rootsoln.valid_root:
+        fvsp = FVSPSolution(
+            wqc_data=wqc_data,
+            rootsoln=rootsoln,
+            valid_partition=False,
+            fvsp_mssg="Invalid Root",
+        )
+        return MassFluxes(), fvsp
+
+    mass_fluxes = _mass_fluxes(
+        var_cp=rootsoln.var_cp,
+        corr_cp_cr=rootsoln.corr_cp_cr,
+        wqc_data=wqc_data,
+        wue=wue,
+        co2soln_id=rootsoln.co2soln_id,
+    )
+    isvalid, mssg = _isvalid_partition(mass_fluxes)
+    if not isvalid and wipe_if_invalid:
+        mass_fluxes = MassFluxes()
+    fvsps = FVSPSolution(
+        wqc_data=wqc_data,
+        rootsoln=rootsoln,
+        valid_partition=isvalid,
+        fvsp_mssg=mssg,
+    )
+    return mass_fluxes, fvsps
+
+
+def findroot(wqc_data, wue):
+    """Calculate (corr_cp_cr, var_cp).
+
+    Parameters
+    ----------
+    wqc_data : namedtuple or equivalent namespace
+        :class:`~fluxpart.containers.WQCData`
+    wue : float
+        Leaf-level water use efficiency, `wue` < 0, kg CO2 / kg H2O.
+
+    Returns
+    -------
+    namedtuple
+        :class:`~fluxpart.containers.RootSoln`
+
+    """
+    try:
+        _check_fvsp_assumptions(wqc_data, wue)
+    except FVSError:
+        raise
+
+    co2soln_id = None
+    sig_cr, var_cp, corr_cp_cr = np.nan, np.nan, np.nan
+    valid_root = False
+
+    var_q, var_c = wqc_data.var_q, wqc_data.var_c
+    sd_q, sd_c = math.sqrt(var_q), math.sqrt(var_c)
+    wq, wc = wqc_data.wq, wqc_data.wc
+    corr_qc = wqc_data.corr_qc
+
+    # wcwq = wc / wq
+    # scsq = sd_c / sd_q
+
+    # if corr_qc > 0 and ((wue < wcwq < 0) or (0 < wcwq < scsq * corr_qc)):
+    #     co2soln_id = 0 # minus root
+    # if corr_qc < 0:
+    #     if wcwq * corr_qc < scsq < wcwq / corr_qc:
+    #         co2soln_id = 1 if scsq <= corr_qc * wue else 0
+
+    co2soln_id = 0 # minus root
+    if corr_qc < 0 and sd_c / sd_q < corr_qc * wue:
+        co2soln_id = 1 # plus root
+
+    # if co2soln_id in (0, 1):
+    if 1:
+        valid_root = True
+        mssg = ""
+
+        numer = -2 * corr_qc * sd_c * sd_q * wq * wc
+        numer += var_c * wq ** 2 + var_q * wc ** 2
+        numer *= -(corr_qc ** 2 - 1) * var_c * var_q * wue ** 2
+        denom = -corr_qc * sd_c * sd_q * (wc + wq * wue)
+        denom += var_c * wq + var_q * wc * wue
+        denom = denom ** 2
+
+        var_cp = numer / denom
+
+        numer = -(corr_qc ** 2 - 1) * var_c * var_q * (wc - wq * wue) ** 2
+        denom = -2 * corr_qc * sd_c * sd_q * wc * wq
+        denom += var_c * wq ** 2 + var_q * wc ** 2
+        denom *= -2 * corr_qc * sd_c * sd_q * wue + var_c + var_q * wue ** 2
+
+        rho_sq = numer / denom
+        corr_cp_cr = -math.sqrt(rho_sq)
+
+        wcr_ov_wcp = flux_ratio(
+            var_cp, corr_cp_cr, wqc_data, "co2", co2soln_id
+        )
+        sig_cr = wcr_ov_wcp * math.sqrt(var_cp) / corr_cp_cr
+
+    return RootSoln(
+        corr_cp_cr=corr_cp_cr,
+        var_cp=var_cp,
+        sig_cr=sig_cr,
+        co2soln_id=co2soln_id,
+        valid_root=valid_root,
+    )
+
+
+def flux_ratio(var_cp, corr_cp_cr, wqc_data, ftype, farg):
+    """Compute the nonstomatal:stomatal ratio of the H2O or CO2 flux.
+
+    The ratio (either wqe/wqt or wcr/wcp) is found by solving Eq. 13
+    of [SS08]_.
+
+    Parameters
+    ---------
+    wqc_data : namedtuple or equivalent namespace
+        :class:`~fluxpart.containers.WQCData`
+    ftype : {'co2', 'h2o'}
+        Specifies whether the flux is CO2 or H2O
+    farg : number
+        If `ftype` = 'co2', then `farg` = {0 or 1} specifies the root
+        of Eq. 13b to be used to calculate the CO2 flux ratio wcr/wcp:
+        `farg`=1 uses the '+' solution, `farg`=0 uses the '-' solution.
+        If `ftype` = 'h2o', then `farg` is a float equal to the water
+        use efficiency (wue < 0), kg/kg.
+
+    Returns
+    -------
+    fratio : float or np.nan
+        The requested flux ratio, wqe/wqt or wcr/wcp. Set to np.nan
+        if solution is not real.
+
+    Notes
+    -----
+    When solving for wqe/wqt, the '-' solution of the quadratic Eq. 13a
+    is not relevant because only the '+' solution yields wqe/wqt > 0,
+    which is required/assumed by the physical model in [SS08]_.
+
+    """
+    if ftype.lower() == "co2":
+        sign = 1 if farg == 1 else -1
+        num = wqc_data.var_c
+    else:  # is "h2o"
+        sign = 1
+        num = farg ** 2 * wqc_data.var_q
+    disc = 1 - 1 / corr_cp_cr ** 2 + num / var_cp / corr_cp_cr ** 2
+    if disc < 0:
+        fratio = np.nan
+    else:
+        fratio = corr_cp_cr ** 2 * (sign * math.sqrt(disc) - 1)
+    return fratio
+
+
+def _mass_fluxes(var_cp, corr_cp_cr, wqc_data, wue, co2soln_id):
+    """Calculate flux components for given (var_cp, corr_cp_cr) pair."""
+    wqe_ov_wqt = flux_ratio(var_cp, corr_cp_cr, wqc_data, "h2o", wue)
+    wqt = wqc_data.wq / (wqe_ov_wqt + 1)
+    wcp = wqt * wue
+    wcr = wqc_data.wc - wcp
+    wqe = wqc_data.wq - wqt
+    return MassFluxes(
+        Fq=wqc_data.wq, Fqt=wqt, Fqe=wqe, Fc=wqc_data.wc, Fcp=wcp, Fcr=wcr
+    )
+
+
+def _check_fvsp_assumptions(qcdat, wue):
+    pqc = qcdat.corr_qc
+    wcwq = qcdat.wc / qcdat.wq
+    scsq = np.sqrt(qcdat.var_c / qcdat.var_q)
+    mssg = ""
+    if wue > wcwq:
+        mssg += "wue>Fc/Fq; ".format(wue, wcwq)
+    if wcwq >= pqc * scsq:
+        m = "Fc/Fq>=pqc*sigc/sigq; "
+        mssg += m.format(wcwq, scsq * pqc)
+    if pqc < 0 and wcwq < scsq / pqc:
+        m = "Fc/Fq<sigc/sigq/pqc; "
+        mssg += m.format(wcwq, scsq / pqc)
+    if math.isclose(pqc, 0, abs_tol=1e-15):
+        mssg += "pqc=0;".format(pqc)
+    if mssg:
+        # vals = "(Fc/Fq={:.4}; sigc/sigq={:.4}; W={:.4}; pqc={:.4})"
+        # vals = vals.format(wcwq, scsq, wue, pqc)
+        raise FVSError(mssg) # + vals)
+
+
+def _isvalid_partition(flux_components):
+    """Test if partitioned flux directions (signs) are valid."""
+    fc = flux_components
+    isvalid = True
+    mssg = ""
+    if fc.Fqt <= 0:
+        isvalid = False
+        mssg += "Fqt <= 0; "
+    if fc.Fqe <= 0:
+        isvalid = False
+        mssg += "Fqe <= 0; "
+    if fc.Fcp >= 0:
+        isvalid = False
+        mssg += "Fcp >= 0; "
+    if fc.Fcr <= 0:
+        isvalid = False
+        mssg += "Fcr <= 0; "
+    return isvalid, mssg or "OK"
+
+
+def _adjust_fluxes(flux_components, wue, Fq_tot, Fc_tot):
+    """Adjust partitioned fluxes so they match measured totals.
+
+    If filtering has been applied to the series data, covariances in
+    the filtered data may differ from those in the original data.
+    Consequently, partitioned flux totals may not match exactly the
+    total fluxes indicated by the original data. Here, partitioned
+    fluxes are adjusted proportionally so that they match the totals in
+    the original data.
+
+    Parameters
+    ----------
+    flux_components : namedtuple or equivalent
+        Attributes (floats) specify the mass flux components (Fq, Fqe,
+        Fq, Fc, Fcr, Fcp), kg/m^2/s.
+    wue : float
+        Leaf-level water use efficiency (`wue` < 0), kg CO2 / kg H2O
+    Fq_tot, Fc_tot : float
+        Desired net total H2O (`Fq_tot`) and CO2 (`Fc_tot`) fluxes,
+        kg/m^2/s.
+
+    Returns
+    -------
+    namedtuple
+        :class:`~fluxpart.containers.MassFluxes`
+
+    """
+    fc = flux_components
+    Fq_diff = Fq_tot - (fc.Fqe + fc.Fqt)
+    Fqe = fc.Fqe + Fq_diff * (fc.Fqe / (fc.Fqt + fc.Fqe))
+    Fqt = Fq_tot - Fqe
+    Fcp = wue * Fqt
+    Fcr = Fc_tot - Fcp
+    return MassFluxes(Fq=Fq_tot, Fqt=Fqt, Fqe=Fqe, Fc=Fc_tot, Fcp=Fcp, Fcr=Fcr)
+
+
+def _progressive_lowcut(wind, vapor, co2):
+    """Apply progressive lowcut filter to wind, vapor, and CO2 series.
+
+    Use wavelet decomposition to yield a sequence of (w, q, c) series
+    in which low frequency (large scale) components are progressively
+    removed from w, q, c.
+
+    Parameters
+    ----------
+    wind,vapor,co2 : array-like
+        1D time series of vertical wind velocity (w), water vapor
+        concentration (q), and carbon dioxide concentration (c).
+
+    Yields
+    ------
+    (lc_w, lc_q, lc_c) : tuple of arrays
+        Arrays lc_w, lc_q, and lc_c are low cut (high pass) filtered
+        versions of the passed w,q,c series.
+
+    Notes
+    -----
+    Before the filter is applied, the data are truncated so that the
+    length is a power of 2.
+
+    """
+    max_pow2_len = 2 ** int(np.log2(np.asarray(co2).shape[0]))
+    trunc_w = np.asarray(wind)[:max_pow2_len]
+    trunc_q = np.asarray(vapor)[:max_pow2_len]
+    trunc_c = np.asarray(co2)[:max_pow2_len]
+    lowcut_w = util.progressive_lowcut_series(trunc_w)
+    lowcut_q = util.progressive_lowcut_series(trunc_q)
+    lowcut_c = util.progressive_lowcut_series(trunc_c)
+    for lowcut_series in zip(lowcut_w, lowcut_q, lowcut_c):
+        yield lowcut_series
---
+++ packages/fluxpart/fluxpart-0.2.10-py3-none-any.whl/fluxpart/wue.py
@@ -0,0 +1,327 @@
+from math import log, sqrt
+
+from .constants import GRAVITY, VON_KARMAN
+from .constants import MOLECULAR_WEIGHT as MW
+from .constants import SPECIFIC_GAS_CONSTANT as Rgas
+from .containers import WUE
+from .util import (
+    sat_vapor_press,
+    vapor_press_deficit,
+    vapor_press_deficit_mass,
+)
+
+
+_C3_DEFAULTS = dict(
+    const_ppm=280.0,  # ppm
+    const_ratio=0.7,
+    linear=(1, 1.6e-4),
+    sqrt=22e-9,  # kg-co2 / m^3 / Pa
+)
+
+_C4_DEFAULTS = dict(const_ppm=130.0, const_ratio=0.44, linear=(1, 2.7e-4))
+
+CI_DEFAULT_PARAMS = dict(C3=_C3_DEFAULTS, C4=_C4_DEFAULTS)
+
+
+class Error(Exception):
+    pass
+
+
+class WUEError(Error):
+    def __init__(self, message):
+        self.message = message
+
+
+def water_use_efficiency(
+    hfs,
+    ci_mod,
+    ci_mod_param=None,
+    leaf_temper=None,
+    leaf_temper_corr=0,
+    meas_ht=None,
+    canopy_ht=None,
+    ppath=None,
+    diff_ratio=1.6,
+):
+
+    """Estimate leaf-level water use efficiency.
+
+    Parameters
+    ----------
+    hfs : :class:`~fluxpart.hfdata.HFSummary` namedtuple
+        Summary statistics for high frequency eddy covariance data interval.
+        Possesses the following attributes (all floats):
+        `rho_vapor`, mean vapor density (kg/m^3);
+        `rho_co2`, mean carbon dioxide concentration (kg/m^3);
+        `T`, mean air temperature (K);
+        `P`, mean atmospheric pressure (Pa);
+        `cov_w_q`, covariance of vert wind velocity and H2O density (kg/m^2/s);
+        `cov_w_q`, covariance of vert wind velocity and CO2 density (kg/m^2/s);
+        `cov_w_T`, covariance of wind and temperature (K m/s);
+        `ustar`, friction velocity (m/s);
+        `rho_totair`, moist air density (kg/m^3).
+    meas_ht : float or callable, optional
+        Eddy covariance measurement height (m).
+    canopy_ht : float or callable, optional
+        Vegetation canopy height (m).
+    ppath : {'C3', 'C4'}
+        photosynthetic pathway
+    ci_mod : {'const_ratio', 'const_ppm', 'linear', 'sqrt', 'opt'}
+        Specifies the model to be used to determine the leaf
+        intercellular CO2 concentration. See Notes below for model
+        descriptions.
+    ci_mod_param : float or 2-tuple of floats, optional
+        Paramter values to be used with `ci_mod`. The number of
+        parameters required depends on the model (see Notes). The
+        default is ci_mod_param=None, in which case default values are
+        used.
+    leaf_temper : float, optional
+        Measured canopy tempeature in degrees K. If None (defalut), the
+        leaf temperature is taken to be equal to the air temperature in
+        `hfs`.
+    leaf_temper_corr : float, optional
+        Optional adjustment to leaf temperature. The temperature used to
+        calculate intercelluar vapor and CO2 concentrations is
+        leaf_T + `leaf_temp_corr`, where leaf_T is `leaf_temper` if
+        provided, and the air temperature in `hfs` otherwise. Default is
+        `leaf_temper_corr` = 0.
+    diff_ratio: float, optional
+        Ratio of molecular diffusivities for water vapor and CO2.
+        Default is `diff_ratio` = 1.6.
+
+    Returns
+    -------
+    namedtuple
+        :class:`~fluxpart.containers.WUE`
+
+    Notes
+    -----
+    Leaf-level water use efficiency is estimated as [CN98]_ (pg. 238)::
+
+        wue = (1 / DR) * (ca - ci) / (qa - qi)
+
+    where::
+
+        DR = `diff_ratio`
+        ca, ci = ambient and intercellular CO2 concentration, resp.
+        qa, qi = ambient and intercellular H2O concentration, resp.
+
+    ca and qa are estimated from above-canopy tower measurements by
+    extrapolating a logarithmic mean profile with stability corrections
+    to the zero-plane displacement height [SS08]_.
+
+    qi corresponds to 100 percent relative humidity at `leaf_temper`.
+
+    To estimate ci, the following models are available:
+
+    'const_ppm'
+        ci (kg/m^3) is determined from a specified constant ppm value,
+        ci_ppm::
+
+            ci = f(ci_ppm; temperature, pressure)
+
+        Default parameter values for ci_ppm are 280 ppm when ppath='C3',
+        and 130 ppm when ppath='C4'. [CN98]_ (pg. 237).
+
+    'const_ratio'
+        The ratio ci/ca is constant::
+
+            ci/ca = K.
+
+        Default parameter values are K=0.7 for C3 plants and K=0.44 for
+        C4.
+
+    'linear'
+        ci/ca is a linear function of vpd (the atmospheric vapor
+        pressure deficit, which is calculated internally)::
+
+            ci/ca = b - m * vpd
+
+        b is dimensionless with a value of ~1 while m has units of 1/Pa.
+        The parameter pair (b, m) defaluts to (1, 1.6e-4) for C3 plants
+        and (1, 2.7e-4) for C4. See [MG83]_.
+
+    'sqrt'
+        ci/ca is a function of sqrt(vpd/ca) [KPO09]_::
+
+            ci/ca = 1 - sqrt(1.6 * lambd *  vpd / ca)
+
+        The paramater lambd has units of kg-CO2 / m^3 / Pa, and defaults
+        to 22e-9 for C3 plants. The sqrt model is not enabled for C4
+        plants.
+
+    'opt'
+        ci/ca is determined based on an optimization model. The
+        estimated value is given by Eq. 19 of [SSS19]_. With this
+        approach, no paramters (`ci_mod_param`) are specified. The model
+        may not be suitable for C4 vegetation.   
+
+    """
+    if canopy_ht > meas_ht:
+        raise WUEError("canopy_ht is less than meas_ht")
+
+    # Assume zero-plane and roughness params for vapor and CO2 are the same.
+    # d0 = Zero-plane displacement height (L), Eq. 5.2 of [CN98]
+    d0 = 0.65 * canopy_ht
+    # zv = Roughness parameter (L), Eqs. 5.3 and 7.19 of [CN98]
+    zv = 0.2 * (0.1 * canopy_ht)
+
+    # Virtual temperature flux
+    Qflux = hfs.cov_w_T + 0.61 * hfs.T * hfs.cov_w_q / hfs.rho_totair  # K m/s
+    # Obukhov length
+    obukhov_len = -hfs.T * hfs.ustar ** 3 / (VON_KARMAN * GRAVITY * Qflux)  # m
+
+    # Stability correction
+    zeta = (meas_ht - d0) / obukhov_len
+    # Unstable
+    if zeta < -0.04:
+        psi_v = 2.0 * log((1 + (1 - 16.0 * zeta) ** 0.5) / 2)
+    # Neutral
+    elif zeta <= 0.04:
+        psi_v = 0.0
+    # Stable
+    else:
+        psi_v = -5.0 * zeta
+
+    # Ambient concentrations (kg/m^3)
+    arg = (log((meas_ht - d0) / zv) - psi_v) / VON_KARMAN / hfs.ustar
+    ambient_h2o = hfs.rho_vapor + hfs.cov_w_q * arg
+    ambient_co2 = hfs.rho_co2 + hfs.cov_w_c * arg
+
+    # Intercellular saturation vapor pressure `esat`
+    leaf_T = (leaf_temper or hfs.T) + leaf_temper_corr
+    esat = sat_vapor_press(leaf_T)
+
+    # Intercellular vapor density.
+    eps = MW.vapor / MW.dryair
+    inter_h2o = hfs.rho_totair * eps * esat / (hfs.P - (1 - eps) * esat)
+
+    if ci_mod == "opt":
+        varq = hfs.var_vapor
+        varc = hfs.var_co2
+        wq = hfs.cov_w_q
+        wc = hfs.cov_w_c
+        pqc = hfs.corr_q_c
+        dr = diff_ratio
+        vpdm = vapor_press_deficit_mass(ambient_h2o, leaf_T)
+        vpd = vapor_press_deficit(ambient_h2o, leaf_T)
+        if vpdm < 0:
+            _bad_vpdm = "vpd = {:.4} kg/m^3 <= 0"
+            raise WUEError(_bad_vpdm.format(vpdm))
+        m = -(varc * wq - pqc * sqrt(varq * varc) * wc)
+        m /= varq * wc - pqc * sqrt(varq * varc) * wq
+        if m < 0:
+            raise WUEError("opt wue m arg = {:.4} < 0".format(m))
+        arg = dr * vpdm * m
+        rootarg = arg * (ambient_co2 + arg)
+        if rootarg < 0:
+            raise WUEError("opt wue root arg = {:.4} < 0".format(rootarg))
+        wue = (arg - sqrt(rootarg)) / (dr * vpdm)
+        return WUE(
+            wue=wue,
+            inter_h2o=inter_h2o,
+            ambient_h2o=ambient_h2o,
+            ambient_co2=ambient_co2,
+            vpd=vpd,
+            meas_ht=meas_ht,
+            leaf_temper=leaf_T,
+            ci_mod=ci_mod,
+            diff_ratio=diff_ratio,
+        )
+
+    if meas_ht is None or canopy_ht is None or ppath is None:
+        mssg = "meas_ht, canopy_ht, and ppath are required args for ci_mod= {}"
+        raise WUEError(mssg.format(ci_mod))
+
+    # Intercellular CO2 concentration, aka ci (kg/m^3)
+    if isinstance(ci_mod, str):
+        ci_mod = (ci_mod, None)
+
+    # Ambient vapor pressure deficit
+    # TODO should this use the extrapolated ambient h2o?
+    vpd = vapor_press_deficit(ambient_h2o, leaf_T)
+    if vpd < 0:
+        raise WUEError("Negative vapor pressure deficit {:.4} Pa".format(vpd))
+
+    ci_mod_name = ci_mod[0]
+    if ci_mod_name == "sqrt" and ppath == "C4":
+        err = "Combination of 'sqrt' ci model and 'C4' ppath not enabled"
+        raise WUEError(err)
+    ci_mod_params = ci_mod[1] or CI_DEFAULT_PARAMS[ppath][ci_mod_name]
+
+    ci_dispatch = {
+        "const_ppm": _ci_const_ppm(hfs.P, leaf_T, Rgas.co2),
+        "const_ratio": _cica_const_ratio(ambient_co2),
+        "linear": _cica_linear(ambient_co2, vpd),
+        "sqrt": _cica_sqrt(ambient_co2, vpd),
+    }
+    inter_co2 = ci_dispatch[ci_mod_name](ci_mod_params)
+
+    coef = 1.0 / diff_ratio
+    wue = coef * (ambient_co2 - inter_co2) / (ambient_h2o - inter_h2o)
+
+    if ambient_co2 <= inter_co2:
+        mssg = "WUE estimate, ci={}, ca={}".format(ambient_co2, inter_co2)
+        raise WUEError(mssg)
+    if ambient_h2o >= inter_h2o:
+        mssg = "WUE estimate, qi={}, qa={}".format(ambient_h2o, inter_h2o)
+        raise WUEError(mssg)
+
+    return WUE(
+        wue=wue,
+        inter_h2o=inter_h2o,
+        inter_co2=inter_co2,
+        ambient_h2o=ambient_h2o,
+        ambient_co2=ambient_co2,
+        vpd=vpd,
+        ppath=ppath,
+        meas_ht=meas_ht,
+        leaf_temper=leaf_T,
+        canopy_ht=canopy_ht,
+        ci_mod=ci_mod_name,
+        ci_mod_param=ci_mod_params,
+        diff_ratio=diff_ratio,
+    )
+
+
+def _ci_const_ppm(pressure, temperature, Rco2):
+    """ci is a fixed ppm value."""
+
+    def ci_func(ci_ppm):
+        """Return ci = intercellular CO2 concentration, kg/m^3."""
+        return ci_ppm * 1e-6 * pressure / Rco2 / temperature
+
+    return ci_func
+
+
+def _cica_const_ratio(ambient_co2):
+    """ci/ca is constant."""
+
+    def ci_func(const):
+        """Return ci = intercellular CO2 concentration, kg/m^3."""
+        return const * ambient_co2
+
+    return ci_func
+
+
+def _cica_linear(ambient_co2, vpd):
+    """ci/ca is a decreasing linear function of vapor pressure deficit."""
+
+    def ci_func(linear_params):
+        """Return ci = intercellular CO2 concentration, kg/m^3."""
+        # b is unitless with a value of ~1, and m (> 0) has units of Pa^-1
+        b, m = linear_params
+        return ambient_co2 * (b - m * vpd)
+
+    return ci_func
+
+
+def _cica_sqrt(ambient_co2, vpd):
+    """ci/ca is a function of sqrt(`vpd`/ca)."""
+
+    def ci_func(lambd):
+        """Return ci = intercellular CO2 concentration, kg/m^3."""
+        # lambd has units of kg-co2 / m^3 / Pa
+        return ambient_co2 * (1 - sqrt(1.6 * lambd * vpd / ambient_co2))
+
+    return ci_func
