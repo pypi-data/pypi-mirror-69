21843781998e9291129ba8f66fbacd07e2987bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package pyDoodle2Web/0.0.3/pyDoodle2Web-0.0.3.tar.gz

---
+++ packages/pyDoodle2Web/pyDoodle2Web-0.0.3.tar.gz/pyDoodle2Web-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyDoodle2Web",
+    version="0.0.3",
+    author="pyDoodle2Web",
+    author_email="renerjake@gmail.com",
+    description="",
+    long_description=long_description,
+    url="https://github.com/pyDoodle2Web/pyDoodle2Web",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'pytesseract',
+    ],
+    keywords=['bootstrap', 'ocr', 'website', 'genertion'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
