72d85d5ff5ee1c1a67739960565ae380f2b0fee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:40 +0000

    Add package onthefly/0.4.0/onthefly-0.4.0.tar.gz

---
+++ packages/onthefly/onthefly-0.4.0.tar.gz/onthefly-0.4.0/onthefly/cli.py
@@ -0,0 +1,16 @@
+"""Console script for onthefly."""
+import sys
+import click
+from .onthefly import onthefly
+
+
+@click.command()
+@click.argument("input_file", default="/home/mroavi/Desktop/input.jl")
+def main(input_file):
+    """Emulates typing each character of an input file"""
+    onthefly(input_file)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
