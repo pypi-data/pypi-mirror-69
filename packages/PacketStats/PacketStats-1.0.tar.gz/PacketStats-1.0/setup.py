e3c05b865e763513c79fa364cd5bbedbb91324dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:20 +0000

    Add package PacketStats/1.0/PacketStats-1.0.tar.gz

---
+++ packages/PacketStats/PacketStats-1.0.tar.gz/PacketStats-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PacketStats',
+      version='1.0',
+      description='Package to work with pcap files and converting them to txt,csv and then creating stats',
+      url='http://github.com/mattkozlowski/packet-stats',
+      author='Matt Kozlowski',
+      author_email='mateusz.a.kozlowski@gmail.com',
+      license='MIT',
+      packages=['PacketStats'],
+      zip_safe=False)
