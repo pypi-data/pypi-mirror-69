9bafafe032f4f37a1d4e91c5f021a2055934fd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:01 +0000

    Add package rangerjson/0.1.2/rangerjson-0.1.2.tar.gz

---
+++ packages/rangerjson/rangerjson-0.1.2.tar.gz/rangerjson-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rangerjson']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'rangerjson',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '# README\n\nInteract with Ranger json dump from api\n\n## Ranger model\n\n![overview](img/ranger-general.png)\n\nEach policies has a number of similarties with each other but have some specialities (hdf path ...)\n\n![policies](img/ranger-policies.png)\n',
+    'author': 'Khalid',
+    'author_email': 'khalidck@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
