6e7170ce18250a941cc0a1adf8d312c6bad0f069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package netbox-topology-views/0.4.8/netbox_topology_views-0.4.8-py3-none-any.whl

---
+++ packages/netbox-topology-views/netbox_topology_views-0.4.8-py3-none-any.whl/netbox_topology_views/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import View
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+class TopologyHomeView(PermissionRequiredMixin, View):
+    permission_required = ('dcim.view_site', 'dcim.view_device')
+
+    """
+    Show the home page
+    """
+    def get(self, request):        
+        return render(request, 'netbox_topology_views/index.html')
