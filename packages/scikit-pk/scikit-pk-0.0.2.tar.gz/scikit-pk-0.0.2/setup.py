8e9c77b33a7cfe8ae3ece82f0d1f76370485245f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package scikit-pk/0.0.2/scikit-pk-0.0.2.tar.gz

---
+++ packages/scikit-pk/scikit-pk-0.0.2.tar.gz/scikit-pk-0.0.2/setup.py
@@ -0,0 +1,13 @@
+# Setup.py file
+from setuptools import setup
+
+setup(name='scikit-pk',
+        version='0.0.2',
+        description='PK Multi-Compartment Modelling Library ',
+        author='Kenneth',
+        author_email='test@testmail.com',
+        license='LICENSE.txt',
+        #packages=['scikit-pk'],
+        install_requires=['matplotlib',
+                        'numpy',
+                        'pandas'])
