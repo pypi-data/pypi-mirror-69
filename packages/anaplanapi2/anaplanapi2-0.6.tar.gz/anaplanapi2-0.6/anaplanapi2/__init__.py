73d9fcc655896778b3891e5fcb503407ead24249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:28 +0000

    Add package anaplanapi2/0.6/anaplanapi2-0.6.tar.gz

---
+++ packages/anaplanapi2/anaplanapi2-0.6.tar.gz/anaplanapi2-0.6/anaplanapi2/__init__.py
@@ -0,0 +1,4 @@
+from anaplanapi2.AnaplanConnection import AnaplanConnection
+from anaplanapi2 import anaplan_auth
+from anaplanapi2 import anaplan
+from anaplanapi2 import anaplan_resource_dictionary
