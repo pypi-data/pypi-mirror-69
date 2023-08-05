3b55adad13ad96124b7f91c2b494ca1c8be2146d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:31 +0000

    Add package django-personals/0.0.1/django_personals-0.0.1-py3-none-any.whl

---
+++ packages/django-personals/django_personals-0.0.1-py3-none-any.whl/django_personals/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig as AppConfigBase
+
+class AppConfig(AppConfigBase):
+    name = 'django_personals'
+    label = 'django_personals'
+    verbose_name = 'Django Personals'
