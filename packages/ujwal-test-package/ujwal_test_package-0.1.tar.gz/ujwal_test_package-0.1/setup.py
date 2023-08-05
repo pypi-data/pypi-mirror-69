abab869b3000663cdea5ca8ecc0f353bd5e6ea63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:33 +0000

    Add package ujwal-test-package/0.1/ujwal_test_package-0.1.tar.gz

---
+++ packages/ujwal-test-package/ujwal_test_package-0.1.tar.gz/ujwal_test_package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ujwal_test_package',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ujwal_test_package'],
+      zip_safe=False,
+      
+      author_name = 'Ujwal',
+      author_email = 'ujwal98@gmail.com'
+     )
