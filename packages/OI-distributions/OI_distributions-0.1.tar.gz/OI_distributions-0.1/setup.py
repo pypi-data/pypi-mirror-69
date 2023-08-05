b30f09d661da6c9c94ad16a28445067813c7d080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:57 +0000

    Add package OI-distributions/0.1/OI_distributions-0.1.tar.gz

---
+++ packages/OI-distributions/OI_distributions-0.1.tar.gz/OI_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='OI_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['OI_distributions'],
+      author="Odeajo Israel",
+      email="odeajoisrael@gmail.com",
+      zip_safe=False)
