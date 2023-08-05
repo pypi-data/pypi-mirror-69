5133332c665bf08d613fb062c13517abf6818000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:37 +0000

    Add package pi-team-frontoffice-client/1.0.0/pi_team_frontoffice_client-1.0.0-py3-none-any.whl

---
+++ packages/pi-team-frontoffice-client/pi_team_frontoffice_client-1.0.0-py3-none-any.whl/openapi_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.contactgegevens_api import ContactgegevensApi
+from openapi_client.api.developer_tools_api import DeveloperToolsApi
+from openapi_client.api.persoonsgegevens_api import PersoonsgegevensApi
