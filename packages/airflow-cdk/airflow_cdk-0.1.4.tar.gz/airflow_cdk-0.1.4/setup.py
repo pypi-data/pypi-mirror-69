0195a985df4a8cee5beea59b8477fed364d27c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:06 +0000

    Add package airflow-cdk/0.1.4/airflow_cdk-0.1.4.tar.gz

---
+++ packages/airflow-cdk/airflow_cdk-0.1.4.tar.gz/airflow_cdk-0.1.4/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="airflow_cdk",
+    version="0.1.4",
+    description="Custom cdk constructs for apache airflow",
+    url="https://github.com/knowsuchagency/airflow-cdk",
+    keywords=["aws", "cdk", "airflow", "k8s"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Stephan Fitzpatrick",
+    author_email="stephan@knowsuchagency.com",
+    license_files=["LICENSE"],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "aws-cdk.core>=1.34.1",
+        "aws-cdk.aws_ecs>=1.34.1",
+        "aws-cdk.aws_ecs_patterns>=1.34.1",
+        "aws-cdk.aws_rds>=1.34.1",
+        "aws-cdk.aws_s3>=1.34.1",
+    ],
+    extras_require={"dev": ["pytest", "toml", "black", "twine"]},
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Code Generators",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+)
