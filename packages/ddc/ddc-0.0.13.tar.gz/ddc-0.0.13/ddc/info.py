044721b81c229fce278dccf3c3886a398dcffcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:36 +0000

    Add package ddc/0.0.13/ddc-0.0.13.tar.gz

---
+++ packages/ddc/ddc-0.0.13.tar.gz/ddc-0.0.13/ddc/info.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.13"
+__package_name__ = "ddc"
+
+
+def print_about_version():
+    print("Devision Developers Cli")
+    print("Version: " + __version__)
+
+
+if __name__ == "__main__":
+    print_about_version()
