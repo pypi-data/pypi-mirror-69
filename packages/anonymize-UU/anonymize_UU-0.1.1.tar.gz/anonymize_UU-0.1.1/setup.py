c9beb626539b8f56b0c225da4581227123396853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package anonymize-UU/0.1.1/anonymize_UU-0.1.1.tar.gz

---
+++ packages/anonymize-UU/anonymize_UU-0.1.1.tar.gz/anonymize_UU-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='anonymize_UU',
+    version='0.1.1',
+    description='',
+    url='https://github.com/cskaandorp/anonymize',
+    author='C.S. Kaandorp',
+    author_email='c.s.kaandorp@uu.nl',
+    license='MIT',
+    packages=['anonymize'],
+    zip_safe=False
+)
