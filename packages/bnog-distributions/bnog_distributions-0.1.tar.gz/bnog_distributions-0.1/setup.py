a1b30ead80a09a2335626a364ae4ed5dbd649985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:32 +0000

    Add package bnog-distributions/0.1/bnog_distributions-0.1.tar.gz

---
+++ packages/bnog-distributions/bnog_distributions-0.1.tar.gz/bnog_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='bnog_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bnog_distributions'],
+      zip_safe=False)
