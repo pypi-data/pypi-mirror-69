b08cf4dcebaa746d7cc27ae53cd21281b1af8ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:21 +0000

    Add package matador-db/0.9.1/matador_db-0.9.1-py3-none-any.whl

---
+++ packages/matador-db/matador_db-0.9.1-py3-none-any.whl/matador/__init__.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# Distributed under the terms of the MIT License.
+
+""" `matador` performs, aggregates, and analyses high-throughput
+electronic structure calculations, primarily for crystal structure
+prediction, leveraging CASTEP and Quantum Epsresso as density-functional
+theory compute engines.
+"""
+
+
+__all__ = ['__version__']
+__author__ = 'Matthew Evans'
+__maintainer__ = 'Matthew Evans'
+
+__version__ = "0.9.1"
