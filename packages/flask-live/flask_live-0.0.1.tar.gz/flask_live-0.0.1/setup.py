88202cab40d294f60f70a849fc6e9617c98ff6dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:06 +0000

    Add package flask-live/0.0.1/flask_live-0.0.1.tar.gz

---
+++ packages/flask-live/flask_live-0.0.1.tar.gz/flask_live-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='flask_live',
+    version='0.0.1',
+    description='Flask Live',
+    url='https://github.com/cn86/flask-live',
+    author='Conrad Nguyen',
+    author_email='conradn86@gmail.com',
+    license='GPLv3',
+    packages=setuptools.find_packages(),
+)
