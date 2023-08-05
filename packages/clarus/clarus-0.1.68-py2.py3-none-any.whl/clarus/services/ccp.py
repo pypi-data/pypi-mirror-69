2a661ff7a03bd47522635f6c64a0eaf9e57bf470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:27 +0000

    Add package clarus/0.1.68/clarus-0.1.68-py2.py3-none-any.whl

---
+++ packages/clarus/clarus-0.1.68-py2.py3-none-any.whl/clarus/services/ccp.py
@@ -0,0 +1,11 @@
+import clarus.services
+
+def defaultresources(output=None, **params):
+    return clarus.services.api_request('CCP', 'DefaultResources', output=output, **params)
+
+def disclosures(output=None, **params):
+    return clarus.services.api_request('CCP', 'Disclosures', output=output, **params)
+
+def volume(output=None, **params):
+    return clarus.services.api_request('CCP', 'Volume', output=output, **params)
+
