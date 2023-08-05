137f9fcaf9d6cc1b20360ee22ea06bbebf30ae05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:43 +0000

    Add package cro-validate/0.0.8/cro_validate-0.0.8-py3-none-any.whl

---
+++ packages/cro-validate/cro_validate-0.0.8-py3-none-any.whl/cro_validate/api/parse_api.py
@@ -0,0 +1,31 @@
+
+import base64
+import dateutil.parser
+import datetime
+import cro_validate.api.exception_api as ExceptionApi
+
+
+def tenant_code(value):
+	split = value.split('+')
+	if len(split) != 2:
+		raise ExceptionApi.create_input_error('tenant_code', 'Invalid format')
+	
+	yard_code = split[0]
+	try:
+		location_id = int(split[1])
+	except:
+		raise ExceptionApi.create_input_error('tenant_code', 'Invalid format')
+
+	return (yard_code, location_id)
+
+
+def parse_datetime(value):
+	dt = dateutil.parser.parse(value, fuzzy=False)
+	if not dt.tzinfo:
+		dt = dt.replace(tzinfo=datetime.timezone.utc)
+	return dt
+
+
+def parse_base64(value):
+	decoded = base64.b64decode(value, validate=True)
+	return decoded
