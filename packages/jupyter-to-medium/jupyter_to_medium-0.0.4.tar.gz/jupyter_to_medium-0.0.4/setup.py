1b7edc2296b99f78b493f7ed893ba8c0f0d650d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:00 +0000

    Add package jupyter-to-medium/0.0.4/jupyter_to_medium-0.0.4.tar.gz

---
+++ packages/jupyter-to-medium/jupyter_to_medium-0.0.4.tar.gz/jupyter_to_medium-0.0.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import re
+
+from jupyter_to_medium import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+pat = r'!\[png\]\('
+repl = r'![png](https://raw.githubusercontent.com/dexplo/jupyter_to_medium/master/'
+long_description = re.sub(pat, repl, long_description)
+
+setuptools.setup(
+    name="jupyter_to_medium",
+    version=__version__,
+    author="Ted Petrou",
+    author_email="petrou.theodore@gmail.com",
+    description="Publish a Jupyter Notebook as a Medium blogpost",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Jupyter Notebook Medium Blog",
+    url="https://github.com/dexplo/jupyter-to-medium",
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={'console_scripts': ['jupyter_to_medium=jupyter_to_medium._command_line:main']},
+    include_package_data=True
+)
