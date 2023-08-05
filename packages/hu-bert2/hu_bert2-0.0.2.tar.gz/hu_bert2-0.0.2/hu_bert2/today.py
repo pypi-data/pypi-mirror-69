efeca0efd9a0e0c5ad999464aa6dca05648bd9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package hu-bert2/0.0.2/hu_bert2-0.0.2.tar.gz

---
+++ packages/hu-bert2/hu_bert2-0.0.2.tar.gz/hu_bert2-0.0.2/hu_bert2/today.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 22 14:22:31 2020
+
+@author: huzhen
+"""
+import time
+
+
+class Today:
+    def __init__(self):
+        self.name = 'HU_TODAY_'
+        pass
+    def today(self):
+        _ = time.gmtime()
+        _ = list(_)[:3]
+        return str(_[0]) + '年' + str(_[1]) + '月' + str(_[2]) + '日'
+    
+    def get_name(self):
+        return self.name
+    
