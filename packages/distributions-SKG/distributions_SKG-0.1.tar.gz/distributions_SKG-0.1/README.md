5a0c03709232c4d6f5c6c4f5f665e3b0f8bed254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:42 +0000

    Add package distributions-SKG/0.1/distributions_SKG-0.1.tar.gz

---
+++ packages/distributions-SKG/distributions_SKG-0.1.tar.gz/distributions_SKG-0.1/README.md
@@ -0,0 +1,6 @@
+## Example package for distributions
+
+1. Normal Distribution
+2. Binomial Distribution
+
+## Thanks to Udacity
---
+++ packages/distributions-SKG/distributions_SKG-0.1.tar.gz/distributions_SKG-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_SKG',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_SKG'],
+      zip_safe=False)
