e9b55cd02c6014164618f2d944843fb97acb2bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:30 +0000

    Add package crack/0.1.5/crack-0.1.5.tar.gz

---
+++ packages/crack/crack-0.1.5.tar.gz/crack-0.1.5/crack/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @author: leesoar
+
+"""something."""
+import sys
+
+from crack.util import *
+
+
+__version__ = "0.1.5"
+
+__all__ = ["__version__", "util.*"]
+
+if sys.version_info >= (3, 6):
+    from secrets import *
+    __all__ = ["__version__", "util.*", "secrets.*"]
+
+
