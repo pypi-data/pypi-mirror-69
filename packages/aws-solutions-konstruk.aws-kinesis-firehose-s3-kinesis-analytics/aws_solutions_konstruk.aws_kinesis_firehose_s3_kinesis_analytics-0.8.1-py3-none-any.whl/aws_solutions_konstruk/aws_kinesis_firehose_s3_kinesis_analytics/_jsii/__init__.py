96e446ee35c0487a11037590595fbba9015e3eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:57 +0000

    Add package aws-solutions-konstruk.aws-kinesis-firehose-s3-kinesis-analytics/0.8.1/aws_solutions_konstruk.aws_kinesis_firehose_s3_kinesis_analytics-0.8.1-py3-none-any.whl

---
+++ packages/aws-solutions-konstruk.aws-kinesis-firehose-s3-kinesis-analytics/aws_solutions_konstruk.aws_kinesis_firehose_s3_kinesis_analytics-0.8.1-py3-none-any.whl/aws_solutions_konstruk/aws_kinesis_firehose_s3_kinesis_analytics/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_iam
+import aws_cdk.aws_kinesis
+import aws_cdk.aws_kinesisanalytics
+import aws_cdk.aws_kinesisfirehose
+import aws_cdk.aws_s3
+import aws_cdk.core
+import aws_solutions_konstruk.aws_kinesis_firehose_s3
+import aws_solutions_konstruk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-solutions-konstruk/aws-kinesisfirehose-s3-and-kinesisanalytics", "0.8.1", __name__[0:-6], "aws-kinesisfirehose-s3-and-kinesisanalytics@0.8.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
