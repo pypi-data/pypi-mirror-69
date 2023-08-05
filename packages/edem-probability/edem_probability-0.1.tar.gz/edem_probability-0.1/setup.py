7dff0aab4408fbef414471d47e2a2f52204292be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:42 +0000

    Add package edem-probability/0.1/edem_probability-0.1.tar.gz

---
+++ packages/edem-probability/edem_probability-0.1.tar.gz/edem_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='edem_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['edem_probability'],
+      author='Edem',
+      author_email='edemdj@gmail.com',
+      zip_safe=False)
