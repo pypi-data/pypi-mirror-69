4b95abfd8379ac6c066228303a58ff027fd31f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:37 +0000

    Add package sphinx-scylladb-theme/0.1.3/sphinx-scylladb-theme-0.1.3.tar.gz

---
+++ packages/sphinx-scylladb-theme/sphinx-scylladb-theme-0.1.3.tar.gz/sphinx-scylladb-theme-0.1.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sphinx_scylladb_theme', 'sphinx_scylladb_theme.extensions']
+
+package_data = \
+{'': ['*'],
+ 'sphinx_scylladb_theme': ['static/*',
+                           'static/css/*',
+                           'static/css/doc/*',
+                           'static/css/doc/ext/*',
+                           'static/fonts/*',
+                           'static/img/*',
+                           'static/js/*',
+                           'static/js/foundation/*',
+                           'static/js/vendor/*']}
+
+install_requires = \
+['sphinx==1.8.0']
+
+setup_kwargs = {
+    'name': 'sphinx-scylladb-theme',
+    'version': '0.1.3',
+    'description': 'A Sphinx Theme for ScyllaDB projects documentation',
+    'long_description': None,
+    'author': 'David García',
+    'author_email': 'dgarcia360@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
