337fa4a1f8c6f406e34df8100d866de6b89107bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:02 +0000

    Add package gb-distribution/1.2/gb_distribution-1.2.tar.gz

---
+++ packages/gb-distribution/gb_distribution-1.2.tar.gz/gb_distribution-1.2/gb_distribution/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+from .Gaussiandistribution import Gaussian
+
+
+# In[ ]:
+
+
+
+
---
+++ packages/gb-distribution/gb_distribution-1.2.tar.gz/gb_distribution-1.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from setuptools import setup
+setup(name='gb_distribution',
+      version='1.2',
+     description='Gaussian and Binomial distribution',
+     packages=['gb_distribution'],
+     author='Dhruva Suman',
+     author_email = 'technical.insight@gmail.com',
+     zip_safe=False)
+
+
+# In[ ]:
+
+
+
+
