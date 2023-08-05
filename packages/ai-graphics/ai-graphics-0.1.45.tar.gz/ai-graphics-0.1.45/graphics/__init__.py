cd76f0c067e9a998f4af03897e47ac59be2c5ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:10 +0000

    Add package ai-graphics/0.1.45/ai-graphics-0.1.45.tar.gz

---
+++ packages/ai-graphics/ai-graphics-0.1.45.tar.gz/ai-graphics-0.1.45/graphics/__init__.py
@@ -0,0 +1,29 @@
+from .function import Classifier
+from .function import DeepLab
+from .function import Detection
+from .function import FontWriter
+from .function import Format
+from .function import Graphics
+from .function import ImageCrop
+from .function import Matting
+from .function import MtcnnDetector
+from .function import OcrDetector
+from .function import Segmentation
+from .function import VideoProcess
+from .function import Watermark
+from .function import train_test_split
+
+__all__ = ['Graphics',
+           'Format',
+           'VideoProcess',
+           'FontWriter',
+           'MtcnnDetector',
+           'OcrDetector',
+           'Matting',
+           'ImageCrop',
+           'Classifier',
+           'train_test_split',
+           'Detection',
+           'Segmentation',
+           'DeepLab',
+           'Watermark']
---
+++ packages/ai-graphics/ai-graphics-0.1.45.tar.gz/ai-graphics-0.1.45/graphics/function/__init__.py
@@ -0,0 +1,9 @@
+from .Classifier import Classifier, train_test_split
+from .DeepLab import DeepLab
+from .Detection import Detection
+from .ImageCrop import ImageCrop
+from .Matting import Matting
+from .MtcnnDetector import MtcnnDetector
+from .OcrDetector import OcrDetector
+from .Segmentation import Segmentation
+from .Watermark import Watermark
