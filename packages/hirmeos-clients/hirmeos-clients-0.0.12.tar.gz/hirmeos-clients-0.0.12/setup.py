1dd42876e0aed773053ee85623d17f0e7195c8c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:27 +0000

    Add package hirmeos-clients/0.0.12/hirmeos-clients-0.0.12.tar.gz

---
+++ packages/hirmeos-clients/hirmeos-clients-0.0.12.tar.gz/hirmeos-clients-0.0.12/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='hirmeos-clients',
+    version='0.0.12',
+    author='Rowan Hatheryley',
+    author_email='rowan.hatherley@ubiquitypress.com',
+    description='Python API clients for the HIRMEOS project.',
+    install_requires=[
+        'requests>=2.22.0',
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/ubiquitypress/hirmeos-clients/',
+    packages=['hirmeos_clients'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7'
+)
