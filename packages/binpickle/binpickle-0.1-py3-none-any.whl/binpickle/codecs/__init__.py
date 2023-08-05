48f67a79184e7b4aae0193047b04279369ecf228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:36 +0000

    Add package binpickle/0.1/binpickle-0.1-py3-none-any.whl

---
+++ packages/binpickle/binpickle-0.1-py3-none-any.whl/binpickle/codecs/__init__.py
@@ -0,0 +1,38 @@
+"""
+Codecs for encoding and decoding buffers in BinPickle.
+
+This is similar in spirit to numcodecs_, but automatically handles some cases
+such as splitting arrays into blocks.
+
+.. _numcodecs: https://numcodecs.readthedocs.io/en/stable/
+"""
+
+from ._base import Codec  # noqa: F401
+from .null import Null
+from .blosc import Blosc
+
+KNOWN_CODECS = [
+    Null,
+    Blosc
+]
+
+
+def get_codec(name, config):
+    """
+    Get a codec by name and configuration.
+
+    Args:
+        name(str or None): the codec name.
+        config: the codec configuration, as returned by :meth:`Codec.config`.
+
+    Returns:
+        Codec: the configured codec.
+    """
+    if name is None:
+        return Null()
+    else:
+        for c in KNOWN_CODECS:
+            if c.NAME == name:
+                return c(**config)
+
+        raise ValueError(f'unknown codec {name}')
