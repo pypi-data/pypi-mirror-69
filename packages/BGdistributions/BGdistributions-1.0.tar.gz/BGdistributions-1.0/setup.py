92352e796c7c69ad1a790e34ef4893e92de3f2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:16 +0000

    Add package BGdistributions/1.0/BGdistributions-1.0.tar.gz

---
+++ packages/BGdistributions/BGdistributions-1.0.tar.gz/BGdistributions-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='BGdistributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['BGdistributions'],
+      zip_safe=False)
