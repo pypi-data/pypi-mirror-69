4ed3eebd6686829f70b59cf09e9b1c19709c42b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:59 +0000

    Add package raincloudy/0.0.8/raincloudy-0.0.8.tar.gz

---
+++ packages/raincloudy/raincloudy-0.0.8.tar.gz/raincloudy-0.0.8/setup.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+"""RainCloudy setup script."""
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as desc:
+        return desc.read()
+
+
+setup(
+    name='raincloudy',
+    packages=['raincloudy'],
+    version='0.0.8',
+    description='A Python library to communicate with Melnor \
+                 RainCloud Smart Garden Watering Irrigation Timer \
+                 (https://wwww.melnor.com/)',
+    long_description=readme(),
+    author='Marcelo Moreira de Mello',
+    author_email='tchello.mello@gmail.com',
+    url='https://github.com/tchellomello/raincloudy',
+    license='Apache License 2.0',
+    include_package_data=True,
+    install_requires=['requests>=2.0', 'bs4'],
+    test_suite='tests',
+    keywords=[
+        'garden',
+        'irrigation',
+        'melnor',
+        'rain cloud',
+        'water',
+    ],
+)
