f5d2a14067d3f1409258fe234f46f016816f4b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:27 +0000

    Add package backports.zoneinfo/0.1.0b0/backports.zoneinfo-0.1.0b0-cp38-cp38-win32.whl

---
+++ packages/backports.zoneinfo/backports.zoneinfo-0.1.0b0-cp38-cp38-win32.whl/backports/zoneinfo/__init__.py
@@ -0,0 +1,32 @@
+__all__ = [
+    "ZoneInfo",
+    "reset_tzpath",
+    "available_timezones",
+    "TZPATH",
+    "ZoneInfoNotFoundError",
+    "InvalidTZPathWarning",
+]
+
+from . import _tzpath
+from ._common import ZoneInfoNotFoundError
+from ._version import __version__
+
+try:
+    from ._czoneinfo import ZoneInfo
+except ImportError:  # pragma: nocover
+    from ._zoneinfo import ZoneInfo
+
+reset_tzpath = _tzpath.reset_tzpath
+available_timezones = _tzpath.available_timezones
+InvalidTZPathWarning = _tzpath.InvalidTZPathWarning
+
+
+def __getattr__(name):
+    if name == "TZPATH":
+        return _tzpath.TZPATH
+    else:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+def __dir__():
+    return sorted(list(globals()) + ["TZPATH"])
