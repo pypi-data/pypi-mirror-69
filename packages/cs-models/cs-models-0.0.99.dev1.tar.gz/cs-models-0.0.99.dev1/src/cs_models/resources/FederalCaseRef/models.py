adb65826166f4a2957d83f0c059db5954513c7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:12 +0000

    Add package cs-models/0.0.99.dev1/cs-models-0.0.99.dev1.tar.gz

---
+++ packages/cs-models/cs-models-0.0.99.dev1.tar.gz/cs-models-0.0.99.dev1/src/cs_models/resources/FederalCaseRef/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    ForeignKey,
+    UniqueConstraint,
+)
+
+from ...database import Base
+
+
+class FederalCaseRefModel(Base):
+    __tablename__ = "federal_case_refs"
+
+    id = Column(Integer, primary_key=True)
+    case_name = Column(String(128), nullable=False)
+    volume = Column(Integer, nullable=False)
+    reporter_type = Column(String(50), nullable=False)
+    page = Column(Integer, nullable=False)
+    title = Column(String(128), nullable=False)
+    link = Column(String(128), nullable=False)
+    federal_case_file_id = Column(Integer, ForeignKey("files.id"))
+
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
+
+    __table_args__ = (UniqueConstraint("volume", "reporter_type", "page",
+                                       "title"),)
