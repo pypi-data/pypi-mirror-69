abb5bdb3952be3f593c5d80df1663a3db8f92aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:50 +0000

    Add package gb-distributions-t/1.1/gb_distributions_t-1.1.tar.gz

---
+++ packages/gb-distributions-t/gb_distributions_t-1.1.tar.gz/gb_distributions_t-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gb_distributions_t',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_probability_t'],
+      author = 'Jiahui Li',
+      author_email = 'jiahuili858@gmail.com',
+      zip_safe=False)
