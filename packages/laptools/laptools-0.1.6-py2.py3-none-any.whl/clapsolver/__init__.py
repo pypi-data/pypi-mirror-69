96149fa87f96ada7c64372c554945eeb5b1008f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package laptools/0.1.6/laptools-0.1.6-py2.py3-none-any.whl

---
+++ packages/laptools/laptools-0.1.6-py2.py3-none-any.whl/clapsolver/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for clapsolver."""
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.1.6"
+
+__author__ = "Jacob Moorman"
+__email__ = "jacob@moorman.me"
+
+__license__ = "MIT license".rstrip(" license")
+__copyright__ = "Copyright (c) 2020, Jacob Moorman"
+
+
+from . import clap, dynamic_lsap
+
+__all__ = ["clap", "dynamic_lsap"]
