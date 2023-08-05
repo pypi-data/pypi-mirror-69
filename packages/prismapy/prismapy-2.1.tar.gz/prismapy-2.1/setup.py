eb3c8fcaedcb31e09f37044cdb1731571aa3d1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:33 +0000

    Add package prismapy/2.1/prismapy-2.1.tar.gz

---
+++ packages/prismapy/prismapy-2.1.tar.gz/prismapy-2.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name='prismapy', 
+    version='2.1',                         
+    packages=setuptools.find_packages(),
+    author="Uzay-G",
+    url="https://github.com/Uzay-G/prisma.py"           
+)
