01b3884b0b739fc14e92b94ce454904fe607d67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package clevercsv/0.6.2/clevercsv-0.6.2-cp38-cp38-manylinux1_i686.whl

---
+++ packages/clevercsv/clevercsv-0.6.2-cp38-cp38-manylinux1_i686.whl/clevercsv/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from .__version__ import __version__
+
+from csv import QUOTE_ALL, QUOTE_MINIMAL, QUOTE_NONNUMERIC, QUOTE_NONE
+
+from .cparser_util import field_size_limit
+from .detect import Detector
+from .detect import Detector as Sniffer
+from .dict_read_write import DictReader, DictWriter
+from .exceptions import Error
+from .read import reader
+from .wrappers import (
+    detect_dialect,
+    csv2df,
+    read_csv,
+    read_as_dicts,
+    stream_csv,
+    write_table,
+)
+from .write import writer
