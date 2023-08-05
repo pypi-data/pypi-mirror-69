697c9ac81dbdda1f567b8fdb67c95d72cf682600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:07 +0000

    Add package mypy-boto3-athena/1.13.15.0/mypy-boto3-athena-1.13.15.0.tar.gz

---
+++ packages/mypy-boto3-athena/mypy-boto3-athena-1.13.15.0.tar.gz/mypy-boto3-athena-1.13.15.0/mypy_boto3_athena/__init__.py
@@ -0,0 +1,38 @@
+"""
+Main interface for athena service.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.athena import (
+        AthenaClient,
+        Client,
+        GetQueryResultsPaginator,
+        ListNamedQueriesPaginator,
+        ListQueryExecutionsPaginator,
+        )
+
+    session = boto3.Session()
+
+    client: AthenaClient = boto3.client("athena")
+    session_client: AthenaClient = session.client("athena")
+
+    get_query_results_paginator: GetQueryResultsPaginator = client.get_paginator("get_query_results")
+    list_named_queries_paginator: ListNamedQueriesPaginator = client.get_paginator("list_named_queries")
+    list_query_executions_paginator: ListQueryExecutionsPaginator = client.get_paginator("list_query_executions")
+"""
+from mypy_boto3_athena.client import AthenaClient, AthenaClient as Client
+from mypy_boto3_athena.paginator import (
+    GetQueryResultsPaginator,
+    ListNamedQueriesPaginator,
+    ListQueryExecutionsPaginator,
+)
+
+
+__all__ = (
+    "AthenaClient",
+    "Client",
+    "GetQueryResultsPaginator",
+    "ListNamedQueriesPaginator",
+    "ListQueryExecutionsPaginator",
+)
---
+++ packages/mypy-boto3-athena/mypy-boto3-athena-1.13.15.0.tar.gz/mypy-boto3-athena-1.13.15.0/mypy_boto3_athena/paginator.py
@@ -0,0 +1,69 @@
+"""
+Main interface for athena service client paginators.
+
+Usage::
+
+    import boto3
+    from mypy_boto3.athena import (
+        GetQueryResultsPaginator,
+        ListNamedQueriesPaginator,
+        ListQueryExecutionsPaginator,
+    )
+
+    client: AthenaClient = boto3.client("athena")
+
+    get_query_results_paginator: GetQueryResultsPaginator = client.get_paginator("get_query_results")
+    list_named_queries_paginator: ListNamedQueriesPaginator = client.get_paginator("list_named_queries")
+    list_query_executions_paginator: ListQueryExecutionsPaginator = client.get_paginator("list_query_executions")
+"""
+# pylint: disable=arguments-differ,redefined-outer-name,redefined-builtin
+from typing import Iterator, TYPE_CHECKING
+from botocore.paginate import Paginator as Boto3Paginator
+from mypy_boto3_athena.type_defs import (
+    GetQueryResultsOutputTypeDef,
+    ListNamedQueriesOutputTypeDef,
+    ListQueryExecutionsOutputTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+
+__all__ = ("GetQueryResultsPaginator", "ListNamedQueriesPaginator", "ListQueryExecutionsPaginator")
+
+
+class GetQueryResultsPaginator(Boto3Paginator):
+    """
+    [Paginator.GetQueryResults documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.15/reference/services/athena.html#Athena.Paginator.GetQueryResults)
+    """
+
+    def paginate(
+        self, QueryExecutionId: str, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[GetQueryResultsOutputTypeDef]:
+        """
+        [GetQueryResults.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.15/reference/services/athena.html#Athena.Paginator.GetQueryResults.paginate)
+        """
+
+
+class ListNamedQueriesPaginator(Boto3Paginator):
+    """
+    [Paginator.ListNamedQueries documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.15/reference/services/athena.html#Athena.Paginator.ListNamedQueries)
+    """
+
+    def paginate(
+        self, WorkGroup: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListNamedQueriesOutputTypeDef]:
+        """
+        [ListNamedQueries.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.15/reference/services/athena.html#Athena.Paginator.ListNamedQueries.paginate)
+        """
+
+
+class ListQueryExecutionsPaginator(Boto3Paginator):
+    """
+    [Paginator.ListQueryExecutions documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.15/reference/services/athena.html#Athena.Paginator.ListQueryExecutions)
+    """
+
+    def paginate(
+        self, WorkGroup: str = None, PaginationConfig: PaginatorConfigTypeDef = None
+    ) -> Iterator[ListQueryExecutionsOutputTypeDef]:
+        """
+        [ListQueryExecutions.paginate documentation](https://boto3.amazonaws.com/v1/documentation/api/1.13.15/reference/services/athena.html#Athena.Paginator.ListQueryExecutions.paginate)
+        """
