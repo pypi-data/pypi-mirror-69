9380e6b32c85016dc8858da5dc4d190330a42e79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:29 +0000

    Add package data-notary/0.2.0/data-notary-0.2.0.tar.gz

---
+++ packages/data-notary/data-notary-0.2.0.tar.gz/data-notary-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = "data-notary",
+    version = "0.2.0",
+    description = "Simple data validators for Python",
+    long_description = "README",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/threefunkymonkeys/data-notary",
+    author = "Three Funky Monkeys",
+    author_email = "kandalf@threefunkymonkeys.com",
+    license = "MIT",
+    packages  =["notary"]
+)
