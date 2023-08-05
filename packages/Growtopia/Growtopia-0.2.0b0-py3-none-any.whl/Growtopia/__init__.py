4ef7535a75ba45565a65a51c06234ec7a97646ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:48 +0000

    Add package Growtopia/0.2.0b0/Growtopia-0.2.0b0-py3-none-any.whl

---
+++ packages/Growtopia/Growtopia-0.2.0b0-py3-none-any.whl/Growtopia/__init__.py
@@ -0,0 +1,58 @@
+
+"""
+UNOFFICIAL GROWTOPIA PYTHON PACKAGE
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This package is dumb, don't even use this for your product
+or it will get dumb.
+"""
+
+import os, random, socket, requests, struct, json
+#from functools import wraps
+from enet import *
+__version__ = '0.2.0-beta'
+
+# V0.1.0
+# Name taking
+
+# V0.1.1
+# Bug Fixing
+
+# V0.1.2
+# Bug Fixing
+
+# V0.1.3
+# Bug Fixing
+
+# v0.1.4
+# Package fixing
+
+# v0.1.5
+# Bug Fixing
+
+# v0.1.6
+# Exception solved
+
+# v0.1.7
+# Bug Fixing
+
+# v0.2.0-beta
+# Login system [BETA]
+# Added 2 Generate functions
+# Added server.py file and its functions
+# I upgrade my python from 3.7 into 3.8.3
+# +11 Growtopia.Player features
+# @Growtopia.Player.on added as event listener
+
+
+
+
+#######################################
+##	Importing The Required Things	 ##
+#######################################
+
+from .generate import *
+from .exceptions import *
+from .packet import *
+from .player import *
+from .server import *
