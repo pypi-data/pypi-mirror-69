36df6b4683a26b4b7e01cbc0242800cd72a6f2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:27 +0000

    Add package random-distributions/0.1/random%20distributions-0.1.tar.gz

---
+++ packages/random-distributions/random%20distributions-0.1.tar.gz/random distributions-0.1/README.md
@@ -0,0 +1,1 @@
+blah blah blah....bob lablaws lawblog
---
+++ packages/random-distributions/random%20distributions-0.1.tar.gz/random distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='random distributions',
+      version='0.1',
+      description='random distributions',
+      author='Luz Orejel',
+      author_email='deutschluz@gmail.com',
+      packages=['random distributions'],
+      zip_safe=False)
