3a91728c1961ae5ebf522663a64f8515c938ed43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package castle.cms/2.6.10/castle.cms-2.6.10.tar.gz

---
+++ packages/castle.cms/castle.cms-2.6.10.tar.gz/castle.cms-2.6.10/castle/cms/interfaces/content.py
@@ -0,0 +1,57 @@
+from plone.namedfile.interfaces import INamedImage
+from zope.interface import Attribute
+from OFS.interfaces import IApplication
+from plone.app.contenttypes.interfaces import IFile
+from plone.supermodel import model
+from Products.CMFPlone.interfaces import IHideFromBreadcrumbs
+from zope.interface import Interface
+from zope import schema
+
+
+class ICastleApplication(IApplication):
+    pass
+
+
+class IDashboard(IHideFromBreadcrumbs):
+    pass
+
+
+class IMedia(model.Schema, IFile):
+    pass
+
+
+class IVideo(IMedia):
+    pass
+
+
+class IAudio(IMedia):
+    pass
+
+
+class ISlideshow(Interface):
+    custom_dom_id = schema.Text(
+        title=u"ID for the slideshow element",
+        description=u"If custom styling desired for this slideshow",
+        required=False
+    )
+
+
+class ITrashed(Interface):
+    """
+    marker for object that is in the trash
+    """
+
+
+class IHasDefaultImage(Interface):
+    """
+    marker interface for content to specify that it has default image
+    when a manual image is not specified
+    """
+
+
+class IReferenceNamedImage(INamedImage):
+    reference = Attribute('')
+
+
+class IUploadedToYoutube(Interface):
+    """Marker interface for videos that have been uploaded to YouTube"""
