b4d5a4398150a9c154ea4da6a3dd81344f1b1f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:18 +0000

    Add package ddc/0.0.26/ddc-0.0.26-py3-none-any.whl

---
+++ packages/ddc/ddc-0.0.26-py3-none-any.whl/ddc/task_test.py
@@ -0,0 +1,50 @@
+import os
+from ddc.utils import stream_exec_cmd, __build_path
+
+
+def start_test(cwd: str, lang: str, save_test_results: bool = False):
+    all_output = []
+    exit_code = -1
+    try:
+        if lang == "py":
+            if not os.path.exists(cwd + "/tests"):
+                all_output.append("DDC: Directory /tests is not exists")
+                exit_code = 1
+                raise StopIteration()
+
+            image_tag: str = "ddc_local/tmp:latest"
+            exit_code, mix_output = stream_exec_cmd(
+                "docker build -t " + image_tag + " " + cwd
+            )
+            if exit_code:
+                all_output.append("DDC: Build docker image for test")
+                all_output.append(mix_output)
+                raise StopIteration()
+
+            rwmeta_path = __build_path("/.rwmeta/")
+
+            all_output.append("DDC: Run tests")
+            mounts = [
+                "-v {rwmeta_path}:/root/.rwmeta".format(rwmeta_path=rwmeta_path),
+                "-v {cwd}/test-reports:/usr/app/test-reports".format(cwd=cwd),
+            ]
+            pytest_args = "--reruns 2"
+            if save_test_results:
+                pytest_args += (
+                    " -o junit_family=xunit2 --junitxml=/usr/app/test-reports/tests.xml"
+                )
+            exit_code, mix_output = stream_exec_cmd(
+                "docker run --rm {mounts} {image_tag} pytest ./tests {pytest_args}".format(
+                    image_tag=image_tag,
+                    mounts=" ".join(mounts),
+                    pytest_args=pytest_args,
+                )
+            )
+
+            all_output.append(mix_output)
+            if exit_code:
+                raise StopIteration()
+
+    except StopIteration:
+        pass
+    return exit_code, "\n".join(all_output)
