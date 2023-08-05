d207bc94b4bfd6f57138cb70cc4739e221d4c8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:34 +0000

    Add package selenium-datatable/0.3.2/selenium_datatable-0.3.2.tar.gz

---
+++ packages/selenium-datatable/selenium_datatable-0.3.2.tar.gz/selenium_datatable-0.3.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='selenium_datatable',
+    version='0.3.2',
+    author='Łukasz Fundakowski',
+    author_email='fundakol@yahoo.com',
+    description="A small library for simplifying a table object in selenium",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fundakol/selenium_datatable',
+    packages=['selenium_datatable'],
+    python_requires='>=3.6',
+    install_requires=['selenium'],
+    keywords='selenium table',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ]
+)
