f8b3fcf8267d961d5746dd0818326de65f948ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:11 +0000

    Add package extras-require/0.0.2/extras_require-0.0.2.tar.gz

---
+++ packages/extras-require/extras_require-0.0.2.tar.gz/extras_require-0.0.2/__pkginfo__.py
@@ -0,0 +1,97 @@
+# This file is managed by `git_helper`. Don't edit it directly
+# Copyright (C) 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This script based on https://github.com/rocky/python-uncompyle6/blob/master/__pkginfo__.py
+
+import pathlib
+
+__all__ = [
+		"__copyright__",
+		"__version__",
+		"modname",
+		"pypi_name",
+		"py_modules",
+		"entry_points",
+		"__license__",
+		"short_desc",
+		"author",
+		"author_email",
+		"github_username",
+		"web",
+		"github_url",
+		"project_urls",
+		"repo_root",
+		"long_description",
+		"install_requires",
+		"extras_require",
+		"classifiers",
+		"keywords",
+		"import_name",
+		]
+
+__copyright__ = """
+2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+"""
+
+__version__ = "0.0.2"
+
+modname = "extras_require"
+pypi_name = "extras_require"
+import_name = "sphinxcontrib.extras_require"
+py_modules = []
+entry_points = {
+		"console_scripts": []
+		}
+
+__license__ = "BSD License"
+
+short_desc = "Display a warning at the top of module documentation that it has additional requirements."
+
+__author__ = author = "Dominic Davis-Foster"
+author_email = "dominic@davis-foster.co.uk"
+github_username = "domdfcoding"
+web = github_url = f"https://github.com/domdfcoding/extras_require"
+project_urls = {
+		"Documentation": f"https://extras_require.readthedocs.io",  # TODO: Make this link match the package version
+		"Issue Tracker": f"{github_url}/issues",
+		"Source Code": github_url,
+		}
+
+repo_root = pathlib.Path(__file__).parent
+
+# Get info from files; set: long_description
+long_description = (repo_root / "README.rst").read_text().replace("0.0.2", __version__) + '\n'
+
+install_requires = (repo_root / "requirements.txt").read_text().split('\n')
+extras_require = {'all': []}
+
+classifiers = [
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: Developers',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Framework :: Sphinx :: Extension',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Utilities',
+		'Topic :: Documentation',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Programming Language :: Python :: 3 :: Only',
+		'License :: OSI Approved :: BSD License',
+
+		]
+
+keywords = ""
