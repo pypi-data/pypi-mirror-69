5a6d7df71fd50b9204ef162cf3ca99e2d5c35a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:18 +0000

    Add package PathPandem/1.0.1.2/PathPandem-1.0.1.2.tar.gz

---
+++ packages/PathPandem/PathPandem-1.0.1.2.tar.gz/PathPandem-1.0.1.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+with open("./LongDescription", 'r') as README_FILE:
+    long_description = README_FILE.read()
+
+setup(
+   name='PathPandem',
+   version='1.0.1.2',
+   description='Simulate Pandemic Pathogen Outbreak',
+   license="GPLv3",
+   long_description=long_description,
+   author='Pradyumna Paranjape',
+   author_email='pradyparanjpe@rediffmail.com',
+   url="",
+   packages=['PathPandem'],  #same as name
+   install_requires=['numpy', 'gooey', 'matplotlib'], #external packages as dependencies
+   scripts=[
+            'PathPandem/spread_simul.py',
+           ]
+)
