ee9d4b5b10c40ceef9de1174ef84718e56eb913e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:56 +0000

    Add package aws-solutions-konstruk.aws-dynamodb-stream-lambda/0.8.1/aws_solutions_konstruk.aws_dynamodb_stream_lambda-0.8.1-py3-none-any.whl

---
+++ packages/aws-solutions-konstruk.aws-dynamodb-stream-lambda/aws_solutions_konstruk.aws_dynamodb_stream_lambda-0.8.1-py3-none-any.whl/aws_solutions_konstruk/aws_dynamodb_stream_lambda/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_lambda
+import aws_cdk.aws_lambda_event_sources
+import aws_cdk.core
+import aws_solutions_konstruk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-solutions-konstruk/aws-dynamodb-stream-lambda", "0.8.1", __name__[0:-6], "aws-dynamodb-stream-lambda@0.8.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
