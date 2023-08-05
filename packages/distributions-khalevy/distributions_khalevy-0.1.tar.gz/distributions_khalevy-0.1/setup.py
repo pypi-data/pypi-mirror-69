0b89a7bbab646af9dfa35d0462e128d5cbf74a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:46 +0000

    Add package distributions-khalevy/0.1/distributions_khalevy-0.1.tar.gz

---
+++ packages/distributions-khalevy/distributions_khalevy-0.1.tar.gz/distributions_khalevy-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_khalevy',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_khalevy'],
+      zip_safe=False)
