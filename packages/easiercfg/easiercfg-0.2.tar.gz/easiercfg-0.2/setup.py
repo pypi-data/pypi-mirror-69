a407874cf8627d44e2f05f9f74bc2cda716a9f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:42 +0000

    Add package easiercfg/0.2/easiercfg-0.2.tar.gz

---
+++ packages/easiercfg/easiercfg-0.2.tar.gz/easiercfg-0.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name='easiercfg',
+    packages=['easiercfg'],
+    version='0.2',
+    license='MIT',
+    description='Python Module for Config-Files focused on easy usage.',
+    author='kistee',
+    url='https://github.com/kistee/easiercfg',
+    download_url='https://github.com/kistee/easiercfg/archive/v_02.tar.gz',
+    keywords=['config', 'cfg', 'settings'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
