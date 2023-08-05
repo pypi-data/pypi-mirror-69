dcf11e91ce653e2bdd33960a29a73f22a321c851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package mock-mysql/0.0.1/mock-mysql-0.0.1.tar.gz

---
+++ packages/mock-mysql/mock-mysql-0.0.1.tar.gz/mock-mysql-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import os
+import re
+from setuptools import setup
+
+
+setup(name='mock-mysql',
+      version='0.0.1',
+      description='mysqld 服务端模板程序',
+      author="Neeky",
+      author_email="neeky@live.com",
+      maintainer='Neeky',
+      maintainer_email='neeky@live.com',
+      scripts=['bin/mock-mysql'],
+      packages=['mockmysql'],
+      python_requires='>=3.8.*',
+      )
