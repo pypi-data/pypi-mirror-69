be5cd75d5a8c441c4252f03499d56f8fbf0bcd08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:06 +0000

    Add package AIArena/0.0.47/AIArena-0.0.47-py3-none-any.whl

---
+++ packages/AIArena/AIArena-0.0.47-py3-none-any.whl/AIArena/games/__init__.py
@@ -0,0 +1,7 @@
+from AIArena.games.Connect4 import Connect4 as Connect4
+from AIArena.games.GymBlackjack import GymBlackjack as GymBlackjack
+from AIArena.games.GymFrozenLake import GymFrozenLake as GymFrozenLake
+from AIArena.games.Arena import Arena as Arena
+from AIArena.games.StockMarket import StockMarket as StockMarket
+from AIArena.games.SpaceFighter import SpaceFighter as SpaceFighter
+from AIArena.games.BattleTanks import BattleTanks as BattleTanks
