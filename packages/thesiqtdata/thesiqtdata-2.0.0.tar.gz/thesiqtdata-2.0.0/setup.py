2c8820b2ceeed9ee5e90f5b4527a2ab79392aa7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package thesiqtdata/2.0.0/thesiqtdata-2.0.0.tar.gz

---
+++ packages/thesiqtdata/thesiqtdata-2.0.0.tar.gz/thesiqtdata-2.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools 
+setuptools.setup(
+    name = "thesiqtdata",
+    py_modules=["thesiqtdata"],
+    version = "2.0.0",
+    author = "Cai The Si",
+    author_email = "caithesi21041997@gmail.com",
+    description = "my simple package",
+    packages = setuptools.find_packages(),
+)
