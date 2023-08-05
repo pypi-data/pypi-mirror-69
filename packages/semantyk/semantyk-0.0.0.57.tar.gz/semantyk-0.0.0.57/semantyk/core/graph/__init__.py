247903e501188075cb3144a285e9e4a61f3428f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:58 +0000

    Add package semantyk/0.0.0.57/semantyk-0.0.0.57.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.57.tar.gz/semantyk-0.0.0.57/semantyk/core/graph/__init__.py
@@ -0,0 +1,15 @@
+"""
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+# Graph
+Package | `__init__.py`
+
+Daniel Bakas Amuchastegui\
+May 21, 2020
+
+Copyright © Semantyk 2020. All rights reserved.\
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+"""
+
+from . import conjunctive_graph, graph
+from .conjunctive_graph import *
+from .graph import *
