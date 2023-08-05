f9da44aa63c1485fb062f848a0e868f12861253d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:58 +0000

    Add package pymysql2/1.2.3/pymysql2-1.2.3.tar.gz

---
+++ packages/pymysql2/pymysql2-1.2.3.tar.gz/pymysql2-1.2.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymysql2",
+    version="1.2.3",
+    author="AlaBouali",
+    author_email="trap.leader.123@gmail.com",
+    description="Simple and secure module to perform safe MySQL queries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlaBouali/bane",
+    python_requires=">=2.7",
+    install_requires=['pymysql'],
+    packages=["pymysql2"],
+    license="MIT License",
+    entry_points={
+       'console_scripts': [
+           'xmysql = pymysql2.__main__:run',
+       ],
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License ",
+    ],
+)
