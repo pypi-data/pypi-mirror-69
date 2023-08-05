46963933362866f6da2d9fa6c2b3edfe411c5c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:22 +0000

    Add package distributions-zxcvb/0.1/distributions_zxcvb-0.1.tar.gz

---
+++ packages/distributions-zxcvb/distributions_zxcvb-0.1.tar.gz/distributions_zxcvb-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_zxcvb',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_zxcvb'],
+      zip_safe=False)
