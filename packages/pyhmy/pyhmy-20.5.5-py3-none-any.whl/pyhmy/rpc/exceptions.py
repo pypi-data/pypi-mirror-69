4e5c6d9274c48818594af22e7e0f6781852518cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package pyhmy/20.5.5/pyhmy-20.5.5-py3-none-any.whl

---
+++ packages/pyhmy/pyhmy-20.5.5-py3-none-any.whl/pyhmy/rpc/exceptions.py
@@ -0,0 +1,36 @@
+import json
+import requests
+
+
+class RPCError(RuntimeError):
+    """
+    Exception raised when RPC call returns an error
+    """
+
+    def __init__(self, method, endpoint, error):
+        super().__init__(f'Error in reply from {endpoint}: {method} returned {error}')
+
+class InvalidRPCReplyError(RuntimeError):
+    """
+    Exception raised when RPC call returns unexpected result
+    Generally indicates Harmony API has been updated & pyhmy library needs to be updated as well
+    """
+
+    def __init__(self, method, endpoint):
+        super().__init__(f'Unexpected reply for {method} from {endpoint}')
+
+class RequestsError(requests.exceptions.RequestException):
+    """
+    Wrapper for requests lib exceptions
+    """
+
+    def __init__(self, endpoint):
+        super().__init__(f'Error connecting to {endpoint}')
+
+class RequestsTimeoutError(requests.exceptions.Timeout):
+    """
+    Wrapper for requests lib Timeout exceptions
+    """
+
+    def __init__(self, endpoint):
+        super().__init__(f'Error connecting to {endpoint}')
