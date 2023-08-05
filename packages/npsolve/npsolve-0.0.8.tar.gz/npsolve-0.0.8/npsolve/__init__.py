10fbe8c3f8af1fb2c1d4bf69dbc9fe106d469bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:52 +0000

    Add package npsolve/0.0.8/npsolve-0.0.8.tar.gz

---
+++ packages/npsolve/npsolve-0.0.8.tar.gz/npsolve-0.0.8/npsolve/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 20:46:26 2019
+
+@author: Reuben
+"""
+
+from . import settings
+from .core import Partial, Solver
+from .cache import multi_cached, mono_cached
+from . import soft_functions
+from .utils import *
