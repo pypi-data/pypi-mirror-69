c6f5b130cbe326b29bf7faaf00042715b0704475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:02 +0000

    Add package xmapper/1.0.1/xmapper-1.0.1.tar.gz

---
+++ packages/xmapper/xmapper-1.0.1.tar.gz/xmapper-1.0.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+xmapper
+--------
+xmapper is a XML format convert tool.
+
+See https://github.com/xxh840912/xmapper
+"""
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = '1.0.1'
+
+setup(
+    name="xmapper",
+    version=version,
+    description='Easy XML format converter',
+    long_description=__doc__,
+    long_description_content_type='text/x-rst',
+    author='Alex xi',
+    author_email='alexxi0213@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url="https://github.com/xxh840912/xmapper",
+    install_requires=[
+        "lxml==4.4.2",
+        "PyYAML==5.2",
+        "untangle==1.1.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
