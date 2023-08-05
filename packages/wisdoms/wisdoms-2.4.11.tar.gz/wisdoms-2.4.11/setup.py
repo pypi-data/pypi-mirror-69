fb073f3ee633623ee609d607ea04b62d69d60472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:44 +0000

    Add package wisdoms/2.4.11/wisdoms-2.4.11.tar.gz

---
+++ packages/wisdoms/wisdoms-2.4.11.tar.gz/wisdoms-2.4.11/setup.py
@@ -0,0 +1,28 @@
+# Created by Q-ays.
+# whosqays@gmail.com
+
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wisdoms",
+    version="2.4.11",
+    author="tzxd group",
+    author_email="975663670@qq.com",
+    description="公共查询添加取反查询，限制字段是否存在查询",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/soft-project/wisdom-lib",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pyYaml'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
