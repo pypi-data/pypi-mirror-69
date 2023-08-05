54faa5cd3069972be2bcdec90214750d6696f622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:19 +0000

    Add package corenetworks/0.1.5/corenetworks-0.1.5.tar.gz

---
+++ packages/corenetworks/corenetworks-0.1.5.tar.gz/corenetworks-0.1.5/corenetworks/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Default package."""
+
+from corenetworks.client import CoreNetworks  # noqa
+
+__author__ = "Robert Kaussow"
+__project__ = "corenetworks"
+__license__ = "MIT"
+__maintainer__ = "Robert Kaussow"
+__email__ = "mail@geeklabor.de"
+__url__ = "https://github.com/xoxys/corenetworks"
+__version__ = "0.1.5"
