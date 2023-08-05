3b1580fb106a926073c1cd2aa29d53a26207097f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:47 +0000

    Add package va-timeseries/1.0.2/va-timeseries-1.0.2.tar.gz

---
+++ packages/va-timeseries/va-timeseries-1.0.2.tar.gz/va-timeseries-1.0.2/timeseries/linearregression.py
@@ -0,0 +1,22 @@
+from .general import *
+
+def linear_regression(X, Y): 
+    x_prime = []
+    y_prime = []
+    
+    for i in range(len(Y)):
+        if (Y[i] / 1) == (Y[i]):
+            x_prime.append(X[i])
+            y_prime.append(Y[i])
+            
+    mean_x = mean(x_prime)
+    mean_y = mean(y_prime)
+    n = len(x_prime)
+    numer = 0
+    denom = 0
+    for i in range(n):
+        numer += (x_prime[i] - mean_x) * (y_prime[i] - mean_y)
+        denom += (x_prime[i] - mean_x) ** 2.0
+    m = numer / denom
+    c = mean_y - (m * mean_x)
+    return m, c
