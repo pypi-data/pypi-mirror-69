534cceb874a2b8578f185c7d9b3a2ead66d9a172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:35 +0000

    Add package ganb-probability/1.0/ganb_probability-1.0.tar.gz

---
+++ packages/ganb-probability/ganb_probability-1.0.tar.gz/ganb_probability-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ganb_probability',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['ganb_probability'],
+      author = 'Cecil Wiafe',
+      zip_safe=False)
