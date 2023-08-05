2cc12dee79750b47589f3c03cbca30e5fdedea6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:05 +0000

    Add package asyncpy/1.0.2/asyncpy-1.0.2.tar.gz

---
+++ packages/asyncpy/asyncpy-1.0.2.tar.gz/asyncpy-1.0.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="asyncpy",
+    url="https://github.com/lixi5338619/asyncpy.git",
+    version="1.0.2",
+    description="Use asyncio and aiohttp's concatenated web crawler framework",
+    long_description=long_description,
+    author="lx",
+    author_email="125066648@qq.com",
+    keywords="python web crawl asyncio",
+    maintainer='lx',
+    packages = find_packages(),
+    platforms=["all"],
+
+    install_requires=[
+        'lxml',
+        'parsel',
+        'docopt',
+        'aiohttp',
+        ],
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={'console_scripts': [
+        'asyncpy = asyncpy.__init__:cli',
+    ]},
+)
+
+
+## python setup.py sdist bdist_wheel
+## twine upload dist/*
