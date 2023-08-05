a65cc5ab2de8f9cdb9f2c5ca005814925330c879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:10 +0000

    Add package moban/0.7.5/moban-0.7.5-py2.py3-none-any.whl

---
+++ packages/moban/moban-0.7.5-py2.py3-none-any.whl/moban/core/plugins.py
@@ -0,0 +1,15 @@
+from lml.loader import scan_plugins_regex
+
+from moban import constants
+
+BUILTIN_EXENSIONS = [
+    "moban.plugins.jinja2.engine",
+    "moban.plugins.yaml_loader",
+    "moban.plugins.json_loader",
+    "moban.plugins.copy",
+    "moban.plugins.delete",
+]
+
+
+def make_sure_all_pkg_are_loaded():
+    scan_plugins_regex(constants.MOBAN_ALL, "moban", None, BUILTIN_EXENSIONS)
---
+++ packages/moban/moban-0.7.5-py2.py3-none-any.whl/moban/exceptions.py
@@ -0,0 +1,42 @@
+class DirectoryNotFound(Exception):
+    pass
+
+
+class FileNotFound(Exception):
+    pass
+
+
+class NoThirdPartyEngine(Exception):
+    pass
+
+
+class MobanfileGrammarException(Exception):
+    pass
+
+
+class NoTemplate(Exception):
+    pass
+
+
+class IncorrectDataInput(Exception):
+    pass
+
+
+class GroupTargetNotFound(Exception):
+    pass
+
+
+class NoGitCommand(Exception):
+    pass
+
+
+class UnsupportedPyFS2Protocol(Exception):
+    pass
+
+
+class NoPermissionsNeeded(Exception):
+    pass
+
+
+class SingleHTTPURLConstraint(Exception):
+    pass
