feab0e231680f2025a028bc1a17817805127c84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:20 +0000

    Add package asyncpy/1.0.0/asyncpy-1.0.0-py3-none-any.whl

---
+++ packages/asyncpy/asyncpy-1.0.0-py3-none-any.whl/asyncpy/demo_middleware.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Author  : lx
+
+
+
+from asyncpy.middleware import Middleware
+
+
+
+middleware = Middleware()
+
+
+
+@middleware.request
+async def UserAgentMiddleware(spider, request):
+    if request.meta.get('valid'):
+        print("当前爬虫名称:%s"%spider.name)
+        ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3100.0 Safari/537.36"
+        request.headers.update({"User-Agent": ua})
+
+
+
+@middleware.request
+async def ProxyMiddleware(spider, request):
+    if spider.name == 'demo':
+        #request.aiohttp_kwargs.update({"proxy": "http://121.56.38.77:4267"})
+        pass
+
