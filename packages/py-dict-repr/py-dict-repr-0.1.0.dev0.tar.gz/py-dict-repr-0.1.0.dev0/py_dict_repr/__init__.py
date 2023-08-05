c7422d2647d53fe4f1bf331786b409723698853b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:12 +0000

    Add package py-dict-repr/0.1.0.dev0/py-dict-repr-0.1.0.dev0.tar.gz

---
+++ packages/py-dict-repr/py-dict-repr-0.1.0.dev0.tar.gz/py-dict-repr-0.1.0.dev0/py_dict_repr/__init__.py
@@ -0,0 +1,13 @@
+from .__about__ import (
+    __version__,
+    __title__,
+    __summary__
+)
+
+from .py_dict_repr import DictRepr  # noqa: F401
+
+__all__ = [
+    "__version__",
+    "__title__",
+    "__summary__"
+]
