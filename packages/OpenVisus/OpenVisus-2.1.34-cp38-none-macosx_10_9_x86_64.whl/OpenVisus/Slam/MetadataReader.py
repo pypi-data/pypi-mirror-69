cef3dc57b465e45b29c96d395b9746dbd5df8097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:20 +0000

    Add package OpenVisus/2.1.34/OpenVisus-2.1.34-cp38-none-macosx_10_9_x86_64.whl

---
+++ packages/OpenVisus/OpenVisus-2.1.34-cp38-none-macosx_10_9_x86_64.whl/OpenVisus/Slam/MetadataReader.py
@@ -0,0 +1,40 @@
+import json
+import subprocess
+
+
+exiftool_command = os.path.join(ThisDir(__file__),"exiftool.exe") if WIN32 else "exiftool"
+
+# micasense source code need to find exiftool
+os.environ['exiftoolpath']=exiftool_command
+
+# ///////////////////////////////////////////////////////////////
+class MetadataReader(object):
+   
+   # constructor
+	def __init__(self):
+		cmd=[exiftool_command, "-stay_open", "True",  "-@", "-","-common_args", "-G", "-n"]
+		print(" ".join(cmd))
+		self.process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
+		
+	# close
+	def close(self):
+		self.process.stdin.write(b"-stay_open\nFalse\n")
+		self.process.stdin.flush()
+		self.process.communicate()
+		del self.process
+
+	# readMetadata
+	def readMetadata(self,filename):
+		
+		print("Reading metadata",filename)
+		self.process.stdin.write(b"-j\n" + filename.encode() + b"\n-execute\n")
+		self.process.stdin.flush()	
+		
+		output = b""
+		while not output.strip().endswith(b"{ready}"):
+			output+=os.read(self.process.stdout.fileno(), 4096)
+		output=output.strip()[:-len(b"{ready}")].decode("utf-8")
+			
+		ret=json.loads(output)[0]
+		return ret
+
