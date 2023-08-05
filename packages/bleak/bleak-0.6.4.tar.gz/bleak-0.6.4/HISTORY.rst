894e85dc3304fcf517533ea6a05b28771e96950a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package bleak/0.6.4/bleak-0.6.4.tar.gz

---
+++ packages/bleak/bleak-0.6.4.tar.gz/bleak-0.6.4/HISTORY.rst
@@ -0,0 +1,151 @@
+=======
+History
+=======
+
+0.6.4 (2020-05-20)
+------------------
+
+* Fix for bumpversion usage
+
+0.6.3 (2020-05-20)
+------------------
+
+* Building and releasing from Github Actions
+
+0.6.2 (2020-05-15)
+------------------
+
+* Better cleanup of Bluez notifications (#154)
+* Fix for ``read_gatt_char`` in Core Bluetooth (#177)
+* Fix for ``is_disconnected`` in Core Bluetooth (#187 & #185)
+* Added ``disconnection_callback`` functionality for Core Bluetooth (#184 & #186)
+* Documentation fixes
+* Added ``requirements.txt``
+
+0.6.1 (2020-03-09)
+------------------
+
+* Including #156, lost notifications on macOS backend, which was accidentally missed on previous release.
+
+0.6.0 (2020-03-09)
+------------------
+
+* New Scanner object to allow for async device scanning.
+* Updated ``txdbus`` requirement to version 1.1.1 (Merged #122)
+* Implemented ``write_gatt_descriptor`` for Bluez backend.
+* Large change in Bluez backend handling of Twisted reactors. Fixes #143
+* Modified ``set_disconnect_callback`` to actually call the callback as a callback. Fixes #108.
+* Added another required parameter to disconnect callbacks.
+* Added Discovery filter option in BlueZ backend (Merged #124)
+* Merge #138: comments about Bluez version check.
+* Improved scanning data for macOS backend. Merge #126.
+* Merges #141, a critical fix for macOS.
+* Fix for #114, write with response on macOS.
+* Fix for #87, DIctionary changes size on .NET backend.
+* Fix for #127, uuid or str on macOS.
+* Handles str/uuid for characteristics better.
+* Merge #148, Run .NET backend notifications on event loop instead of main loop.
+* Merge #146, adapt characteristic write log to account for WriteWithoutResponse on macOS.
+* Fix for #145, Error in cleanup on Bluez backend.
+* Fix for #151, only subscribe to BlueZ messages on DBus. Merge #152.
+* Fix for #142, Merge #144, Improved scanning for macOS backend.
+* Fix for #155, Merge #156, lost notifications on macOS backend.
+* Improved type hints
+* Improved error handling for .NET backend.
+* Documentation fixes.
+
+
+0.5.1 (2019-10-09)
+------------------
+
+* Active Scanning on Windows, #99 potentially solving #95
+* Longer timeout in service discovery on BlueZ
+* Added ``timeout`` to constructors and connect methods
+* Fix for ``get_services`` on macOS. Relates to #101
+* Fixes for disconnect callback on BlueZ, #86 and #83
+* Fixed reading of device name in BlueZ. It is not readable as regular characteristic. #104
+* Removed logger feedback in BlueZ discovery method.
+* More verbose exceptions on macOS, #117 and #107
+
+0.5.0 (2019-08-02)
+------------------
+
+* macOS support added (thanks to @kevincar)
+* Merged #90 which fixed #89: Leaking callbacks in BlueZ
+* Merged #92 which fixed #91, Prevent leaking of DBus connections on discovery
+* Merged #96: Regex patterns
+* Merged #86 which fixed #83 and #82
+* Recovered old .NET discovery method to try for #95
+* Merged #80: macOS development
+
+0.4.3 (2019-06-30)
+------------------
+
+* Fix for #76
+* Fix for #69
+* Fix for #74
+* Fix for #68
+* Fix for #70
+* Merged #66
+
+0.4.2 (2019-05-17)
+------------------
+
+* Fix for missed part of PR #61.
+
+0.4.1 (2019-05-17)
+------------------
+
+* Merging of PR #61, improvements and fixes for multiple issues for BlueZ backend
+* Implementation of issue #57
+* Fixing issue #59
+* Documentation fixes.
+
+0.4.0 (2019-04-10)
+------------------
+
+* Transferred code from the BleakUWPBridge C# support project to pythonnet code
+* Fixed BlueZ >= 5.48 issues regarding Battery Service
+* Fix for issue #55
+
+0.3.0 (2019-03-18)
+------------------
+
+* Fix for issue #53: Windows and Python 3.7 error
+* Azure Pipelines used for CI
+
+0.2.4 (2018-11-30)
+------------------
+
+* Fix for issue #52: Timing issue getting characteristics
+* Additional fix for issue #51.
+* Bugfix for string method for BLEDevice.
+
+0.2.3 (2018-11-28)
+------------------
+
+* Fix for issue #51: ``dpkg-query not found on all Linux systems``
+
+0.2.2 (2018-11-08)
+------------------
+
+* Made it compliant with Python 3.5 by removing f-strings
+
+0.2.1 (2018-06-28)
+------------------
+
+* Improved logging on .NET discover method
+* Some type annotation fixes in .NET code
+
+0.2.0 (2018-04-26)
+------------------
+
+* Project added to Github
+* First version on PyPI.
+* Working Linux (BlueZ DBus API) backend.
+* Working Windows (UWP Bluetooth API) backend.
+
+0.1.0 (2017-10-23)
+------------------
+
+* Bleak created.
---
+++ packages/bleak/bleak-0.6.4.tar.gz/bleak-0.6.4/bleak/backends/corebluetooth/CentralManagerDelegate.py
@@ -0,0 +1,233 @@
+"""
+CentralManagerDelegate will implement the CBCentralManagerDelegate protocol to
+manage CoreBluetooth serivces and resources on the Central End
+
+Created on June, 25 2019 by kevincar <kevincarrolldavis@gmail.com>
+
+"""
+
+import asyncio
+import logging
+from enum import Enum
+from typing import List
+
+import objc
+from Foundation import (
+    NSObject,
+    CBCentralManager,
+    CBPeripheral,
+    CBUUID,
+    NSArray,
+    NSDictionary,
+    NSNumber,
+    NSError,
+)
+
+from bleak.backends.corebluetooth.PeripheralDelegate import PeripheralDelegate
+from bleak.backends.corebluetooth.device import BLEDeviceCoreBluetooth
+
+
+logger = logging.getLogger(__name__)
+
+CBCentralManagerDelegate = objc.protocolNamed("CBCentralManagerDelegate")
+
+
+class CMDConnectionState(Enum):
+    DISCONNECTED = 0
+    PENDING = 1
+    CONNECTED = 2
+
+
+class CentralManagerDelegate(NSObject):
+    """macOS conforming python class for managing the CentralManger for BLE"""
+
+    ___pyobjc_protocols__ = [CBCentralManagerDelegate]
+
+    def init(self):
+        """macOS init function for NSObject"""
+        self = objc.super(CentralManagerDelegate, self).init()
+
+        if self is None:
+            return None
+
+        self.central_manager = CBCentralManager.alloc().initWithDelegate_queue_(
+            self, None
+        )
+
+        self.connected_peripheral_delegate = None
+        self.connected_peripheral = None
+        self._connection_state = CMDConnectionState.DISCONNECTED
+
+        self.ready = False
+        self.devices = {}
+
+        self.disconnected_callback = None
+
+        if not self.compliant():
+            logger.warning("CentralManagerDelegate is not compliant")
+
+        return self
+
+    # User defined functions
+
+    def compliant(self):
+        """Determins whether the class adheres to the CBCentralManagerDelegate protocol"""
+        return CentralManagerDelegate.pyobjc_classMethods.conformsToProtocol_(
+            CBCentralManagerDelegate
+        )
+
+    @property
+    def enabled(self):
+        """Check if the bluetooth device is on and running"""
+        return self.central_manager.state() == 5
+
+    @property
+    def isConnected(self) -> bool:
+        return self._connection_state == CMDConnectionState.CONNECTED
+
+    async def is_ready(self):
+        """is_ready allows an asynchronous way to wait and ensure the
+        CentralManager has processed it's inputs before moving on"""
+        while not self.ready:
+            await asyncio.sleep(0)
+        return self.ready
+
+    async def scanForPeripherals_(self, scan_options) -> List[CBPeripheral]:
+        """
+        Scan for peripheral devices
+        scan_options = { service_uuids, timeout }
+        """
+        # remove old
+        self.devices = {}
+        service_uuids = []
+        if "service_uuids" in scan_options:
+            service_uuids_str = scan_options["service_uuids"]
+            service_uuids = NSArray.alloc().initWithArray_(
+                list(map(string2uuid, service_uuids_str))
+            )
+
+        timeout = 0
+        if "timeout" in scan_options:
+            timeout = float(scan_options["timeout"])
+
+        self.central_manager.scanForPeripheralsWithServices_options_(
+            service_uuids, None
+        )
+
+        if timeout > 0:
+            await asyncio.sleep(timeout)
+
+        self.central_manager.stopScan()
+        while self.central_manager.isScanning():
+            await asyncio.sleep(0.1)
+
+        return []
+
+    async def connect_(self, peripheral: CBPeripheral) -> bool:
+        self._connection_state = CMDConnectionState.PENDING
+        self.central_manager.connectPeripheral_options_(peripheral, None)
+
+        while self._connection_state == CMDConnectionState.PENDING:
+            await asyncio.sleep(0)
+
+        self.connected_peripheral = peripheral
+
+        return self._connection_state == CMDConnectionState.CONNECTED
+
+    async def disconnect(self) -> bool:
+        self._connection_state = CMDConnectionState.PENDING
+        self.central_manager.cancelPeripheralConnection_(self.connected_peripheral)
+
+        while self._connection_state == CMDConnectionState.PENDING:
+            await asyncio.sleep(0)
+
+        return self._connection_state == CMDConnectionState.DISCONNECTED
+
+    # Protocol Functions
+
+    def centralManagerDidUpdateState_(self, centralManager):
+        if centralManager.state() == 0:
+            logger.debug("Cannot detect bluetooth device")
+        elif centralManager.state() == 1:
+            logger.debug("Bluetooth is resetting")
+        elif centralManager.state() == 2:
+            logger.debug("Bluetooth is unsupported")
+        elif centralManager.state() == 3:
+            logger.debug("Bluetooth is unauthorized")
+        elif centralManager.state() == 4:
+            logger.debug("Bluetooth powered off")
+        elif centralManager.state() == 5:
+            logger.debug("Bluetooth powered on")
+
+        self.ready = True
+
+    def centralManager_didDiscoverPeripheral_advertisementData_RSSI_(
+        self,
+        central: CBCentralManager,
+        peripheral: CBPeripheral,
+        advertisementData: NSDictionary,
+        RSSI: NSNumber,
+    ):
+        # Note: this function might be called several times for same device.
+        # Example a first time with the following keys in advertisementData:
+        # ['kCBAdvDataLocalName', 'kCBAdvDataIsConnectable', 'kCBAdvDataChannel']
+        # ... and later a second time with other keys (and values) such as:
+        # ['kCBAdvDataServiceUUIDs', 'kCBAdvDataIsConnectable', 'kCBAdvDataChannel']
+        #
+        # i.e it is best not to trust advertisementData for later use and data
+        # from it should be copied.
+        #
+        # This behaviour could be affected by the
+        # CBCentralManagerScanOptionAllowDuplicatesKey global setting.
+
+        uuid_string = peripheral.identifier().UUIDString()
+
+        if uuid_string in self.devices:
+            device = self.devices[uuid_string]
+        else:
+            address = uuid_string
+            name = peripheral.name() or None
+            details = peripheral
+            device = BLEDeviceCoreBluetooth(address, name, details)
+            self.devices[uuid_string] = device
+
+        device._rssi = float(RSSI)
+        device._update(advertisementData)
+
+        logger.debug("Discovered device {}: {} @ RSSI: {} (kCBAdvData {})".format(
+                uuid_string, device.name, RSSI, advertisementData.keys()))
+
+    def centralManager_didConnectPeripheral_(self, central, peripheral):
+        logger.debug(
+            "Successfully connected to device uuid {}".format(
+                peripheral.identifier().UUIDString()
+            )
+        )
+        peripheralDelegate = PeripheralDelegate.alloc().initWithPeripheral_(peripheral)
+        self.connected_peripheral_delegate = peripheralDelegate
+        self._connection_state = CMDConnectionState.CONNECTED
+
+    def centralManager_didFailToConnectPeripheral_error_(
+        self, centralManager: CBCentralManager, peripheral: CBPeripheral, error: NSError
+    ):
+        logger.debug(
+            "Failed to connect to device uuid {}".format(
+                peripheral.identifier().UUIDString()
+            )
+        )
+        self._connection_state = CMDConnectionState.DISCONNECTED
+
+    def centralManager_didDisconnectPeripheral_error_(
+        self, central: CBCentralManager, peripheral: CBPeripheral, error: NSError
+    ):
+        logger.debug("Peripheral Device disconnected!")
+        self._connection_state = CMDConnectionState.DISCONNECTED
+
+        if self.disconnected_callback is not None:
+            self.disconnected_callback()
+
+
+def string2uuid(uuid_str: str) -> CBUUID:
+    """Convert a string to a uuid"""
+    return CBUUID.UUIDWithString_(uuid_str)
+
