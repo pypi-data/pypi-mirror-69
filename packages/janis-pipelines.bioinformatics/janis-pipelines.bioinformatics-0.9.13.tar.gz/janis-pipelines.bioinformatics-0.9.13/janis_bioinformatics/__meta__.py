1234c95b183a88e151b3273ea6ca543e312fb395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:37 +0000

    Add package janis-pipelines.bioinformatics/0.9.13/janis-pipelines.bioinformatics-0.9.13.tar.gz

---
+++ packages/janis-pipelines.bioinformatics/janis-pipelines.bioinformatics-0.9.13.tar.gz/janis-pipelines.bioinformatics-0.9.13/janis_bioinformatics/__meta__.py
@@ -0,0 +1,2 @@
+__version__ = "v0.9.13"
+description = "Bioinformatics tools for Janis; the Pipeline creation helper"
---
+++ packages/janis-pipelines.bioinformatics/janis-pipelines.bioinformatics-0.9.13.tar.gz/janis-pipelines.bioinformatics-0.9.13/janis_bioinformatics/tools/freebayes/versions.py
@@ -0,0 +1,21 @@
+from .base_1_2 import FreeBayesBase_1_2
+from .base_1_3 import FreeBayesBase_1_3
+
+
+class FreeBayes_1_2(FreeBayesBase_1_2):
+    def container(self):
+        return "papaemmelab/docker-freebayes:v0.1.5"
+
+    def version(self):
+        return "1.2"
+
+
+class FreeBayes_1_3(FreeBayesBase_1_3):
+    def container(self):
+        return "shollizeck/freebayes:1.3.1"
+
+    def version(self):
+        return "1.3.1"
+
+
+FreeBayesLatest = FreeBayes_1_3
