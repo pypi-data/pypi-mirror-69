76e36ba41a0d09768c3b33ab068b2cca5d0a5034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:35 +0000

    Add package big-todo/0.0.5/big-todo-0.0.5.tar.gz

---
+++ packages/big-todo/big-todo-0.0.5.tar.gz/big-todo-0.0.5/README.md
@@ -0,0 +1,31 @@
+# big-todo
+
+Make a big TODO file by concatenating all TODO files in a directory tree.
+
+# install
+
+    pip install big-todo
+
+# usage
+
+usage: btodo - make a big TODO file [-h] [--name NAME] [--include-header] dir
+
+positional arguments:
+  dir               the root directory
+
+optional arguments:
+  -h, --help        show this help message and exit
+  --name NAME       the name of the file to combine
+  --include-header  prepend the relative file path to each section
+
+# example use
+
+From the repo root, run:
+
+    big-todo demo_folder --include-header
+    
+This will traverse the demo_folder and generate a virtual TODO file with
+headers turned on, meaning the path of the file being appended will be
+inserted as the first line of output.  
+    
+   
---
+++ packages/big-todo/big-todo-0.0.5.tar.gz/big-todo-0.0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="big-todo",
+    version="0.0.5",
+    author="Jake Kara",
+    author_email="jake@jakekara.com",
+    description="CLI tool to combine TODO files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jakekara/big-todo",
+    packages=['btodo'],  # setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "glob2"
+    ],
+    entry_points={
+        'console_scripts': ['btodo=btodo:main'],
+    },
+    python_requires='>=3.6',
+)
