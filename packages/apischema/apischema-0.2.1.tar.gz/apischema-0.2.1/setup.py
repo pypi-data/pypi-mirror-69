bb2d62a7701ebd6f52edd3d76355f4c5e20a7ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package apischema/0.2.1/apischema-0.2.1.tar.gz

---
+++ packages/apischema/apischema-0.2.1.tar.gz/apischema-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    README = f.read()
+
+# cannot use Cython because of https://github.com/cython/cython/issues/3537
+setup(
+    name='apischema',
+    url="https://github.com/wyfo/apischema",
+    author="Joseph Perez",
+    author_email="joperez@hotmail.fr",
+    description="Another Python API schema handling and JSON (de)serialization "
+                "through typing annotation; light, simple, powerful.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    version='0.2.1',
+    license='MIT',
+    packages=find_packages(include=["apischema*"]),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.7'
+)
