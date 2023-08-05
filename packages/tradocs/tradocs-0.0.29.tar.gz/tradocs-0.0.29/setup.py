843ed4f63fa22425dfbff659363e1d13fe649425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:09 +0000

    Add package tradocs/0.0.29/tradocs-0.0.29.tar.gz

---
+++ packages/tradocs/tradocs-0.0.29.tar.gz/tradocs-0.0.29/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+REQUIREMENTS = [
+    'Click',
+    'translate-toolkit',
+    'lxml',
+    'requests',
+    'colorama',
+]
+
+CLASSIFIERS = [
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+]
+
+setup(
+    name='tradocs',
+    version='0.0.29',
+    author='Eber Rodrigues',
+    author_email='eberjoe@gmail.com',
+    description='DocFX localization',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/eberjoe/ui-docs-zanata-md',
+    license = 'MIT',
+    packages = find_packages(),
+    entry_points = {
+        'console_scripts': ['tradocs = tradocs.tradocs:root']
+    },
+    classifiers = CLASSIFIERS,
+    install_requires = REQUIREMENTS,
+    python_requires='>=3.6',
+)
