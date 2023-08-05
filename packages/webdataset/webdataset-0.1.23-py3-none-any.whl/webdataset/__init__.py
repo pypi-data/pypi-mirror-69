0952aa5554759f67b7434479aaa9c861b61bcab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package webdataset/0.1.23/webdataset-0.1.23-py3-none-any.whl

---
+++ packages/webdataset/webdataset-0.1.23-py3-none-any.whl/webdataset/__init__.py
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2017-2019 NVIDIA CORPORATION. All rights reserved.
+# This file is part of the WebDataset library.
+# See the LICENSE file for licensing terms (BSD-style).
+#
+# flake8: noqa
+
+__all__ = "tenbin dataset writer".split()
+
+from . import tenbin
+from .dataset import (
+    Dataset,
+    ChoppedDataset,
+    reraise_exception,
+    ignore_and_continue,
+    warn_and_continue,
+    ignore_and_stop,
+    warn_and_stop,
+)
+from .autodecode import default_handlers
+from .writer import ShardWriter, TarWriter
