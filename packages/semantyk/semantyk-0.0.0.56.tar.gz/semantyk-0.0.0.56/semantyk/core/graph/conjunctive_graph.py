295a2175a13cec3c967e6fb8bd87b05d059416ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:39 +0000

    Add package semantyk/0.0.0.56/semantyk-0.0.0.56.tar.gz

---
+++ packages/semantyk/semantyk-0.0.0.56.tar.gz/semantyk-0.0.0.56/semantyk/core/graph/conjunctive_graph.py
@@ -0,0 +1,18 @@
+"""
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+# Conjunctive Graph
+Module | `conjunctive_graph.py`
+
+Daniel Bakas Amuchastegui\
+May 21, 2020
+
+Copyright © Semantyk 2020. All rights reserved.\
+–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+"""
+
+__all__ = ['ConjunctiveGraph']
+
+from .graph import Graph
+
+class ConjunctiveGraph(Graph):
+    pass
