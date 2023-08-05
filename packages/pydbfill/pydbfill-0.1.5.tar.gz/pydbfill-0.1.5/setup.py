4d438dfdbefb0372fb4118928dee7c2b0392f320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package pydbfill/0.1.5/pydbfill-0.1.5.tar.gz

---
+++ packages/pydbfill/pydbfill-0.1.5.tar.gz/pydbfill-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pydbfill']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['faker>=4.1.0,<5.0.0', 'pymysql>=0.9.3,<0.10.0', 'pytest>=5.4.2,<6.0.0']
+
+setup_kwargs = {
+    'name': 'pydbfill',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Oren Mazor',
+    'author_email': 'oren.mazor@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
