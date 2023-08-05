4731d07191f0a63d016e2b0792336208d89baa57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:14 +0000

    Add package distributed-prob/1.0/distributed_prob-1.0.tar.gz

---
+++ packages/distributed-prob/distributed_prob-1.0.tar.gz/distributed_prob-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributed_prob',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distributed_prob'],
+      author = "Prerna Singh",
+      author_email = "writeprernasingh@gmail.com",
+    zip_safe=False)
