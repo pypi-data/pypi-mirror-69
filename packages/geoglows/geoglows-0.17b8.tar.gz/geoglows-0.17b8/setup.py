aea11c071fb2aaeeb29f25877f224dcaa8d7f6ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:22 +0000

    Add package geoglows/0.17b8/geoglows-0.17b8.tar.gz

---
+++ packages/geoglows/geoglows-0.17b8.tar.gz/geoglows-0.17b8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name='geoglows',
+    packages=['geoglows'],
+    version='0.17.b8',
+    description='Package for accessing data and APIs developed for the GEOGloWS initiative',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Riley Hales',
+    url='https://geoglows.org',
+    project_urls=dict(Documentation='https://geoglows.readthedocs.io', Source='https://github.com/rileyhales/geoglows'),
+    license='BSD 3-Clause',
+    license_family='BSD',
+    package_data={'': ['*.ipynb', '*.html']},
+    include_package_data=True,
+    python_requires='>=3',
+    install_requires=['requests', 'plotly>=4.6', 'jinja2', 'pandas', 'shapely', 'scipy']
+)
