a800f0d4428d82788c78eb947efbbd4467e2490f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:40 +0000

    Add package icmp-sniffer/0.1.0/icmp_sniffer-0.1.0.tar.gz

---
+++ packages/icmp-sniffer/icmp_sniffer-0.1.0.tar.gz/icmp_sniffer-0.1.0/README.md
@@ -0,0 +1,12 @@
+[![<Paulius11>](https://circleci.com/gh/Paulius11/icmp-sniffer.svg?style=svg)](<https://app.circleci.com/pipelines/github/Paulius11/icmp-sniffer>) 
+
+Instruction: 
+
+Program requires admin right
+
+`$ icmp_sniffer -h` 
+
+
+pypi: 
+
+https://pypi.org/project/icmp-sniffer/
---
+++ packages/icmp-sniffer/icmp_sniffer-0.1.0.tar.gz/icmp_sniffer-0.1.0/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="icmp_sniffer",
+    version="0.1.0",
+    description="Scans ping packages and posts them to mongodb.",
+    url="https://github.com/Paulius11/icmp-sniffer",
+    long_description=README,
+    author="Paulius",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["icmp_sniffer"],
+    include_package_data=True,
+    install_requires=["pymongo", "scapy"],
+    entry_points={
+        "console_scripts": [
+            "icmp_sniffer=icmp_sniffer.__main__:main",
+        ]
+    },
+)
