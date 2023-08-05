15e012ef6a11b3200463bf1d9b43df3e834742c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:05 +0000

    Add package wechaty/0.4.3/wechaty-0.4.3.tar.gz

---
+++ packages/wechaty/wechaty-0.4.3.tar.gz/wechaty-0.4.3/src/wechaty/__init__.py
@@ -0,0 +1,23 @@
+"""doc"""
+
+from wechaty_puppet import (    # type: ignore
+    FileBox,
+)
+from .accessory import Accessory
+from .wechaty import Wechaty
+from .user import (
+    Contact,
+    Favorite,
+)
+from .config import (
+    logging,
+)
+
+__all__ = [
+    'logging',
+    'Accessory',
+    'Contact',
+    'Favorite',
+    'FileBox',
+    'Wechaty',
+]
