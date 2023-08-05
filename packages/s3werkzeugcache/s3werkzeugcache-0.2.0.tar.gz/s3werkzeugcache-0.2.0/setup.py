8ff77bf4c3450c84641de73942ffc6754fa02e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:59 +0000

    Add package s3werkzeugcache/0.2.0/s3werkzeugcache-0.2.0.tar.gz

---
+++ packages/s3werkzeugcache/s3werkzeugcache-0.2.0.tar.gz/s3werkzeugcache-0.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+version = '0.2.0'
+
+setup(
+    name='s3werkzeugcache',
+    description=(
+        "S3 implementation of the werkzeug cache"),
+    version=version,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'boto3',
+        'cachelib',
+    ],
+    author='Bogdan Kyryliuk',
+    author_email='b.kyryliuk@gmail.com',
+    url='https://github.com/bkyryliuk/s3werkzeugcache',
+    download_url=(
+        'https://github.com/bogdan-kyryliuk/s3cache/tarball/' + version),
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
