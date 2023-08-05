216dfe29240e71d49d4c305091121f19c885d3c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:36 +0000

    Add package itick-dist/0.1/itick_dist-0.1.tar.gz

---
+++ packages/itick-dist/itick_dist-0.1.tar.gz/itick_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='itick_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['itick_dist'],
+      author = 'Aitik Dandapat',
+      author_email = 'aitik2000@gmail.com',
+      zip_safe=False)
