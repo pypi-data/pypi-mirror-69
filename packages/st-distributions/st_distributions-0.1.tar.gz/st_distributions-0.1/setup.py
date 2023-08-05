0a9288d6bdc1b7dc7fb30465ef03499aa69996bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:48 +0000

    Add package st-distributions/0.1/st_distributions-0.1.tar.gz

---
+++ packages/st-distributions/st_distributions-0.1.tar.gz/st_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='st_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['st_distributions'],
+      author='Yifei Tong',
+      author_email='tongyf.shane2013@outlook.com',
+      zip_safe=False)
