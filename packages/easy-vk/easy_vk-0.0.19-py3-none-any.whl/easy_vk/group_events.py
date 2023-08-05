4b148ce8efc9fb53b3fb7f83b6ef46cb861d9ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package easy-vk/0.0.19/easy_vk-0.0.19-py3-none-any.whl

---
+++ packages/easy-vk/easy_vk-0.0.19-py3-none-any.whl/easy_vk/group_events.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from .vk_types import *
+
+####################
+# Events:
+
+def get_event(j):
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_reply':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+    if j.get('type') == 'message_new':
+        pass
+
+
+
+
