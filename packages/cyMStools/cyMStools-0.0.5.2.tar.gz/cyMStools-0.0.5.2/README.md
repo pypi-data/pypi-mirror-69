f2c2ca01aef9015df41e0941935cd308952d78b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:42 +0000

    Add package cyMStools/0.0.5.2/cyMStools-0.0.5.2.tar.gz

---
+++ packages/cyMStools/cyMStools-0.0.5.2.tar.gz/cyMStools-0.0.5.2/README.md
@@ -0,0 +1,6 @@
+# cyMStools
+mass spec tools
+summary the plink2 reports file
+# Useage 
+1. from cyMStools import *
+2. summary_plink2_report(reports_path, spectra_cutoff, e-value_cutoff)
---
+++ packages/cyMStools/cyMStools-0.0.5.2.tar.gz/cyMStools-0.0.5.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+# 读取项目的readme介绍
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cyMStools",# 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
+    version="0.0.5.2",
+    author="zhyCAO", # 项目作者
+    author_email="zhycao@outlook.com",
+    description="mass spec tools", # 项目的一句话描述
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/daheitu/cyMStools",# 项目地址
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
