cf2f0523fca3c33ece0ea09315007186fba42892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:33 +0000

    Add package greyli/0.1/greyli-0.1.tar.gz

---
+++ packages/greyli/greyli-0.1.tar.gz/greyli-0.1/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+greyli
+---------------
+Package for Grey Li.
+"""
+from setuptools import setup
+
+setup(
+    name='greyli',
+    version='0.1',
+    url='https://github.com/greyli/greyli',
+    license='MIT',
+    author='Grey Li',
+    author_email='withlihui@gmail.com',
+    description='',
+    long_description=__doc__,
+    long_description_content_type='text/markdown',
+    platforms='any',
+    packages=['greyli'],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
