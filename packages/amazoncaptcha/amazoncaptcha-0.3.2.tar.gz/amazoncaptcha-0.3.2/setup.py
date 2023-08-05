dff9c5b15f078f7baab89f8543de446b9584539a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:06 +0000

    Add package amazoncaptcha/0.3.2/amazoncaptcha-0.3.2.tar.gz

---
+++ packages/amazoncaptcha/amazoncaptcha-0.3.2.tar.gz/amazoncaptcha-0.3.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="amazoncaptcha",
+    version="0.3.2",
+    description="Solving captchas never ends...",
+    packages=['amazoncaptcha'],
+    py_modules=['objects'],
+    include_package_data = True,
+    package_data = {
+        '': ['*.json'],
+        'amazoncaptcha': ['data/*.*'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = [
+        "pillow ~= 7.1.2"
+    ],
+    author="Anatolii Maliarov",
+    author_email="tly.mov@gmail.com",
+    url="https://github.com/a-maliarov/amazon-captcha-solver",
+)
