ca48384a4c0c378a771c341197aaa6c3693b4b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:10 +0000

    Add package ChineseTimeNLP/2.0.0/ChineseTimeNLP-2.0.0.tar.gz

---
+++ packages/ChineseTimeNLP/ChineseTimeNLP-2.0.0.tar.gz/ChineseTimeNLP-2.0.0/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+
+import setuptools
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+with open(HERE / "README.md", encoding="utf-8") as f:
+    README = f.read()
+
+
+with open("requirements.txt") as f:
+    install_requires = [line for line in f if line and line[0] not in "#-"]
+
+setup(
+    name="ChineseTimeNLP",
+    version="2.0.0",
+    keywords=("time", "nlp", "time nlp"),
+    author="BudyLab",
+    author_email="lengthmin@gmail.com",
+    long_description_content_type="text/markdown",
+    description="将中文时间表达词转为相应的时间",
+    long_description=README,
+    license="MIT Licence",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    classifiers=["Programming Language :: Python :: 3.7"],
+)
