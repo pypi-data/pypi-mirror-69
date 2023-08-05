59ca2a7cfb0d38f7d84a83b83ea67a680bd78a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:03 +0000

    Add package stats-distributions-ud-scholarship/0.1/stats_distributions_ud_scholarship-0.1.tar.gz

---
+++ packages/stats-distributions-ud-scholarship/stats_distributions_ud_scholarship-0.1.tar.gz/stats_distributions_ud_scholarship-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stats_distributions_ud_scholarship',
+      version='0.1',
+      description='Gaussian distributions and Binomial dsitribution',
+      packages=['stats_distributions_ud_scholarship'],
+      author = 'Peter Olaomi',
+      author_email = 'p.olaomi@gmail.com',
+      zip_safe=False)
