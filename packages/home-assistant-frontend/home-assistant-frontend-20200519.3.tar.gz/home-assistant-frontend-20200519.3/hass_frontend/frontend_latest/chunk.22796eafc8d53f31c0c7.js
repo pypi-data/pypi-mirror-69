7def70ea1a6989d34d7daaaa3c974df8678a30aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:20 +0000

    Add package home-assistant-frontend/20200519.3/home-assistant-frontend-20200519.3.tar.gz

---
+++ packages/home-assistant-frontend/home-assistant-frontend-20200519.3.tar.gz/home-assistant-frontend-20200519.3/hass_frontend/frontend_latest/chunk.22796eafc8d53f31c0c7.js
@@ -0,0 +1,24 @@
+(self.webpackJsonp=self.webpackJsonp||[]).push([[127],{735:function(e,a,l){"use strict";l.r(a);l(179);var o=l(4),r=l(31);l(156),l(119);customElements.define("ha-panel-iframe",class extends r.a{static get template(){return o.a`
+      <style include="ha-style">
+        iframe {
+          border: 0;
+          width: 100%;
+          position: absolute;
+          height: calc(100% - 64px);
+          background-color: var(--primary-background-color);
+        }
+      </style>
+      <app-toolbar>
+        <ha-menu-button hass="[[hass]]" narrow="[[narrow]]"></ha-menu-button>
+        <div main-title>[[panel.title]]</div>
+      </app-toolbar>
+
+      <iframe
+        src="[[panel.config.url]]"
+        sandbox="allow-forms allow-popups allow-pointer-lock allow-same-origin allow-scripts"
+        allowfullscreen="true"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+      ></iframe>
+    `}static get properties(){return{hass:Object,narrow:Boolean,panel:Object}}})}}]);
+//# sourceMappingURL=chunk.22796eafc8d53f31c0c7.js.map
