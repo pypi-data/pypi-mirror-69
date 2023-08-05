f5e39725cea187ecb87a55cdcaa39d5fbf18aef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:14 +0000

    Add package Auxjad/0.7.1/auxjad-0.7.1.tar.gz

---
+++ packages/Auxjad/auxjad-0.7.1.tar.gz/auxjad-0.7.1/auxjad/core/_AdriftContainer.py
@@ -0,0 +1,56 @@
+import copy
+import random
+import abjad
+
+
+class _AdriftContainer():
+    r"""xxx
+    """
+
+    ### INITIALISER ###
+
+    def __init__(self,
+                 container: abjad.Container,
+                 *,
+                 head_position: int = 0,
+                 ):
+        r'Initialises self.'
+        self._initial_container = copy.deepcopy(container)
+
+    ### SPECIAL METHODS ###
+
+    def __call__(self):
+        r"""Calls the drifting process for one iteration, returning an
+        ``abjad.Selection``.
+        """
+        self._fade_process()
+        return self.current_container
+
+    ### PUBLIC METHODS ###
+
+    def output_all(self):
+        r'xxx'
+        pass
+
+    ### PRIVATE METHODS ###
+
+    def _fade_process(self):
+        r'xxx'
+        pass
+
+    def _remove_random_leaf(self):
+        r'xxx'
+        pass
+
+    def _add_random_leaf(self):
+        r'xxx'
+        pass
+
+    ### PUBLIC PROPERTIES ###
+
+    @property
+    def current_container(self):
+        r"""Read-only property, returns the index of the previously output
+        element.
+        """
+        return copy.deepcopy(self._current_container)
