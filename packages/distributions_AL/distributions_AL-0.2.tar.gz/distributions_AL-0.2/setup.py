9c8a0be3d39e6231d32e0859ad201d4f531acce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:57 +0000

    Add package distributions_AL/0.2/distributions_AL-0.2.tar.gz

---
+++ packages/distributions_AL/distributions_AL-0.2.tar.gz/distributions_AL-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_AL',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_AL'],
+      author = "Abhishek Lomsh",
+      author_email = "abhisheklomsh@gmail.com",
+      zip_safe=False)
