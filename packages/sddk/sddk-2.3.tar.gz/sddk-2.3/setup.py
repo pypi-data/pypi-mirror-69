1cf41e2af641a2ac90914c434350a198ff64ddf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:36 +0000

    Add package sddk/2.3/sddk-2.3.tar.gz

---
+++ packages/sddk/sddk-2.3.tar.gz/sddk-2.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sddk", # Replace with your own username
+    version="2.3",
+    author="Vojtech Kase",
+    author_email="vojtech.kase@gmail.com",
+    description="A package to access sciencedata.dk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sdam-au/sddk",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    	"pyarrow>=0.17.0",
+        "requests",
+        "matplotlib",
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
