157e899a9319c0f02e377e5146552968d1d5a133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:56 +0000

    Add package cf666/1.0.0/cf666-1.0.0.tar.gz

---
+++ packages/cf666/cf666-1.0.0.tar.gz/cf666-1.0.0/setup.py
@@ -0,0 +1,19 @@
+
+# from distutils.core import setup
+from setuptools import setup
+
+setup(
+    name="cf666",
+    version="1.0.0",
+    description="this is a niubi lib",
+    packages=["sztestlib"],
+    py_modules=["Tool"],
+    author="Sz",
+    author_email="1169983240@qq.com",
+    long_description="this is a long long long lib",
+    uril="http://wsdfs.be.com"
+
+
+
+
+)
