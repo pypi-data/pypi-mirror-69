edbf5adc7ebd005792e68d768c71c7f1f6ebb615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package biocircuits/0.0.19/biocircuits-0.0.19.tar.gz

---
+++ packages/biocircuits/biocircuits-0.0.19.tar.gz/biocircuits-0.0.19/biocircuits/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for biocircuits"""
+
+__all__ = ["viz", "utils", "reg", "dynsys"]
+
+from .viz import *
+from .utils import *
+from .reg import *
+from .dynsys import *
+from .gillespie import *
+from .rd import *
+
+__author__ = """Justin Bois"""
+__email__ = "bois@caltech.edu"
+__version__ = "0.0.19"
