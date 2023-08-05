908df56a32a3a8ffed022d976dffc74b907cfe33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package sentry-dingtalk-new11/1.1.2/sentry-dingtalk-new11-1.1.2.tar.gz

---
+++ packages/sentry-dingtalk-new11/sentry-dingtalk-new11-1.1.2.tar.gz/sentry-dingtalk-new11-1.1.2/README.md
@@ -0,0 +1,32 @@
+
+# sentry-dingtalk-new11
+
+| Version | Sentry Version |
+|---- |----|
+| 1.0.4 | 9.0.* |
+| 1.1.0 | 9.1.* |
+
+===============
+
+配置sentry 钉钉报警通知
+
+安装 - install 
+
+```bash
+
+- pip install sentry-dingtalk-new11
+```
+
+* or docker 
+
+```bash
+
+  echo sentry-dingtalk-new11 >> requirements.txt
+```
+
+ 重新构建或者重启(rebuild or reboot)
+
+* sentry version: 9.0.0
+* dingding webhook
+* Sentry sending DingTalk. See https://www.dingtalk.com/
+* DingTalk is a product of Alibaba Inc.
---
+++ packages/sentry-dingtalk-new11/sentry-dingtalk-new11-1.1.2.tar.gz/sentry-dingtalk-new11-1.1.2/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+sentry-Dingtalk
+==============
+
+An extension for Sentry which integrates with Dingtalk. It will send
+notifications to dingtalk robot.
+
+:copyright: (c) 2017 by guoyong yi, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+from setuptools import setup, find_packages
+
+# See http://stackoverflow.com/questions/9352656/python-assertionerror-when-running-nose-tests-with-coverage
+# for why we need to do this.
+from multiprocessing import util
+
+
+tests_require = [
+]
+
+install_requires = [
+    'sentry>=9.1.0',
+]
+
+
+setup(
+    name='sentry-dingtalk-new11',
+    version='1.1.2',
+    keywords='sentry dingding dingtalk',
+    author='ZhangShiJie',
+    author_email='zhangshijie62302@offcn.com',
+    url='https://github.com/zhangshj/sentry-dingtalk.git',
+    description='A Sentry extension which integrates with Dingtalk robot.',
+    long_description=__doc__,
+    long_description_content_type='text/markdown',
+    license='BSD',
+    platforms='any',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    test_suite='nose.collector',
+    entry_points={
+        'sentry.plugins': [
+            'dingtalk = sentry_dingtalk.plugin:DingtalkPlugin'
+        ],
+    },
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development'
+    ],
+)
+
