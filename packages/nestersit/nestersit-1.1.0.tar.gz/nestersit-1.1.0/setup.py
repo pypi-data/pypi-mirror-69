f9409db21272f83b5071c00cff71b1f291da0e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:01 +0000

    Add package nestersit/1.1.0/nestersit-1.1.0.tar.gz

---
+++ packages/nestersit/nestersit-1.1.0.tar.gz/nestersit-1.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='nestersit',
+    version='1.1.0',
+    py_modules=['nester'],
+    author='qcj',
+    author_email='xiaofishy@163.com',
+    url='http://www.baidu.com',
+    description='A simple printer of nested lists',
+)
