58a652bbbfc5576dc5f56fc0c1104bd5020a6ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package saladware/0.5.1/saladware-0.5.1.tar.gz

---
+++ packages/saladware/saladware-0.5.1.tar.gz/saladware-0.5.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='saladware',
+      version='0.5.1',
+      description='two bears',
+      packages=['saladware'],
+      author='SALAD37',
+      author_email='nonemovo@gmail.com',
+      zip_safe=False)
