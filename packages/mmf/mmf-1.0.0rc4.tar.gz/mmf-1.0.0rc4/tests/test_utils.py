ce1223538d7b8c6f11fa7ef072925ddb270a9f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:12 +0000

    Add package mmf/1.0.0rc4/mmf-1.0.0rc4.tar.gz

---
+++ packages/mmf/mmf-1.0.0rc4.tar.gz/mmf-1.0.0rc4/tests/test_utils.py
@@ -0,0 +1,57 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+import argparse
+import socket
+import unittest
+
+import torch
+
+
+def compare_tensors(a, b):
+    return torch.equal(a, b)
+
+
+def dummy_args(model="cnn_lstm", dataset="clevr"):
+    args = argparse.Namespace()
+    args.opts = ["model={}".format(model), "dataset={}".format(dataset)]
+    args.config_override = None
+    return args
+
+
+def is_network_reachable():
+    try:
+        # check if host name can be resolved
+        host = socket.gethostbyname("one.one.one.one")
+        # check if host is actually reachable
+        s = socket.create_connection((host, 80), 2)
+        s.close()
+        return True
+    except IOError as e:
+        if e.errno == 101:
+            pass
+    return False
+
+
+NETWORK_AVAILABLE = is_network_reachable()
+
+
+def skip_if_no_network(testfn, reason="Network is not available"):
+    return unittest.skipUnless(NETWORK_AVAILABLE, reason)(testfn)
+
+
+def compare_state_dicts(a, b):
+    same = True
+    same = same and (list(a.keys()) == list(b.keys()))
+    if not same:
+        return same
+
+    for val1, val2 in zip(a.values(), b.values()):
+        if isinstance(val1, torch.Tensor):
+            same = same and compare_tensors(val1, val2)
+        elif not isinstance(val2, torch.Tensor):
+            same = same and val1 == val2
+        else:
+            same = False
+        if not same:
+            return same
+
+    return same
