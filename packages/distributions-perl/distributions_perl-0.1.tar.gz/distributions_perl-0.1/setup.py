f596407b19bf0540ed08255d888a3819d2b59318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package distributions-perl/0.1/distributions_perl-0.1.tar.gz

---
+++ packages/distributions-perl/distributions_perl-0.1.tar.gz/distributions_perl-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'distributions_perl',
+      version = '0.1',
+      description = 'Gaussian distributions',
+      packages = ['distributions_perl'],
+      author = 'Gustavo Perl',
+      author_email = 'gustavopperl@gmail.com',
+      zip_safe = False)
