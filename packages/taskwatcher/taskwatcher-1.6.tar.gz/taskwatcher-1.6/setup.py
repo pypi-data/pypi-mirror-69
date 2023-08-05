c79757f58a8e847dc6d0d5dae36aba2a99a2af75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:28 +0000

    Add package taskwatcher/1.6/taskwatcher-1.6.tar.gz

---
+++ packages/taskwatcher/taskwatcher-1.6.tar.gz/taskwatcher-1.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="taskwatcher",
+    version="1.6",
+    author="Cedric GUSTAVE",
+    author_email="cgustave@free.fr",
+    description="Package taskwatcher",
+	long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/cgustave/taskwatcher",
+    packages=find_packages(),
+    classifiers=[
+	    "Development Status :: 4 - Beta",
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: POSIX :: Linux",
+		"Natural Language :: English",
+		"Topic :: System :: Networking",
+    ],
+    python_requires='>=3.5',
+)
+
