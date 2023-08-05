febeb2d660db90c0076bf63f21eb47ada160742e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:07 +0000

    Add package iox/0.0.22/iox-0.0.22.tar.gz

---
+++ packages/iox/iox-0.0.22.tar.gz/iox-0.0.22/iox/_config.py
@@ -0,0 +1,26 @@
+
+from configparser import RawConfigParser
+import os.path
+import sys
+
+this = sys.modules[__name__]
+this.google = {}
+
+home = os.path.expanduser('~')
+
+# If ~/.iox_profile exists, read it
+config_file = os.path.join(home, '.ioxrc')
+if os.path.exists(config_file):
+    # Parse the configuration file
+    parser = RawConfigParser()
+    parser.read(config_file)
+
+    # Get credentials
+    this.google['credentials'] = parser.get('google', 'credentials')
+
+    # Get project ID
+    this.google['project_id'] = parser.get('google', 'project-id')
+
+
+
+
