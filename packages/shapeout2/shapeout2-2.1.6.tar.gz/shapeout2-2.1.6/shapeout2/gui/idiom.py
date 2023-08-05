fe554719833241b6db52cf0146b521d5e49e1324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package shapeout2/2.1.6/shapeout2-2.1.6.tar.gz

---
+++ packages/shapeout2/shapeout2-2.1.6.tar.gz/shapeout2-2.1.6/shapeout2/gui/idiom.py
@@ -0,0 +1,17 @@
+from dclab.rtdc_dataset import ancillaries
+
+#: integer-valued features
+INTEGER_FEATURES = [
+    "fl1_max",
+    "fl1_npeaks",
+    "fl2_max",
+    "fl2_npeaks",
+    "fl3_max",
+    "fl3_npeaks",
+    "frame",
+    "index",
+    "nevents",
+]
+
+#: ancillary features that are easily computed
+QUICK_FEATURES = ancillaries.FEATURES_RAPID
