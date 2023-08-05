b12ecf117302c01fa83e29282a2397d7eecf0ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:39 +0000

    Add package mnp-distributions/0.1/mnp_distributions-0.1.tar.gz

---
+++ packages/mnp-distributions/mnp_distributions-0.1.tar.gz/mnp_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mnp_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mnp_distributions'],
+      zip_safe=False)
