f8812c548a5fe07a441d28cd0a47b8ad339ecfaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:52 +0000

    Add package snotra-rss/0.2.5.4/snotra_rss-0.2.5.4.tar.gz

---
+++ packages/snotra-rss/snotra_rss-0.2.5.4.tar.gz/snotra_rss-0.2.5.4/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as rp:
+    req = rp.read()
+
+setuptools.setup(
+    name="snotra_rss",
+    version="0.2.5.4",
+    author="Samuel MEYNARD",
+    author_email="samuel@meyn.fr",
+    description="Wagtail app - with rss aggregator and tweeter aggregator feature",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/olopost/snotra_rss",
+    install_requires=req,
+    packages=setuptools.find_packages(),
+    package_data={
+        'examples': ['templates/*.html']
+    },
+    include_package_data=True,
+    package_dir={'snotra_rss': 'snotra_rss'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
