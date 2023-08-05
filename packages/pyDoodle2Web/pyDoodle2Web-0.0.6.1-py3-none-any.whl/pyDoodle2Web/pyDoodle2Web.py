806a7f4a040529491f2b230e1104e28dc67d9e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package pyDoodle2Web/0.0.6.1/pyDoodle2Web-0.0.6.1-py3-none-any.whl

---
+++ packages/pyDoodle2Web/pyDoodle2Web-0.0.6.1-py3-none-any.whl/pyDoodle2Web/pyDoodle2Web.py
@@ -0,0 +1,12 @@
+from .ocr import OCR
+from .htmlGenerator import HTMLGenerator
+
+class PyDoodle2Web:
+    def __init__(self, path:str, darkMode:bool = True):
+        self.path = path
+        self.darkMode = True
+
+    def generate(self):
+        tags = OCR(self.path).readText()
+        html, _ = HTMLGenerator(tagsList=tags, darkMode=self.darkMode).generateHTML()
+        return html
