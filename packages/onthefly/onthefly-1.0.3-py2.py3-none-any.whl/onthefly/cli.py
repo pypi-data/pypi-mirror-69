4ed990aa826f43c1a3bd9a7b283109b0292b757c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:00 +0000

    Add package onthefly/1.0.3/onthefly-1.0.3-py2.py3-none-any.whl

---
+++ packages/onthefly/onthefly-1.0.3-py2.py3-none-any.whl/onthefly/cli.py
@@ -0,0 +1,19 @@
+"""Console script for onthefly."""
+import sys
+import click
+from .onthefly import onthefly
+from elevate import elevate
+
+
+@click.command()
+@click.argument('filename', type=click.Path(exists=True))
+@click.option('--keyboard', help='A match string used to identify your keyboard.')
+def main(filename, keyboard):
+    """Emulates typing each character contained inside FILENAME."""
+    elevate()
+    onthefly(filename, keyboard)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
