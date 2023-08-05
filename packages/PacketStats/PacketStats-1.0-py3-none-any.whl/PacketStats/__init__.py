3ea481924d8d70fe6909fbce739dcf052fc7b858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:19 +0000

    Add package PacketStats/1.0/PacketStats-1.0-py3-none-any.whl

---
+++ packages/PacketStats/PacketStats-1.0-py3-none-any.whl/PacketStats/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+import csv
+
+from .statistics import *
+from .convertion import *
+
+
+def generate_csv(f_name, move, percentage, header_names):
+    print("%s %s %s" % (f_name, move, percentage))
+    result = permutations(statistics(f_name, header_names), header_names, move, percentage)
+    with open(f_name.split('.')[0] + '-generated-data.csv', 'w', newline='') as file:
+        csv_file = csv.writer(file, delimiter='\t')
+        for row in result:
+            csv_file.writerow(row)
