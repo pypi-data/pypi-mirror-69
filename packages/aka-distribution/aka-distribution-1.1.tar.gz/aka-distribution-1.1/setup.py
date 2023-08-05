269ade3c8a93fe97f8a818f974f38abc8bf2f824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:30 +0000

    Add package aka-distribution/1.1/aka-distribution-1.1.tar.gz

---
+++ packages/aka-distribution/aka-distribution-1.1.tar.gz/aka-distribution-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aka-distribution',
+      version='1.1',
+      description='Gaussian-Binomial distributions',
+      packages=['aka-distribution'],
+      author= 'Akash Salvi',
+      author_email = 'akash@gmail.com',
+      zip_safe=False)
