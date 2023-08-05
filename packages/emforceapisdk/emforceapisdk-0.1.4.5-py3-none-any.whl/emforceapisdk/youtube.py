0e7b9fbd7a0cbaedfc96935c8c0dace71ab02706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:00 +0000

    Add package emforceapisdk/0.1.4.5/emforceapisdk-0.1.4.5-py3-none-any.whl

---
+++ packages/emforceapisdk/emforceapisdk-0.1.4.5-py3-none-any.whl/emforceapisdk/youtube.py
@@ -0,0 +1,12 @@
+class Youtube:
+    def __init__(self):
+        pass
+
+    def get_mostPopular_videos(self):
+        """
+            chart:mostPopular
+            regionCode:KR
+            part:id,snippet
+            maxResults:50
+        """
+        pass
