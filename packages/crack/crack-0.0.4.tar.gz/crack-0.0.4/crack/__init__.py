4416a4cf8a396d11cc22c4d5156c7f4a01d91b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:42 +0000

    Add package crack/0.0.4/crack-0.0.4.tar.gz

---
+++ packages/crack/crack-0.0.4.tar.gz/crack-0.0.4/crack/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @author: leesoar
+
+"""something."""
+
+from crack.util import *
+from crack.setting import __version__
+
+__all__ = ["partition", "b64encode", "b64decode"]
