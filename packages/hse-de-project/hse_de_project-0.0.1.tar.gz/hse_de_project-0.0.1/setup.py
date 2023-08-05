fad20cbbd5d0b8205ee9f5be7ff805f99533d99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:41 +0000

    Add package hse-de-project/0.0.1/hse_de_project-0.0.1.tar.gz

---
+++ packages/hse-de-project/hse_de_project-0.0.1.tar.gz/hse_de_project-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hse_de_project',
+    version='0.0.1',
+    description='HSE AMI Differential Equations Project',
+    license='MIT',
+    packages=['hse_de_project'],
+    author='Artem Streltsov',
+    author_email='adstreltsov@edu.hse.ru',
+    keywords=['differential', 'equations', 'study'],
+    url='https://github.com/kekekekule/Differential-equations-project'
+)
