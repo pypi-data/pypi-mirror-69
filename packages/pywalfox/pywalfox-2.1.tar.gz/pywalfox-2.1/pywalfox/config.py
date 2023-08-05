e0213e76397de9288ebcd9c573b0698cd9bc0600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:19 +0000

    Add package pywalfox/2.1/pywalfox-2.1.tar.gz

---
+++ packages/pywalfox/pywalfox-2.1.tar.gz/pywalfox-2.1/pywalfox/config.py
@@ -0,0 +1,33 @@
+import os
+
+DAEMON_VERSION='2.1'
+
+UNIX_SOCKET_PATH='/tmp/pywalfox_socket'
+WIN_SOCKET_HOST = ('127.0.0.1', 56744)
+
+SUPPORTED_BROWSERS=['firefox']
+
+HOME_PATH=os.getenv('XDG_CONFIG_HOME', os.path.expanduser('~'))
+PYWAL_COLORS_PATH=os.path.join(HOME_PATH, '.cache/wal/colors')
+
+APP_PATH=os.path.dirname(os.path.abspath(__file__))
+BIN_PATH_WIN=os.path.join(APP_PATH, 'bin/win.bat')
+BIN_PATH_UNIX=os.path.join(APP_PATH, 'bin/main.sh')
+CSS_PATH=os.path.join(APP_PATH, 'assets/css')
+
+LOG_FILE_PATH=os.path.join(APP_PATH, 'daemon.log')
+LOG_FILE_COUNT=1
+LOG_FILE_MAX_SIZE=1000*200 # 0.2 mb
+LOG_FILE_FORMAT='[%(asctime)s] %(levelname)s:%(message)s'
+LOG_FILE_DATE_FORMAT='%m-%d-%Y %I:%M:%S'
+
+ACTIONS={
+    'VERSION': 'debug:version',
+    'OUTPUT': 'debug:output',
+    'COLORS': 'action:colors',
+    'INVALID_ACTION': 'action:invalid',
+    'CSS_ENABLE': 'css:enable',
+    'CSS_DISABLE': 'css:disable',
+    'CSS_FONT_SIZE': 'css:font:size',
+}
+
