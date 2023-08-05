3395c5093e76dc491ee5e6cb11e882958ca40c53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:05 +0000

    Add package generalDistributions/0.2/generalDistributions-0.2.tar.gz

---
+++ packages/generalDistributions/generalDistributions-0.2.tar.gz/generalDistributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='generalDistributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['generalDistributions'],
+      author = 'abdelwahid benzerrouk',
+      author_email = 'benzerrouk.wahid@gmail.com',
+      zip_safe=False)
