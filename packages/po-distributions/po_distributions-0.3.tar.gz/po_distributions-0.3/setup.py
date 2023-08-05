461f9752ff57e54e976b309ecac52a536f8c44b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:13 +0000

    Add package po-distributions/0.3/po_distributions-0.3.tar.gz

---
+++ packages/po-distributions/po_distributions-0.3.tar.gz/po_distributions-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='po_distributions',
+      version='0.3',
+      description='Gaussian and binomial distributions',
+      packages=['po_distributions'],
+      zip_safe=False)
