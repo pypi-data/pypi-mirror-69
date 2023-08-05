5d17d0873287a5e0acd85762af6937f45d742f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:09 +0000

    Add package msticpy/0.5.1/msticpy-0.5.1.tar.gz

---
+++ packages/msticpy/msticpy-0.5.1.tar.gz/msticpy-0.5.1/msticpy/data/drivers/kql_driver.py
@@ -0,0 +1,182 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""KQL Driver class."""
+from typing import Tuple, Union, Any, Dict, Optional
+
+import pandas as pd
+from IPython import get_ipython
+
+from .driver_base import DriverBase, QuerySource
+from ...common.utility import export, MsticpyException
+from ..._version import VERSION
+
+__version__ = VERSION
+__author__ = "Ian Hellen"
+
+
+@export
+class KqlDriver(DriverBase):
+    """KqlDriver class to execute kql queries."""
+
+    def __init__(self, connection_str: str = None, **kwargs):
+        """
+        Instantiaite KqlDriver and optionally connect.
+
+        Parameters
+        ----------
+        connection_str : str, optional
+            Connection string
+
+        """
+        self._ip = get_ipython()
+        self._debug = kwargs.get("debug", False)
+        super().__init__()
+
+        self._loaded = self._is_kqlmagic_loaded()
+
+        if not self._loaded:
+            self._load_kql_magic()
+
+        if connection_str:
+            self.current_connection = connection_str
+            self.connect(connection_str)
+
+        self._schema: Dict[str, Any] = {}
+
+    def connect(self, connection_str: Optional[str] = None, **kwargs):
+        """
+        Connect to data source.
+
+        Parameters
+        ----------
+        connection_str : str
+            Connect to a data source
+
+        """
+        if not connection_str:
+            raise MsticpyException("No connection string supplied.")
+        self.current_connection = connection_str
+        result = False
+        if self._ip is not None:
+            result = self._ip.run_cell_magic("kql", line="", cell=connection_str)
+            self._connected = True
+            self._schema = self._get_schema()
+        else:
+            print(f"Could not connect to kql query provider for {connection_str}")
+        return result
+
+    @property
+    def schema(self) -> Dict[str, Dict]:
+        """
+        Return current data schema of connection.
+
+        Returns
+        -------
+        Dict[str, Dict]
+            Data schema of current connection.
+
+        """
+        return self._schema
+
+    def query(
+        self, query: str, query_source: QuerySource = None
+    ) -> Union[pd.DataFrame, Any]:
+        """
+        Execute query string and return DataFrame of results.
+
+        Parameters
+        ----------
+        query : str
+            The query to execute
+        query_source : QuerySource
+            The query definition object
+
+        Returns
+        -------
+        Union[pd.DataFrame, results.ResultSet]
+            A DataFrame (if successfull) or
+            the underlying provider result if an error.
+
+        """
+        del query_source
+        data, result = self.query_with_results(query)
+        return data if data is not None else result
+
+    # pylint: disable=too-many-branches
+    def query_with_results(self, query: str, **kwargs) -> Tuple[pd.DataFrame, Any]:
+        """
+        Execute query string and return DataFrame of results.
+
+        Parameters
+        ----------
+        query : str
+            The kql query to execute
+
+        Returns
+        -------
+        Tuple[pd.DataFrame, results.ResultSet]
+            A DataFrame (if successfull) and
+            Kql ResultSet.
+
+        """
+        # connect or switch the connection if our connection string
+        # is not the current KqlMagic connection.
+        self.connect(self.current_connection)
+        if not self.connected:
+            raise ConnectionError(
+                "Source is not connected. Please call connect() and retry."
+            )
+
+        if self._debug:
+            print(query)
+
+        # save current auto_dataframe setting so that we can set to false
+        # and restore current setting
+        auto_dataframe = self._ip.run_line_magic(
+            "config", line="Kqlmagic.auto_dataframe"
+        )
+        self._ip.run_line_magic("config", line="Kqlmagic.auto_dataframe=False")
+        # run the query
+        result = self._ip.run_cell_magic("kql", line="", cell=query)
+        self._ip.run_line_magic(
+            "config", line=f"Kqlmagic.auto_dataframe={auto_dataframe}"
+        )
+        if result is not None:
+            if isinstance(result, pd.DataFrame):
+                return result, None
+            if (
+                hasattr(result, "completion_query_info")
+                and result.completion_query_info["StatusCode"] == 0
+            ):
+                data_frame = result.to_dataframe()
+                if result.is_partial_table:
+                    print("Warning - query returned partial results.")
+                return data_frame, result
+
+        print("Warning - query did not complete successfully.")
+        if hasattr(result, "completion_query_info"):
+            print(
+                result.completion_query_info["StatusCode"],
+                "(code: {}".format(result.completion_query_info["StatusCode"]),
+            )
+        return None, result
+
+    def _load_kql_magic(self):
+        """Load KqlMagic if not loaded."""
+        # KqlMagic
+        print("Please wait. Loading Kqlmagic extension...")
+        if self._ip is not None:
+            self._ip.run_line_magic("reload_ext", "Kqlmagic")
+            self._loaded = True
+
+    def _is_kqlmagic_loaded(self) -> bool:
+        """Return true if kql magic is loaded."""
+        if self._ip is not None:
+            return self._ip.find_magic("kql") is not None
+        return False
+
+    def _get_schema(self) -> Dict[str, Dict]:
+        return self._ip.run_line_magic("kql", line="--schema")
---
+++ packages/msticpy/msticpy-0.5.1.tar.gz/msticpy-0.5.1/setup.py
@@ -0,0 +1,93 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""Setup script for msticpy."""
+
+import re
+import setuptools
+
+
+INSTALL_REQUIRES = [
+    "adal>=1.2.2",
+    "attrs>=18.2.0",
+    "azure-common>=1.1.18",
+    "azure-cli-core==2.5.0",
+    "azure-core>=1.2.2",
+    "azure-identity>=1.3.0",
+    "azure-keyvault-secrets>=4.0.0",
+    "azure-mgmt-compute>=4.6.2",
+    "azure-mgmt-keyvault>=2.0.0",
+    "azure-mgmt-monitor>=0.5.2",
+    "azure-mgmt-network>=2.7.0",
+    "azure-mgmt-resource>=2.2.0",
+    "azure-mgmt-subscription>=0.2.0",
+    "beautifulsoup4>=4.8.0",
+    "bokeh>=1.4.0",
+    "cryptography>=2.8",
+    "deprecated>=1.2.4",
+    "dnspython>=1.16.0",
+    "folium>=0.9.0",
+    "geoip2>=2.9.0",
+    "ipwhois>=1.1.0",
+    "ipython>=7.2.0",
+    "ipywidgets>=7.4.2",
+    "keyring>=18.0.0",
+    "Kqlmagic>=0.1.106",
+    "matplotlib>=3.0.2",
+    "msal~=1.0.0",
+    "msrest>=0.6.0",
+    "msrestazure>=0.6.0",
+    "networkx>=2.2",
+    "numpy>=1.15.4",
+    "pandas>=0.25.0",
+    "pytz>=2019.2",
+    "pyyaml>=3.13",
+    "requests>=2.21.1",
+    "scikit-learn>=0.20.2",
+    "seaborn>=0.9.0",
+    "setuptools>=40.6.3",
+    "tldextract>=2.2.2",
+    "tqdm>=4.36.1",
+    "typing>=3.6.6",
+    "urllib3>=1.24.1",
+]
+
+
+# pylint: disable=locally-disabled, invalid-name
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("msticpy/_version.py", "r") as fd:
+    v_match = re.search(r'^VERSION\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE)
+    __version__ = v_match.group(1) if v_match else "no version"
+# pylint: enable=locally-disabled, invalid-name
+
+setuptools.setup(
+    name="msticpy",
+    version=__version__,
+    author="Ian Hellen",
+    author_email="ianhelle@microsoft.com",
+    description="MSTIC Security Tools",
+    license="MIT License",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/microsoft/msticpy",
+    project_urls={
+        "Documentation": "https://msticpy.readthedocs.io",
+        "Code": "https://github.com/microsoft/msticpy",
+    },
+    python_requires=">=3.6",
+    packages=setuptools.find_packages(exclude=["*.tests"]),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    install_requires=INSTALL_REQUIRES,
+    keywords=["security", "azure", "sentinel"],
+    zip_safe=False,
+    include_package_data=True,
+)
