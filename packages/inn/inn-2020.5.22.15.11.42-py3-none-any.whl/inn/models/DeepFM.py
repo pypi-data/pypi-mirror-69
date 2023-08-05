86016015eb583d8972a69e3e6ffb0c98537248bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:32 +0000

    Add package inn/2020.5.22.15.11.42/inn-2020.5.22.15.11.42-py3-none-any.whl

---
+++ packages/inn/inn-2020.5.22.15.11.42-py3-none-any.whl/inn/models/DeepFM.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : inn.
+# @File         : DeepFM
+# @Time         : 2020/5/22 10:03 上午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+from typing import Any, Callable, Dict, List, Optional, Sequence, Type, Union
+
+from collections import OrderedDict
+
+from inn.models.BaseModel import BaseModel
+
+import tensorflow as tf
+from inn.features.fc import *
+
+from inn.layers import DNN
+from inn.layers.interaction import FM
+
+
+class DeepFM(BaseModel):
+
+    def __init__(self, fcs: List[Column],
+                 fm=FM(),
+                 dnn=DNN(),
+                 num_class=2,
+                 model_name='DeepFM',
+                 **kwargs):
+        super().__init__(fcs=fcs,
+                         fm=fm,
+                         dnn=dnn,
+                         num_class=num_class,
+                         model_name=model_name,
+                         **kwargs)
+
+    def _build_model(self, **kwargs):
+        """
+        sigmoid(fm, dnn)
+        """
+
+        if len(self.embedding_output_layer) > 0 and len(self.dense_output_layer) > 0:
+            k_out = tf.keras.layers.concatenate([self.embedding_output_layer, self.dense_output_layer])
+        elif len(self.embedding_output_layer) > 0:
+            k_out = self.embedding_output_layer
+        else:  # len(self.dense_output_layer) > 0
+            k_out = self.embedding_output_layer
+
+        fm_out = self.fm(k_out)
+        fm_out = tf.keras.layers.add([self.linear_output_layer, fm_out])
+
+        dnn_out = self.dnn(k_out)
+
+        out = tf.keras.layers.concatenate([fm_out, dnn_out])
+        out = self.prediction_output_layer()(out)
+
+        tf.keras.models.Model(inputs=self.inputs, outputs=out)
+
+        self.compile()
