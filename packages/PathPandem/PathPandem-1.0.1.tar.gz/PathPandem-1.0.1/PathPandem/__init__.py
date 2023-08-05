4844bc82b11e5d8ce87a4253e42df68ac881ba76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:16 +0000

    Add package PathPandem/1.0.1/PathPandem-1.0.1.tar.gz

---
+++ packages/PathPandem/PathPandem-1.0.1.tar.gz/PathPandem-1.0.1/PathPandem/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''__init__.py for python v3.2 or earlier'''
+
+from . import cli.py
+from . import compose_pop.py
+from . import db-gen-irwin_hall.py
+from . import definitions.py
+from . import misc.py
+from . import pathogen.py
+from . import person.py
+from . import plot.py
+from . import population.py
+from . import simul.py
