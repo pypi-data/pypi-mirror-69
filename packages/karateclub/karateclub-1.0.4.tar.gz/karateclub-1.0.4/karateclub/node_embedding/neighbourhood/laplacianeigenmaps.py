d3c33d3bd61256cee73a76812ad080485281b896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package karateclub/1.0.4/karateclub-1.0.4.tar.gz

---
+++ packages/karateclub/karateclub-1.0.4.tar.gz/karateclub-1.0.4/karateclub/node_embedding/neighbourhood/laplacianeigenmaps.py
@@ -0,0 +1,39 @@
+import numpy as np
+import networkx as nx
+import scipy.sparse as sps
+from karateclub.estimator import Estimator
+
+class LaplacianEigenmaps(Estimator):
+    r"""An implementation of `"Laplacian Eigenmaps" <https://papers.nips.cc/paper/1961-laplacian-eigenmaps-and-spectral-techniques-for-embedding-and-clustering>`_
+    from the NIPS '01 paper "Laplacian Eigenmaps and Spectral Techniques for Embedding and Clustering".
+    The procedure extracts the eigenvectors corresponding to the largest values 
+    of the graph Laplacian. These vectors are used as the node embedding.
+
+    Args:
+        dimensions (int): Dimensionality of embedding. Default is 128.
+    """
+    def __init__(self, dimensions=128):
+
+        self.dimensions = dimensions
+
+    def fit(self, graph):
+        """
+        Fitting a Laplacian EigenMaps model.
+
+        Arg types:
+            * **graph** *(NetworkX graph)* - The graph to be embedded.
+        """
+        self._check_graph(graph)
+        number_of_nodes = graph.number_of_nodes()
+        L_tilde = nx.normalized_laplacian_matrix(graph, nodelist=range(number_of_nodes))
+        eigenvalues, embedding = sps.linalg.eigsh(L_tilde, k=self.dimensions, return_eigenvectors=True)
+        self._embedding = embedding
+
+
+    def get_embedding(self):
+        r"""Getting the node embedding.
+
+        Return types:
+            * **embedding** *(Numpy array)* - The embedding of nodes.
+        """
+        return self._embedding
