7917003a939bce0a41dee0ef8caba5db18532c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:35 +0000

    Add package nbplugins-watch/0.0.3/nbplugins-watch-0.0.3.tar.gz

---
+++ packages/nbplugins-watch/nbplugins-watch-0.0.3.tar.gz/nbplugins-watch-0.0.3/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['nbplugins_watch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['logzero>=1.5.0,<2.0.0',
+ 'nonebot>=1.6.0,<2.0.0',
+ 'pytest-cov>=2.8.1,<3.0.0',
+ 'watchgod>=0.6,<0.7']
+
+setup_kwargs = {
+    'name': 'nbplugins-watch',
+    'version': '0.0.3',
+    'description': 'watch and (re)load plugins from a directory',
+    'long_description': None,
+    'author': 'ffreemt',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
