1bd6672290bfc9af75879be6dadbd49db368f6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package enkeksi/0.1.0/enkeksi-0.1.0-py3-none-any.whl

---
+++ packages/enkeksi/enkeksi-0.1.0-py3-none-any.whl/enkeksi/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .mdprocess import get_cursor, process
+
+__version__ = "0.1.0"
+__author__ = "Jukka Aho <ahojukka5@gmail.com>"
+__all__ = [get_cursor, process]
