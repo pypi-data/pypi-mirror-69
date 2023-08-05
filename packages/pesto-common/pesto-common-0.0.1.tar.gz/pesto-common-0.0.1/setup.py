47636d4436d8e669f790509fa6261a2a2934e9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:11 +0000

    Add package pesto-common/0.0.1/pesto-common-0.0.1.tar.gz

---
+++ packages/pesto-common/pesto-common-0.0.1.tar.gz/pesto-common-0.0.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+requirements = []
+
+setup(name='pesto-common',
+      version='0.0.1',
+      author='Dreampie',
+      author_email='Dreampie@outlook.com',
+      url='https://github.com/Dreampie/pesto',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requirements,
+      platforms=['all']
+      )
