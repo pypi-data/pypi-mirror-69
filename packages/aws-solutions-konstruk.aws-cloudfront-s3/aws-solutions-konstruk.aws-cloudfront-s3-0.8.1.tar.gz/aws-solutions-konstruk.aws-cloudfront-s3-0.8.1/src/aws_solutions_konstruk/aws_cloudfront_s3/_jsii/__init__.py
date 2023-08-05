baff1033f8b3faafda8348ef436dd9e1dff68983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:58 +0000

    Add package aws-solutions-konstruk.aws-cloudfront-s3/0.8.1/aws-solutions-konstruk.aws-cloudfront-s3-0.8.1.tar.gz

---
+++ packages/aws-solutions-konstruk.aws-cloudfront-s3/aws-solutions-konstruk.aws-cloudfront-s3-0.8.1.tar.gz/aws-solutions-konstruk.aws-cloudfront-s3-0.8.1/src/aws_solutions_konstruk/aws_cloudfront_s3/_jsii/__init__.py
@@ -0,0 +1,23 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_s3
+import aws_cdk.core
+import aws_solutions_konstruk.core
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("@aws-solutions-konstruk/aws-cloudfront-s3", "0.8.1", __name__[0:-6], "aws-cloudfront-s3@0.8.1.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
