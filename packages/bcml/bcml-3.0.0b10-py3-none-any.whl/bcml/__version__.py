b5d303156a07f0ae22ec3fa26858d6e0462948d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package bcml/3.0.0b10/bcml-3.0.0b10-py3-none-any.whl

---
+++ packages/bcml/bcml-3.0.0b10-py3-none-any.whl/bcml/__version__.py
@@ -0,0 +1,9 @@
+_major = 3
+_minor = 0
+_patch = "0b10"
+VERSION = f"{_major}.{_minor}.{_patch}"
+USER_VERSION = f"""{_major}.{_minor}.{_patch[0:1]} {
+    'alpha' if _major < 1 else ''
+}{
+    f'beta {_patch[-1:]}' if 'b' in _patch else ''
+}"""
