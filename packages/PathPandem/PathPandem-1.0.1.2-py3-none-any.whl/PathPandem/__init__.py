4997d15d9162be0be997ec12db0a292439648f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:18 +0000

    Add package PathPandem/1.0.1.2/PathPandem-1.0.1.2-py3-none-any.whl

---
+++ packages/PathPandem/PathPandem-1.0.1.2-py3-none-any.whl/PathPandem/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''__init__.py for python v3.2 or earlier'''
+
+from . import cli
+from . import compose_pop
+from . import definitions
+from . import misc
+from . import pathogen
+from . import person
+from . import plot
+from . import population
+from . import simul
