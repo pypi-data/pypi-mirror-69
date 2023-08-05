61b84e8375610b58c92576adebd7a2b7e91f55a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:42 +0000

    Add package srt/3.2.1/srt-3.2.1.tar.gz

---
+++ packages/srt/srt-3.2.1.tar.gz/srt-3.2.1/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import codecs
+from distutils.core import setup
+
+with codecs.open("README.rst", encoding="utf8") as readme_f:
+    README = readme_f.read()
+
+setup(
+    name="srt",
+    version="3.2.1",
+    python_requires=">=3.5",
+    description="A tiny library for parsing, modifying, and composing SRT files.",
+    long_description=README,
+    author="Chris Down",
+    author_email="chris@chrisdown.name",
+    url="https://github.com/cdown/srt",
+    py_modules=["srt", "srt_tools.utils"],
+    scripts=[
+        "srt_tools/srt",
+        "srt_tools/srt-normalise",
+        "srt_tools/srt-fixed-timeshift",
+        "srt_tools/srt-linear-timeshift",
+        "srt_tools/srt-lines-matching",
+        "srt_tools/srt-mux",
+        "srt_tools/srt-play",
+        "srt_tools/srt-process",
+    ],
+    license="Public Domain",
+    keywords="srt",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Text Processing",
+    ],
+)
