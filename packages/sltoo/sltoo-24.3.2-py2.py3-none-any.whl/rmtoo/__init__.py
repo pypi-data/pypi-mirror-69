647b4e0a64bc47a76540d3febfa66112097e0f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:44 +0000

    Add package sltoo/24.3.2/sltoo-24.3.2-py2.py3-none-any.whl

---
+++ packages/sltoo/sltoo-24.3.2-py2.py3-none-any.whl/rmtoo/__init__.py
@@ -0,0 +1,17 @@
+'''
+ rmtoo
+   Free and Open Source Requirements Management Tool
+
+  Master __init__.py
+
+ (c) 2011,2017 by flonatel GmbH & Co. KG
+
+ For licensing details see COPYING
+'''
+__all__ = ["inputs", "lib", "tests", "outputs"]
+__version__ = "24.3.2"
+
+import os
+filepath = os.path.dirname(os.path.realpath(__file__))
+os.environ['contribdir'] = os.path.join(filepath, '..',
+                                        'contrib', 'template_project')
