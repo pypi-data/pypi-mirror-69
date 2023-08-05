44e54e304c4b51c15f2e47dec265e60761f60e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:14 +0000

    Add package mpex-probabilities/0.2/mpex_probabilities-0.2.tar.gz

---
+++ packages/mpex-probabilities/mpex_probabilities-0.2.tar.gz/mpex_probabilities-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='mpex_probabilities',
+      version='0.2',
+      description='mpex distributions',
+      packages=['mpex_probabilities'],
+      zip_safe=False)
