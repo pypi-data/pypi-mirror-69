d827b5fc9167af2a40a1f57f0cfffe4d8afcd067
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:24 +0000

    Add package aguantest/1.0.0/aguantest-1.0.0.tar.gz

---
+++ packages/aguantest/aguantest-1.0.0.tar.gz/aguantest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='aguantest',
+    version='1.0.0',
+    description='a automated test tools',
+    author='szmgithubcode',
+    author_email='13023297816@163.com',
+    url='https://github.com/szmgithubcode/',
+    packages=['aguantest'],
+)
