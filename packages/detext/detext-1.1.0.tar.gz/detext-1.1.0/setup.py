d8312b4221651b0da4e95e7c2ead283a03d82081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:08 +0000

    Add package detext/1.1.0/detext-1.1.0.tar.gz

---
+++ packages/detext/detext-1.1.0.tar.gz/detext-1.1.0/setup.py
@@ -0,0 +1,18 @@
+# Copyright (c) LinkedIn Corporation. All rights reserved. Licensed under the BSD-2 Clause license.
+# See LICENSE in the project root for license information.
+import setuptools
+setuptools.setup(
+    name='detext',
+    version='1.1.0',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+    include_package_data=True,
+    install_requires=[
+        'numpy<1.17',
+#        'tensorflow==1.14.0',
+#        'tensorflow_ranking==0.1.4',
+#        'gast==0.2.2'
+    ],
+    tests_require=[
+        'pytest',
+    ])
