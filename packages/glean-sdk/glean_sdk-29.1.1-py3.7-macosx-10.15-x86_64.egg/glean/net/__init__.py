c37fb644fbfaaf085cced82235cb5eaa32876185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:56 +0000

    Add package glean-sdk/29.1.1/glean_sdk-29.1.1-py3.7-macosx-10.15-x86_64.egg

---
+++ packages/glean-sdk/glean_sdk-29.1.1-py3.7-macosx-10.15-x86_64.egg/glean/net/__init__.py
@@ -0,0 +1,24 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+"""
+Network functionality for Glean.
+"""
+
+
+from .base_uploader import BaseUploader
+from .http_client import HttpClientUploader
+from .ping_uploader import PingUploader
+from .ping_upload_worker import PingUploadWorker
+from .deletion_ping_upload_worker import DeletionPingUploadWorker
+
+
+__all__ = [
+    "BaseUploader",
+    "HttpClientUploader",
+    "PingUploader",
+    "PingUploadWorker",
+    "DeletionPingUploadWorker",
+]
