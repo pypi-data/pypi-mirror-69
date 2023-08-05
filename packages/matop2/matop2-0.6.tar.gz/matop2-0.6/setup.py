d7927040462680b7ee752c091a48077a388d48e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:03 +0000

    Add package matop2/0.6/matop2-0.6.tar.gz

---
+++ packages/matop2/matop2-0.6.tar.gz/matop2-0.6/setup.py
@@ -0,0 +1,15 @@
+# TODO: Fill out this file with information about your package
+from setuptools import setup
+
+setup(name='matop2',
+      version='0.6',
+      description='Matrix Operations',
+      packages=['matop2'],
+      author = 'Rajesh Kumar Bandaru',
+      author_email = 'rkumar70900@gmail.com',
+      zip_safe=False)
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
