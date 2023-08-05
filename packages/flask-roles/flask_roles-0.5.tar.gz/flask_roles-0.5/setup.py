673102317928e232a5bc3983e83649437132c878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:25 +0000

    Add package flask-roles/0.5/flask_roles-0.5.tar.gz

---
+++ packages/flask-roles/flask_roles-0.5.tar.gz/flask_roles-0.5/setup.py
@@ -0,0 +1,46 @@
+"""
+Flask-Roles
+-------------
+
+A small plugin to flask to add roles support.
+
+"""
+from setuptools import setup
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+
+setup(
+    name="flask_roles",
+    version="0.5",
+    url="https://github.com/labeneator/flask-roles",
+    license="GPLv3",
+    author="Laban Mwangi",
+    author_email="support@east36.co.ke",
+    description="Flask plugin to add roles to a project",
+    long_description=readme,
+    packages=["flask_roles"],
+    zip_safe=False,
+    include_package_data=True,
+    extras_require={
+        "testing": [
+            "pip-tools == 4.5.1",
+            "mccabe",
+            "pytest-cov",
+            "pytest",
+            "Flask-Login>=0.5.0",
+            "Flask-Principal>=0.4.0",
+        ]
+    },
+    platforms="any",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
