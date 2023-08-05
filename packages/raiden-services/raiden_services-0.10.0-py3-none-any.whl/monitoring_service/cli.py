d88a2fffa48709ca25309077f66c70f8ca96ca89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:10 +0000

    Add package raiden-services/0.10.0/raiden_services-0.10.0-py3-none-any.whl

---
+++ packages/raiden-services/raiden_services-0.10.0-py3-none-any.whl/monitoring_service/cli.py
@@ -0,0 +1,107 @@
+from typing import Dict
+
+import click
+import structlog
+from eth_utils import to_checksum_address
+from web3 import Web3
+from web3.contract import Contract
+
+from monitoring_service.constants import DEFAULT_MIN_REWARD, MS_DISCLAIMER
+from monitoring_service.service import MonitoringService
+from raiden.settings import DEFAULT_NUMBER_OF_BLOCK_CONFIRMATIONS
+from raiden.utils.typing import BlockNumber, BlockTimeout
+from raiden_contracts.constants import (
+    CONTRACT_MONITORING_SERVICE,
+    CONTRACT_SERVICE_REGISTRY,
+    CONTRACT_TOKEN_NETWORK_REGISTRY,
+    CONTRACT_USER_DEPOSIT,
+)
+from raiden_libs.blockchain import get_web3_provider_info
+from raiden_libs.cli import blockchain_options, common_options, setup_sentry
+from raiden_libs.constants import CONFIRMATION_OF_UNDERSTANDING, DEFAULT_POLL_INTERVALL
+
+log = structlog.get_logger(__name__)
+
+
+@blockchain_options(
+    contracts=[
+        CONTRACT_TOKEN_NETWORK_REGISTRY,
+        CONTRACT_USER_DEPOSIT,
+        CONTRACT_MONITORING_SERVICE,
+        CONTRACT_SERVICE_REGISTRY,
+    ]
+)
+@click.command()
+@click.option(
+    "--min-reward",
+    default=DEFAULT_MIN_REWARD,
+    type=click.IntRange(min=0),
+    help="Minimum reward which is required before processing requests",
+)
+@click.option(
+    "--confirmations",
+    default=DEFAULT_NUMBER_OF_BLOCK_CONFIRMATIONS,
+    type=click.IntRange(min=0),
+    help="Number of block confirmations to wait for",
+)
+@click.option(
+    "--debug-shell",
+    default=False,
+    type=bool,
+    help="Open a python shell with an initialized MonitoringService instance",
+)
+@click.option(
+    "--accept-disclaimer",
+    type=bool,
+    default=False,
+    help="Bypass the experimental software disclaimer prompt",
+    is_flag=True,
+)
+@common_options("raiden-monitoring-service")
+def main(  # pylint: disable=too-many-arguments
+    private_key: str,
+    state_db: str,
+    web3: Web3,
+    contracts: Dict[str, Contract],
+    start_block: BlockNumber,
+    confirmations: BlockTimeout,
+    min_reward: int,
+    debug_shell: bool,
+    accept_disclaimer: bool,
+) -> int:
+    """ The Monitoring service for the Raiden Network. """
+    log.info("Starting Raiden Monitoring Service")
+    click.secho(MS_DISCLAIMER, fg="yellow")
+    if not accept_disclaimer:
+        click.confirm(CONFIRMATION_OF_UNDERSTANDING, abort=True)
+    log.info("Using RPC endpoint", rpc_url=get_web3_provider_info(web3))
+    hex_addresses = {
+        name: to_checksum_address(contract.address) for name, contract in contracts.items()
+    }
+    log.info("Contract information", addresses=hex_addresses, start_block=start_block)
+
+    ms = MonitoringService(
+        web3=web3,
+        private_key=private_key,
+        contracts=contracts,
+        sync_start_block=start_block,
+        required_confirmations=confirmations,
+        poll_interval=DEFAULT_POLL_INTERVALL,
+        db_filename=state_db,
+        min_reward=min_reward,
+    )
+
+    if debug_shell:
+        import IPython
+
+        IPython.embed()
+        return 0
+
+    ms.start()
+
+    return 0
+
+
+if __name__ == "__main__":
+    setup_sentry()
+    main(auto_envvar_prefix="MS")
---
+++ packages/raiden-services/raiden_services-0.10.0-py3-none-any.whl/raiden_libs/utils.py
@@ -0,0 +1,22 @@
+from typing import Union
+
+from coincurve import PrivateKey, PublicKey
+from eth_utils import decode_hex, keccak
+
+from raiden.utils.typing import Address
+
+
+def public_key_to_address(public_key: PublicKey) -> Address:
+    """ Converts a public key to an Ethereum address. """
+    key_bytes = public_key.format(compressed=False)
+    return Address(keccak(key_bytes[1:])[-20:])
+
+
+def private_key_to_address(private_key: Union[str, bytes]) -> Address:
+    """ Converts a private key to an Ethereum address. """
+    if isinstance(private_key, str):
+        private_key = decode_hex(private_key)
+
+    assert isinstance(private_key, bytes)
+    privkey = PrivateKey(private_key)
+    return public_key_to_address(privkey.public_key)
