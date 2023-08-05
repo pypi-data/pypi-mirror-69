bb20149b95a62fb97f8b796f602c39240464a71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:57 +0000

    Add package ktimeout/0.0.4/ktimeout-0.0.4.tar.gz

---
+++ packages/ktimeout/ktimeout-0.0.4.tar.gz/ktimeout-0.0.4/ktimeout/timeout.py
@@ -0,0 +1,21 @@
+from typing import Callable, Optional
+from types import FrameType
+
+def __default_timeout_handler(signal_number: int, frame: Optional[FrameType]):
+    raise Exception("Operation has timed out")
+
+# If you decide to use a custom handler, keep in mind, that the handler will be called with 2 arguments:
+#   - int:                  the signal number
+#   - Optional[FrameType]:  the current stack frame (None or a frame object)
+def run(func: Callable, timeout: int, timeout_handler: Callable = __default_timeout_handler):
+    import signal
+
+    signal.signal(signal.SIGALRM, timeout_handler)
+    signal.alarm(timeout)
+
+    return func()
+
+def partial(func: Callable, *args, **kwargs):
+    from functools import partial as _partial
+
+    return _partial(func, *args, **kwargs)
