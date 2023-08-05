1301675b5b7bf9d3396bbad233249cdcaa9d6d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:09 +0000

    Add package pangeamt-nlp/0.4.15/pangeamt-nlp-0.4.15.tar.gz

---
+++ packages/pangeamt-nlp/pangeamt-nlp-0.4.15.tar.gz/pangeamt-nlp-0.4.15/pangeamt_nlp/processor/validator/repeated_chars_val.py
@@ -0,0 +1,34 @@
+from pangeamt_nlp.processor.base.validator_base import ValidatorBase
+from pangeamt_nlp.seg import Seg
+
+
+class RepeatedCharsVal(ValidatorBase):
+    NAME = "repeated_chars_val"
+
+    DESCRIPTION_TRAINING = """
+            Remove pairs with chars repeated more than half the large the sentence 
+        """
+
+    DESCRIPTION_DECODING = """
+            Validators do not apply to decoding.
+        """
+
+    def __init__(self, src_lang: str, tgt_lang: str) -> None:
+        super().__init__(src_lang, tgt_lang)
+
+    def validate(self, seg: Seg) -> bool:
+        for char in seg.src:
+            if not char.isspace() and char:
+                count = seg.src.count(char)
+                if count > len(seg.src) * 0.5:
+                    # print(">>>>><SRC", count, "---",char, src)
+                    # time.sleep(4)
+                    return False
+        for char in seg.tgt:
+            if not char.isspace() and char:
+                count = seg.tgt.count(char)
+                if count > len(seg.tgt) * 0.5:
+                    # print(">>>>><TGT", count,"---", char, tgt)
+                    # time.sleep(4)
+                    return False
+        return True
