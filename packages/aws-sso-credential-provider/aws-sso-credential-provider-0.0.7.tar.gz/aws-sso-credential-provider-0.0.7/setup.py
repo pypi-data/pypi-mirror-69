b3659447bdc6b4195ee582f3b1a26dd031fba8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:07 +0000

    Add package aws-sso-credential-provider/0.0.7/aws-sso-credential-provider-0.0.7.tar.gz

---
+++ packages/aws-sso-credential-provider/aws-sso-credential-provider-0.0.7.tar.gz/aws-sso-credential-provider-0.0.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="aws-sso-credential-provider",
+    version="0.0.7",
+    author="Kacey Cerdena",
+    author_email="6180729+kcerdena@users.noreply.github.com",
+    description="Session credential provider for AWS SSO roles",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kcerdena/aws_sso",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX",
+        "Topic :: System :: Systems Administration"
+    ],
+    install_requires=[
+        'python-dateutil>=2.8',
+        'boto3>=1.13'
+    ],
+    python_requires='>=3.6',
+)
