ccf7346df713ed3d46e1a02884cf735b3bc87d00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:07 +0000

    Add package athena-run/0.1.20.dev0/athena_run-0.1.20.dev0-py3-none-any.whl

---
+++ packages/athena-run/athena_run-0.1.20.dev0-py3-none-any.whl/patch/main.py
@@ -0,0 +1,15 @@
+import logging; log = logging.getLogger(__name__)
+
+
+def patch_all():
+    try:
+        import patch.pandas as patch_pandas
+        patch_pandas.patch()
+    except ModuleNotFoundError:
+        log.info("Couldn't patch module %s", module.__str__())
+
+    try:
+        import patch.sklearn as patch_sklearn
+        patch_sklearn.patch()
+    except ModuleNotFoundError:
+        log.info("Couldn't patch module %s", module.__str__())
