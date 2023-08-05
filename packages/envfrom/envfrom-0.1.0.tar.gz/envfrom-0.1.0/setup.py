34f945731ffc7e4e07cb1adec8dff1f7d0af57a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:33 +0000

    Add package envfrom/0.1.0/envfrom-0.1.0.tar.gz

---
+++ packages/envfrom/envfrom-0.1.0.tar.gz/envfrom-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="envfrom",
+    description="Call child process with custom environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1.0",
+    author="rm_ass",
+    author_email="contact@romanmasse.com",
+    packages=find_packages(),
+    url='http://github.com/rambobinator/envfrom',
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python"
+    ],
+    entry_points={
+        "console_scripts": [
+            "envfrom = envfrom.envfrom:run"
+        ]
+    },
+    install_requires=[
+        "kubernetes==10.0.1",
+        "requests==2.23.0",
+        "nested-lookup==0.2.21"
+    ],
+    license="MIT"
+)
