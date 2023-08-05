0ebbd173e6576d956647121d89a7bc5cbdff6570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:47 +0000

    Add package distributions-pyprojects/0.1/distributions_pyprojects-0.1.tar.gz

---
+++ packages/distributions-pyprojects/distributions_pyprojects-0.1.tar.gz/distributions_pyprojects-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_pyprojects',
+      version='0.1',
+      description='Gaussian and Binomial distributions_pyprojects',
+      packages=['distributions_pyprojects'],
+      author='Lily Ng',
+      author_email='lilyng15@outlook.com',
+      zip_safe=False)
