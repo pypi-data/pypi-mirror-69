d8afb209cc6a9762c6c18692b757fba96a451b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package chistesESP/0.0.1/chistesESP-0.0.1.tar.gz

---
+++ packages/chistesESP/chistesESP-0.0.1.tar.gz/chistesESP-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chistesESP", # Replace with your own username
+    version="0.0.1",
+    author="netherpills",
+    author_email="mari394334@gmail.com",
+    description="Un paquete que contiene un comando que obtiene chistes de chistes.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/netherpills/chistesESP-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
