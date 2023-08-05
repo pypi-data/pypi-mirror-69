38f8ea41480e8b2ff2da223477e417dbfb050c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:52 +0000

    Add package chistesESP/0.0.1/chistesESP-0.0.1-py3-none-any.whl

---
+++ packages/chistesESP/chistesESP-0.0.1-py3-none-any.whl/chistesESP/__init__.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+def get_random_chiste():
+    response = requests.get('http://www.chistes.com/ChisteAlAzar.asp?n=3')
+    soup = BeautifulSoup(response.text, "html.parser")
+    chiste = soup.find("div", "chiste").text
