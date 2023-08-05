1db47ac78a6840704af7f2d1b4136e7561a2f512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:25 +0000

    Add package pywalfox/2.0/pywalfox-2.0.tar.gz

---
+++ packages/pywalfox/pywalfox-2.0.tar.gz/pywalfox-2.0/pywalfox/channel/unix/server.py
@@ -0,0 +1,38 @@
+import os
+import socket
+import logging
+
+from ..connector import Connector
+
+class Server(Connector):
+    """UNIX-socket server used to communicate with clients."""
+    def __init__(self):
+        Connector.__init__(self, 'unix')
+
+    def delete_existing_socket(self):
+        """Deletes the existing UNIX-socket if it exists."""
+        if os.path.exists(self.host):
+            os.remove(self.host)
+
+    def start(self):
+        """
+        Binds the socket to file.
+
+        :return: if the socket could be bound to the file
+        :rType: bool
+        """
+        self.delete_existing_socket()
+        try:
+            self.socket.bind(self.host)
+            logging.debug('Successfully bound socket to: %s' % self.host)
+            return True
+        except OSError as e:
+            logging.error('Failed to create UNIX socket: %s' % e.strerror)
+        
+        return False
+
+    def close(self):
+        """Unbinds the socket and deletes the file."""
+        self.socket.close()
+        os.remove(self.host)
+
---
+++ packages/pywalfox/pywalfox-2.0.tar.gz/pywalfox-2.0/pywalfox/custom_css.py
@@ -0,0 +1,100 @@
+import os
+import re
+import glob
+import shutil
+import logging
+import fileinput
+
+from .config import CSS_PATH
+
+def get_firefox_chrome_path():
+    """
+    Retrieves the path to the 'chrome' folder in the default Firefox profile
+
+    :return: the absolute path to the chrome folder
+    :rType: str
+    """
+    profile_path = glob.glob('%s/*.default-release' % os.path.expanduser('~/.mozilla/firefox'))
+
+    if len(profile_path) < 1:
+        logging.error('Could not find default Firefox profile')
+        return False
+
+    chrome_path = os.path.join(profile_path[0], 'chrome')
+    if not os.path.exists(chrome_path):
+        logging.debug('Creating non-existant chrome directory')
+        os.makedirs(chrome_path)
+
+    logging.debug('Found chrome directory at path: %s' % chrome_path)
+    return chrome_path
+
+def enable_custom_css(chrome_path, name):
+    """
+    Applies a CSS file but putting it in the 'chrome' directory.
+
+    :param chrome_path str: the path to the chrome directory
+    :param name str: the name of the css file to apply
+    :return: (success, message)
+    :rType: tuple
+    """
+    filename = add_css_file_extension(name)
+    logging.debug('Enabling custom CSS file: %s' % filename)
+    try:
+        shutil.copy(os.path.join(CSS_PATH, filename), os.path.join(chrome_path, filename))
+        logging.debug('%s was enabled' % filename)
+        return (True, 'Custom CSS: %s has been enabled' % filename)
+    except Exception as e:
+        logging.error('%s could not be enabled: %s' % (filename, str(e)))
+        return (False, 'Could not copy custom CSS to folder: %s' % str(e))
+
+def disable_custom_css(chrome_path, name):
+    """
+    Disabled a CSS file but removing it from the 'chrome' directory.
+
+    :param chrome_path str: the path to the chrome directory
+    :param name str: the name of the css file to disable
+    :return: (success, message)
+    :rType: tuple
+    """
+    filename = add_css_file_extension(name)
+    logging.debug('Disabling custom CSS file: %s' % filename)
+    try:
+        os.remove(os.path.join(chrome_path, filename))
+        logging.debug('%s was disabled' % filename)
+        return (True, 'Custom CSS: %s has been disabled' % filename)
+    except Exception as e:
+        logging.error('%s could not be disabled: %s' % (filename, str(e)))
+        return (False, 'Could not remove custom CSS: %s' % str(e))
+
+def set_font_size(chrome_path, name, size):
+    """
+    Sets the default font size in a CSS file.
+
+    :param chrome_path str: the path to the chrome directory
+    :param name str: the name of the css file to disable
+    :param size int: the new font size
+    :return: (success, message)
+    :rType: tuple
+    """
+    filename = add_css_file_extension(name)
+    logging.debug('Setting font size to %s in custom CSS file: %s' % (size, filename))
+    try:
+        for line in fileinput.input(os.path.join(chrome_path, filename), inplace=True):
+            if '--pywalfox-font-size:' in line:
+                print('  --pywalfox-font-size: %spx;' % size)
+            else:
+                print(line, end='')
+        return (True, 'Font size was set to: %s' % size)
+    except Exception as e:
+        error_msg = 'Could not set font size: %s' % str(e)
+        logging.error(error_msg)
+        return (False, error_msg)
+
+def add_css_file_extension(name):
+    """
+    Appends the CSS file extension to a string.
+
+    :return: name with '.css' append at the end append at the end
+    :rType: string
+    """
+    return '%s.css' % name
