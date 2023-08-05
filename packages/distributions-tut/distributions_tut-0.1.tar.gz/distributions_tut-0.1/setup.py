00cff99a0ec3fe56e3beaf4d1bfc26d8ee02d4e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:35 +0000

    Add package distributions-tut/0.1/distributions_tut-0.1.tar.gz

---
+++ packages/distributions-tut/distributions_tut-0.1.tar.gz/distributions_tut-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_tut',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_tut'],
+      author='Andrii Iudin',
+      zip_safe=False)
