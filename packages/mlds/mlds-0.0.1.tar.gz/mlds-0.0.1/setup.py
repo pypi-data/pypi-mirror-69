76fd719e7b797edc203e3169d76af4753e8489f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:37 +0000

    Add package mlds/0.0.1/mlds-0.0.1.tar.gz

---
+++ packages/mlds/mlds-0.0.1.tar.gz/mlds-0.0.1/setup.py
@@ -0,0 +1,20 @@
+# Lint as: python3
+import setuptools
+
+setuptools.setup(
+    name='mlds',
+    version='0.0.1',
+    author='TBD',
+    author_email='tbd@example.com',
+    description='TBD',
+    license='Apache 2.0',
+    long_description='# mlds',
+    long_description_content_type='text/markdown',
+    url='http://example.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    python_requires='>=3.6',
+)
