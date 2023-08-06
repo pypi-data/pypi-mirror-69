fd92f2a8b28ba8b73cc11817d18b5e2d4666e132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:45 +0000

    Add package volumio-websocket/1.0.10/volumio_websocket-1.0.10-py3-none-any.whl

---
+++ packages/volumio-websocket/volumio_websocket-1.0.10-py3-none-any.whl/volumio_websocket/api2websocket.py
@@ -0,0 +1,47 @@
+"""Transforms any http api request to a websocket call."""
+
+from .websocket import Websocket
+from functools import wraps
+from asyncio import sleep
+
+
+def api2websocket(method, params, **kwargs):
+    """Transform method and params from api to websocket calls."""
+    if method == "commands":
+        method = params["cmd"]
+
+        if method in params:
+            params = params[method]
+        elif "value" in params:
+            params = params["value"]
+        elif "name" in params:
+            params = params["name"]
+        else:
+            params = params["cmd"]
+
+    if method == params:
+        params = None
+
+    return method, params, kwargs
+
+
+def patch_api(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        return f(api2websocket(args, kwargs))
+
+    return wrapper
+
+
+patch_api(Websocket.emit)
+
+
+async def request(host, port, method, params=None, path=None):
+    """Handles api methods to make sync websocket requests."""
+
+    socket = Websocket(host, port, path=path)
+    await socket.connect()
+    data = await socket.call(method, params)
+    await socket.disconnect()
+
+    return data
---
+++ packages/volumio-websocket/volumio_websocket-1.0.10-py3-none-any.whl/volumio_websocket/websocket.py
@@ -0,0 +1,121 @@
+
+import socketio
+
+# taken from https://github.com/volumio/Volumio2/blob/master/app/plugins/user_interface/websocket/index.js
+_methods = {
+    "getDeviceInfo": "pushDeviceInfo",
+    "getState": "pushState",
+    "getQueue": "pushQueue",
+    "getMultiRoomDevices": "pushMultiRoomDevices",
+    "getLibraryListing": "pushLibraryListing",
+    "getMenuItems": "pushMenuItems",
+    "getUiConfig": "pushUiConfig",
+    "getBrowseSources": "pushBrowseSources",
+    "browseLibrary": "pushBrowseLibrary",
+    "search": "pushBrowseLibrary",
+    "goTo": "pushBrowseLibrary",
+    "GetTrackInfo": "pushGetTrackInfo",
+    "addWebRadio": "pushAddWebRadio",
+    "removeWebRadio": "pushBrowseLibrary",
+    "getPlaylistContent": "pushPlaylistContent",
+    "createPlaylist": "pushCreatePlaylist",
+    "deletePlaylist": "pushListPlaylist",
+    "listPlaylist": "pushListPlaylist",
+    "addToPlaylist": "pushListPlaylist",
+    "removeFromPlaylist": "pushBrowseLibrary",
+    "playPlaylist": "pushPlayPlaylist",
+    "enqueue": "pushEnqueue",
+    "addToFavourites": "urifavourites",
+    "removeFromFavourites": "pushBrowseLibrary",
+    "playFavourites": "pushPlayFavourites",
+    "addToRadioFavourites": "pushAddToRadioFavourites",
+    "removeFromRadioFavourites": "pushRemoveFromRadioFavourites",
+    "playRadioFavourites": "pushPlayRadioFavourites",
+    "getSleep": "pushSleep"
+    # TODO: There are lots more, continue at L673
+}
+
+
+class Websocket:
+    def __init__(self, host, port, path=None):
+        """Host, port and path must be all str."""
+        self.host = host
+        self.port = port
+        self.path = path
+        self.is_connected = False
+
+    async def connect(self):
+        url = f"http://{self.host}:{self.port}"
+
+        if self.path is not None:
+            if not str(self.path).startswith("/"):
+                url += "/"
+            url += str(self.path)
+
+        self._sio = socketio.AsyncClient()
+        await self._sio.connect(url)
+        self.is_connected = True
+
+    async def emit(self, method, params=None):
+        """
+        Emit a method with params.
+
+        Returns a  *name* in this object,
+        where you can find your results.
+        Use obj.get(*name*) to get the results.
+        """
+        if method in _methods:
+            state_name = self.get_answer_name(method)
+        else:
+            state_name = None
+
+        await self._sio.emit(method, params)
+
+        return state_name
+
+    def get(self, name):
+        return getattr(self, name, None)
+
+    @classmethod
+    def get_answer_name(cls, method):
+        return _methods[method]
+
+    async def command(self, command):
+        """Emit a call without any params."""
+        await self.emit(command)
+
+    async def call(self, method, params=None, wait=2):
+        """
+        Emit a method with params and returns the result.
+        *wait* specifies, how long you want to wait for a result.
+        This method makes the websocket a lot more like
+        a request to an api endpoint.
+
+        Returns None, if no result was present in the meanwhile.
+        """
+
+        name = self.get_answer_name(method)
+
+        @self._sio.on(name)
+        def func(data):
+            nonlocal name, self
+            setattr(self, name, data)
+
+        await self.emit(method, params)
+        if name is None:
+            return None
+
+        counter = 0
+        sleeptimer = 0.1
+
+        data = self.get(name)
+        while counter < wait and data is None:
+            self._sio.sleep(sleeptimer)
+            counter += sleeptimer
+            data = self.get(name)
+
+        return data
+
+    async def disconnect(self):
+        await self._sio.disconnect()
+        self.is_connected = False
