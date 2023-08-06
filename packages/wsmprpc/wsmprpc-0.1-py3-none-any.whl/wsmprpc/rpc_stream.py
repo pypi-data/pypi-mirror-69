6102629ddd1e3fc1de4c8ff7b4c5ce87dac6e1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:53 +0000

    Add package wsmprpc/0.1/wsmprpc-0.1-py3-none-any.whl

---
+++ packages/wsmprpc/wsmprpc-0.1-py3-none-any.whl/wsmprpc/rpc_stream.py
@@ -0,0 +1,20 @@
+import asyncio
+
+class RPCStream(asyncio.Queue):
+    def __init__(self, maxsize=0):
+        asyncio.Queue.__init__(self, maxsize)
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        d = await self.get()
+        if isinstance(d, Exception):
+            raise d
+        else:
+            return d
+
+    def force_put_nowait(self, o):
+        # if queue is full, then pop out an element to make room
+        self.full() and self.get_nowait()
+        self.put_nowait(o)
