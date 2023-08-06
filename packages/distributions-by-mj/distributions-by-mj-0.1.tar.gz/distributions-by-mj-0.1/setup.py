f8cd34d5e0ec35c53217bbe57cabf92fd1e3666f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:18 +0000

    Add package distributions-by-mj/0.1/distributions-by-mj-0.1.tar.gz

---
+++ packages/distributions-by-mj/distributions-by-mj-0.1.tar.gz/distributions-by-mj-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-by-mj',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['distributions-by-mj'],
+      zip_safe=False)
