d44546fc25fda87e2657c63b97368ba7abc890e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:02 +0000

    Add package checkov/1.0.319/checkov-1.0.319.tar.gz

---
+++ packages/checkov/checkov-1.0.319.tar.gz/checkov-1.0.319/checkov/terraform/checks/resource/aws/S3MFADelete.py
@@ -0,0 +1,28 @@
+from checkov.common.models.enums import CheckCategories, CheckResult
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+
+
+class S3MFADelete(BaseResourceCheck):
+
+
+    def __init__(self):
+        name = "Ensure S3 bucket has MFA delete enabled"
+        id = "CKV_AWS_52"
+        supported_resources = ['aws_s3_bucket']
+        categories = [CheckCategories.BACKUP_AND_RECOVERY]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+
+        if 'versioning' in conf.keys():
+            versioning_block = conf['versioning']
+            for block in versioning_block:
+                if type(block) is not dict:
+                    return CheckResult.PASSED
+                if 'mfa_delete' in block.keys():
+                    if block['mfa_delete']:
+
+                        return CheckResult.PASSED
+        return CheckResult.FAILED
+
+scanner = S3MFADelete()
