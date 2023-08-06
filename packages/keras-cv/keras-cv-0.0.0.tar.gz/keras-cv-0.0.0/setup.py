60976c9bfeafb83c6251752329640574f9044f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:13 +0000

    Add package keras-cv/0.0.0/keras-cv-0.0.0.tar.gz

---
+++ packages/keras-cv/keras-cv-0.0.0.tar.gz/keras-cv-0.0.0/setup.py
@@ -0,0 +1,43 @@
+# Copyright 2019 The Keras Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Setup script."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+version = '0.0.0'
+
+setup(
+    name='keras-cv',
+    version=version,
+    description='Industry-strength Computer Vision workflows with Keras',
+    url='https://github.com/keras-team/keras-cv',
+    author='The KerasCV authors',
+    author_email='fchollet@google.com',
+    license='Apache License 2.0',
+    install_requires=[
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development'
+    ],
+    packages=find_packages(exclude=('tests',))
+)
