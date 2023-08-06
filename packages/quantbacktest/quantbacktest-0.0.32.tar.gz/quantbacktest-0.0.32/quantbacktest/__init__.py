9c4d84e751629176aa7c3eea46ecb5d6cca08fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:46 +0000

    Add package quantbacktest/0.0.32/quantbacktest-0.0.32.tar.gz

---
+++ packages/quantbacktest/quantbacktest-0.0.32.tar.gz/quantbacktest-0.0.32/quantbacktest/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.0.32"
+
+# Importing modules from this repository
+
+from _0_wrappers import backtest_visualizer
+
+
+if __name__ == '__main__':
+    raise ImportError(
+        "This module is not supposed to be called directly."
+        "Please import the functions that you need and execute these imported"
+        "functions."
+    )
