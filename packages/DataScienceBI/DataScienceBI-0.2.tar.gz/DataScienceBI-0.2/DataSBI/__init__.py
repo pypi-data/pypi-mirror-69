34e6bb1c7545e86e8a2c378a35ec9744f8e5cf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:42 +0000

    Add package DataScienceBI/0.2/DataScienceBI-0.2.tar.gz

---
+++ packages/DataScienceBI/DataScienceBI-0.2.tar.gz/DataScienceBI-0.2/DataSBI/__init__.py
@@ -0,0 +1,4 @@
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
+from .Clothing import Pants, Shirt, Blouse
+from .SalesPerson import SalesPersons
---
+++ packages/DataScienceBI/DataScienceBI-0.2.tar.gz/DataScienceBI-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='DataScienceBI',
+      version='0.2',
+      description='Data Science for Bussiness Intelligence',
+      packages=['DataSBI'],
+      author='T.T. Pham',
+      author_email='zeniththuy@gmail.com',
+      zip_safe=False)
