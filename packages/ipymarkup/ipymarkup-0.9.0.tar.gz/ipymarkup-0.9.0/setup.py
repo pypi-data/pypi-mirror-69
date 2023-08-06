1b7dd6b1922c8cedff68beba715182b03ffb8dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:28 +0000

    Add package ipymarkup/0.9.0/ipymarkup-0.9.0.tar.gz

---
+++ packages/ipymarkup/ipymarkup-0.9.0.tar.gz/ipymarkup-0.9.0/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as file:
+    description = file.read()
+
+
+with open('requirements/main.txt') as file:
+    requirements = [_.strip() for _ in file]
+
+
+setup(
+    name='ipymarkup',
+    version='0.9.0',
+    description='NER, syntax tree markup visualisations for Jupyter Notebook',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/natasha/ipymarkup',
+    author='Alexander Kukushkin',
+    author_email='alex@alexkuk.ru',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Visualization',
+    ],
+    keywords='ner, syntax tree, markup, jupyter, ipython',
+    packages=find_packages(),
+    install_requires=requirements
+)
