98d3eb68a93bb88a810eb4b21c335128ee919ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:52 +0000

    Add package gaussian-compute/0.1/gaussian-compute-0.1.tar.gz

---
+++ packages/gaussian-compute/gaussian-compute-0.1.tar.gz/gaussian-compute-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussian-compute',
+      version='0.1',
+      description='Python package for Gaussian distributions',
+      packages=['gaussian-compute'],
+      zip_safe=False)
