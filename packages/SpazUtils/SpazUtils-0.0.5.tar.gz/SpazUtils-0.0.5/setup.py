a6fb8256b2ec8ea41833bd5405a9aa6e4845a09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:25 +0000

    Add package SpazUtils/0.0.5/SpazUtils-0.0.5.tar.gz

---
+++ packages/SpazUtils/SpazUtils-0.0.5.tar.gz/SpazUtils-0.0.5/setup.py
@@ -0,0 +1,43 @@
+'''SpazUtils setup.py'''
+
+import re
+from os import path
+from setuptools import setup
+
+packageName = 'SpazUtils'
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.rst'), encoding='utf-8') as fp:
+    README = fp.read()
+# with open(path.join(path.abspath(path.dirname(__file__)), packageName, 'info.py'), encoding='utf-8') as fp:
+#     VERSION = re.search("__version__ = '([^']+)'", fp.read()).group(1)
+
+VERSION = '0.0.5'
+
+setup(
+    author='Lil_SpazJoekp',
+    author_email='lilspazjoekp@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    description='Utilities for Automating Moderation on Reddit',
+    install_requires=[
+        'discord.py>=1.0.1',
+        'praw>=6.1.1',
+        'prawcore>=1.0.1',
+        'psycopg2_binary>=2.7.7',
+        'requests>=2.21.0',
+        'timeago>=1.0.9',
+        'typing>=3.6.6',
+        'termcolor>=1.1.0'
+    ],
+    keywords='spaz utils utilities',
+    license='gpl-3.0',
+    long_description=README,
+    name=packageName,
+    url = 'https://gitlab.jesassn.org/spazbots/SpazUtils',
+    version=VERSION,
+)
