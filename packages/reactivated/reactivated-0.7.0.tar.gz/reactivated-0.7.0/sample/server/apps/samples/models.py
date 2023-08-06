6b440dcfb1d9913bf6ae08f1fc8c9e98eaabe613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:46 +0000

    Add package reactivated/0.7.0/reactivated-0.7.0.tar.gz

---
+++ packages/reactivated/reactivated-0.7.0.tar.gz/reactivated-0.7.0/sample/server/apps/samples/models.py
@@ -0,0 +1,83 @@
+from typing import Optional, cast
+
+from django.db import models
+
+models.QuerySet.__class_getitem__ = classmethod(  # type: ignore[assignment]
+    lambda cls, key: cls
+)
+models.Manager.__class_getitem__ = classmethod(  # type: ignore[assignment]
+    lambda cls, key: cls
+)
+
+
+class Continent(models.Model):
+    name = models.CharField(max_length=100)
+    hemisphere = models.CharField(max_length=20)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    continent = models.ForeignKey(
+        Continent, on_delete=models.CASCADE, related_name="countries"
+    )
+
+
+class ComposerCountry(models.Model):
+    country = models.ForeignKey(
+        Country, on_delete=models.CASCADE, related_name="composer_countries"
+    )
+    composer = models.ForeignKey(
+        "Composer", on_delete=models.CASCADE, related_name="composer_countries"
+    )
+    was_born = models.BooleanField(default=False)
+
+
+class ComposerQuerySet(models.QuerySet["Composer"]):
+    def autocomplete(self, query: str) -> models.QuerySet["Composer"]:
+        return self.filter(name__icontains=query)
+
+
+class Composer(models.Model):
+    name = models.CharField(max_length=100)
+    objects: ComposerQuerySet = cast(  # type: ignore[assignment]
+        ComposerQuerySet, ComposerQuerySet.as_manager()
+    )
+
+    countries = models.ManyToManyField(
+        Country, through=ComposerCountry, related_name="composers"
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class OperaManager(models.Manager["Opera"]):
+    pass
+
+
+class OperaQuerySet(models.QuerySet["Opera"]):
+    def autocomplete(self, query: str) -> models.QuerySet["Opera"]:
+        return self.filter(name__icontains=query)
+
+
+class Opera(models.Model):
+    name = models.CharField(max_length=100)
+    composer = models.ForeignKey(
+        Composer, on_delete=models.CASCADE, related_name="operas"
+    )
+    has_piano_transcription = models.BooleanField(default=False)
+
+    # objects = cast(OperaManager, OperaManager.from_queryset(OperaQuerySet)())
+
+    def __str__(self) -> str:
+        return f"{self.name}: {self.composer.name}"
+
+    def get_birthplace_of_composer(self) -> Optional[str]:
+        country = self.composer.countries.filter(
+            composer_countries__was_born=True
+        ).first()
+
+        if country is not None:
+            return country.name
+
+        return None
---
+++ packages/reactivated/reactivated-0.7.0.tar.gz/reactivated-0.7.0/tests/serialization.py
@@ -0,0 +1,118 @@
+from typing import Any, List, NamedTuple, Tuple
+
+import pytest
+import simplejson
+from jsonschema import validate
+
+from reactivated import Pick
+from reactivated.serialization import create_schema, serialize
+from sample.server.apps.samples import forms, models
+
+
+class Spam(NamedTuple):
+    thing: List[str]
+    again: str
+
+
+class Bar(NamedTuple):
+    a: str
+    b: bool
+
+
+class Foo(NamedTuple):
+    bar: Bar
+    spam: Spam
+    pick: Pick[models.Composer, "name", "operas.name"]
+    pick_many: List[Pick[models.Composer, "name", "operas.name"]]
+    pick_method: Pick[models.Opera, "name", "get_birthplace_of_composer"]
+    fixed_tuple_different_types: Tuple[str, int]
+    fixed_tuple_same_types: Tuple[str, str]
+    complex_type_we_do_not_type: Any
+
+
+def convert_to_json_and_validate(instance, schema):
+    converted = simplejson.loads(simplejson.dumps(instance))
+    validate(
+        instance=converted, schema={"definitions": schema.definitions, **schema.schema}
+    )
+
+
+@pytest.mark.django_db
+def test_serialization():
+    continent = models.Continent.objects.create(name="Europe")
+    birth_country = models.Country.objects.create(name="Germany", continent=continent)
+    other = models.Country.objects.create(name="Switzerland", continent=continent)
+
+    composer = models.Composer.objects.create(name="Wagner")
+    models.ComposerCountry.objects.create(
+        composer=composer, country=birth_country, was_born=True
+    )
+    models.ComposerCountry.objects.create(composer=composer, country=other)
+
+    opera = models.Opera.objects.create(name="Götterdämmerung", composer=composer)
+
+    instance = Foo(
+        bar=Bar(a="a", b=True),
+        spam=Spam(thing=["one", "two", "three", "four"], again="ok"),
+        pick=composer,
+        pick_many=list(models.Composer.objects.all()),
+        pick_method=opera,
+        fixed_tuple_different_types=("ok", 5),
+        fixed_tuple_same_types=("alright", "again"),
+        complex_type_we_do_not_type={
+            "I am": "very complex",
+            "because": {"I am": "nested", "ok?": []},
+        },
+    )
+    definitions = {}
+    generated_schema = create_schema(Foo, definitions)
+
+    serialized = serialize(instance, generated_schema)
+
+    assert serialized == {
+        "bar": {"a": "a", "b": True},
+        "spam": {"thing": ["one", "two", "three", "four"], "again": "ok"},
+        "pick": {"name": composer.name, "operas": [{"name": opera.name}]},
+        "pick_many": [{"name": "Wagner", "operas": [{"name": "Götterdämmerung"}]}],
+        "pick_method": {
+            "name": "Götterdämmerung",
+            "get_birthplace_of_composer": "Germany",
+        },
+        "fixed_tuple_different_types": ["ok", 5],
+        "fixed_tuple_same_types": ["alright", "again"],
+        "complex_type_we_do_not_type": {
+            "I am": "very complex",
+            "because": {"I am": "nested", "ok?": []},
+        },
+    }
+
+    convert_to_json_and_validate(serialized, generated_schema)
+
+
+def test_form():
+    generated_schema = create_schema(forms.OperaForm, {})
+    form = forms.OperaForm()
+    serialized_form = serialize(form, generated_schema)
+    convert_to_json_and_validate(serialized_form, generated_schema)
+
+    form_with_errors = forms.OperaForm({})
+    form_with_errors.is_valid()
+    serialized_form = serialize(form_with_errors, generated_schema)
+    assert "name" in serialized_form.errors
+    convert_to_json_and_validate(serialized_form, generated_schema)
+
+
+@pytest.mark.django_db
+def test_form_set():
+    generated_schema = create_schema(forms.OperaFormSet, {})
+    form_set = forms.OperaFormSet()
+    serialized_form_set = serialize(form_set, generated_schema)
+    convert_to_json_and_validate(serialized_form_set, generated_schema)
+
+    form_set_with_errors = forms.OperaFormSet(
+        {"form-TOTAL_FORMS": 20, "form-INITIAL_FORMS": 0}
+    )
+    form_set_with_errors.is_valid()
+    serialized_form_set = serialize(form_set_with_errors, generated_schema)
+    assert "name" in serialized_form_set.forms[0].errors
+    convert_to_json_and_validate(serialized_form_set, generated_schema)
