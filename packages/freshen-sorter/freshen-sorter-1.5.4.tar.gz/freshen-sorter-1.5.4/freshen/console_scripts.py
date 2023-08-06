807514f390db749a2288096ce2c38f0aaba296c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:09 +0000

    Add package freshen-sorter/1.5.4/freshen-sorter-1.5.4.tar.gz

---
+++ packages/freshen-sorter/freshen-sorter-1.5.4.tar.gz/freshen-sorter-1.5.4/freshen/console_scripts.py
@@ -0,0 +1,27 @@
+import argparse
+import os
+import ctypes
+import sys
+from freshen import reginstall
+
+print(sys.version)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    config = parser.add_argument_group(
+        "Configuration Commands", "Commands to manage the registry/installing/uninstalling etc.")
+    config.add_argument(
+        '-i', '--install', help='Installs the program to the registry. Adds option to context menu', action='store_true')
+    config.add_argument('-u', '--uninstall',
+                        help='Removes the program from the registry and the context menu.', action='store_true')
+    args = vars(parser.parse_args())
+    print(args)
+
+    if args['install']:
+        reginstall.install()
+
+    if args['uninstall']:
+        reginstall.uninstall()
+
+main()
