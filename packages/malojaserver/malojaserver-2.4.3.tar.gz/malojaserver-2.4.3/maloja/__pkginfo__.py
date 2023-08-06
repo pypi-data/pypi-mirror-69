773b8d35a01bc4baae2ce7084282cfca067eb426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:34 +0000

    Add package malojaserver/2.4.3/malojaserver-2.4.3.tar.gz

---
+++ packages/malojaserver/malojaserver-2.4.3.tar.gz/malojaserver-2.4.3/maloja/__pkginfo__.py
@@ -0,0 +1,36 @@
+name = "maloja"
+desc = "Self-hosted music scrobble database"
+author = {
+	"name":"Johannes Krattenmacher",
+	"email":"maloja@krateng.dev",
+	"github": "krateng"
+}
+version = 2,4,3
+versionstr = ".".join(str(n) for n in version)
+links = {
+	"pypi":"malojaserver",
+	"github":"maloja"
+}
+
+requires = [
+	"bottle>=0.12.16",
+	"waitress>=1.3",
+	"doreah>=1.5.6",
+	"nimrodel>=0.6.3",
+	"setproctitle>=1.1.10",
+	"wand>=0.5.4",
+	"lesscpy>=0.13",
+	"jinja2>2.11"
+]
+resources = [
+	"web/*/*/*",
+	"web/*/*",
+	"web/*",
+	"static/*/*",
+	"data_files/*/*",
+	"data_files/*/*/*"
+]
+
+commands = {
+	"maloja":"controller:main"
+}
