403d47155c751b9e1f68bcbe4f73fa646e18b7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:48 +0000

    Add package merpy/1.7.0/merpy-1.7.0.tar.gz

---
+++ packages/merpy/merpy-1.7.0.tar.gz/merpy-1.7.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="merpy",
+    version="1.7.0",
+    description="use MER inside python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Andre Lamurias",
+    author_email="alamurias@lasige.di.fc.ul.pt",
+    packages=["merpy"],
+    keywords=["ner", "mer"],
+    url="https://github.com/lasigeBioTM/merpy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={"merpy": ["MER/*", "MER/data/*"]},
+    install_requires=["requests"],
+    extras_require={"dev": ["setuptools-changelog"]},
+)
