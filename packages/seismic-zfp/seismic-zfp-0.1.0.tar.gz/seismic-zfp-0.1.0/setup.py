c271135d3e00e7cf1067840b6e01a0bfda44ba73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:52 +0000

    Add package seismic-zfp/0.1.0/seismic-zfp-0.1.0.tar.gz

---
+++ packages/seismic-zfp/seismic-zfp-0.1.0.tar.gz/seismic-zfp-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
+
+long_description = read('README.md')
+
+setuptools.setup(name='seismic-zfp',
+                 author='equinor',
+                 description='Compress and decompress seismic data',
+                 long_description=long_description,
+                 long_description_content_type='text/markdown',
+                 url='https://github.com/equinor/seismic-zfp',
+                 license='LGPL-3.0',
+
+                 use_scm_version=True,
+                 install_requires=['functools32;python_version<"3"',
+                                   'numpy>=1.16', 'segyio', 'zfpy', 'psutil', 'pillow', 'matplotlib', 'Cython'],
+                 setup_requires=['setuptools', 'setuptools_scm'],
+
+                 packages=['seismic_zfp']
+                 )
