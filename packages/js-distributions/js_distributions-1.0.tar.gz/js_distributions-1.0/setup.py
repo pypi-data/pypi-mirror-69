6ea7c3319d4729981792f4ada9186b6111bb5855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:57 +0000

    Add package js-distributions/1.0/js_distributions-1.0.tar.gz

---
+++ packages/js-distributions/js_distributions-1.0.tar.gz/js_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name = "js_distributions",
+      version ="1.0",
+      description = "Gaussian and Binomial distribution",
+      package = ["js_distributions"],
+      author_email ="jitedrasjeena8@gmail.com",
+      zip_safe = False)
