a5ec62bdb50e07bcff600e32c55c9ee817e03e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:06 +0000

    Add package Misskey.py/2.4.1/Misskey.py-2.4.1-py3-none-any.whl

---
+++ packages/Misskey.py/Misskey.py-2.4.1-py3-none-any.whl/Misskey/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Misskey.py
+
+:copyright: (C) 2019 YuzuRyo61.
+:license: MIT License, see LICENSE for more details.
+"""
+
+from Misskey.Misskey import Misskey
+from Misskey.Util import MisskeyUtil
+from Misskey.Exceptions import *
+
+__all__ = ['Misskey', 'MisskeyUtil', 'MisskeyAiException', 'MisskeyInitException', 'MisskeyAPIException', 'MisskeyFileException', 'MisskeyAPITokenException', 'MisskeyNotImplementedVersionException']
