530b3563ddc782a8b2d5bd0f64e05738049f9621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:27 +0000

    Add package mpython-conn/0.0.1/mpython_conn-0.0.1.tar.gz

---
+++ packages/mpython-conn/mpython_conn-0.0.1.tar.gz/mpython_conn-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+setup(
+
+    name='mpython_conn',
+    version='0.0.1',
+    keywords=('pip','mpython'),
+    description="control mpython board by pc",
+    long_description="control mpython board by pc via usb",
+    licence='MIT Licence',
+
+    url='https://github.com/jim0575/desktop-tutorial',
+    author='james',
+    author_email="jim0575@qq.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=['pySerial']
+    )
