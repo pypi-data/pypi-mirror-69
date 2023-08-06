6211ccc38377201f013396a2e463e31ff01084c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:08 +0000

    Add package hyperspyUI/1.1.2/hyperspyUI-1.1.2-py3-none-any.whl

---
+++ packages/hyperspyUI/hyperspyUI-1.1.2-py3-none-any.whl/hyperspyui/tests/conftest.py
@@ -0,0 +1,58 @@
+
+import os
+import tempfile
+import shutil
+
+import hyperspyui
+from hyperspyui.__main__ import get_splash
+
+import pytest
+
+from qtpy import QtCore
+
+QCoreApplication = QtCore.QCoreApplication
+QSettings = QtCore.QSettings
+
+QCoreApplication.setApplicationName("HyperSpyUI-tests")
+QCoreApplication.setOrganizationName("Hyperspy")
+QCoreApplication.setApplicationVersion(hyperspyui.info.__version__)
+
+QSettings.setDefaultFormat(QSettings.IniFormat)
+
+
+_tmpdirpath = ''
+
+
+def pytest_configure(config):
+    global _tmpdirpath
+    _tmpdirpath = tempfile.mkdtemp()
+    userpath = os.path.join(_tmpdirpath, 'user')
+    syspath = os.path.join(_tmpdirpath, 'sys')
+    os.mkdir(userpath)
+    os.mkdir(syspath)
+
+    QSettings.setPath(QSettings.IniFormat,
+                      QSettings.UserScope, userpath)
+    QSettings.setPath(QSettings.IniFormat,
+                      QSettings.SystemScope, syspath)
+
+    settings = QSettings()
+    settings.setValue(
+        'plugins/Version selector/check_for_updates_on_start', False)
+
+
+def pytest_unconfigure(config):
+    shutil.rmtree(_tmpdirpath)
+
+
+@pytest.fixture(scope='session')
+def mainwindow(qapp):
+    from hyperspyui.mainwindow import MainWindow
+
+    window = MainWindow(get_splash(), argv=[])
+    yield window
+    qapp.processEvents()
+    window.close()
+    window.deleteLater()
+    del window
+    qapp.processEvents()
