d44bf2a59a747e120b603c5d5c31eef8b83b3a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:03 +0000

    Add package hebpipe/1.0.0.1/hebpipe-1.0.0.1.tar.gz

---
+++ packages/hebpipe/hebpipe-1.0.0.1.tar.gz/hebpipe-1.0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'hebpipe',
+  packages = find_packages(),
+  version = '1.0.0.1',
+  description = 'A pipeline for Hebrew NLP',
+  author = 'Amir Zeldes',
+  author_email = 'amir.zeldes@georgetown.edu',
+  package_data = {'':['README.md','LICENSE.md','requirements.txt'],'hebpipe':['lib/*','data/*','bin/*','models/*']},
+   install_requires=['numpy','pandas','scipy','joblib','xgboost==0.81','rftokenizer','depedit','xmltodict'],
+  url = 'https://github.com/amir-zeldes/HebPipe',
+  license='Apache License, Version 2.0',
+  download_url = 'https://github.com/amir-zeldes/HebPipe/releases/tag/v1.0.0.1',
+  keywords = ['NLP', 'Hebrew', 'segmentation', 'tokenization', 'tagging', 'parsing','morphology','POS'],
+  classifiers = ['Programming Language :: Python',
+'Programming Language :: Python :: 2',
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: Apache Software License',
+'Operating System :: OS Independent'],
+)
