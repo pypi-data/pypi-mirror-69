220c7f2528fd04565245ccf4b7b7905d45f31d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:52 +0000

    Add package banditml/0.6.2/banditml-0.6.2.tar.gz

---
+++ packages/banditml/banditml-0.6.2.tar.gz/banditml-0.6.2/setup.py
@@ -0,0 +1,37 @@
+## How to release package to PyPi:
+## 1) cd in package directory banditml_pkg/
+## 2) bump version in setup.py
+## 3) pipenv run python setup.py sdist bdist_wheel
+## 4) pipenv run twine upload dist/* --skip-existing
+## 5) See new version at https://pypi.org/project/banditml/
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="banditml",
+    version="0.6.2",
+    author="Edoardo Conti, Lionel Vital, Joseph Gilley",
+    author_email="team@banditml.com",
+    description="Portable Bandit ML code for training & serving consistency.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/banditml/banditml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "google-cloud-bigquery~=1.24",
+        "pandas>=1.0.3,<1.1",
+        "scikit-learn>=0.22.2.post1,<0.23",
+        "torch~=1.4",
+        "typing_extensions>=3.7.4",
+    ],
+)
