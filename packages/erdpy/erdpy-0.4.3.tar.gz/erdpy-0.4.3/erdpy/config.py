8d9de16f4ef9659b5832dcdd08181ac8f1f814fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:44 +0000

    Add package erdpy/0.4.3/erdpy-0.4.3.tar.gz

---
+++ packages/erdpy/erdpy-0.4.3.tar.gz/erdpy-0.4.3/erdpy/config.py
@@ -0,0 +1,7 @@
+# Configuration that should not be edited by the end user:
+DOWNLOAD_MIRROR = "https://ide.elrond.com"
+
+# Configuration that may be edited by the end user:
+ROOT_FOLDER_NAME = "ElrondSCTools"
+DEFAULT_GASPRICE = 200000000000
+DEFAULT_GASLIMIT = 50000000
