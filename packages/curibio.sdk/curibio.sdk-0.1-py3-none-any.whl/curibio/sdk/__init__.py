af4b9820fb73c682661024d756c93b337b550899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:14 +0000

    Add package curibio.sdk/0.1/curibio.sdk-0.1-py3-none-any.whl

---
+++ packages/curibio.sdk/curibio.sdk-0.1-py3-none-any.whl/curibio/sdk/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""Curi Bio Software Development Kit.
+
+SDK for utilizing Curi bio data files and online databases.
+"""
+from .files import WellFile
+
+__all__ = [
+    "WellFile",
+]
+
+
+# name = "sdk"
