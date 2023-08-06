9bc922eeb89b7a0b963fdacbbbf613acb67f0889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:53 +0000

    Add package waaazzi/0.1/waaazzi-0.1.tar.gz

---
+++ packages/waaazzi/waaazzi-0.1.tar.gz/waaazzi-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='waaazzi',
+      version='0.1',
+      description='friends',
+      packages=['waaazzi'],
+      author= 'Wasif Ibrahim',
+      author_email= 'wasif.ibrahim29@gmail.com',
+      zip_safe=False)
---
+++ packages/waaazzi/waaazzi-0.1.tar.gz/waaazzi-0.1/waaazzi/friends.py
@@ -0,0 +1,22 @@
+class Bitches:
+
+	def name():
+		return "Wasif Ibrahim"
+
+	def age():
+		return 24
+
+	def skill():
+		return "Being an awesome person"
+
+	def loser_friends():
+		return "Abia, dure and amna"
+
+	def chutiya_kaun():
+		return "dure pehnchod"
+
+	def sasti_randi():
+		return "abia noor"
+
+	def ammi_kaun():
+		return "amna bhosriwala"
