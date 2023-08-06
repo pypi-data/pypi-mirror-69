90ba40f43b71a08d615335f47aec649130d9bb33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:52 +0000

    Add package dash-wrapper/0.2.0/dash-wrapper-0.2.0.tar.gz

---
+++ packages/dash-wrapper/dash-wrapper-0.2.0.tar.gz/dash-wrapper-0.2.0/dash_wrapper/dash_core_components/_wrapper.py
@@ -0,0 +1,14 @@
+import sys
+import dash_core_components as module
+
+from .._wrapper import base_wrapper
+
+this_module = sys.modules[__name__]
+
+
+def __getattr__(name):
+    # print(name)
+    return base_wrapper(name, module)
+
+
+__all__ = module.__all__
