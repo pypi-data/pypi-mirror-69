70434b051eeb39c9669c31a68e0462cf25e3db77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:28 +0000

    Add package gaussian-binomial-dsnd/0.1/gaussian_binomial_dsnd-0.1.tar.gz

---
+++ packages/gaussian-binomial-dsnd/gaussian_binomial_dsnd-0.1.tar.gz/gaussian_binomial_dsnd-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_dsnd',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaussian_binomial_dsnd'],
+      zip_safe=False)
