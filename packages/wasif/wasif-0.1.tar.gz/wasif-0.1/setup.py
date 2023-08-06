94b9cab7527f667fb212970abb00f422f703149e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:49 +0000

    Add package wasif/0.1/wasif-0.1.tar.gz

---
+++ packages/wasif/wasif-0.1.tar.gz/wasif-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='wasif',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['wasif'],
+      author= 'Wasif Ibrahim',
+      author_email= 'wasif.ibrahim29@gmail.com',
+      zip_safe=False)
