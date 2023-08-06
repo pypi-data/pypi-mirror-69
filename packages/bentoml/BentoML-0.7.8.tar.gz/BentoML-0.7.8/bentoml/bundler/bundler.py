97f8c943322c628f12ee14fb0a6a189b0322546f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:25 +0000

    Add package bentoml/0.7.8/BentoML-0.7.8.tar.gz

---
+++ packages/bentoml/BentoML-0.7.8.tar.gz/BentoML-0.7.8/bentoml/bundler/bundler.py
@@ -0,0 +1,184 @@
+# Copyright 2019 Atalaya Tech, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import stat
+import logging
+
+from bentoml.exceptions import BentoMLException
+from bentoml.bundler.py_module_utils import copy_used_py_modules
+from bentoml.bundler.templates import (
+    BENTO_SERVICE_BUNDLE_SETUP_PY_TEMPLATE,
+    MANIFEST_IN_TEMPLATE,
+    MODEL_SERVER_DOCKERFILE_CPU,
+    INIT_PY_TEMPLATE,
+    BENTO_INIT_SH_TEMPLATE,
+    DOCKER_ENTRYPOINT_SH,
+)
+from bentoml.bundler.utils import add_local_bentoml_package_to_repo
+from bentoml.utils import _is_bentoml_in_develop_mode
+from bentoml.utils.usage_stats import track_save
+from bentoml.bundler.config import SavedBundleConfig
+
+
+DEFAULT_SAVED_BUNDLE_README = """\
+# Generated BentoService bundle - {}:{}
+
+This is a ML Service bundle created with BentoML, it is not recommended to edit
+code or files contained in this directory. Instead, edit the code that uses BentoML
+to create this bundle, and save a new BentoService bundle.
+"""
+
+logger = logging.getLogger(__name__)
+
+
+def save_to_dir(bento_service, path, version=None, silent=False):
+    """Save given BentoService along with all its artifacts, source code and
+    dependencies to target file path, assuming path exist and empty. If target path
+    is not empty, this call may override existing files in the given path.
+
+    :param bento_service (bentoml.service.BentoService): a Bento Service instance
+    :param path (str): Destination of where the bento service will be saved
+    :param version (str): Override the service version with given version string
+    :param silent (boolean): whether to hide the log message showing target save path
+    """
+    track_save(bento_service)
+
+    from bentoml.service import BentoService
+
+    if not isinstance(bento_service, BentoService):
+        raise BentoMLException(
+            "save_to_dir only work with instance of custom BentoService class"
+        )
+
+    if version is not None:
+        # If parameter version provided, set bento_service version
+        # Otherwise it will bet set the first time the `version` property get accessed
+        bento_service.set_version(version)
+
+    if not os.path.exists(path):
+        raise BentoMLException("Directory '{}' not found".format(path))
+
+    for artifact in bento_service._artifacts:
+        if artifact.name not in bento_service._packed_artifacts:
+            logger.warning(
+                "Missing declared artifact '%s' for BentoService '%s'",
+                artifact.name,
+                bento_service.name,
+            )
+
+    module_base_path = os.path.join(path, bento_service.name)
+    try:
+        os.mkdir(module_base_path)
+    except FileExistsError:
+        raise BentoMLException(
+            f"Existing module file found for BentoService {bento_service.name}"
+        )
+
+    # write README.md with custom BentoService's docstring if presented
+    saved_bundle_readme = DEFAULT_SAVED_BUNDLE_README.format(
+        bento_service.name, bento_service.version
+    )
+    if bento_service.__class__.__doc__:
+        saved_bundle_readme += "\n"
+        saved_bundle_readme += bento_service.__class__.__doc__.strip()
+
+    with open(os.path.join(path, "README.md"), "w") as f:
+        f.write(saved_bundle_readme)
+
+    # save all model artifacts to 'base_path/name/artifacts/' directory
+    if bento_service.artifacts:
+        bento_service.artifacts.save(module_base_path)
+
+    # write conda environment, requirement.txt
+    bento_service.env.save(path, bento_service)
+
+    # TODO: add bentoml.find_packages helper for more fine grained control over this
+    # process, e.g. packages=find_packages(base, [], exclude=[], used_module_only=True)
+    # copy over all custom model code
+    module_name, module_file = copy_used_py_modules(
+        bento_service.__class__.__module__, os.path.join(path, bento_service.name)
+    )
+
+    # create __init__.py
+    with open(os.path.join(path, bento_service.name, "__init__.py"), "w") as f:
+        f.write(
+            INIT_PY_TEMPLATE.format(
+                service_name=bento_service.name,
+                module_name=module_name,
+                pypi_package_version=bento_service.version,
+            )
+        )
+
+    # write setup.py, this make saved BentoService bundle pip installable
+    setup_py_content = BENTO_SERVICE_BUNDLE_SETUP_PY_TEMPLATE.format(
+        name=bento_service.name,
+        pypi_package_version=bento_service.version,
+        long_description=saved_bundle_readme,
+    )
+    with open(os.path.join(path, "setup.py"), "w") as f:
+        f.write(setup_py_content)
+
+    with open(os.path.join(path, "MANIFEST.in"), "w") as f:
+        f.write(MANIFEST_IN_TEMPLATE.format(service_name=bento_service.name))
+
+    # write Dockerfile
+    with open(os.path.join(path, "Dockerfile"), "w") as f:
+        f.write(
+            MODEL_SERVER_DOCKERFILE_CPU.format(
+                docker_base_image=bento_service._env._docker_base_image
+            )
+        )
+
+    # write docker-entrypoint.sh
+    docker_entrypoint_sh_file = os.path.join(path, "docker-entrypoint.sh")
+    with open(docker_entrypoint_sh_file, "w") as f:
+        f.write(DOCKER_ENTRYPOINT_SH)
+    # chmod +x docker-entrypoint.sh
+    st = os.stat(docker_entrypoint_sh_file)
+    os.chmod(docker_entrypoint_sh_file, st.st_mode | stat.S_IEXEC)
+
+    # write bento init sh for install targz bundles
+    bentoml_init_script_file = os.path.join(path, 'bentoml_init.sh')
+    with open(bentoml_init_script_file, 'w') as f:
+        f.write(BENTO_INIT_SH_TEMPLATE)
+    # chmod +x bentoml_init_script file
+    st = os.stat(bentoml_init_script_file)
+    os.chmod(bentoml_init_script_file, st.st_mode | stat.S_IEXEC)
+
+    # write bentoml.yml
+    config = SavedBundleConfig(bento_service)
+    config["metadata"].update({"module_name": module_name, "module_file": module_file})
+
+    config.write_to_path(path)
+    # Also write bentoml.yml to module base path to make it accessible
+    # as package data after pip installed as a python package
+    config.write_to_path(module_base_path)
+
+    # if bentoml package in editor mode(pip install -e), will include
+    # that bentoml package to saved BentoService bundle
+    if _is_bentoml_in_develop_mode():
+        add_local_bentoml_package_to_repo(path)
+
+    if not silent:
+        logger.info(
+            "BentoService bundle '%s:%s' created at: %s",
+            bento_service.name,
+            bento_service.version,
+            path,
+        )
---
+++ packages/bentoml/BentoML-0.7.8.tar.gz/BentoML-0.7.8/setup.py
@@ -0,0 +1,153 @@
+# Copyright 2019 Atalaya Tech, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
+import versioneer
+
+with open("README.md", "r", encoding="utf8") as f:
+    long_description = f.read()
+
+install_requires = [
+    "ruamel.yaml>=0.15.0",
+    "numpy",
+    "flask",
+    "gunicorn",
+    "click>=7.0",
+    "pandas",
+    "prometheus_client",
+    "python-json-logger",
+    "boto3",
+    "requests",
+    "packaging",
+    "docker",
+    "configparser",
+    "sqlalchemy>=1.3.0",
+    "sqlalchemy-utils",
+    "protobuf>=3.6.0",
+    "grpcio<=1.27.2",
+    "cerberus",
+    "tabulate",
+    "humanfriendly",
+    "alembic",
+    "aiohttp",
+    "py_zipkin",
+    'contextvars;python_version < "3.7"',
+    "psutil",
+    # python-dateutil required by pandas and boto3, this makes sure the version
+    # works for both
+    "python-dateutil>=2.1,<2.8.1",
+    "certifi",
+]
+
+imageio = ["imageio>=2.5.0"]
+pytorch = ["torch", "torchvision"]
+fastai = ["fastai", "matplotlib"]
+tensorflow = ["tensorflow"]
+xgboost = ["xgboost"]
+h2o = ["h2o"]
+api_server = ["gunicorn", "prometheus_client"]
+aws_sam_cli = ["aws-sam-cli==0.33.1"]
+postgres = ['psycopg2', 'psycopg2-binary']
+
+optional_requires = (
+    api_server
+    + imageio
+    + pytorch
+    + tensorflow
+    + fastai
+    + xgboost
+    + h2o
+    + aws_sam_cli
+    + postgres
+)
+
+test_requires = (
+    [
+        "pytest>=4.1.0",
+        "pytest-cov>=2.7.1",
+        "mock>=2.0.0",
+        "coverage>=4.4",
+        "codecov",
+        "moto",
+        "numpy",
+        "scikit-learn",
+        "pylint>=2.5.2",
+        "flake8>=3.8.2",
+    ]
+    + imageio
+    + aws_sam_cli
+)
+
+dev_requires = [
+    "tox>=3.12.1",
+    "tox-conda>=0.2.0",
+    "flake8>=3.8.2",
+    "twine",
+    "setuptools",
+    "gitpython>=2.0.2",
+    "grpcio-tools<=1.27.2",
+    "grpcio-reflection<=1.27.2",
+    "pylint>=2.5.2",
+    "black>=19.0",
+]
+
+
+docs_requires = [
+    "sphinx",
+    "sphinx-click",
+    "sphinx_rtd_theme",
+    "sphinxcontrib-fulltoc",
+    "recommonmark",
+]
+
+dev_all = install_requires + dev_requires + optional_requires + docs_requires
+
+extras_require = {
+    "all": dev_all,
+    "dev": dev_requires,
+    "api_server": api_server,
+    "test": test_requires,
+    "doc_builder": docs_requires + install_requires,  # required by readthedocs.io
+}
+
+setuptools.setup(
+    name="BentoML",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="bentoml.org",
+    author_email="contact@bentoml.ai",
+    description="An open-source platform for machine learning model serving.",
+    long_description=long_description,
+    license="Apache License 2.0",
+    long_description_content_type="text/markdown",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    url="https://github.com/bentoml/BentoML",
+    packages=setuptools.find_packages(exclude=["tests*", "benchmark*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.4",
+    entry_points={"console_scripts": ["bentoml=bentoml.cli:cli"]},
+    project_urls={
+        "Bug Reports": "https://github.com/bentoml/BentoML/issues",
+        "Source Code": "https://github.com/bentoml/BentoML",
+        "Slack User Group": "https://bit.ly/2N5IpbB",
+    },
+    include_package_data=True,  # Required for '.cfg' files under bentoml/config
+)
