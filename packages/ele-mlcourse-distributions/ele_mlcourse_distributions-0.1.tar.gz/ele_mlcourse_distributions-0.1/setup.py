1c820d210f25b118d8203000b53543441adf30f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:24 +0000

    Add package ele-mlcourse-distributions/0.1/ele_mlcourse_distributions-0.1.tar.gz

---
+++ packages/ele-mlcourse-distributions/ele_mlcourse_distributions-0.1.tar.gz/ele_mlcourse_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ele_mlcourse_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ele_mlcourse_distributions'],
+      author='Eleonora Mercalli',
+      author_email='eleonora.mercalli@gmail.com',
+      zip_safe=False)
