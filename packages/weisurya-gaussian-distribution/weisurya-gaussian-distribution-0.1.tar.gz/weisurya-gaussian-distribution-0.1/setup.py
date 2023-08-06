6ee471ee688e9b0e11e581ffc18772289930871b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:15 +0000

    Add package weisurya-gaussian-distribution/0.1/weisurya-gaussian-distribution-0.1.tar.gz

---
+++ packages/weisurya-gaussian-distribution/weisurya-gaussian-distribution-0.1.tar.gz/weisurya-gaussian-distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='weisurya-gaussian-distribution',
+      version='0.1',
+      description='Gaussian distributions by weisurya',
+      packages=['distributions'],
+      zip_safe=False)
