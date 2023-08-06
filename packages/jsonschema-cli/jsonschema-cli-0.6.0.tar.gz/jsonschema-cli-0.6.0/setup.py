be42fdecbb610f469fb4b1ef3af117c1ecc5bd9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:50 +0000

    Add package jsonschema-cli/0.6.0/jsonschema-cli-0.6.0.tar.gz

---
+++ packages/jsonschema-cli/jsonschema-cli-0.6.0.tar.gz/jsonschema-cli-0.6.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['jsonschema_cli', 'jsonschema_cli.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['jsonschema>=3.2.0,<4.0.0', 'pyyaml>=5.3.1,<6.0.0']
+
+entry_points = \
+{'console_scripts': ['jsonschema-cli = jsonschema_cli.cli:run']}
+
+setup_kwargs = {
+    'name': 'jsonschema-cli',
+    'version': '0.6.0',
+    'description': 'A thin wrapper over [Python Jsonschema](https://github.com/Julian/jsonschema) to allow validating shcemas easily using simple CLI commands.',
+    'long_description': None,
+    'author': 'Eyal Mor',
+    'author_email': 'eyalmor94@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
