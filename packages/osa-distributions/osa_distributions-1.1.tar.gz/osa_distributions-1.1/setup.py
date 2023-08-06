9a122a11914f326f259a8ad0c50a4408a163efc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:12 +0000

    Add package osa-distributions/1.1/osa_distributions-1.1.tar.gz

---
+++ packages/osa-distributions/osa_distributions-1.1.tar.gz/osa_distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='osa_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['osa_distributions'],
+      author="Osakpolor Tolulope Obaseki",
+      author_email="obasekiosa@gmail.com",
+      zip_safe=False)
