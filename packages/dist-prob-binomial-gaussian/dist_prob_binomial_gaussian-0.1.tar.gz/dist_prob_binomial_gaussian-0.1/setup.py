d78de1a5cd2ca31ccecb0025ac6125c20432a71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:31 +0000

    Add package dist-prob-binomial-gaussian/0.1/dist_prob_binomial_gaussian-0.1.tar.gz

---
+++ packages/dist-prob-binomial-gaussian/dist_prob_binomial_gaussian-0.1.tar.gz/dist_prob_binomial_gaussian-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_prob_binomial_gaussian',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['dist_prob_binomial_gaussian'],
+      author = 'Abhishek Kumar Singh',
+      author_email = 'abhishek.kumar.singh2101@gmail.com',
+      zip_safe=False)
