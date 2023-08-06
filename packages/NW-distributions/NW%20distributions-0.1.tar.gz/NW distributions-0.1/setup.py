c2e3a418e363e4e3854ef6289cbd0cca40c3b12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:35 +0000

    Add package NW-distributions/0.1/NW%20distributions-0.1.tar.gz

---
+++ packages/NW-distributions/NW%20distributions-0.1.tar.gz/NW distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='NW distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
