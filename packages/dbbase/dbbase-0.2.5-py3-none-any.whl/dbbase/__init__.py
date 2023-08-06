bd28067d4aa6308710a32b575b3e35713a866c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:58 +0000

    Add package dbbase/0.2.5/dbbase-0.2.5-py3-none-any.whl

---
+++ packages/dbbase/dbbase-0.2.5-py3-none-any.whl/dbbase/__init__.py
@@ -0,0 +1,9 @@
+"""
+This package implements base routines for interacting with a database.
+"""
+from ._version import __version__
+
+# relevant class and functions for root package level
+from .utils import db_config
+from .base import DB
+from . import maint
