5810a887fb5ec13de47c8eabc7496dcfecfdb4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:38 +0000

    Add package scprint/1.1.0b0/scprint-1.1.0b0.tar.gz

---
+++ packages/scprint/scprint-1.1.0b0.tar.gz/scprint-1.1.0b0/scprint.py
@@ -0,0 +1,126 @@
+import sys
+import platform
+from colors import colors
+
+if platform.system().lower() == 'windows':
+    from ctypes import windll, c_int, byref
+    stdout_handle = windll.kernel32.GetStdHandle(c_int(-11))
+    mode = c_int(0)
+    windll.kernel32.GetConsoleMode(c_int(stdout_handle), byref(mode))
+    mode = c_int(mode.value | 4)
+    windll.kernel32.SetConsoleMode(c_int(stdout_handle), mode)
+
+orignialprint = print
+orignialinput = input
+
+def getColorCode(color, precode, postcode):
+    colorIsHex = 0
+    if color != None:
+        if color[0] == "#":
+            color = color[1:]
+            colorIsHex = 1
+        for i in range(len(colors)):
+            if colors[i][colorIsHex].lower() == color.lower():
+                code = (precode + str(i) + postcode)
+    else:
+        code=""
+    return code
+
+
+def print(*args, color=None, bcolor=None, sep=" ", end="\n", **kwargs):
+    if len(args) == 1:
+        string = str(args[0])
+    else:
+        string = sep.join(list(map(str,args)))
+
+    if "idlelib.run" in sys.modules:
+        orignialprint(string, end=end, **kwargs)
+    else:
+        colorcode = getColorCode(color, u"\u001b[38;5;", u"m")
+        colorbcode = getColorCode(bcolor, u"\u001b[48;5;", u"m")
+        resetcode = u"\u001b[0m"
+        orignialprint(colorbcode, colorcode, string, sep="", end="", **kwargs)
+        orignialprint(resetcode, end=end, **kwargs)
+
+def input(*args, color=None, bcolor=None, pcolor=None, sep=" ", end="", pend="", **kwargs):
+    if len(args) == 1:
+        string = str(args[0])
+    else:
+        string = sep.join(list(map(str,args)))
+    
+    if "idlelib.run" in sys.modules:
+        orignialprint(string, end=end **kwargs)
+    else:
+        colorcode = getColorCode(color, u"\u001b[38;5;", u"m")
+        bcolorcode = getColorCode(bcolor, u"\u001b[48;5;", u"m")
+        resetcode = u"\u001b[0m"
+        orignialprint(bcolorcode, colorcode, string, sep="", end=end, **kwargs)
+        orignialprint(resetcode, end="", **kwargs)
+        pcolorcode = getColorCode(pcolor, u"\u001b[38;5;", u"m")
+        orignialprint(pcolorcode, sep="", end="", **kwargs)
+    returnInput = orignialinput()
+    orignialprint(resetcode, end=pend, **kwargs)
+    return returnInput
+
+def rainbow(*args, sep=" ", end="\n", **kwargs):
+    if len(args) == 1:
+        string = str(args[0])
+    else:
+        string = sep.join(list(map(str,args)))
+
+    mainColors = [colors[1], colors[2], colors[3], colors[4], colors[5], colors[6], colors[7], colors[8], colors[9], colors[10], colors[11], colors[12], colors[13], colors[14]]
+    for i in range(len(string)):
+        print(string[i], color=mainColors[i%len(mainColors)][0], sep="", end="", **kwargs)
+    print("", end=end, **kwargs)
+
+def demo(showHex=False, showBColor=False):
+    if showHex == True:
+        space = str(20)
+        smallSpace = str(8)
+        for color1, color2, color3, color4 in zip(*[iter(colors)]*4):
+            print(("{:<" + smallSpace + "}").format("#" + color1[1]), color=color1[0], sep="", end="")
+            print(("{:<" + space + "}").format(color1[0]), color=color1[0], sep="", end="")
+            print(("{:<" + smallSpace + "}").format("#" + color2[1]), color=color2[0], sep="", end="")
+            print(("{:<" + space + "}").format(color2[0]), color=color2[0], sep="", end="")
+            print(("{:<" + smallSpace + "}").format("#" + color3[1]), color=color3[0], sep="", end="")
+            print(("{:<" + space + "}").format(color3[0]), color=color3[0], sep="", end="")
+            print(("{:<" + smallSpace + "}").format("#" + color4[1]), color=color4[0], sep="", end="")
+            print(("{:<" + space + "}").format(color4[0]), color=color4[0], sep="", end="\n")
+            if showBColor == True:
+                print(("{:<" + smallSpace + "}").format("#" + color1[1]), bcolor=color1[0], sep="", end="")
+                print(("{:<" + space + "}").format(color1[0]), bcolor=color1[0], sep="", end="")
+                print(("{:<" + smallSpace + "}").format("#" + color2[1]), bcolor=color2[0], sep="", end="")
+                print(("{:<" + space + "}").format(color2[0]), bcolor=color2[0], sep="", end="")
+                print(("{:<" + smallSpace + "}").format("#" + color3[1]), bcolor=color3[0], sep="", end="")
+                print(("{:<" + space + "}").format(color3[0]), bcolor=color3[0], sep="", end="")
+                print(("{:<" + smallSpace + "}").format("#" + color4[1]), bcolor=color4[0], sep="", end="")
+                print(("{:<" + space + "}").format(color4[0]), bcolor=color4[0], sep="", end="\n\n")
+            
+    else:
+        space = str(20)
+        for color1, color2, color3, color4 in zip(*[iter(colors)]*4):
+            print(("{:<" + space + "}").format(color1[0]), color=color1[0], sep="", end="")
+            print(("{:<" + space + "}").format(color2[0]), color=color2[0], sep="", end="")
+            print(("{:<" + space + "}").format(color3[0]), color=color3[0], sep="", end="")
+            print(("{:<" + space + "}").format(color4[0]), color=color4[0], sep="", end="\n")
+            if showBColor == True:
+                print(("{:<" + space + "}").format(color1[0]), bcolor=color1[0], sep="", end="")
+                print(("{:<" + space + "}").format(color2[0]), bcolor=color2[0], sep="", end="")
+                print(("{:<" + space + "}").format(color3[0]), bcolor=color3[0], sep="", end="")
+                print(("{:<" + space + "}").format(color4[0]), bcolor=color4[0], sep="", end="\n\n")
+
+if __name__ == "__main__":
+    if "idlelib.run" in sys.modules:
+        input("This module will not add any features if used in IDLE...")
+    else:
+        if input("Show Hex Values (Y/N): ").lower() == "y":
+            if input("Show Background Colors (Y/N): ").lower() == "y":
+                demo(showHex=True, showBColor=True)
+            else:
+                demo(showHex=True, showBColor=False)
+        else:
+            if input("Show Background Colors (Y/N): ").lower() == "y":
+                demo(showHex=False, showBColor=True)
+            else:
+                demo(showHex=False, showBColor=False)
+        input()
