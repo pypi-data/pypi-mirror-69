6463ced4a0078e9a2907fd7a761ad66ebf010e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:43 +0000

    Add package Better-Console/0.0.6a0/Better%20Console-0.0.6a0.tar.gz

---
+++ packages/Better-Console/Better%20Console-0.0.6a0.tar.gz/Better Console-0.0.6a0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+long_description='''
+Please go to the github [here](https://github.com/ultraflame4/Better-Console-python)
+'''
+setuptools.setup(
+    name="Better Console",
+    version="0.0.6a",
+    author="ultraflame4",
+    author_email="ultraflame4@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description="A Better Console for python programs",
+    url="https://github.com/ultraflame4/Better-Console-python",
+    packages=["Better_Console"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'PySimpleGuiQt'
+    ]
+)
