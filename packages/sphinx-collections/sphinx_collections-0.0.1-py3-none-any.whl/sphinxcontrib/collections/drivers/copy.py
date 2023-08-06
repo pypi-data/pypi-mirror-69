6b2c5baa434e3c8850ea7cf7910f9d85ba27ba41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:48 +0000

    Add package sphinx-collections/0.0.1/sphinx_collections-0.0.1-py3-none-any.whl

---
+++ packages/sphinx-collections/sphinx_collections-0.0.1-py3-none-any.whl/sphinxcontrib/collections/drivers/copy.py
@@ -0,0 +1,29 @@
+import os
+
+from shutil import copytree, ignore_patterns, rmtree
+
+from sphinxcontrib.collections.drivers import Driver
+
+
+class CopyDriver(Driver):
+
+    def run(self):
+        self.info('Copy files...')
+
+        if not os.path.exists(self.config['source']):
+            self.error('Source {} does not exist'.format(self.config['source']))
+            return
+
+        try:
+            copytree(self.config['source'],
+                     self.config['target'],
+                     ignore_patterns(*self.config['ignore']))
+        except IOError as e:
+            self.error('Problems during copying files.', e)
+
+    def clean(self):
+        try:
+            rmtree(self.config['target'])
+            self.info('Folder deleted: {}'.format(self.config['target']))
+        except IOError as e:
+            self.error('Problems during cleaning for collection {}'.format(self.config['name']), e)
