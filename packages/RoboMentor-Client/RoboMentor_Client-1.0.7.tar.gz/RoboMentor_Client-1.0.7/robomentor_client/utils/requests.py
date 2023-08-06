c1fe5e0e323a8e2434089dfd1a52146cedd23f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:12 +0000

    Add package RoboMentor-Client/1.0.7/RoboMentor_Client-1.0.7.tar.gz

---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.0.7.tar.gz/RoboMentor_Client-1.0.7/robomentor_client/utils/requests.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+import requests
+from .log import Log
+
+
+class Request:
+
+    @staticmethod
+    def do(self, url, params, headers, http_method):
+        res = ''
+        if http_method.upper() == "POST":
+            try:
+                res = requests.post(url, params, headers=headers)
+                Log.info("正在发送POST请求")
+            except Exception as e:
+                Log.error("POST请求出现了异常：{0}".format(e))
+        elif http_method.upper() == "GET":
+            try:
+                res = requests.post(url, params, headers=headers)
+                Log.info("正在发送GET请求")
+            except Exception as e:
+                Log.error("GET请求出现了异常：{0}".format(e))
+        return res
