07524ce523c0f4caaa4b0b9319f3c865a72a910a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:33 +0000

    Add package deethon/0.3.2/deethon-0.3.2.tar.gz

---
+++ packages/deethon/deethon-0.3.2.tar.gz/deethon-0.3.2/deethon/types.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+from functools import cached_property, lru_cache
+from typing import Union, TYPE_CHECKING
+
+import requests
+
+from . import consts, errors
+
+if TYPE_CHECKING:
+    from .session import Session
+
+
+@lru_cache
+class Album:
+    def __init__(self, album_id: Union[int, str]):
+        self.r = requests.get(
+            f"https://api.deezer.com/album/{album_id}").json()
+        self.id = self.r["id"]
+        self.title = self.r["title"]
+        self.artist = self.r["artist"]["name"]
+        self.upc = self.r["upc"]
+        self.link = self.r["link"]
+        self.record_type = self.r["record_type"]
+        self.release_date = self.r["release_date"]
+        self.total_tracks = self.r["nb_tracks"]
+        self.genres = [genre["name"] for genre in self.r["genres"]["data"]]
+        self.label = self.r["label"]
+
+    @cached_property
+    def cover_small(self) -> bytes:
+        return requests.get(self.r["cover_small"]).content
+
+    @cached_property
+    def cover_medium(self) -> bytes:
+        return requests.get(self.r["cover_medium"]).content
+
+    @cached_property
+    def cover_big(self) -> bytes:
+        return requests.get(self.r["cover_big"]).content
+
+    @cached_property
+    def cover_xl(self) -> bytes:
+        return requests.get(self.r["cover_xl"]).content
+
+    @cached_property
+    def tracks(self) -> list:
+        return [Track(x["id"]) for x in self.r["tracks"]["data"]]
+
+
+@lru_cache
+class Track:
+    def __init__(self, track_id: Union[int, str]):
+        r = requests.get(f"https://api.deezer.com/track/{track_id}").json()
+        if "error" in r:
+            raise errors.DeezerApiError(r['error']['type'],
+                                        r['error']['message'],
+                                        r['error']['code'])
+        self.artist = r['artist']['name']
+        self.bpm = r['bpm']
+        self.disk_number = r['disk_number']
+        self.duration = r['duration']
+        self.id = r['id']
+        self.isrc = r['isrc']
+        self.link = r['link']
+        self.number = r['track_position']
+        self.replaygain_track_peak = r['gain']
+        self.release_date = r['release_date']
+        self.title = r['title']
+        self.title_short = r['title_short']
+
+        ymd_date = self.release_date.split("-")  # 0 YYYY 1 MM 2 DD
+        self.release_year = ymd_date[0]
+        self.release_date_four_digits = f"{ymd_date[2]}{ymd_date[1]}"
+
+        self.album_id = r['album']["id"]
+
+        self.md5_origin = None
+        self.media_version = None
+        self.composer = None
+        self.author = None
+
+    @cached_property
+    def album(self) -> Album:
+        return Album(self.album_id)
+
+    def add_more_tags(self, session: Session):
+        track_info = session.get_api(consts.METHOD_GET_TRACK, session.csrf_token,
+                                     {"sng_id": self.id})
+        self.md5_origin = track_info["MD5_ORIGIN"]
+        self.media_version = track_info["MEDIA_VERSION"]
+
+        if "composer" in track_info["SNG_CONTRIBUTORS"]:
+            self.composer = track_info["SNG_CONTRIBUTORS"]["composer"]
+        if "author" in track_info["SNG_CONTRIBUTORS"]:
+            self.author = track_info["SNG_CONTRIBUTORS"]["author"]
---
+++ packages/deethon/deethon-0.3.2.tar.gz/deethon-0.3.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['deethon']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['mutagen>=1.44.0,<2.0.0',
+ 'pycryptodome>=3.9.7,<4.0.0',
+ 'requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'deethon',
+    'version': '0.3.2',
+    'description': 'Python3 library to easily download music from Deezer',
+    'long_description': 'Deethon\n=======\nPython3 library to easily download music from `Deezer`_.\n\nQuickstart\n----------\nInstalling\n^^^^^^^^^^\n.. code-block:: sh\n\n    pip3 install deethon\n\nInitialize\n^^^^^^^^^^\n.. code-block:: python\n\n    import deethon\n\n    deezer = deethon.Session("DEEZER ARL TOKEN")\n\nDownload tracks\n^^^^^^^^^^^^^^^\nDownload track by Deezer link\n\n.. code-block:: python\n\n    deezer.download(\n        "Deezer track url",\n        bitrate="FLAC" # MP3_320 / MP3_256 / MP3_128 (optional)\n    )\n\nDisclaimer\n----------\n| Deethon - Python3 library to download music from Deezer\n| Copyright (C) 2020  Aykut Yilmaz\n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License\nalong with this program.  If not, see https://www.gnu.org/licenses/.\n\nDo not use this package illegaly and against Deezer\'s `Terms Of Use`_.\n\n.. _Deezer: https://www.deezer.com\n.. _Terms Of Use: https://www.deezer.com/legal/cgu/',
+    'author': 'Aykut Yilmaz',
+    'author_email': 'aykuxt@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/deethon/deethon',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
