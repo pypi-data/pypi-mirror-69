f19ed88cc63a9a913a404eafc9c458fc5c003cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:42 +0000

    Add package epytech/0.6/epytech-0.6.tar.gz

---
+++ packages/epytech/epytech-0.6.tar.gz/epytech-0.6/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+s = setup(
+    name = "epytech",
+    version = "0.6",
+    license = "MIT",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',  # This is important!
+    description = "The python's api for epitech's intra",
+    url = "https://github.com/Neotoxic-off/epytch",
+    packages = find_packages(),
+    install_requires = [],
+    python_requires = ">= 3.4",
+    author = "Neo",
+    author_email = "paul.gardien@epitech.eu",
+    )
