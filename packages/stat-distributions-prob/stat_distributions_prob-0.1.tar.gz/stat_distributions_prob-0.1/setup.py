a01d25f2d2fbf92aadd1ee5ca690890bb0fa95bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:32 +0000

    Add package stat-distributions-prob/0.1/stat_distributions_prob-0.1.tar.gz

---
+++ packages/stat-distributions-prob/stat_distributions_prob-0.1.tar.gz/stat_distributions_prob-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='stat_distributions_prob',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['stat_distributions_prob'],
+      author ='Philip Wong',
+      zip_safe=False)
