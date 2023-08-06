337d982144756abd4f790dfcfe9e112019915e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package munin2smartphone/0.0.3/munin2smartphone-0.0.3.tar.gz

---
+++ packages/munin2smartphone/munin2smartphone-0.0.3.tar.gz/munin2smartphone-0.0.3/munin2smartphone/__init__.py
@@ -0,0 +1,16 @@
+"""munin2widget
+
+HTTP server converting munin-json to html reports
+that you can display on your smartphone (html widget)
+
+"""
+
+APP_NAME = 'munin2smartphone'
+
+APP_DESCRIPTION = (
+    "HTTP server converting munin-json to html reports "
+    "that you can display on your smartphone (html widget)"
+)
+
+
+__all__ = ("APP_NAME", "APP_DESCRIPTION")
