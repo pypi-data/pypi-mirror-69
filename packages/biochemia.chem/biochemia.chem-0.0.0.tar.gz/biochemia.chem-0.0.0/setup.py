3abecad943b3cd67cc9183766f7d3ca6d6c5b883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:02 +0000

    Add package biochemia.chem/0.0.0/biochemia.chem-0.0.0.tar.gz

---
+++ packages/biochemia.chem/biochemia.chem-0.0.0.tar.gz/biochemia.chem-0.0.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="biochemia.chem",
+    author='Genadij Razdorov',
+    author_email='genadijrazdorov@gmail.com',
+    url='https://biochemia.bitbucket.io/'
+)
