fa34c5f8213d7927c3724946898f3ab5c725d861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:34 +0000

    Add package Automail/0.1.3/Automail-0.1.3.tar.gz

---
+++ packages/Automail/Automail-0.1.3.tar.gz/Automail-0.1.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md","r") as fh:
+	long_description = fh.read()
+
+classifiers=[
+		"Environment :: Console",
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"Topic :: Communications :: Email",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	]
+
+setup(
+	name = "Automail",
+	version = "0.1.3",
+	description ="sends bulk automated emails",
+	py_modules = ["Automail"],
+	package_dir = {'':'src'},
+	data_files = [("", ["LICENSE.txt"])],
+	License = "MIT",
+	classifiers=classifiers,
+	keywords = "Email",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = 'https://github.com/Preetam2114/Automail',
+	author = "Preetam Rane",
+	author_email = "preetamrane2114@gmail.com",
+	packages=find_packages(),
+	zip_safe=False,
+) 
