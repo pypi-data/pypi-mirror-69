ea62292de7aa3c7ed77dbdd337efe74c307bc394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:18 +0000

    Add package az-distributions/0.1/az_distributions-0.1.tar.gz

---
+++ packages/az-distributions/az_distributions-0.1.tar.gz/az_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='az_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['az_distributions'],
+      zip_safe=False)
