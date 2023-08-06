bb6b80712630ad0bf28e4174466d261125a60bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:28 +0000

    Add package apter/0.8/apter-0.8.tar.gz

---
+++ packages/apter/apter-0.8.tar.gz/apter-0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+APP_NAME = 'apter'
+
+# noinspection SpellCheckingInspection
+setup(
+    name=APP_NAME,
+    # version managed by release script
+    version='0.8',
+    description='A sweet project configuration package',
+    url='https://github.com/ulygit/apter',
+    author='U. Melendez',
+    author_email='apterpy@bfjournal.com',
+    license='MIT',
+    packages=find_packages(exclude=['test']),
+    py_modules=['apter'],
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest'
+    ],
+    install_requires=[
+        'parse', 'PyYaml'
+    ],
+)
