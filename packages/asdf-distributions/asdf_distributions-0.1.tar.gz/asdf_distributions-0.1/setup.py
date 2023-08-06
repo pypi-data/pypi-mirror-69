421a0816850ee3327bed08c0deb60c6924cfa60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:53 +0000

    Add package asdf-distributions/0.1/asdf_distributions-0.1.tar.gz

---
+++ packages/asdf-distributions/asdf_distributions-0.1.tar.gz/asdf_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='asdf_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['asdf_distributions'],
+      author= 'Felipe C',
+      zip_safe=False)
