982e7fdc2c5a5189ae3dab2e40c32939730b6719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:58 +0000

    Add package distofgaussbinmadebyashu/0.1/distofgaussbinmadebyashu-0.1.tar.gz

---
+++ packages/distofgaussbinmadebyashu/distofgaussbinmadebyashu-0.1.tar.gz/distofgaussbinmadebyashu-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distofgaussbinmadebyashu',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distofgaussbinmadebyashu'],
+      zip_safe=False)
