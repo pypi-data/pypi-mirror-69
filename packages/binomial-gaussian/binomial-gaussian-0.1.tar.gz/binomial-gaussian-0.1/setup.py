55d63e4bb7d0144af6dde2b6bffcab329b6f1c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:54 +0000

    Add package binomial-gaussian/0.1/binomial-gaussian-0.1.tar.gz

---
+++ packages/binomial-gaussian/binomial-gaussian-0.1.tar.gz/binomial-gaussian-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binomial-gaussian',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['binomial-gaussian'],
+      zip_safe=False)
