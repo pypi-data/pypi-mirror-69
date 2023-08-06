b782bba37fd5ab89c0b050fc530deb15afa42ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:20 +0000

    Add package mghari-distributions/0.1/mghari_distributions-0.1.tar.gz

---
+++ packages/mghari-distributions/mghari_distributions-0.1.tar.gz/mghari_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mghari_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mghari_distributions'],
+      author = 'Mohammed Mghari',
+      author_email = 'med.mghari@gmail.com',
+      zip_safe=False)
