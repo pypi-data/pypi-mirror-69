c0b1bffd5ea283273adfd8db28e5f686f8b10e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:05 +0000

    Add package gauss-binom-test/0.2.1/gauss_binom_test-0.2.1.tar.gz

---
+++ packages/gauss-binom-test/gauss_binom_test-0.2.1.tar.gz/gauss_binom_test-0.2.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gauss_binom_test',
+      version='0.2.1',
+      description='Gaussian and Binomial distributions',
+      license='MIT',
+      author='ludovico',
+      packages=['gauss_binom_test'],
+      zip_safe=False)
