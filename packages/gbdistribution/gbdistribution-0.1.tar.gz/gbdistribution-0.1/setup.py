f354c71c62486899d27fc4fadf767b72b339ccee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:48 +0000

    Add package gbdistribution/0.1/gbdistribution-0.1.tar.gz

---
+++ packages/gbdistribution/gbdistribution-0.1.tar.gz/gbdistribution-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gbdistribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gbdistributions'],
+      author = 'SowmiyaNarayanan G',
+      zip_safe=False)
