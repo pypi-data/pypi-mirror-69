6a2319ae0a4b13ac2bb992777cdc25ff816c3812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:17 +0000

    Add package bip44/0.0.2/bip44-0.0.2.tar.gz

---
+++ packages/bip44/bip44-0.0.2.tar.gz/bip44-0.0.2/bip44/utils.py
@@ -0,0 +1,16 @@
+from coincurve import PublicKey
+from sha3 import keccak_256 as _keccak_256
+
+__all__ = ("keccak_256", "get_eth_addr")
+
+
+def keccak_256(b: bytes) -> bytes:
+    h = _keccak_256()
+    h.update(b)
+    return h.digest()
+
+
+def get_eth_addr(pk: bytes) -> str:
+    if len(pk) != 64:
+        pk = PublicKey(pk).format(False)[1:]
+    return f"0x{keccak_256(pk)[-20:].hex()}"
