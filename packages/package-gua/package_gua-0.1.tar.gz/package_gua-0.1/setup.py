609e64324079efc9e2531ed2b9f5386a25695a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:47 +0000

    Add package package-gua/0.1/package_gua-0.1.tar.gz

---
+++ packages/package-gua/package_gua-0.1.tar.gz/package_gua-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='package_gua',
+      version='0.1',
+      description='package_gua',
+      packages=['package_gua'],
+      author = 'Andrew',
+      author_name = 'myemail@something.com',
+      zip_safe=False)
