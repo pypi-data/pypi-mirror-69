d0576614d65e76e8d00ab09583ae29350b7ae9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:53 +0000

    Add package pydistro/0.1/pydistro-0.1.tar.gz

---
+++ packages/pydistro/pydistro-0.1.tar.gz/pydistro-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pydistro',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['pydistro'],
+      zip_safe=False,
+      author='Praful Mohanan',
+      author_email = 'praful.mohanan@gmail.com')
