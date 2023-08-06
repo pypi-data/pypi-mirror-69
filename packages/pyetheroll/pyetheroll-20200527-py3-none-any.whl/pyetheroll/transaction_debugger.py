f569ac2c7ba1fec273b068851bd33f02fcfb324c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:44 +0000

    Add package pyetheroll/20200527/pyetheroll-20200527-py3-none-any.whl

---
+++ packages/pyetheroll/pyetheroll-20200527-py3-none-any.whl/pyetheroll/transaction_debugger.py
@@ -0,0 +1,154 @@
+import json
+
+from eth_abi import decode_abi
+from eth_utils import decode_hex, function_abi_to_4byte_selector
+from web3 import HTTPProvider, Web3
+
+from pyetheroll.constants import ChainID
+from pyetheroll.etherscan_utils import ChainEtherscanContractFactory
+from pyetheroll.utils import get_etherscan_api_key, get_infura_project_id
+
+
+def decode_contract_call(contract_abi: list, call_data: str):
+    """https://ethereum.stackexchange.com/a/33887/34898"""
+    call_data = call_data.lower().replace("0x", "")
+    call_data_bin = decode_hex(call_data)
+    method_signature = call_data_bin[:4]
+    function_descriptions = filter(
+        lambda x: x.get("type") == "function", contract_abi
+    )
+    description = next(
+        filter(
+            lambda description: function_abi_to_4byte_selector(description)
+            == method_signature,
+            function_descriptions,
+        )
+    )
+    method_name = description["name"]
+    arg_types = [item["type"] for item in description["inputs"]]
+    args = decode_abi(arg_types, call_data_bin[4:])
+    return (method_name, args)
+
+
+class HTTPProviderFactory:
+
+    PROVIDER_URLS = {
+        # ChainID.MAINNET: 'https://api.myetherapi.com/eth',
+        # ChainID.MAINNET: 'https://api.infura.io/v1/jsonrpc/mainnet',
+        # ChainID.MAINNET: 'https://api.mycryptoapi.com/eth',
+        ChainID.MAINNET: (
+            f"https://mainnet.infura.io/v3/{get_infura_project_id()}"
+        ),
+        # ChainID.ROPSTEN: 'https://api.myetherapi.com/rop',
+        # ChainID.ROPSTEN: 'https://api.infura.io/v1/jsonrpc/ropsten',
+        ChainID.ROPSTEN: (
+            f"https://ropsten.infura.io/v3/{get_infura_project_id()}"
+        ),
+    }
+
+    @classmethod
+    def create(cls, chain_id=ChainID.MAINNET):
+        url = cls.PROVIDER_URLS[chain_id]
+        return HTTPProvider(url)
+
+
+class TransactionDebugger:
+    def __init__(self, contract_abi):
+        self.contract_abi = contract_abi
+        self._methods_infos = None
+
+    @staticmethod
+    def get_contract_abi(chain_id, contract_address) -> dict:
+        """
+        Given a contract address returns the contract ABI from Etherscan,
+        refs #2
+        """
+        api_key = get_etherscan_api_key()
+        ChainEtherscanContract = ChainEtherscanContractFactory.create(chain_id)
+        api = ChainEtherscanContract(address=contract_address, api_key=api_key)
+        json_abi = api.get_abi()
+        abi = json.loads(json_abi)
+        return abi
+
+    @staticmethod
+    def get_methods_infos(contract_abi):
+        """List of infos for each events."""
+        methods_infos = {}
+        # only retrieves functions and events, other existing types are:
+        # "fallback" and "constructor"
+        types = ["function", "event"]
+        methods = [a for a in contract_abi if a["type"] in types]
+        for description in methods:
+            method_name = description["name"]
+            types = ",".join([x["type"] for x in description["inputs"]])
+            event_definition = "%s(%s)" % (method_name, types)
+            event_sha3 = Web3.keccak(text=event_definition)
+            method_info = {
+                "definition": event_definition,
+                "sha3": event_sha3,
+                "abi": description,
+            }
+            methods_infos = dict(methods_infos, **{method_name: method_info})
+        return methods_infos
+
+    @property
+    def methods_infos(self):
+        """Cached property so it's computed once."""
+        if not self._methods_infos:
+            self._methods_infos = self.get_methods_infos(self.contract_abi)
+        return self._methods_infos
+
+    def decode_method(self, topics, log_data):
+        """Given a topic and log data, decode the event."""
+        topic = topics[0]
+        # each indexed field generates a new topics and is excluded from data
+        # hence we consider topics[1:] like data, assuming indexed fields
+        # always come first
+        # see https://codeburst.io/deep-dive-into-ethereum-logs-a8d2047c7371
+        topics_log_data = b"".join(topics[1:])
+        log_data = log_data.lower().replace("0x", "")
+        log_data = bytes.fromhex(log_data)
+        topics_log_data += log_data
+        method_info = next(
+            filter(
+                lambda info: info["sha3"].lower() == topic.lower(),
+                self.methods_infos.values(),
+            )
+        )
+        event_inputs = method_info["abi"]["inputs"]
+        types = [e_input["type"] for e_input in event_inputs]
+        # hot patching `bytes` type to replace it with bytes32 since the former
+        # is crashing with `InsufficientDataBytes` during `LogResult` decoding.
+        types = ["bytes32" if t == "bytes" else t for t in types]
+        names = [e_input["name"] for e_input in event_inputs]
+        values = decode_abi(types, topics_log_data)
+        call = {name: value for name, value in zip(names, values)}
+        decoded_method = {"method_info": method_info, "call": call}
+        return decoded_method
+
+    @classmethod
+    def decode_transaction_log(cls, chain_id, log):
+        """
+        Given a transaction event log.
+        1) downloads the ABI associated to the recipient address
+        2) uses it to decode methods calls
+        """
+        contract_address = log.address
+        contract_abi = cls.get_contract_abi(chain_id, contract_address)
+        transaction_debugger = cls(contract_abi)
+        topics = log.topics
+        log_data = log.data
+        decoded_method = transaction_debugger.decode_method(topics, log_data)
+        return decoded_method
+
+    @classmethod
+    def decode_transaction_logs(cls, chain_id, transaction_hash):
+        """Given a transaction hash, reads and decode the event log."""
+        provider = HTTPProviderFactory.create(chain_id)
+        web3 = Web3(provider)
+        transaction_receipt = web3.eth.getTransactionReceipt(transaction_hash)
+        logs = transaction_receipt.logs
+        decoded_methods = tuple(
+            cls.decode_transaction_log(chain_id, log) for log in logs
+        )
+        return decoded_methods
---
+++ packages/pyetheroll/pyetheroll-20200527-py3-none-any.whl/pyetheroll/utils.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime
+
+from pyetheroll.constants import (
+    DEFAULT_ETHERSCAN_API_KEY,
+    DEFAULT_INFURA_PROJECT_ID,
+    ROUND_DIGITS,
+)
+
+
+class EtherollUtils:
+    @staticmethod
+    def compute_profit(bet_size, chances_win):
+        """Helper method to compute profit given a bet_size and chances_win."""
+        if chances_win <= 0 or chances_win >= 100:
+            return
+        house_edge = 1.0 / 100
+        chances_loss = 100 - chances_win
+        payout = ((chances_loss / chances_win) * bet_size) + bet_size
+        payout *= 1 - house_edge
+        profit = payout - bet_size
+        profit = round(profit, ROUND_DIGITS)
+        return profit
+
+
+def timestamp2datetime(timestamp: str) -> datetime:
+    """
+    Converts a timestamp string to datetime.
+    Handles both decimal and hexadecimal bases, this is needed since Etherscan
+    API can return timestamps in both bases.
+    https://www.reddit.com/r/etherscan/comments/cus1m8/
+    >>> timestamp2datetime('1566645978')
+    datetime.datetime(2019, 8, 24, 11, 26, 18)
+    >>> timestamp2datetime('0x5d611eda')
+    datetime.datetime(2019, 8, 24, 11, 26, 18)
+    """
+    base = 16 if timestamp.startswith("0x") else 10
+    date_time = datetime.utcfromtimestamp(int(timestamp, base))
+    return date_time
+
+
+def get_etherscan_api_key():
+    """Returns ETHERSCAN_API_KEY from environment variable."""
+    return os.environ.get("ETHERSCAN_API_KEY", DEFAULT_ETHERSCAN_API_KEY)
+
+
+def get_infura_project_id():
+    """Returns WEB3_INFURA_PROJECT_ID from environment variable."""
+    return os.environ.get("WEB3_INFURA_PROJECT_ID", DEFAULT_INFURA_PROJECT_ID)
