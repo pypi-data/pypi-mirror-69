6a1fafc3b20c6e06f29d757b685a0857fba1879a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:38 +0000

    Add package Cyclic-Voltammogram/0.1/Cyclic_Voltammogram-0.1.tar.gz

---
+++ packages/Cyclic-Voltammogram/Cyclic_Voltammogram-0.1.tar.gz/Cyclic_Voltammogram-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Cyclic_Voltammogram',
+      version='0.1',
+      description='Determine Peaks of Cyclic Voltammogram',
+      packages=['voltammogram'],
+      zip_safe=False)
