459141b13f96f5498e9de4ef3d675d460bb069e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:30 +0000

    Add package davar/0.2.0/davar-0.2.0-py3-none-any.whl

---
+++ packages/davar/davar-0.2.0-py3-none-any.whl/davar/__main__.py
@@ -0,0 +1,20 @@
+import argparse
+from davar.utils import Davar
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Command line tool for the davar experimental intepreted IAL."
+    )
+    parser.add_argument("davartext", metavar="DAVARTEXT", type=str)
+    parser.add_argument(
+        "-l", "--lang", required=True, default=None, help="2 character language code.",
+    )
+
+    args = parser.parse_args()
+    for s in Davar.from_davartext(args.davartext).describe(args.lang):
+        print(s)
+
+
+if __name__ == "__main__":
+    main()
