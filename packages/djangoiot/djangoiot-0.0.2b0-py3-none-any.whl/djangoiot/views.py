46930f407ecb37f3de2a236f7ea389a3bc05cbe3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:48 +0000

    Add package djangoiot/0.0.2b0/djangoiot-0.0.2b0-py3-none-any.whl

---
+++ packages/djangoiot/djangoiot-0.0.2b0-py3-none-any.whl/djangoiot/views.py
@@ -0,0 +1,12 @@
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class Brokers(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        data = {"date": "test"}
+        return Response(data, status.HTTP_200_OK)
