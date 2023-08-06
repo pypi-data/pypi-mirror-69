3db1d3a55136401e8108b0141ed785fdb5eeab60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:24 +0000

    Add package pypmatrix/0.1/pypmatrix-0.1.tar.gz

---
+++ packages/pypmatrix/pypmatrix-0.1.tar.gz/pypmatrix-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pypmatrix',
+      version='0.1',
+      description='Package to manage and manipulate matrices',
+      url='https://github.com/gurneetbhatia/pypmatrix',
+      author='Gurneet Bhatia',
+      author_email='sbgurneet@gmail.com',
+      license='MIT',
+      packages=['pypmatrix'],
+      zip_safe=False)
