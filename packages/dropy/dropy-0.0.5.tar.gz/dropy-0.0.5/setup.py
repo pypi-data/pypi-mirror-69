331cbc23a2f8708dc9184a7c734e57eb7f6d7608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:19 +0000

    Add package dropy/0.0.5/dropy-0.0.5.tar.gz

---
+++ packages/dropy/dropy-0.0.5.tar.gz/dropy-0.0.5/setup.py
@@ -0,0 +1,30 @@
+
+from distutils.core import setup
+setup(
+  name = 'dropy',
+  packages = ['dropy'],
+  version = '0.0.5',
+  license='MIT',
+  description = 'A simple package for a simple drone simulator created in unity',
+  author = 'Aousnik Gupta',
+  author_email = 'guptaaousnik@gmail.com',
+  url = 'https://github.com/gittygupta/dropy',
+  download_url = 'https://github.com/gittygupta/dropy/archive/v0.0.5.tar.gz',
+  keywords = ['simulator', 'python', 'unity', 'flight', 'control', 'NN', 'training', 'utility'],
+  install_requires=[
+          'validators',
+          'pillow',
+          'pywinauto',
+          'pyautogui'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
