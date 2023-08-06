85bd2d8a007b0b78c0dfd9fe252cc0e916292f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:46 +0000

    Add package genome2json/0.0.6/genome2json-0.0.6.tar.gz

---
+++ packages/genome2json/genome2json-0.0.6.tar.gz/genome2json-0.0.6/g2j/gff3.py
@@ -0,0 +1,117 @@
+"""GFF3 parser."""
+
+
+import re
+
+from collections import defaultdict
+
+from g2j import fasta
+from g2j.classes import Feature, Scaffold, Organism, Interval, Location
+
+
+def parse_attributes(string):
+    """Parse keyword:value pairs from the attributes field.
+
+    Uses regular expression to allow for different formatting.
+
+    For example, a GFF3 from JGI is formatted like:
+        name "gm1.1_g"; proteinId 412464; exonNumber 3
+
+    From FungiDB:
+        ID=AN11191-T-p1-CDS1;Parent=AN11191-T
+
+    From NCBI:
+        ID=id1;Parent=rna0;Dbxref=GeneID:36531755
+    """
+    pattern = re.compile(r' ?(\w+?)[ =]"?(.+?)"?(?:;|$)')
+    attributes = {}
+    for match in pattern.finditer(string):
+        key, value = match.groups()
+        attributes.update({key: value})
+    return attributes
+
+
+def set_partiality(feature):
+    """
+
+    """
+    partial = "partial" in feature.qualifiers
+    start_range = "start_range" in feature.qualifiers
+    end_range = "end_range" in feature.qualifiers
+
+    if not (partial and (start_range or end_range)):
+        return
+
+    if feature.location.strand == "+":
+        feature.location.three_is_partial = end_range
+        feature.location.five_is_partial = start_range
+    else:
+        feature.location.three_is_partial = start_range
+        feature.location.five_is_partial = end_range
+
+
+def parse(
+    gff_handle,
+    fasta_handle,
+    name=None,
+    strain=None,
+    feature_types=None,
+    save_scaffold_sequence=True,
+):
+    """Parse a GFF3 file.
+
+    Note that individual rows are treated as separate features. For example, a
+    CDS split over 5 rows will result in 5 separate Feature objects.
+    """
+
+    scaffolds = defaultdict(list)
+    feature = None
+
+    for line in gff_handle:
+        if line.startswith("#"):
+            continue
+
+        scaffold, *fields = line.strip().split("\t")
+
+        assert len(fields) == 8, "Malformed GFF, field number mismatch"
+
+        _, type, start, end, _, strand, phase, attributes = fields
+
+        if feature_types and type not in feature_types:
+            continue
+
+        feature = Feature(type)
+
+        # Location
+        if phase.isdigit():
+            phase = int(phase)
+        interval = Interval(int(start) - 1, int(end), phase=phase)
+        feature.location.intervals.append(interval)
+        feature.location.strand = strand
+
+        # Any extra saved attributes
+        feature.qualifiers.update(parse_attributes(attributes))
+
+        # Partiality, assume non-partial but NCBI files will have partial=true,
+        # start/end_range qualifiers
+        set_partiality(feature)
+
+        # Save to scaffold
+        scaffolds[scaffold].append(feature)
+
+    # Get assembly scaffold sequences from FASTA file
+    if save_scaffold_sequence:
+        sequences = fasta.parse(fasta_handle)
+
+    # Instantiate and return Organism
+    return Organism(
+        name,
+        strain,
+        scaffolds=[
+            Scaffold(
+                accession,
+                sequences[accession] if save_scaffold_sequence else "",
+                features)
+            for accession, features in scaffolds.items()
+        ],
+    )
---
+++ packages/genome2json/genome2json-0.0.6.tar.gz/genome2json-0.0.6/g2j/grouping.py
@@ -0,0 +1,201 @@
+"""Filter features."""
+
+
+from collections import defaultdict
+from itertools import groupby, combinations
+
+
+def split_genic_features(features):
+    genic, other = [], []
+    for feature in features:
+        if feature.is_genic():
+            genic.append(feature)
+        else:
+            other.append(feature)
+    return genic, other
+
+
+def attribute_match(one, two):
+    """Find a matching attribute in two Features."""
+    tags = {
+        "proteinId",
+        "protein_id",
+        "transcriptId",
+        "transcript_id",
+        "name",
+        "Name",
+        "ID",
+        "locus_tag",
+        "Parent",
+        "protein_source_id",
+    }
+    common = tags & set(one.qualifiers).intersection(two.qualifiers)
+    if not common:
+        return False
+    return any(one.qualifiers[key] == two.qualifiers[key] for key in common)
+
+
+def link_features(features):
+    """Find feature groups by single linkage."""
+
+    tags = {
+        "proteinId",
+        "protein_id",
+        "transcriptId",
+        "transcript_id",
+        "name",
+        "Name",
+        "ID",
+        "locus_tag",
+        "Parent",
+    }
+
+    def get_values(f):
+        return set(f.qualifiers[t] for t in tags.intersection(f.qualifiers))
+
+    def adjacency(features):
+        """Generate adjacency list of Features.
+        i.e. Pre-compute graph edges, so we can identify connected components
+        using DFS/BFS
+        """
+        graph = defaultdict(set)
+        total = len(features)
+        values = [get_values(f) for f in features]
+        for i, j in combinations(range(total), 2):
+            if i != j and not values[i].isdisjoint(values[j]):
+                graph[i].add(j)
+                graph[j].add(i)
+        return graph
+
+    def dfs(graph, start, visited=None):
+        """Depth-first search an adjacency list."""
+        if visited is None:
+            visited = set()
+        visited.add(start)
+        for next in graph[start] - visited:
+            dfs(graph, next, visited)
+        return visited
+
+    # Build adjacency list of features
+    graph, groups = adjacency(features), []
+
+    # Depth first search; related nodes are marked as visited.
+    # Save group, remove indices from the graph, repeat until
+    # there is nothing left in the graph.
+    while graph:
+        group = dfs(graph, list(graph)[0])
+        for index in group:
+            graph.pop(index)
+        groups.append([features[i] for i in group])
+
+    return groups
+
+
+def iter_overlapping_features(features, check_attributes=False):
+    """Group overlapping sequence features."""
+
+    if not features:
+        return
+
+    sorted_features = sorted(features, key=lambda f: f.location.min())
+    first = sorted_features.pop(0)
+    group, border = [first], first.location.max()
+
+    for feature in sorted_features:
+        within_border = feature.location.min() <= border
+        shared_attrib = check_attributes and attribute_match(group[-1], feature)
+
+        if within_border or shared_attrib:
+            group.append(feature)
+            border = max(border, feature.location.max())
+        else:
+            yield group
+            group, border = [feature], feature.location.max()
+
+    yield group
+
+
+def group_overlapping_features(features, check_attributes=False):
+    """Find grouped sequence features on forward and reverse strand."""
+    forward, reverse = [], []
+    for feature in features:
+        if feature.location.strand == "+":
+            forward.append(feature)
+        else:
+            reverse.append(feature)
+    forward = list(iter_overlapping_features(forward, check_attributes))
+    reverse = list(iter_overlapping_features(reverse, check_attributes))
+    return forward + reverse
+
+
+def iter_collapsed_features(features):
+    """Find collapsible feature groups.
+    Able to dig out overlapping same-type features this way.
+    """
+    result, previous = [], None
+    for feature in features:
+        if previous and attribute_match(previous, feature):
+            result[-1].append(feature)
+        else:
+            result.append([feature])
+        previous = feature
+    return result
+
+
+def collapse_same_type_features(features):
+    """Combine features of the same type within an overlap group."""
+
+    final = []
+    feats = sorted(features, key=lambda f: f.type)
+
+    for _, type_group in groupby(feats, key=lambda f: f.type):
+        type_group = list(type_group)
+
+        if len(type_group) == 1:
+            final.extend(type_group)
+            continue
+
+        # To collapse, require at least one matching attribute
+        # e.g. multiple CDS should share the same ID | Parent | protein_id
+        for group in iter_collapsed_features(type_group):
+            first, *rest = group
+            for feature in rest:
+                first += feature
+            final.append(first)
+
+    # Now, group different type Feature objects by attribute_link
+    return final
+
+
+def get_minimum(feature):
+    """Get minimum of either a single Feature or Feature group."""
+    if hasattr(feature, "location"):
+        return feature.location.min()
+    return min(f.location.min() for f in feature)
+
+
+def get_maximum(feature):
+    """Get maximum of either a single Feature or Feature group."""
+    if hasattr(feature, "location"):
+        return feature.location.max()
+    return min(f.location.max() for f in feature)
+
+
+def get_length(feature):
+    """Get length of a feature."""
+    if hasattr(feature, "location"):
+        return len(feature.location)
+    return get_maximum(feature) - get_minimum(feature)
+
+
+def group_features(features, check_attributes=True):
+    """Perform grouping on a collection of Feature objects."""
+    genic, other = split_genic_features(features)
+    results = []
+    for group in group_overlapping_features(genic, check_attributes):
+        if not group:
+            continue
+        subgroups = link_features(group)
+        for subgroup in subgroups:
+            results.append(collapse_same_type_features(subgroup))
+    return sorted(results + other, key=lambda f: get_minimum(f))
