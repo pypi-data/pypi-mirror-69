8466d4dc96d20d681b5868042545bbdad1bde8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:53 +0000

    Add package uuid-distributions/0.1/uuid-distributions-0.1.tar.gz

---
+++ packages/uuid-distributions/uuid-distributions-0.1.tar.gz/uuid-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='uuid-distributions',
+      version='0.1',
+      description='uuid-Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
