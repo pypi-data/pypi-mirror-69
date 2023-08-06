dbc9d91289748dce1f3cad9f0517b4327424b89c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:56 +0000

    Add package GaussBinoDist/0.1/GaussBinoDist-0.1.tar.gz

---
+++ packages/GaussBinoDist/GaussBinoDist-0.1.tar.gz/GaussBinoDist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GaussBinoDist',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['GaussBinoDist'],
+      author='Mohak Ashutosh Trivedi',
+      author_email='trivedimohak@gmail.com',
+      zip_safe=False)
