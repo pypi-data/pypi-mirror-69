1b02271efa433cb1052386eacafef937c4c9b96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:03 +0000

    Add package nester-lazy-coder48/1.0.2/nester_lazy_coder48-1.0.2.tar.gz

---
+++ packages/nester-lazy-coder48/nester_lazy_coder48-1.0.2.tar.gz/nester_lazy_coder48-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nester_lazy_coder48",  # Replace with your own username
+    version="1.0.2",
+    author="lazy_coder48",
+    author_email="ajitesh.newnaha@outlook.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
