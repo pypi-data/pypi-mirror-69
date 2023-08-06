283c792dbffa123e4988db8af7f2a8dbadcc45c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:35 +0000

    Add package Silence/0.3.0/Silence-0.3.0.tar.gz

---
+++ packages/Silence/Silence-0.3.0.tar.gz/Silence-0.3.0/silence/server/endpoint_loader.py
@@ -0,0 +1,33 @@
+import importlib
+
+from os import listdir
+from os.path import splitext
+
+from silence.server import manager, default_endpoints
+from silence.settings import settings
+
+###############################################################################
+# Look for .py files inside the project's "/api" folder
+# and force them to run, activating the @endpoint decorators
+###############################################################################
+
+def load_user_endpoints():
+    # Load every .py file inside the api/ folder
+    for pyfile in listdir("api"):
+        module_name = "api." + splitext(pyfile)[0]
+
+        try:
+            importlib.import_module(module_name)
+        except ImportError:
+            raise RuntimeError(f"Could not load the API file {module_name}")
+    
+###############################################################################
+# Register the Silence-provided endpoints
+###############################################################################
+def load_default_endpoints():
+    route_prefix = settings.API_PREFIX
+    if route_prefix.endswith("/"):
+        route_prefix = route_prefix[:-1]
+
+    manager.APP.add_url_rule(f"{route_prefix}/login", "login", default_endpoints.login, methods=["POST"])
+    manager.APP.add_url_rule(f"{route_prefix}/register", "register", default_endpoints.register, methods=["POST"])
---
+++ packages/Silence/Silence-0.3.0.tar.gz/Silence-0.3.0/silence/settings/default_settings.py
@@ -0,0 +1,32 @@
+###############################################################################
+# Default settings, which can be overriden by each individual project.
+###############################################################################
+
+DEBUG_ENABLED = False
+HTTP_PORT = 8080
+API_PREFIX = ""
+
+RUN_API = True
+RUN_WEB = True
+
+DB_CONN = {
+    "host": "localhost",
+    "port": 3306,
+    "username": "default_username",
+    "password": "default_password",
+    "database": "default_database",
+}
+
+USER_AUTH_DATA = {
+    "table": "users",
+    "identifier": "username",
+    "password": "password",
+}
+
+SECRET_KEY = "These are generated automatically for each project."
+MAX_TOKEN_AGE = 86400
+
+SQL_SCRIPTS = []
+
+PROJECT_TEMPLATE_NAME = "silence-app"
+PROJECT_TEMPLATE_REPO = "https://github.com/agu-borrego/Silence-project-template.git"
