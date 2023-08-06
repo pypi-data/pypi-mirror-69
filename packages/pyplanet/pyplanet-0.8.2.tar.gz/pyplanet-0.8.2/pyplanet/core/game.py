c279d7f8271e75e75a049c45dca2954f18dfb9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:13 +0000

    Add package pyplanet/0.8.2/pyplanet-0.8.2.tar.gz

---
+++ packages/pyplanet/pyplanet-0.8.2.tar.gz/pyplanet-0.8.2/pyplanet/core/game.py
@@ -0,0 +1,53 @@
+
+class _Game:
+	"""
+	The game class holds information about the game itself and the server. The properties can be virtually overriden
+	to be able to proxy to new/old syntaxes. This way we can provide a read-only data structure and still
+	maintain the same structure if any of the third party API changes.
+	"""
+	dedicated_build = None
+	dedicated_version = None
+	dedicated_api_version = None
+	dedicated_title = None
+
+	server_is_dedicated = None
+	server_is_server = None
+	server_is_private = None
+	server_ip = None
+	server_p2p_port = None
+	server_port = None
+	server_player_login = None
+	server_player_id = None
+	server_download_rate = None
+	server_upload_rate = None
+	server_data_dir = None
+	server_map_dir = None
+	server_skin_dir = None
+	server_language = None
+	server_name = None
+	server_path = None
+	server_password = None
+	server_spec_password = None
+	server_max_players = None
+	server_next_max_players = None
+	server_max_specs = None
+	server_next_max_specs = None
+
+	ladder_min = None
+	ladder_max = None
+
+	game = None  # tm / sm
+	#  game_long = None  # trackmania / shootmania
+
+	def game_from_environment(self, environment):
+		if environment in ['Canyon', 'Stadium', 'Valley', 'Lagoon']:
+			return 'tm'
+		return 'sm'
+
+	@property
+	def game_full(self):
+		if self.game == 'tm':
+			return 'trackmania'
+		return 'shootmania'
+
+Game = _Game()
