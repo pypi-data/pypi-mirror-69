78b8fe3413232fe9a96786d9c0e03466a7869022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:59 +0000

    Add package gean/0.0.3/gean-0.0.3.tar.gz

---
+++ packages/gean/gean-0.0.3.tar.gz/gean-0.0.3/setup.py
@@ -0,0 +1,26 @@
+# type: ignore
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gean",
+    version="0.0.3",
+    author="Gabriel Garcia",
+    author_email="me@garciat.com",
+    description="A minimal IOC container inspired by Spring",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/garciat/gean",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'pytest-mypy'],
+)
