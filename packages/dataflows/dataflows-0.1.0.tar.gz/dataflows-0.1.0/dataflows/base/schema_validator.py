dc6cf04938541f427a8f1d5f5b0039ea47d79521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:58 +0000

    Add package dataflows/0.1.0/dataflows-0.1.0.tar.gz

---
+++ packages/dataflows/dataflows-0.1.0.tar.gz/dataflows-0.1.0/dataflows/base/schema_validator.py
@@ -0,0 +1,59 @@
+from datapackage import Resource
+from tableschema import Schema
+from tableschema.exceptions import CastError
+
+
+class ValidationError(Exception):
+
+    def __init__(self, resource_name, row, index, cast_error):
+        msg = '\nROW: %r\n----\n' % row
+        if cast_error is not None and hasattr(cast_error, 'errors'):
+            msg += '\n'.join('%d) %s' % (i+1, err)
+                            for i, err
+                            in enumerate(cast_error.errors))
+        super().__init__(msg)
+        self.resource_name = resource_name
+        self.row = row
+        self.index = index
+        self.cast_error = cast_error
+
+
+def raise_exception(res_name, row, i, e):
+    raise ValidationError(res_name, row, i, e)
+
+
+def ignore(res_name, row, i, e):
+    return True
+
+
+def drop(res_name, row, i, e):
+    return False
+
+
+def schema_validator(resource, iterator,
+                     field_names=None, on_error=None):
+    if on_error is None:
+        on_error = raise_exception
+    if isinstance(resource, Resource):
+        schema: Schema = resource.schema
+        assert schema is not None
+        resource = resource.descriptor
+    else:
+        schema: Schema = Schema(resource.get('schema', {}))
+    if field_names is None:
+        field_names = [f.name for f in schema.fields]
+    schema_fields = [f for f in schema.fields if f.name in field_names]
+    for i, row in enumerate(iterator):
+        try:
+            for f in schema_fields:
+                row[f.name] = f.cast_value(row.get(f.name))
+        except CastError as e:
+            if not on_error(resource['name'], row, i, e):
+                continue
+
+        yield row
+
+
+schema_validator.drop = drop
+schema_validator.ignore = ignore
+schema_validator.raise_exception = raise_exception
