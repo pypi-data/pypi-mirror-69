32b9728ca23f2259faa9ffbef425d642739a37a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:49 +0000

    Add package preserves/0.3.0/preserves-0.3.0-py3-none-any.whl

---
+++ packages/preserves/preserves-0.3.0-py3-none-any.whl/preserves/__init__.py
@@ -0,0 +1,10 @@
+from .preserves import Float, Symbol, Record, ImmutableDict
+
+from .preserves import DecodeError, EncodeError, ShortPacket
+
+from .preserves import Decoder, Encoder, decode, decode_with_annotations, encode
+
+from .preserves import Annotated, is_annotated, strip_annotations, annotate
+
+from .preserves import Stream, ValueStream, SequenceStream, SetStream, DictStream
+from .preserves import BinaryStream, StringStream, SymbolStream
