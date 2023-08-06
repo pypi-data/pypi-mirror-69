e7a61333b291fa7383f85bedc3e9397d519cc19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:06 +0000

    Add package DalineUnit/0.0.2/DalineUnit-0.0.2.tar.gz

---
+++ packages/DalineUnit/DalineUnit-0.0.2.tar.gz/DalineUnit-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DalineUnit",
+    version="0.0.2",
+    author="kun.z",
+    author_email="kun.z@daline.com.cn",
+    description="daline unit generator and runner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DalineWH",
+    packages=['DalineUnit'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='daline Unit',
+    install_requires=[
+
+    ],
+)
