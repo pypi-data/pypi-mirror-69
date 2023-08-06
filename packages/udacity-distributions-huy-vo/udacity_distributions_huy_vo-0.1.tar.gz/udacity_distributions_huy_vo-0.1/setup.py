d60e3bec79a4fc2e82497cf11f1c7d0d16b26b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:20 +0000

    Add package udacity-distributions-huy-vo/0.1/udacity_distributions_huy_vo-0.1.tar.gz

---
+++ packages/udacity-distributions-huy-vo/udacity_distributions_huy_vo-0.1.tar.gz/udacity_distributions_huy_vo-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udacity_distributions_huy_vo',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Huy Duc Vo',
+      author_email = 'hdvo1@uci.edu',
+     zip_safe=False)
