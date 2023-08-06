c76d983ad3f2ce684c386f04e27f358fade41899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:06 +0000

    Add package testdistributions/0.1/testdistributions-0.1.tar.gz

---
+++ packages/testdistributions/testdistributions-0.1.tar.gz/testdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='testdistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['testdistributions'],
+      zip_safe=False)
