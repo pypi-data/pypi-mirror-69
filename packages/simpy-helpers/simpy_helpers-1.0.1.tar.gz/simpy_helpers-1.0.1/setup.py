88fef0694e540a15fc637a0fce7467d5bf08da15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:08 +0000

    Add package simpy-helpers/1.0.1/simpy_helpers-1.0.1.tar.gz

---
+++ packages/simpy-helpers/simpy_helpers-1.0.1.tar.gz/simpy_helpers-1.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simpy_helpers",
+    version="1.0.1",
+    author="Brian Ambielli",
+    author_email="brian.ambielli@gmail.com",
+    description="A package to help simplify simpy simulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bambielli/simpy_helpers",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "simpy>=3,<=4",
+        "numpy"
+    ]
+)
