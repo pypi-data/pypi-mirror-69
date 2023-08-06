b72786c88666e616ae9bbd9e7922ca48202ffdfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:06 +0000

    Add package gaubidistributions/0.1/gaubidistributions-0.1.tar.gz

---
+++ packages/gaubidistributions/gaubidistributions-0.1.tar.gz/gaubidistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gaubidistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gaubidistributions'],
+      zip_safe=False)
