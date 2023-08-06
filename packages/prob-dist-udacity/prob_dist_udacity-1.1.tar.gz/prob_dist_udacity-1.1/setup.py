550510db46038d042e7d42713cb77042d710cb7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:07 +0000

    Add package prob-dist-udacity/1.1/prob_dist_udacity-1.1.tar.gz

---
+++ packages/prob-dist-udacity/prob_dist_udacity-1.1.tar.gz/prob_dist_udacity-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='prob_dist_udacity',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_dist_udacity'],
+      author='Sunil Kumar Vuppala',
+      author_email='sunilkumar.vuppala01@gmail.com',
+      zip_safe=False)
