9d9bd5577df7b913e8d6de57604d6442fa730576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:10 +0000

    Add package jerrypycia/0.1.0/jerryPycia-0.1.0.tar.gz

---
+++ packages/jerrypycia/jerryPycia-0.1.0.tar.gz/jerryPycia-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['jerrypycia']
+
+package_data = \
+{'': ['*'], 'jerrypycia': ['datasets/*']}
+
+install_requires = \
+['matplotlib>=3.2,<4.0', 'pandas>=1.0,<2.0']
+
+setup_kwargs = {
+    'name': 'jerrypycia',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Andrew Blance',
+    'author_email': 'andrewblance@live.co.uk',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
