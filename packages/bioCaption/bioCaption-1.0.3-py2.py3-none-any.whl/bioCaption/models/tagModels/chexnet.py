b42d5faf2ce1861f705b1ebc3aae350e0947610b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:03 +0000

    Add package bioCaption/1.0.3/bioCaption-1.0.3-py2.py3-none-any.whl

---
+++ packages/bioCaption/bioCaption-1.0.3-py2.py3-none-any.whl/bioCaption/models/tagModels/chexnet.py
@@ -0,0 +1,250 @@
+import json
+import math
+import os
+import sys
+
+import numpy as np
+from keras.applications.densenet import DenseNet121
+from keras.applications.densenet import preprocess_input
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.initializers import glorot_uniform
+from keras.layers import Dense
+from keras.models import Model
+from keras.preprocessing import image
+from tqdm import tqdm
+
+from bioCaption.models.tagModels.tag_models_evaluation import TagsEvaluation
+
+np.random.seed(42)
+sys.path.append("..")  # Adds higher directory to python modules path.
+
+
+class Chexnet:
+
+    def __init__(self, data_path, images_dir, results_dir,
+                 split_ratio=[0.5, 0.4, 0.1]):
+        """
+        :param data_path: Path to json training data file.
+        :param images_dir: : The folder in the dataset that contains the images
+         with the form: "[dataset_name]_images".
+        :param split_ratio: List of floats that represents the ratios of the
+        dataset to be used as train, test and validation. The default value is
+        [0.5, 0.4, 0.1]. The ratios must always sum to 1.0.
+        :param results_dir: The folder in which to save the results file. If it
+        doesn't exist, it get created automatically in the current working
+        directory.
+        """
+        self.images_dir = images_dir
+        self.results_dir = results_dir
+        self.train_data = {}
+        self.test_data = {}
+        self.val_data = {}
+        self.train_concepts = []
+        self.val_concepts = []
+        self.data_path = data_path
+        self.split_ratio = split_ratio
+        self._create_dataset(self.data_path)
+        self.model = None
+        if not os.path.exists(self.results_dir):
+            os.makedirs(self.results_dir)
+
+    def f1_evaluation(self, gt_pairs, candidate_pairs):
+        f1 = TagsEvaluation(gold_data=gt_pairs,
+                            result_data=candidate_pairs)
+        return f1.evaluate_f1()
+
+    def _create_dataset(self, path):
+        with open(path) as json_file:
+            data = json.load(json_file)
+            keys = list(data.keys())
+            train_pointer = math.ceil(self.split_ratio[0] * len(keys))
+            test_pointer = math.ceil(self.split_ratio[1] * len(keys))
+            val_pointer = math.ceil(self.split_ratio[2] * len(keys))
+            train_keys = keys[:train_pointer]
+            test_keys = keys[train_pointer:train_pointer + test_pointer]
+            val_keys = keys[
+                       train_pointer + test_pointer:val_pointer + train_pointer + test_pointer]
+            train, test, val = {}, {}, {}
+            train_concepts, val_concepts = [], []
+            for key in train_keys:
+                train[key] = data[key]
+                train_concepts.extend(data[key])
+            for key in test_keys:
+                test[key] = data[key]
+            for key in val_keys:
+                val[key] = data[key]
+                val_concepts.extend(data[key])
+            self.train_data = train
+            self.val_data = val
+            self.test_data = test
+            self.train_concepts = list(set(train_concepts))
+            self.val_concepts = list(set(val_concepts))
+
+    def load_data(self, data, concepts_list):
+        x_data, y_data = [], []
+        # read the data file
+        for img_id in tqdm(data.keys()):
+            image_path = os.path.join(self.images_dir, img_id)
+            img = image.load_img(image_path, target_size=(224, 224))
+            x = image.img_to_array(img)
+            x = preprocess_input(x)
+            # encode the tags
+            concepts = np.zeros(len(concepts_list), dtype=int)
+            if len(data[img_id]) != 0:
+                image_concepts = data[img_id]
+            else:
+                image_concepts = []
+            for i in range(0, len(concepts_list)):
+                # if the tag is assigned to the image put 1 in its position in the true binary vector
+                if concepts_list[i] in image_concepts:
+                    concepts[i] = 1
+            x_data.append(x)
+            y_data.append(concepts)
+        # creates images and labels
+        return np.array(x_data), np.array(y_data)
+
+    def load_images_ids(self, data):
+        x_images = []
+        x_images_ids = []
+        for img_id in tqdm(data):
+            image_path = os.path.join(self.images_dir, img_id)
+            img = image.load_img(image_path, target_size=(224, 224))
+            x = image.img_to_array(img)
+            x = preprocess_input(x)
+            x_images.append(x)
+            x_images_ids.append(img_id)
+        return np.array(x_images), x_images_ids
+
+    def train_generator(self, images, labels, batch_size, num_tags):
+        np.random.seed(42)
+        num_of_batches = math.ceil(len(images) / batch_size)
+        while True:
+            lists = list(zip(images, labels))
+            np.random.shuffle(lists)
+            images, labels = zip(*lists)
+            for batch in range(num_of_batches):
+                if len(images) - (batch * batch_size) < batch_size:
+                    current_batch_size = len(images) - (batch * batch_size)
+                else:
+                    current_batch_size = batch_size
+                batch_features = np.zeros((current_batch_size, 224, 224, 3))
+                batch_labels = np.zeros((current_batch_size, num_tags))
+
+                for i in range(batch_size):
+                    index = (batch * batch_size) + i
+                    if index < len(images):
+                        batch_features[i] = images[index]
+                        batch_labels[i] = labels[index]
+                yield batch_features, batch_labels
+
+    def val_generator(self, images, labels, batch_size, num_tags):
+        np.random.seed(42)
+        num_of_batches = math.ceil(len(images) / batch_size)
+        while True:
+            lists = list(zip(images, labels))
+            np.random.shuffle(lists)
+            images, labels = zip(*lists)
+            for batch in range(num_of_batches):
+                if len(images) - (batch * batch_size) < batch_size:
+                    current_batch_size = len(images) - (batch * batch_size)
+                else:
+                    current_batch_size = batch_size
+                batch_features = np.zeros((current_batch_size, 224, 224, 3))
+                batch_labels = np.zeros((current_batch_size, num_tags))
+
+                for i in range(batch_size):
+                    index = (batch * batch_size) + i
+                    if index < len(images):
+                        batch_features[i] = images[index]
+                        batch_labels[i] = labels[index]
+                yield batch_features, batch_labels
+
+    # The number of available concepts
+    def chexnet_model(self, num_tags):
+        my_init = glorot_uniform(seed=42)
+        base_model = DenseNet121(weights='imagenet', include_top=True)
+        x = base_model.get_layer("avg_pool").output
+        concept_outputs = Dense(num_tags, activation="sigmoid",
+                                name="concept_outputs",
+                                kernel_initializer=my_init)(x)
+        model = Model(inputs=base_model.input, outputs=concept_outputs)
+        model.compile(optimizer="adam", loss="binary_crossentropy",
+                      metrics=["binary_accuracy"])
+        self.model = model
+
+    def tune_decision_threshold(self):
+        pass
+
+    def chexnet(self, batch_size=100, epochs=2, model_path=None):
+        """
+        Train the chexnet model for medical image tagging.
+        :param batch_size: size of batches. Default is 100.
+        :param epochs: Training epochs. Default is 2.
+        :param model_path: Path were the best model will be stored.
+        """
+        num_tags = len(self.train_concepts)
+        print(num_tags)
+        self.chexnet_model(num_tags)
+        # add early stopping
+        early_stopping = EarlyStopping(monitor="val_loss", patience=3,
+                                       mode="auto", restore_best_weights=True)
+        # save best model
+        checkpoint = ModelCheckpoint(
+            os.path.join(model_path, "chexnet_checkpoint.hdf5"),
+            monitor="val_loss",
+            save_best_only=True,
+            mode="auto")
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1,
+                                      patience=1, verbose=1, mode="min")
+        # train the model
+        x_train, y_train = self.load_data(self.train_data, self.train_concepts)
+        x_val, y_val = self.load_data(self.val_data, self.train_concepts)
+        history = self.model.fit_generator(
+            self.train_generator(x_train, y_train, batch_size, num_tags),
+            steps_per_epoch=math.ceil(len(x_train) / batch_size),
+            epochs=epochs,
+            verbose=2,
+            callbacks=[early_stopping, checkpoint, reduce_lr],
+            validation_data=self.val_generator(x_val, y_val, batch_size,
+                                               num_tags),
+            validation_steps=math.ceil(len(x_val) / batch_size)
+        )
+
+    def load_trained_model(self, weights_path):
+        self.chexnet_model(len(self.train_concepts))
+        self.model.load_weights(weights_path)
+
+    def chexnet_test(self, decision_threshold=0.5, model_path=None):
+        """
+
+        :param decision_threshold: Decision threshold for chexnet classifier.
+        Default value is 0.5.
+        :param model_path: path where the model is stored. By default its value
+        is None and the chexnet_test is trying to use the value that is stored in
+        the 'model' attribute of the object. In case model_path is not None
+        then it replaces the value of the 'model' attribute into the
+        model that is loaded from the path
+        """
+        if model_path is not None:
+            self.load_trained_model(model_path)
+        print("Loading test images...")
+        test_images, img_ids = self.load_images_ids(self.test_data)
+
+        # get predictions for test
+        test_predictions = self.model.predict(test_images, batch_size=5,
+                                              verbose=1)
+        print("Got predictions for dev set")
+        results = {}
+        for i in range(len(test_predictions)):
+            concepts_pred = []
+            for j in range(len(self.train_concepts)):
+                if test_predictions[i, j] >= decision_threshold:
+                    concepts_pred.append(self.train_concepts[j])
+            results[img_ids[i]] = ";".join(concepts_pred)
+        # evaluate results
+        test_score = self.f1_evaluation(self.test_data, results)
+        print("The F1 score on test set is: ", test_score)
+        # save results
+        with open(os.path.join(self.results_dir, 'chexnet_results.json'),
+                  'w') as json_file:
+            json.dump(results, json_file)
