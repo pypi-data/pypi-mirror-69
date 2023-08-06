82b241c5ae9e72e18db720bc942780d2da026cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:56 +0000

    Add package Mopidy-Sangu/1.0.0rc1/Mopidy-Sangu-1.0.0rc1.tar.gz

---
+++ packages/Mopidy-Sangu/Mopidy-Sangu-1.0.0rc1.tar.gz/Mopidy-Sangu-1.0.0rc1/mopidy_sangu/static/flutter_service_worker.js
@@ -0,0 +1,42 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "favicon.ico": "874092d0198d11661312715c601739ce",
+"main.dart.js": "e940987e469737a3b1eb95478fa423e4",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/LICENSE": "3b7bd347677c32711fc3dd7be0b64f07",
+"assets/images/soundcloud.png": "d6e6ad2e333f4e31dcb72c1b12db56b2",
+"assets/images/spotify.png": "2de93a8478c8d2c30e7b689e08cb9d43",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"manifest.json": "a074344a7299b9f17490c0c7c1888e63",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"index.html": "ea57b96f92f958f18eb55c6687c265cc",
+"/": "ea57b96f92f958f18eb55c6687c265cc"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
