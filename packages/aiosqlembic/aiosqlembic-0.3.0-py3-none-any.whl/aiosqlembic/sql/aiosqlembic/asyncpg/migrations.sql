823a301f256d9a7afe4dbde7587f28e973de35bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:31 +0000

    Add package aiosqlembic/0.3.0/aiosqlembic-0.3.0-py3-none-any.whl

---
+++ packages/aiosqlembic/aiosqlembic-0.3.0-py3-none-any.whl/aiosqlembic/sql/aiosqlembic/asyncpg/migrations.sql
@@ -0,0 +1,23 @@
+--name: create_schema#
+create table public.aiosqlembic
+(
+    id         serial    not null,
+    version_id bigint    not null,
+    is_applied boolean   not null,
+    tstamp     timestamp null default now(),
+    primary key (id)
+);
+
+--name: insert_revision!
+insert into public.aiosqlembic(version_id, is_applied) values(:version_id, :is_applied);
+
+--name: get_version
+--record_class: Version
+select public.aiosqlembic.version_id, is_applied from public.aiosqlembic order by id desc;
+
+--name: migrate_revision^
+--record_class: MigrateRevision
+select tstamp, is_applied from public.aiosqlembic where version_id=:version_id order by tstamp desc limit 1;
+
+--name: delete_revision
+delete from public.aiosqlembic where version_id=:version_id;
