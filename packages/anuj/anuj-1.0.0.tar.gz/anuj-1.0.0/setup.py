7f18199775cd25cd61884c2c03ffc76c97c5ba9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:51 +0000

    Add package anuj/1.0.0/anuj-1.0.0.tar.gz

---
+++ packages/anuj/anuj-1.0.0.tar.gz/anuj-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='anuj',
+      version='1.0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['anuj'],
+      author = 'Anuj Jhamb',
+      author_email = 'anuj.jhamb23@gmail.com',
+      zip_safe=False)
