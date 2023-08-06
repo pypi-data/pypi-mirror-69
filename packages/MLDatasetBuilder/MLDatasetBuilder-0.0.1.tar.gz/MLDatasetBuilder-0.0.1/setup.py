4a02187c0a8e08ad9cec5f828f515ed82a261597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:01 +0000

    Add package MLDatasetBuilder/0.0.1/MLDatasetBuilder-0.0.1.tar.gz

---
+++ packages/MLDatasetBuilder/MLDatasetBuilder-0.0.1.tar.gz/MLDatasetBuilder-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+  name="MLDatasetBuilder",
+  version="0.0.1",
+  author='Karthick Nagarajan',
+	author_email='karthick965938@gmail.com',
+  description="Build a Package for Machine Learning Dataset",
+  long_description_content_type="text/markdown",
+  license='MIT',
+  url="https://github.com/karthick965938/ML-Dataset-Builder",
+  packages=['MLDatasetBuilder'],
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
