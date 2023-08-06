49749f0af30dff9411fa17fed890b87dff89a6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:55 +0000

    Add package itarj-console/1.0.1/itarj-console-1.0.1.tar.gz

---
+++ packages/itarj-console/itarj-console-1.0.1.tar.gz/itarj-console-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+        
+
+setup(
+    name ="itarj-console",
+    version = "1.0.1",
+    description="A python package to register or view fake job alerts",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/startng/forward4-itarj-adekams",
+    author="Adenike Salau",
+    author_email="kifademi@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords = "fake job alerts",
+    packages=find_packages(),
+    include_package_data = True,
+    install_requires = ["requests"],   
+)
+
