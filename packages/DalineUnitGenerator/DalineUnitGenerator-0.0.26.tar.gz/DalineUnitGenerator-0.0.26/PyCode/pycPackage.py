492e672f58e4784ec55660a8672229f804945e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:58 +0000

    Add package DalineUnitGenerator/0.0.26/DalineUnitGenerator-0.0.26.tar.gz

---
+++ packages/DalineUnitGenerator/DalineUnitGenerator-0.0.26.tar.gz/DalineUnitGenerator-0.0.26/PyCode/pycPackage.py
@@ -0,0 +1,10 @@
+import py_compile
+import shutil
+py_compile.compile('__init__.py',r'.\pyc\Animal.pyc' )
+py_compile.compile('Animal.py',r'.\pyc\Animal.pyc' )
+py_compile.compile('Cat.py',r'.\pyc\Cat.pyc' )
+shutil.copyfile(r'.\pyc\Cat.pyc','..\CodePackage\Cat.pyc')
+shutil.copyfile(r'.\pyc\Animal.pyc','..\CodePackage\Animal.pyc')
+
+# import compileall
+# compileall.compile_dir(r'E:\Code\PythonTB')
