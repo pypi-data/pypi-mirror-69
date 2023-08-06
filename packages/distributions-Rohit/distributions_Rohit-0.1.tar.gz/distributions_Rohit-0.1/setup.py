43ba5c65d927203e8256e23e5ddbb56dd65e00cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:29 +0000

    Add package distributions-Rohit/0.1/distributions_Rohit-0.1.tar.gz

---
+++ packages/distributions-Rohit/distributions_Rohit-0.1.tar.gz/distributions_Rohit-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_Rohit',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_Rohit'],
+      zip_safe=False)
