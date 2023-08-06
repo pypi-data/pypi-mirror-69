fd2d8e9f5a5fe9d2ede5075584d55e2952310aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:54 +0000

    Add package ana-py/0.1/ana_py-0.1.tar.gz

---
+++ packages/ana-py/ana_py-0.1.tar.gz/ana_py-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ana_py',
+      version='0.1',
+      description='Python package for Gaussian and Binomial Distribution',
+      packages=['ana_py'],
+      author='Shaswat Rajput',
+      author_email='shaswatrajput@outlook.com',
+      zip_safe=False)
