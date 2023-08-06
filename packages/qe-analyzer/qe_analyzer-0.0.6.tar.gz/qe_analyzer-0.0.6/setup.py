05758f6d26a4a065ea583ce5c5a469eed09f7cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:14 +0000

    Add package qe-analyzer/0.0.6/qe_analyzer-0.0.6.tar.gz

---
+++ packages/qe-analyzer/qe_analyzer-0.0.6.tar.gz/qe_analyzer-0.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+	long_description=fh.read()
+	
+setuptools.setup(
+    name="qe_analyzer", # Replace with your own username
+    version="0.0.6",
+    author="Elizabeth Pogue",
+    author_email="epogue1@jhu.edu",
+    description="A small package for analyzing quantum espresso outputs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/euclidmenot2/qe_analyzer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
