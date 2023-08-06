bc0f53c8201d7f20d220ac1b2dff86201d3191a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:52 +0000

    Add package Silence/0.0.4/Silence-0.0.4.tar.gz

---
+++ packages/Silence/Silence-0.0.4.tar.gz/Silence-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='Silence',
+    version='0.0.4',
+    description='An educational API+Web framework.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/agu-borrego/Silence',
+    author='Agustín Borrego',
+    author_email='borrego@us.es',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "silence = silence.cli.manager:run_from_command_line",
+        ],
+    },
+    install_requires=[
+        'dload==0.6',
+        'Flask==1.1.2',
+        'PyMySQL==0.9.3',
+    ],
+    python_requires='~=3.6',
+    zip_safe=False
+)
