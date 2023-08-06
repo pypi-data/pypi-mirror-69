5efde5156d38de44cc648fb569e6e8d14eece9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:40 +0000

    Add package pycayennelpp/1.5.0/pycayennelpp-1.5.0.tar.gz

---
+++ packages/pycayennelpp/pycayennelpp-1.5.0.tar.gz/pycayennelpp-1.5.0/cayennelpp/__init__.py
@@ -0,0 +1,4 @@
+from .lpp_data import LppData
+from .lpp_frame import LppFrame
+
+__all__ = ['LppData', 'LppFrame']
---
+++ packages/pycayennelpp/pycayennelpp-1.5.0.tar.gz/pycayennelpp-1.5.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='pycayennelpp',
+    version='1.5.0',
+    python_requires='>=3.4',
+    description='Encoder and Decoder for CayenneLLP',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords='cayenne lpp iot lora lorawan ttn',
+    url='http://github.com/smlng/pycayennelpp',
+    author='smlng',
+    author_email='s@mlng.net',
+    license='MIT',
+    packages=['cayennelpp'],
+    setup_requires=["pytest-runner"],
+    tests_require=['pytest'],
+    include_package_data=True
+)
