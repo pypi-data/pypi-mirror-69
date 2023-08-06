4463b07c181aa339b3f8bb0d85a21f433f6315ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:49 +0000

    Add package probability-distributions99/0.1/probability_distributions99-0.1.tar.gz

---
+++ packages/probability-distributions99/probability_distributions99-0.1.tar.gz/probability_distributions99-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probability_distributions99',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_distributions99'],
+      zip_safe=False)
