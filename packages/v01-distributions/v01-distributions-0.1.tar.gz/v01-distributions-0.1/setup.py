f944fdd246f9093c45e10f9bd664c125e51707f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:38 +0000

    Add package v01-distributions/0.1/v01-distributions-0.1.tar.gz

---
+++ packages/v01-distributions/v01-distributions-0.1.tar.gz/v01-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='v01-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['v01-distributions'],
+      author = 'Jhon Nagasako',
+      author_email = 'test@test.com',
+      zip_safe=False)
