8097b865ed09275b37f063c4169802be8bb0ae80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:58 +0000

    Add package ytmusicapi/0.8.0/ytmusicapi-0.8.0-py3-none-any.whl

---
+++ packages/ytmusicapi/ytmusicapi-0.8.0-py3-none-any.whl/ytmusicapi/helpers.py
@@ -0,0 +1,52 @@
+import re
+from http.cookies import SimpleCookie
+from hashlib import sha1
+import time
+
+
+def prepare_browse_endpoint(type, browseId):
+    return {
+        'browseEndpointContextSupportedConfigs': {
+            "browseEndpointContextMusicConfig": {
+                "pageType": "MUSIC_PAGE_TYPE_" + type
+            }
+        },
+        'browseId': browseId
+    }
+
+
+def prepare_like_endpoint(rating):
+    if rating == 'LIKE':
+        return 'like/like'
+    elif rating == 'DISLIKE':
+        return 'like/dislike'
+    elif rating == 'INDIFFERENT':
+        return 'like/removelike'
+    else:
+        return None
+
+
+def html_to_txt(html_text):
+    tags = re.findall("<[^>]+>", html_text)
+    for tag in tags:
+        html_text = html_text.replace(tag, '')
+    return html_text
+
+
+def sapisid_from_cookie(raw_cookie):
+    cookie = SimpleCookie()
+    cookie.load(raw_cookie)
+    return cookie['SAPISID'].value
+
+
+# SAPISID Hash reverse engineered by
+# https://stackoverflow.com/a/32065323/5726546
+def get_authorization(auth):
+    sha_1 = sha1()
+    unix_timestamp = str(int(time.time()))
+    sha_1.update((unix_timestamp + ' ' + auth).encode('utf-8'))
+    return "SAPISIDHASH " + unix_timestamp + "_" + sha_1.hexdigest()
+
+
+def to_int(string):
+    return int(string.split(' ')[0].replace(',', ''))
