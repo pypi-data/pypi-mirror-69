c7d73e39454608a3f756b6f1eff70a401f34a785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:06 +0000

    Add package prepl/1.0.0/prepl-1.0.0.tar.gz

---
+++ packages/prepl/prepl-1.0.0.tar.gz/prepl-1.0.0/prepl/watch_command.py
@@ -0,0 +1,39 @@
+import subprocess
+import logging
+from .watch import Watch
+from .run_command import run_command
+
+logger = logging.getLogger(__name__)
+
+
+def watch_command(cmd, shell=False):
+    while True:
+        with Watch() as watch:
+            blacklist = set()
+
+            def _event_handler(evt):
+                if evt.readonly:
+                    if evt.path not in blacklist:
+                        watch.watch(evt.path)
+                else:
+                    blacklist.add(str(evt.path))
+
+            try:
+                logger.info(
+                    "running command: " + (" ".join(cmd) if type(cmd) == list else cmd)
+                )
+                run_command(cmd, _event_handler, shell)
+            except subprocess.CalledProcessError as err:
+                logger.error(err)
+
+            logger.info("waiting for file change...")
+            while True:
+                file_list = [
+                    str(f) for f in watch.wait_for_events() if str(f) not in blacklist
+                ]
+
+                for f in file_list:
+                    logger.info(f"file changed: {f}")
+
+                if len(file_list) > 0:
+                    break
