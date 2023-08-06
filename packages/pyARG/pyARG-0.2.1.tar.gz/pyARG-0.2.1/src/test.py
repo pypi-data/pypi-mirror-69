a4908be76be7bf3ca986061df00f408aecbc3ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:53 +0000

    Add package pyARG/0.2.1/pyARG-0.2.1.tar.gz

---
+++ packages/pyARG/pyARG-0.2.1.tar.gz/pyARG-0.2.1/src/test.py
@@ -0,0 +1,14 @@
+import numpy
+print(numpy.__path__)
+import yaml
+print(yaml.__path__)
+import pylatex
+print(pylatex.__path__)
+import docx
+print(docx.__path__)
+import matplotlib
+print(matplotlib.__path__)
+import PySide2
+print(PySide2.__path__)
+import clr
+print(clr.__path__)
