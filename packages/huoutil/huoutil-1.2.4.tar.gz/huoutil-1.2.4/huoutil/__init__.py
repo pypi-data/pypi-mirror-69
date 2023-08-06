5e4435d2545757493ce6a8fd515172231c2ae5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:45 +0000

    Add package huoutil/1.2.4/huoutil-1.2.4.tar.gz

---
+++ packages/huoutil/huoutil-1.2.4.tar.gz/huoutil-1.2.4/huoutil/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '1.2.4'
+__description__ = 'util module of Huarong Huo'
+__author__ = 'Huarong Huo'
+__author_email__ = 'huohuarong@baidu.com'
+
+from .util import *
+from .cache import *
+from .uni import *
