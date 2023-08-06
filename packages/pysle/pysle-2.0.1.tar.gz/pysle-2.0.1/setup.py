6a75ed401fe707694f49e16895d2681ea9cf7735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:03 +0000

    Add package pysle/2.0.1/pysle-2.0.1.tar.gz

---
+++ packages/pysle/pysle-2.0.1.tar.gz/pysle-2.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+Created on Oct 15, 2014
+
+@author: tmahrt
+'''
+from setuptools import setup
+import io
+setup(name='pysle',
+      version='2.0.1',
+      author='Tim Mahrt',
+      author_email='timmahrt@gmail.com',
+      url='https://github.com/timmahrt/pysle',
+      package_dir={'pysle':'pysle'},
+      packages=['pysle'],
+      license='LICENSE',
+      description="An interface to ISLEX, an IPA pronunciation dictionary for English with stress and syllable markings.",
+      long_description=io.open('README.rst', 'r', encoding="utf-8").read()
+      )
