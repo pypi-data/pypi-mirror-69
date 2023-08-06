e9e3e2db31d2aa74a33cbc9a51631564dafab931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:21 +0000

    Add package gaussBino-probability/0.1/gaussBino_probability-0.1.tar.gz

---
+++ packages/gaussBino-probability/gaussBino_probability-0.1.tar.gz/gaussBino_probability-0.1/README.md
@@ -0,0 +1,6 @@
+this package is for calculating:- 
+1. Gaussian distributoin for given data set
+2. Binomial distributoin for given data set
+
+it also shows Histogram and graph representation.
+
---
+++ packages/gaussBino-probability/gaussBino_probability-0.1.tar.gz/gaussBino_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaussBino_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      auther='krishna kumar',
+      packages=['gaussBino_probability'],
+      zip_safe=False)
