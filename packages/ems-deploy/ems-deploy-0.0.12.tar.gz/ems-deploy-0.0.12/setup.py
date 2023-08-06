382ef93e7f3916c48b640c41ee5024f43430a885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:36 +0000

    Add package ems-deploy/0.0.12/ems-deploy-0.0.12.tar.gz

---
+++ packages/ems-deploy/ems-deploy-0.0.12.tar.gz/ems-deploy-0.0.12/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name='ems-deploy',
+    author="Jesper Halkjær Jensen",
+    author_email="gedemagt@gmail.com",
+    description="A small deploy utility package",
+    version='0.0.12',
+    url='https://github.com/',
+    packages=['deploy'],
+    entry_points={
+        'console_scripts': [
+            'deploy=deploy.deploy:run'
+        ]
+    },
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires='>=3.6',
+    install_requires=['pyyaml', 'urllib3<1.25', 'requests==2.18.4']
+)
