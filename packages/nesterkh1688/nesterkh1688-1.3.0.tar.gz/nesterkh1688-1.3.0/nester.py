dce74fa6111130d988a23e9eca928e94f2e6affb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:13 +0000

    Add package nesterkh1688/1.3.0/nesterkh1688-1.3.0.tar.gz

---
+++ packages/nesterkh1688/nesterkh1688-1.3.0.tar.gz/nesterkh1688-1.3.0/nester.py
@@ -0,0 +1,14 @@
+"""This is the nester.py module and it provides one function called print_lol
+which prints lists that may or may not include nested lists"""
+
+def print_lol(the_list,indent=False,level=0):
+    """This functions takes an argument called the_list and print it on screen.
+    if the_list is nested, it will be recursively exhausted and printed out """
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,indent,level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(each_item)
