ac6907761b99379da4b6d60e7481291175c73bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:22 +0000

    Add package pygwidgets/1.0.2/pygwidgets-1.0.2.tar.gz

---
+++ packages/pygwidgets/pygwidgets-1.0.2.tar.gz/pygwidgets-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pygwidgets',
+    version='1.0.2',
+    author='Irv Kalb',
+    author_email='Irv@furrypants.com',
+    description='User interface widgets for use with Pygame',
+    long_description='User interface widgets for building programs using Pygame',
+    packages=find_packages(),
+    include_package_data=True,
+    license="BSD",
+    url='https://github.com/IrvKalb/pygwidgets',
+    install_requires=[
+        'pygame>=1.9',
+        ],
+    keywords="pygame widgets user interface buttons text dragger animation image",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent"
+      ]
+    )
