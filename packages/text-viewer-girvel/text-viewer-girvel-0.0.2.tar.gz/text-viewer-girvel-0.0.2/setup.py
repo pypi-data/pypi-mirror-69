0301f641f598a2894d4ab7639177e037e4106393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:46 +0000

    Add package text-viewer-girvel/0.0.2/text-viewer-girvel-0.0.2.tar.gz

---
+++ packages/text-viewer-girvel/text-viewer-girvel-0.0.2.tar.gz/text-viewer-girvel-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="text-viewer-girvel",
+    version="0.0.2",
+    author="girvel",
+    author_email="widauka@yandex.ru",
+    description="Package for displaying stuff in console",
+    url="https://github.com/girvel/text_actions",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
