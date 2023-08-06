e0790606ab6768c27e6d724dd0eb73933bfd6a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:25 +0000

    Add package distributions-trial/0.1/distributions_trial-0.1.tar.gz

---
+++ packages/distributions-trial/distributions_trial-0.1.tar.gz/distributions_trial-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_trial',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_trial'],
+      zip_safe=False)
