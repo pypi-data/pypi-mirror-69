db74bd5c88baf892480c26a2db0c814c2c107d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:12 +0000

    Add package PyDigiPio/20.146.171707/PyDigiPio-20.146.171707.tar.gz

---
+++ packages/PyDigiPio/PyDigiPio-20.146.171707.tar.gz/PyDigiPio-20.146.171707/setup.py
@@ -0,0 +1,20 @@
+import DateVersioning
+from setuptools import setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="PyDigiPio",
+    version=DateVersioning.generate(),
+    description="Python module for Raspberry Pi GPIO",
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    author="Einar Arnason",
+    author_email="einsiarna@gmail.com",
+    url="https://github.com/EinarArnason/PyDigiPio",
+    license="MIT License",
+    py_modules=["PyDigiPio"],
+    test_suite="tests",
+    platforms=["linux"],
+)
