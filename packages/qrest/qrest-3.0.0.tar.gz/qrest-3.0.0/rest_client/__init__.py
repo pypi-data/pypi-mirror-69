4e8de4f01f2a64feaab0358f98126054b1198f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:42 +0000

    Add package qrest/3.0.0/qrest-3.0.0.tar.gz

---
+++ packages/qrest/qrest-3.0.0.tar.gz/qrest-3.0.0/rest_client/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '3.0.0'
+
+from .resource import JSONResource
+from .conf import APIConfig, ResourceConfig, BodyParameter, QueryParameter
+from .exception import  RestClientConfigurationError
+from .resource import API
+
