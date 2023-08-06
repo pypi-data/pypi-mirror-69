d3989892f96807c08ab37f8ab47a458281287bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:24 +0000

    Add package covid-surge/0.0.41/covid_surge-0.0.41-py3-none-any.whl

---
+++ packages/covid-surge/covid_surge-0.0.41-py3-none-any.whl/covid_surge/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the COVID-surge application.
+# https://github/dpploy/covid-surge
+"""Package for computing COVID-19 critical surge periods.
+
+A single class, Surge, provides all interfaces for obtaining COVID-19 data and
+computing the critical surge periods of all data involved.
+
+See documentation for the Surge class.
+"""
+
+from .src.surge import Surge
