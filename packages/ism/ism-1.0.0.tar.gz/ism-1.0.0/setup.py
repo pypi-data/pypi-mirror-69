e923ead27d1787616e5f0588073a272825e1ef5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:41 +0000

    Add package ism/1.0.0/ism-1.0.0.tar.gz

---
+++ packages/ism/ism-1.0.0.tar.gz/ism-1.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ism',
+    version='1.0.0',
+    description='Interactive SSH menu',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    url='https://github.com/Sch-Tomi/ism',
+    author='Sch-Tomi',
+    py_modules=['ism'],
+    entry_points={
+        'console_scripts': [
+            'ism = ism:main',
+        ],
+    }
+    ,
+    zip_safe=False,
+    install_requires=[
+        ['questionary'],
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
