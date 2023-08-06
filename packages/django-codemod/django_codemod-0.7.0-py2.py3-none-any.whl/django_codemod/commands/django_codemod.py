2a49fd7046ecad5fd072c0c44033fe19e8d72368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:04 +0000

    Add package django-codemod/0.7.0/django_codemod-0.7.0-py2.py3-none-any.whl

---
+++ packages/django-codemod/django_codemod-0.7.0-py2.py3-none-any.whl/django_codemod/commands/django_codemod.py
@@ -0,0 +1,58 @@
+from ..visitors.django_30 import (
+    InlineHasAddPermissionsTransformer,
+    RenderToResponseToRenderTransformer,
+)
+from ..visitors.django_40 import (
+    ForceTextToForceStrTransformer,
+    SmartTextToForceStrTransformer,
+    UGetTextLazyToGetTextLazyTransformer,
+    UGetTextNoopToGetTextNoopTransformer,
+    UGetTextToGetTextTransformer,
+    UNGetTextLazyToNGetTextLazyTransformer,
+    UNGetTextToNGetTextTransformer,
+    URLToRePathTransformer,
+)
+from .base import BaseCodemodCommand
+
+
+class Django30Command(BaseCodemodCommand):
+    """
+    Resolve following deprecations:
+
+    - Replaces ``render_to_response()`` by ``render()`` and add ``request=None``
+      as the first argument of ``render()``.
+    - Add the ``obj`` argument to ``InlineModelAdmin.has_add_permission()``.
+    """
+
+    DESCRIPTION: str = "Resolve deprecations for removals in Django 3.0."
+    transformers = [
+        RenderToResponseToRenderTransformer,
+        InlineHasAddPermissionsTransformer,
+    ]
+
+
+class Django40Command(BaseCodemodCommand):
+    """
+    Resolve following deprecations:
+
+    - ``django.utils.encoding.force_text``
+    - ``django.utils.encoding.smart_text``
+    - ``django.utils.translation.ugettext``
+    - ``django.utils.translation.ugettext_lazy``
+    - ``django.utils.translation.ugettext_noop``
+    - ``django.utils.translation.ungettext``
+    - ``django.utils.translation.ungettext_lazy``
+    - ``django.conf.urls.url``
+    """
+
+    DESCRIPTION: str = "Resolve deprecations of things removed in Django 4.0"
+    transformers = [
+        ForceTextToForceStrTransformer,
+        SmartTextToForceStrTransformer,
+        UGetTextLazyToGetTextLazyTransformer,
+        UGetTextNoopToGetTextNoopTransformer,
+        UGetTextToGetTextTransformer,
+        UNGetTextLazyToNGetTextLazyTransformer,
+        UNGetTextToNGetTextTransformer,
+        URLToRePathTransformer,
+    ]
