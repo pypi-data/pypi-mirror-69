77bd8cb3031c8c80e791c77bd8b6f409f44dcfb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:59 +0000

    Add package destlogger/1.2/destlogger-1.2.tar.gz

---
+++ packages/destlogger/destlogger-1.2.tar.gz/destlogger-1.2/destlogger/__init__.py
@@ -0,0 +1,5 @@
+__title__ = "destlogger"
+__author__ = 'Dest0re'
+__version__ = '1.2'
+
+from .logger import Logger
---
+++ packages/destlogger/destlogger-1.2.tar.gz/destlogger-1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import destlogger
+
+setup(
+    name=destlogger.__title__,
+    version=destlogger.__version__,
+    packages=['destlogger'],
+    install_requires=['colorama', 'cursor'],
+    url='https://github.com/Dest0re/Dest0res-logger',
+    license='',
+    author='Dest0re',
+    author_email='dest0re.louisell@gmail.com',
+    description=''
+)
