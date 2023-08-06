413d5108f5eefc121ab32482750c4f1426ed3103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:41 +0000

    Add package nbnhhsh/1.0/nbnhhsh-1.0-py3-none-any.whl

---
+++ packages/nbnhhsh/nbnhhsh-1.0-py3-none-any.whl/nbnhhsh/cmd.py
@@ -0,0 +1,14 @@
+import sys
+import argparse
+
+from .nbnhhsh import suo
+
+def main():
+    parser = argparse.ArgumentParser(description="一个转换缩写的工具")
+    parser.add_argument("text")
+    parser.parse_args()
+    text = parser.parse_args().text
+    print(suo(text))
+
+if __name__ == '__main__':
+    main()
