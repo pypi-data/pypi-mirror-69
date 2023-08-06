431e4ab30030f799bc73230bea62ef6d7745dd25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:36 +0000

    Add package dsnd-distributions-3502/0.1/dsnd_distributions_3502-0.1.tar.gz

---
+++ packages/dsnd-distributions-3502/dsnd_distributions_3502-0.1.tar.gz/dsnd_distributions_3502-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_distributions_3502',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_distributions_3502'],
+      author = 'Mehdi Zarei',
+      author_email = 'mehdy.zar@gmail.com',
+      zip_safe=False)
