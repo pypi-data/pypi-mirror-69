33816f980cf5de268d8fec563c89d6fc3e8cf969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:41 +0000

    Add package muller-gaussian-binomial-distributions/0.1/muller_gaussian_binomial_distributions-0.1.tar.gz

---
+++ packages/muller-gaussian-binomial-distributions/muller_gaussian_binomial_distributions-0.1.tar.gz/muller_gaussian_binomial_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='muller_gaussian_binomial_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions package',
+      packages=['muller_gaussian_binomial_distributions'],
+      author='Wuppschlander Müller',
+      author_email='wuppschlandermuller@gmail.com',
+      zip_safe=False)
