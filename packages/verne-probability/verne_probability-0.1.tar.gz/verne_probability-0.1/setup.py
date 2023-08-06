fe54ca48a038a9a759f40cf990f24f609b93fc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:51 +0000

    Add package verne-probability/0.1/verne_probability-0.1.tar.gz

---
+++ packages/verne-probability/verne_probability-0.1.tar.gz/verne_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='verne_probability',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['verne_probability'],
+      author='Verne Ongteco',
+      zip_safe=False)
