d0a3da9fda020ff04fd89869d06a3f59d86eb1e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:17 +0000

    Add package nba-tracking-plotly/0.0.1.11/nba_tracking_plotly-0.0.1.11.tar.gz

---
+++ packages/nba-tracking-plotly/nba_tracking_plotly-0.0.1.11.tar.gz/nba_tracking_plotly-0.0.1.11/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nba_tracking_plotly", # Replace with your own username
+    version="0.0.1.11",
+    author="Avyay Varadarajan",
+    author_email="avyayv@gmail.com",
+    description="Plot nba tracking data using plotly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avyayv/nba_tracking_plotly",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
