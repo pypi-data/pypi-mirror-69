e040991088a6e6a58b6a829aeb480d55588fa8bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:30 +0000

    Add package tuda-distributions/0.1/tuda_distributions-0.1.tar.gz

---
+++ packages/tuda-distributions/tuda_distributions-0.1.tar.gz/tuda_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tuda_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['tuda_distributions'],
+      author = 'Richa Jain',
+      author_email = 'richajain44@gmail.com',
+      zip_safe=False)
