288690b166f6c4b4628f6e22da6729bf2aeb7ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:47 +0000

    Add package baizhanSuperMath7/1.0/baizhanSuperMath7-1.0.tar.gz

---
+++ packages/baizhanSuperMath7/baizhanSuperMath7-1.0.tar.gz/baizhanSuperMath7-1.0/setup.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+import sys
+defaultencoding = 'utf-8'
+from setuptools import setup,find_packages
+
+setup(
+    name="baizhanSuperMath7",
+    version="1.0",
+    description="这是第一个对外发布的模块",
+    author="宋海兵",
+    author_email="2403181713@qq.com",
+    package=["baizhanSuperMath7.demo1","baizhanSuperMath7.demo2"]
+    )
