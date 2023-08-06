e6d73b07bfcb52ad9c594868fd356aa7f335dc80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:50 +0000

    Add package bidistributions-probablity/0.1/bidistributions_probablity-0.1.tar.gz

---
+++ packages/bidistributions-probablity/bidistributions_probablity-0.1.tar.gz/bidistributions_probablity-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bidistributions_probablity',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['bidistributions_probability'],
+      author = 'Jayanth Adithya',
+      author_email = 'njayanthaithya@gmail.com',
+      zip_safe=False)
