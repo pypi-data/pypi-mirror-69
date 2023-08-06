d6c30d13a6dc1ec6256368ebc2d30530bbc4ecc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:29 +0000

    Add package ems-gcp-toolkit/0.1.63/ems-gcp-toolkit-0.1.63.tar.gz

---
+++ packages/ems-gcp-toolkit/ems-gcp-toolkit-0.1.63.tar.gz/ems-gcp-toolkit-0.1.63/bigquery/job/ems_extract_job.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from typing import List, Union
+
+from bigquery.job.ems_job import EmsJob
+from bigquery.job.ems_job_state import EmsJobState
+
+
+class EmsExtractJob(EmsJob):
+    def __init__(self,
+                 job_id: str,
+                 table: str,
+                 destination_uris: List[str],
+                 state: EmsJobState,
+                 error_result: Union[dict, None],
+                 created: datetime = None):
+        super(EmsExtractJob, self).__init__(job_id, state, error_result, created)
+
+        self.__table = table
+        self.__destination_uris = destination_uris
+
+    @property
+    def table(self) -> str:
+        return self.__table
+
+    @property
+    def destination_uris(self) -> List[str]:
+        return self.__destination_uris
