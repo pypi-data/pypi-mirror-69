ed839e02436a3a81d6ac1f29f5eada22d4751213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:51 +0000

    Add package pfrl/0.0.0/pfrl-0.0.0.tar.gz

---
+++ packages/pfrl/pfrl-0.0.0.tar.gz/pfrl-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pfrl',
+    version='0.0.0',
+    description='',
+    long_description='',
+    author='Yasuhiro Fujita',
+    author_email='muupan@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
