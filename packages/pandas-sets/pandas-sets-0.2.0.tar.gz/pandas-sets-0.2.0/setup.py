4d649f0354be912bc83c29bf9c1978caa2c521e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:37 +0000

    Add package pandas-sets/0.2.0/pandas-sets-0.2.0.tar.gz

---
+++ packages/pandas-sets/pandas-sets-0.2.0.tar.gz/pandas-sets-0.2.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pandas-sets',
+    version='v0.2.0',
+    packages=find_packages(),
+    license='BSD',
+    description='Pandas - Sets:  Set-oriented Operations in Pandas',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Florents Tselai',
+    author_email='florents@tselai.com',
+    url='https://github.com/Florents-Tselai/pandas-sets',
+    install_requires=open('requirements.txt').read().splitlines()
+)
