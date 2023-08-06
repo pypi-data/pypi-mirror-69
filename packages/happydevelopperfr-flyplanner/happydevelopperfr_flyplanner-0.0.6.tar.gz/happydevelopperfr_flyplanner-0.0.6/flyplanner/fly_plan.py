401d8746e30c834e21e531049ee4454331b1395b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:56 +0000

    Add package happydevelopperfr-flyplanner/0.0.6/happydevelopperfr_flyplanner-0.0.6.tar.gz

---
+++ packages/happydevelopperfr-flyplanner/happydevelopperfr_flyplanner-0.0.6.tar.gz/happydevelopperfr_flyplanner-0.0.6/flyplanner/fly_plan.py
@@ -0,0 +1,6 @@
+from flyplanner.fly import Fly
+
+
+class FlyPlan:
+    def __init__(self, fly: Fly):
+        self.fly = fly
---
+++ packages/happydevelopperfr-flyplanner/happydevelopperfr_flyplanner-0.0.6.tar.gz/happydevelopperfr_flyplanner-0.0.6/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="happydevelopperfr_flyplanner",
+    version="0.0.6",
+    description="fly planner for happy.developper.fr",
+    author="COTTET Julien",
+    author_email="happy.developper.fr@gmail.com",
+    url="https://github.com/happy-developer-fr/fly-planner",
+    packages=["flyplanner"],
+)
