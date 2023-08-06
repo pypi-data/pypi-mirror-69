283fa9347524ac62ad0f8abc9400063dbcbb48c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:16 +0000

    Add package eventmonitoring-client/1.9.9/eventmonitoring-client-1.9.9.tar.gz

---
+++ packages/eventmonitoring-client/eventmonitoring-client-1.9.9.tar.gz/eventmonitoring-client-1.9.9/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eventmonitoring-client",
+    version='1.9.9',
+    packages=find_packages(),
+    install_requires=["requests>=2.18.2", "zappa==0.50.0", "pytz"],
+)
