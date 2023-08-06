38c4a703ab6db760e753fcd288e4b8d8d9ea80e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:51 +0000

    Add package clerkai/0.2.3/clerkai-0.2.3.tar.gz

---
+++ packages/clerkai/clerkai-0.2.3.tar.gz/clerkai-0.2.3/clerkai/transactions/defaults.py
@@ -0,0 +1,16 @@
+from typing import List
+
+default_transaction_files_editable_columns: List[str] = [
+    "Ignore",
+    "Account provider",
+    "Account",
+    "Content type",
+    "Account currency",
+]
+
+default_transactions_editable_columns: List[str] = [
+    "Clarification",
+    "Type",
+    "Income/Expense Category",
+    "Currency",
+]
