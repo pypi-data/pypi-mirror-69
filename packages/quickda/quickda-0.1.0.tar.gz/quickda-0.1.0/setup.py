65ea29c3516a9d0c27791b8b94f0e3cc7d08d98d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:18 +0000

    Add package quickda/0.1.0/quickda-0.1.0.tar.gz

---
+++ packages/quickda/quickda-0.1.0.tar.gz/quickda-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="quickda", 
+    version="0.1.0",
+    author="Sidheswar Venkatachalapathi",
+    author_email="gauty95@gmail.com",
+    description="Simple & Easy-to-use python modules to perform Quick Exploratory Data Analysis for any structured dataset!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sid-the-coder/Quick-EDA",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
