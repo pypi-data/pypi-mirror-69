230915993b84eedf3d2d46157c4b590c5e92be92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:11 +0000

    Add package infrastructure/1.5.4/infrastructure-1.5.4-py3.7.egg

---
+++ packages/infrastructure/infrastructure-1.5.4-py3.7.egg/infrastructure/hb/local_server.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: yongfanmao
+# @Date:   2020-05-29 11:41:39
+# @E-mail: maoyongfan@163.com
+# @Last Modified by:   yongfanmao
+# @Last Modified time: 2020-05-29 11:42:32
+class LocalServer(object):
+	def __init__(self,remoteServer):
+		"""
+			操作本地服务器
+		"""
+		self.remoteServer = remoteServer
