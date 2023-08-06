c62010e120b475bf370037120d352b91ccec34e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:15 +0000

    Add package skitai/0.35.0.41/skitai-0.35.0.41.tar.gz

---
+++ packages/skitai/skitai-0.35.0.41.tar.gz/skitai-0.35.0.41/skitai/corequest/pth/sp_task.py
@@ -0,0 +1,55 @@
+# Subprocess Task
+
+from . import task
+import subprocess
+from concurrent.futures import TimeoutError
+from ..tasks import Mask
+import time
+from skitai import was
+
+class Task (task.Task):
+    def __init__ (self, cmd, meta, timeout):
+        self.setup (cmd, meta, timeout)
+        self.proc = subprocess.Popen (cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell = True)
+
+    @property
+    def lines (self):
+        for line in iter (self.proc.stdout.readline, b''):
+            yield line
+
+    def _polling (self):
+        mask = self._create_mask (self._timeout)
+        self._late_respond (mask)
+
+    def then (self, func):
+        self._fulfilled = func
+        self._was = self._get_was ()
+        was.Thread (self._polling)
+
+    def kill (self):
+        self.proc.kill ()
+
+    def terminate (self):
+        self.proc.terminate ()
+
+    def _create_mask (self, timeout):
+        self._timeout = timeout
+        if self._mask:
+            return self._mask
+
+        data, expt = None, None
+        try:
+            data, err = self.proc.communicate (timeout = timeout)
+        except subprocess.TimeoutExpired:
+            expt = TimeoutError
+            self.proc.terminate ()
+            self.proc.wait ()
+        else:
+            if self.proc.returncode:
+                if isinstance (err, bytes):
+                    err = err.decode ()
+                expt = SystemError ('code:{} {}'.format (self.proc.returncode, err))
+        self._mask = Mask (data, expt, meta = self.meta)
+        return self._mask
+
+
---
+++ packages/skitai/skitai-0.35.0.41.tar.gz/skitai-0.35.0.41/skitai/wastuff/api.py
@@ -0,0 +1,161 @@
+import json
+import ujson
+import sys
+from xmlrpc import client
+from ..utility import catch
+import copy
+from datetime import timezone, datetime, date
+
+TZ_LOCAL = datetime.now (timezone.utc).astimezone().tzinfo
+TZ_UTC = timezone.utc
+
+class StringDateTimeEncoder (json.JSONEncoder):
+	def _to_utc (self, obj):
+		try:
+			return obj.astimezone (TZ_UTC)
+		except ValueError:
+			return obj.replace (tzinfo = TZ_LOCAL).astimezone (TZ_UTC)
+
+	def default (self, obj):
+		if isinstance (obj, date):
+			return str (obj)
+		return json.JSONEncoder.default (self, obj)
+
+class DigitTimeEncoder (StringDateTimeEncoder):
+	def default (self, obj):
+		if isinstance (obj, date):
+			try: return self._to_utc (obj).strftime ('%Y%m%d%H%M%S')
+			except AttributeError: return obj.isoformat ()
+		return json.JSONEncoder.default (self, obj)
+
+class ISODateTimeEncoder (StringDateTimeEncoder):
+	def default (self, obj):
+		if isinstance (obj, date):
+			try: return self._to_utc (obj).isoformat ()
+			except AttributeError: return obj.isoformat ()
+		return json.JSONEncoder.default (self, obj)
+
+class UNIXEpochDateTimeEncoder (StringDateTimeEncoder):
+	def default (self, obj):
+		if isinstance (obj, date):
+			try: return self._to_utc (obj).timestamp ()
+			except AttributeError: return obj.isoformat ()
+		return json.JSONEncoder.default (self, obj)
+
+class ISODateTimeWithOffsetEncoder (StringDateTimeEncoder):
+	def default (self, obj):
+		if isinstance (obj, date):
+			try: return self._to_utc (obj).strftime ('%Y-%m-%d %H:%M:%S+00')
+			except AttributeError: return obj.isoformat ()
+		return json.JSONEncoder.default (self, obj)
+
+def encode_hook (obj):
+	if isinstance (obj, date):
+		try:
+			try:
+				obj = obj.astimezone (TZ_UTC)
+			except ValueError:
+				obj = obj.replace (tzinfo = TZ_LOCAL).astimezone (TZ_UTC)
+		except AttributeError:
+			pass
+		return obj.isoformat ()
+	return obj
+
+
+class API:
+	ENCODER_MAP = {
+		'str': StringDateTimeEncoder,
+		'iso': ISODateTimeEncoder,
+		'unixepoch': UNIXEpochDateTimeEncoder,
+		'digit': DigitTimeEncoder,
+		'utcoffset': ISODateTimeWithOffsetEncoder
+	}
+
+	@classmethod
+	def add_custom_encoder (cls, name, encoder):
+		cls.ENCODER_MAP [name] = encoder
+
+	def __init__ (self, request, data = None):
+		self.request = request # for response typing
+		self.data = data or {}
+		self.content_type = self.set_content_type ()
+		self.data_encoder_class = None
+		self.pretty = False
+
+	def set_json_encoder (self, encoder, pretty = True):
+		self.pretty = pretty
+		if not encoder:
+			return
+		if isinstance (encoder, str):
+			self.data_encoder_class = self.ENCODER_MAP [encoder]
+			return
+		self.data_encoder_class = encoder
+
+	def __setitem__ (self, k, v):
+		self.set (k, v)
+
+	def __getitem__ (self, k, v = None):
+		return self.get (k, v)
+
+	def set (self, k, v):
+		self.data [k] = v
+
+	def get (self, k, v = None):
+		return self.data.get (k, v)
+
+	def get_content_type (self):
+		return self.content_type
+
+	def set_content_type (self):
+		content_type = self.request.get_header ("accept", 'application/json')
+		if not content_type.startswith ("text/xml"):
+			content_type = 'application/json'
+		return content_type
+
+	def encode (self, charset):
+		return self.to_string ().encode (charset)
+
+	def __str__ (self):
+		return self.to_string ()
+
+	def to_string (self):
+		if self.content_type.startswith ("text/xml"):
+			return client.dumps ((self.data,))
+
+		if self.data_encoder_class is ISODateTimeWithOffsetEncoder or not self.data_encoder_class:
+			return ujson.dumps (self.data, ensure_ascii = False, indent = self.pretty and 2 or 0, pre_encode_hook = encode_hook)
+		else:
+			return json.dumps (self.data, cls = self.data_encoder_class or self.ENCODER_MAP ['utcoffset'], ensure_ascii = False, indent = self.pretty and 2 or None)
+
+	def traceback (self, message = 'exception occured', code = 20000, debug = 'see traceback', more_info = None):
+		self.error (message, code, debug, more_info, sys.exc_info ())
+
+	def error (self, message, code = 20000, debug = None, more_info = None, exc_info = None):
+		self.data = {}
+		self.data ['code'] = code
+		self.data ['message'] = message
+		if debug:
+			self.data ['debug'] = debug
+		if more_info:
+			self.data ['more_info'] = more_info
+		if exc_info:
+			self.data ["traceback"] = type (exc_info) is tuple and catch (2, exc_info) or exc_info
+
+	def set_spec (self, app):
+		resource_id = self.request.routable ["func_id"]
+		routable = copy.deepcopy (self.request.routable)
+		del routable ["func_id"]
+		routable ["methods"] = list (routable ["methods"])
+		props = {"current_request": {}}
+		props ["current_request"]["method"] = self.request.method
+		props ["current_request"]["version"] = self.request.version
+		props ["current_request"]["uri"] = self.request.uri
+		props ['routeopt'] = routable
+		props ["parameter_requirements"] = app.get_parameter_requirements (resource_id)
+		props ['auth_requirements'] = app.get_auth_flags (resource_id)
+		props ["doc"] = self.request.routed.__doc__
+		props ["id"] = resource_id
+		self.data ["__spec__"] = props
+
+		#import pprint
+		#pprint.pprint (self.data ["__spec__"])
