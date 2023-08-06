02be5b4936663130aaa9e147d7c30137200ee351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:12 +0000

    Add package distributions-aws-ml/0.1/distributions_aws_ml-0.1.tar.gz

---
+++ packages/distributions-aws-ml/distributions_aws_ml-0.1.tar.gz/distributions_aws_ml-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_aws_ml',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_aws_ml'],
+      zip_safe=False)
