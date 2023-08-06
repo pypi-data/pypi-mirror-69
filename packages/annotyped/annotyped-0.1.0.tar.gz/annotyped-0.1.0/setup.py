ea5acc5f5dd2a162a2f99ada0fb6185e5aae7014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:02 +0000

    Add package annotyped/0.1.0/annotyped-0.1.0.tar.gz

---
+++ packages/annotyped/annotyped-0.1.0.tar.gz/annotyped-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+NAME = 'annotyped'
+DESCRIPTION = ''
+URL = 'https://gitlab.com/sj1k/typer'
+EMAIL = '42Echo6Alpha@gmail.com'
+AUTHOR = 'Steven "sj1k" Core'
+REQUIRES_PYTHON = '>=3.5.0'
+VERSION = '0.1.0'
+
+
+setup(
+    name=NAME,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    version=VERSION,
+    packages=['annotyped'],
+)
