53e9f36f7ad1896d498ea2f459bdb451663288e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:20 +0000

    Add package ValidX/0.6.post1/ValidX-0.6.post1-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/ValidX/ValidX-0.6.post1-cp36-cp36m-macosx_10_9_x86_64.whl/validx/compat/frozendict.py
@@ -0,0 +1,30 @@
+from .colabc import Mapping
+
+
+class frozendict(Mapping):
+    def __init__(self, *args, **kw):
+        self.__data = dict(*args, **kw)
+
+    def __repr__(self):
+        return "%s(%r)" % (self.__class__.__name__, self.__data)
+
+    def __getitem__(self, key):
+        return self.__data[key]
+
+    def __iter__(self):
+        return iter(self.__data)
+
+    def __len__(self):
+        return len(self.__data)
+
+    def __contains__(self, key):
+        return key in self.__data
+
+    def keys(self):
+        return self.__data.keys()
+
+    def items(self):
+        return self.__data.items()
+
+    def values(self):
+        return self.__data.values()
