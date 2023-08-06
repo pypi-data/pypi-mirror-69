f76c892d7e11991c992a9b933012bec0daa103b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:41 +0000

    Add package probability-distro/0.1/probability_distro-0.1.tar.gz

---
+++ packages/probability-distro/probability_distro-0.1.tar.gz/probability_distro-0.1/README.md
@@ -0,0 +1,1 @@
+This is a simple python package for calculating Gaussian and Binomial Distributions.
---
+++ packages/probability-distro/probability_distro-0.1.tar.gz/probability_distro-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distro',
+      version='0.1',
+      description='Gaussian and Binomal distributions',
+      packages=['probability_distro'],
+      author = "Vamsi Krishna Gutha",
+      author_email  = "vamsigutha27@gmail.com",
+      zip_safe=False)
