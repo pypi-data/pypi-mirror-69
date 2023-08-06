d1963850b59d1458290d9ffec361021dba77c7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:05 +0000

    Add package dmj-distributions/0.1/dmj_distributions-0.1.tar.gz

---
+++ packages/dmj-distributions/dmj_distributions-0.1.tar.gz/dmj_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dmj_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dmj_distributions'],
+      author='David Lin',
+      author_email='dbspace@yahoo.com',
+      zip_safe=False)
