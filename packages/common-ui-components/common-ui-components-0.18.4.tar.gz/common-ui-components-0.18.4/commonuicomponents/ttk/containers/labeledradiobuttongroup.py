1797d69b43dff50e8659381799a5a2d86ca9c32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:21 +0000

    Add package common-ui-components/0.18.4/common-ui-components-0.18.4.tar.gz

---
+++ packages/common-ui-components/common-ui-components-0.18.4.tar.gz/common-ui-components-0.18.4/commonuicomponents/ttk/containers/labeledradiobuttongroup.py
@@ -0,0 +1,19 @@
+from .labeledcontainer import LabeledContainer
+
+class LabeledRadioButtonGroup(LabeledContainer):
+   def __init__(self, master = None, **kw):
+      LabeledContainer._setVariable(kw, "IntVar")
+      
+      super().__init__(master, **kw)
+      
+      self._initValueAndTraceAdd()
+   
+   def _inflateChildren(self):
+      for child in self._baseContainerChildren:
+         if "type" not in child:
+            child["type"] = "Radiobutton"
+      
+      LabeledContainer._inflateChildren(self)
+      
+      for index, proxy in enumerate(self._baseContainerChildren.values()):
+         proxy.value.configure(value = index, variable = self.getRawValue())
