69245ac35c2f354fafdd7c05b85a0d798b4701d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:02 +0000

    Add package wysdom/0.1.2/wysdom-0.1.2-py3-none-any.whl

---
+++ packages/wysdom/wysdom-0.1.2-py3-none-any.whl/wysdom/dom/DOMProperties.py
@@ -0,0 +1,29 @@
+from typing import Dict, Union
+
+from ..base_schema import Schema
+
+
+class DOMProperties(object):
+    """
+    A container for property information for a :class:`.DOMObject`.
+    """
+
+    properties: Dict[str, Schema] = None
+    additional_properties: Union[bool, Schema] = False
+
+    def __init__(
+            self,
+            properties: Dict[str, Schema] = None,
+            additional_properties: Union[bool, Schema] = False
+    ) -> None:
+        """
+        :param properties:            A dictionary of :class:`~wysdom.base_schema.Schema` objects
+                                      defining the expected names and types of a :class:`.DOMObject`'s
+                                      properties.
+        :param additional_properties: Defines whether a :class:`.DOMObject` permits additional
+                                      dynamically-named properties. Can be True or False, or
+                                      can be set to a specific :class:`~wysdom.Schema` to restrict the permitted
+                                      types of any additional properties.
+        """
+        self.properties = properties or {}
+        self.additional_properties = additional_properties
