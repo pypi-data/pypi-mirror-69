67a4c40bc63d42ba9da230650e8b80739a97b3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:53 +0000

    Add package probadist/0.1/probadist-0.1.tar.gz

---
+++ packages/probadist/probadist-0.1.tar.gz/probadist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probadist',
+      version='0.1',
+      description='Probability distributions',
+      packages=['probadist'],
+      author='Lavinan Selvaratnam',
+      author_email='lavinan26@gmail.com',
+      zip_safe=False)
