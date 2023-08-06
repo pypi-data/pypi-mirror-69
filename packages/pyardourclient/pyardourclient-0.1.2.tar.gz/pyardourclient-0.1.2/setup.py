060604547ab67e8339fa1972a6baf80628187229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:15 +0000

    Add package pyardourclient/0.1.2/pyardourclient-0.1.2.tar.gz

---
+++ packages/pyardourclient/pyardourclient-0.1.2.tar.gz/pyardourclient-0.1.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='pyardourclient',
+    version='0.1.2',
+    packages=['pyardourclient'],
+    url='https://github.com/lucianoiam/pyardourclient',
+    license='GPLv3',
+    author='Luciano Iam',
+    author_email='lucianito@Gmail.com',
+    description='Provides a client library to interact with the Ardour WebSockets Server surface',
+    python_requires='>=3.7',
+)
