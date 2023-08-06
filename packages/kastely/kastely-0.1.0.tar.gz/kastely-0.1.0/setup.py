13c3a11a3ca2e085ac3eb6f6a488d6f78ad35535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:32 +0000

    Add package kastely/0.1.0/kastely-0.1.0.tar.gz

---
+++ packages/kastely/kastely-0.1.0.tar.gz/kastely-0.1.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['kastely']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Unidecode>=1.1,<2.0',
+ 'colorama>=0.4.3,<0.5.0',
+ 'pyfiglet>=0.8.0,<0.9.0',
+ 'tabulate>=0.8.7,<0.9.0']
+
+setup_kwargs = {
+    'name': 'kastely',
+    'version': '0.1.0',
+    'description': '🏰 Race to the castle with this fun CLI Board Game!',
+    'long_description': None,
+    'author': 'Sam Poder',
+    'author_email': 'hi@sampoder.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
