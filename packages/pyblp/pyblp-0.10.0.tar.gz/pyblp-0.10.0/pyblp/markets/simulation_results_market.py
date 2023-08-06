d4823e90dbb08f1aafc5b155767470e0f92e7140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:44 +0000

    Add package pyblp/0.10.0/pyblp-0.10.0.tar.gz

---
+++ packages/pyblp/pyblp-0.10.0.tar.gz/pyblp-0.10.0/pyblp/markets/simulation_results_market.py
@@ -0,0 +1,18 @@
+"""Market level structuring of simulated synthetic BLP data."""
+
+from typing import List, Tuple
+
+from .market import Market
+from .. import exceptions
+from ..utilities.basics import Array, Error, NumericalErrorHandler
+
+
+class SimulationResultsMarket(Market):
+    """A market in a solved simulation of synthetic BLP data."""
+
+    @NumericalErrorHandler(exceptions.SyntheticMicroMomentsNumericalError)
+    def safely_compute_micro(self) -> Tuple[Array, List[Error]]:
+        """Compute micro moments, handling any numerical errors."""
+        errors: List[Error] = []
+        micro = self.compute_micro()[0]
+        return micro, errors
