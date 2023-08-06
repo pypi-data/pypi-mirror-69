efea40d817c1feddb883c855b4578a50cfe17a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:34 +0000

    Add package flyingkoala/0.0.10b2/flyingkoala-0.0.10b2-py3-none-any.whl

---
+++ packages/flyingkoala/flyingkoala-0.0.10b2-py3-none-any.whl/flyingkoala/__init__.py
@@ -0,0 +1,12 @@
+name = "flyingkoala"
+from .flyingkoala import FlyingKoala
+from .flyingkoala import generate_model_graph
+from .flyingkoala import reload_koala
+from .flyingkoala import reset_koala_model_cache
+from .flyingkoala import get_cached_koala_model_names
+from .flyingkoala import unload_koala_model_from_cache
+from .flyingkoala import evaluate_koala_model
+from .flyingkoala import get_model_cache_count
+from .flyingkoala import get_named_range_count
+from .flyingkoala import get_named_ranges
+from .flyingkoala import load_model
---
+++ packages/flyingkoala/flyingkoala-0.0.10b2-py3-none-any.whl/flyingkoala/horticulture/__init__.py
@@ -0,0 +1,4 @@
+from .horticulture import DegreeDay
+from .horticulture import DegreeDayDynamicArray
+from .horticulture import DegreeDayByName
+from .horticulture import DegreeDayDynamicArrayByName
