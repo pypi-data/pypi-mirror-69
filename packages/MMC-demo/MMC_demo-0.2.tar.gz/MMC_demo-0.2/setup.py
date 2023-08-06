f310ea3b3e4eab6b0b98e8f8428748dd8ad64dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:13 +0000

    Add package MMC-demo/0.2/MMC_demo-0.2.tar.gz

---
+++ packages/MMC-demo/MMC_demo-0.2.tar.gz/MMC_demo-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MMC_demo",
+    version = "0.2",
+    packages = find_packages(),
+    description = 'MicroMOOC demo test',
+    author = 'MicroMOOC',
+    author_email = 'support@micromooc.com',
+    url = 'https://micromooc.com/',
+)
