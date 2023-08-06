98e497ebcd6d67f3a1c3e5405663578a8b06564a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:58 +0000

    Add package dclab/0.27.2/dclab-0.27.2-cp27-cp27m-win32.whl

---
+++ packages/dclab/dclab-0.27.2-cp27-cp27m-win32.whl/dclab/compat.py
@@ -0,0 +1,25 @@
+# python 2+3 compatibility functions
+import io
+import sys
+
+
+if sys.version_info[0] == 2:
+    from backports.functools_lru_cache import lru_cache  # noqa: F401
+    str_types = basestring  # noqa: F821
+    hdf5_str = unicode  # noqa: F821
+    PyImportError = ImportError
+
+    def is_file_obj(obj):
+        return isinstance(obj, file)  # noqa: F821
+
+else:
+    from functools import lru_cache  # noqa: F401
+    if sys.version_info[1] <= 5:
+        PyImportError = ImportError
+    else:
+        PyImportError = ModuleNotFoundError  # noqa: F821
+    str_types = str
+    hdf5_str = str
+
+    def is_file_obj(obj):
+        return isinstance(obj, io.IOBase)
