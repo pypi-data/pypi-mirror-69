abe4e3f27d2c9c6110e9b5269217abcafe0e7fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:28 +0000

    Add package python3-anticaptcha/1.6.2/python3-anticaptcha-1.6.2.tar.gz

---
+++ packages/python3-anticaptcha/python3-anticaptcha-1.6.2.tar.gz/python3-anticaptcha-1.6.2/python3_anticaptcha/config.py
@@ -0,0 +1,24 @@
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Адрес для создания задачи
+create_task_url = "https://api.anti-captcha.com/createTask"
+# Адрес для получения ответа
+get_result_url = "https://api.anti-captcha.com/getTaskResult"
+# ключ приложения
+app_key = "867"
+
+"""
+Параметры для callback
+"""
+# IP для работы callback`a
+HOST = "85.255.8.26"
+# PORT для работы callback`a
+PORT = 8001
+# данные для подключения к RabbitMQ на callback сервере
+RTMQ_USERNAME = "hardworker_1"
+RTMQ_PASSWORD = "password"
+RTMQ_HOST = "85.255.8.26"
+RTMQ_PORT = 5672
+RTMQ_VHOST = "anticaptcha_vhost"
