b067b861717bd46ede66b5e81653846981844722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:58 +0000

    Add package PyFiler/1.0/PyFiler-1.0.tar.gz

---
+++ packages/PyFiler/PyFiler-1.0.tar.gz/PyFiler-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyFiler',
+    version='1.0',
+    description='A Wrapper for dealing with text-like files within python, especially python files.',
+    packages=find_packages('pyfiler')
+)
