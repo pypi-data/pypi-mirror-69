3f3fa068ecbded622e42e8cb24f99ebb32a366e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:50 +0000

    Add package shioaji/0.2.1.dev6/shioaji-0.2.1.dev6-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/shioaji/shioaji-0.2.1.dev6-cp36-cp36m-manylinux2010_x86_64.whl/shioaji/config.py
@@ -0,0 +1,6 @@
+import os
+
+SJCLIENT_SOL_HOST = os.environ.get("SJCLIENT_SOL_HOST", "203.66.91.161:80")
+SJCLIENT_SOL_VPN = os.environ.get("SJCLIENT_SOL_VPN", "sinopac")
+SJCLIENT_SOL_USER = os.environ.get("SJCLIENT_SOL_USER", "shioaji")
+SJCLIENT_SOL_PASSWORD = os.environ.get("SJCLIENT_SOL_PASSWORD", "sahjiio111")
