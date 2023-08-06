98d02ea7c159b85bf5b4b54954b78c0e168a1690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:22 +0000

    Add package gaussian-dist/1.0/gaussian_dist-1.0.tar.gz

---
+++ packages/gaussian-dist/gaussian_dist-1.0.tar.gz/gaussian_dist-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaussian_dist',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian_dist'],
+      author = 'Aditya Bisoi',
+      zip_safe=False)
