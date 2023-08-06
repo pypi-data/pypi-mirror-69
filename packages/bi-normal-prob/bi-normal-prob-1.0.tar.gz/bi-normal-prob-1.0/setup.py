7d04792ebad024086054dcde125b56a1314963b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:31 +0000

    Add package bi-normal-prob/1.0/bi-normal-prob-1.0.tar.gz

---
+++ packages/bi-normal-prob/bi-normal-prob-1.0.tar.gz/bi-normal-prob-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bi-normal-prob',
+      version='1.0',
+      description='Python package for Binomial and Bell-Curved-Shaped Normal Distribution',
+      packages=['bi-normal-prob'],
+      author = 'Kush Shah',
+      author_email = 'kushshah253@gmail.com',
+      zip_safe=False)
