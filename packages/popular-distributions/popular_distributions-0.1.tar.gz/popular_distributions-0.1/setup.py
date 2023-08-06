96d28a6bc7b78780f0a18ed536c3b52a0c1cc4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:44 +0000

    Add package popular-distributions/0.1/popular_distributions-0.1.tar.gz

---
+++ packages/popular-distributions/popular_distributions-0.1.tar.gz/popular_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='popular_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['popular_distributions'],
+      author='Roman Ivashkin',
+      author_email='ivashkinrg@gmail.com',
+      zip_safe=False)
