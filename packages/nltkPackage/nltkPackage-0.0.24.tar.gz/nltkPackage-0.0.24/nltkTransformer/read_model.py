2de4da0c60f771a08eb0529c0059f25c33aec31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:08 +0000

    Add package nltkPackage/0.0.24/nltkPackage-0.0.24.tar.gz

---
+++ packages/nltkPackage/nltkPackage-0.0.24.tar.gz/nltkPackage-0.0.24/nltkTransformer/read_model.py
@@ -0,0 +1,25 @@
+import mlflow
+import mlflow.spark
+import mlflow.pyfunc
+import site
+# import nlp
+
+
+def read_pyspark_model():
+    # newv = "nltkTransformer-0.0.12/nlp/"
+    # return mlflow.spark.load_model(site.getsitepackages()[0] + "/nlp/")
+    return mlflow.spark.load_model("/opt/anaconda3/envs/azurefunc/lib/python3.7/site-packages" + "/nlp")
+
+def read_pyfunc_model():
+    # newv = "nltkTransformer-0.0.12/nlp/"
+    # return mlflow.pyfunc.load_model(site.getsitepackages()[0] + "/nlp/")
+    return mlflow.pyfunc.load_model("/opt/anaconda3/envs/azurefunc/lib/python3.7/site-packages" + "/nlp")
+#
+# print(read_model())
+# # from pyspark.sql import SparkSession
+#
+# # spark = SparkSession.builder.getOrCreate()
+# mlflow.spark.load_model("/Users/ravi.teja/Work/nltkTransformer/nlp/")
+# from platform import python_version
+#
+# print(python_version())
