478b8908694cb8726b2186add2b54c114f3e0ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:14 +0000

    Add package allosaurus/0.0.0/allosaurus-0.0.0.tar.gz

---
+++ packages/allosaurus/allosaurus-0.0.0.tar.gz/allosaurus-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+   name='allosaurus',
+   version='0.0.0',
+   description='phone recognizer',
+   author='Xinjian Li',
+   packages=find_packages(),
+)
