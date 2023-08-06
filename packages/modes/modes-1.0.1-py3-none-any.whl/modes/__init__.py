1196a5ea09abf0428d5668b32b407d01eb13c2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:46 +0000

    Add package modes/1.0.1/modes-1.0.1-py3-none-any.whl

---
+++ packages/modes/modes-1.0.1-py3-none-any.whl/modes/__init__.py
@@ -0,0 +1,29 @@
+from modes import _analyse as fit
+from modes import materials
+from modes.config import CONFIG
+from modes.group_index import group_index
+from modes.mode_solver_full import mode_solver_full
+from modes.mode_solver_semi import mode_solver_semi
+from modes.sweep_waveguide import sweep_waveguide
+from modes.sweep_wavelength import sweep_wavelength
+from modes.waveguide import waveguide, waveguide_array, write_material_index
+
+__all__ = [
+    "fit",
+    "CONFIG",
+    "materials",
+    "mode_solver_full",
+    "mode_solver_semi",
+    "sweep_waveguide",
+    "sweep_wavelength",
+    "group_index",
+    "waveguide",
+    "waveguide_array",
+    "write_material_index",
+]
+
+
+__version__ = "1.0.1"
+
+if __name__ == "__main__":
+    print(__all__)
