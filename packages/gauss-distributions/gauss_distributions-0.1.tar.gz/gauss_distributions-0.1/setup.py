16a28b82ad87b9bb067a29d4b8a70ff2906cc222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:03 +0000

    Add package gauss-distributions/0.1/gauss_distributions-0.1.tar.gz

---
+++ packages/gauss-distributions/gauss_distributions-0.1.tar.gz/gauss_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gauss_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gauss_distributions'],
+      zip_safe=False)
