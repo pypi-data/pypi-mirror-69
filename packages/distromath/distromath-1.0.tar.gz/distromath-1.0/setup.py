6629f6ebbc89c3afca949978af59d267fb23ac78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:08 +0000

    Add package distromath/1.0/distromath-1.0.tar.gz

---
+++ packages/distromath/distromath-1.0.tar.gz/distromath-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distromath',
+      version='1.00',
+      description='Gaussian and Binomial distributions',
+      packages=['distromath'],
+      author = 'Nathaniel Solomon',
+      author_email = 'nathaniel.o.solomon@gmail.com',
+      zip_safe=False)
