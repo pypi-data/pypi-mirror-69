e43fb12fc136fa52a717c659799c6e9442d010e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:58 +0000

    Add package gauss-bi-distributions/0.1/gauss_bi_distributions-0.1.tar.gz

---
+++ packages/gauss-bi-distributions/gauss_bi_distributions-0.1.tar.gz/gauss_bi_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gauss_bi_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gauss_bi_distributions'],
+      zip_safe=False)
