617e067c815773863f0fb7464ee079ec381bf6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:52 +0000

    Add package flass/0.1.1/flass-0.1.1.tar.gz

---
+++ packages/flass/flass-0.1.1.tar.gz/flass-0.1.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="flass",
+    author="Kristian Flikka",
+    author_email="kristian.flikka@gmail.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=[
+        "click",
+        "graphviz",
+        "matplotlib",
+        "mlflow",
+        "numpy",
+        "pandas",
+        "pydot",
+        "sklearn",
+        "scikit-image",
+        "tensorflow",
+    ],
+    setup_requires=["wheel"],
+    description="Train Keras Convolutional Neural Network for image classification",
+    long_description="Train Keras Convolutional Neural Network for image classification",
+    entry_points={"console_scripts": ["flass=flass.cli:flass"]},
+    packages=["flass"],
+    license="MIT",
+    url="http://flikka.net",
+    version="0.1.1",
+)
