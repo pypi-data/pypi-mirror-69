2ab8c0b3df0aaacd63f85bed37a6c4c70e8a8c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:03 +0000

    Add package suanpan/0.12.45/suanpan-0.12.45-py3-none-any.whl

---
+++ packages/suanpan/suanpan-0.12.45-py3-none-any.whl/suanpan/stream/objects.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan import objects, utils
+from suanpan.mstorage import vars, mstorage
+
+
+class Variables(vars.Variables):
+    VARS_EXPIRE = 60
+
+    def __init__(self, context, mstorage):
+        super(Variables, self).__init__(mstorage)
+        self.context = context
+
+    def _getFullKey(self, key):
+        return f"{self.context.message['id']}.{key}"
+
+    def _getOriginKey(self, fullKey):
+        prefixLen = len(self.context.message["id"]) + 1
+        return fullKey[prefixLen:]
+
+    def _getRegisterKey(self):
+        return f"{self.context.message['id']}.vars"
+
+
+class Context(objects.Context):
+    @property
+    def vars(self):
+        return Variables(self, mstorage)
---
+++ packages/suanpan/suanpan-0.12.45-py3-none-any.whl/suanpan/utils/image.py
@@ -0,0 +1,82 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan import asyncio, path
+from suanpan.utils import convert
+
+
+def read(file, *args, **kwargs):
+    import cv2  # pylint: disable=import-outside-toplevel
+    img = cv2.imread(file, *args, **kwargs)  # pylint: disable-msg=e1101
+    if img is None:
+        raise Exception(f"Image read failed: {file}")
+    return img
+
+
+def _save(file, image):
+    import cv2  # pylint: disable=import-outside-toplevel
+    path.safeMkdirsForFile(file)
+    success = cv2.imwrite(file, image)  # pylint: disable-msg=e1101
+    if not success:
+        path.remove(file)
+        raise Exception(f"Image save failed: {file}")
+    return file
+
+
+def _saveflat(file, data):
+    image = convert.flatAsImage(data)
+    return _save(file, image)
+
+
+def _savegif(file, data):
+    import imageio  # pylint: disable=import-outside-toplevel
+    image3D = convert.to3D(data)
+    path.safeMkdirsForFile(file)
+    imageio.mimsave(file, image3D)
+    return file
+
+
+def save(file, data, flag=None):
+    mapping = {None: _save, "flat": _saveflat, "animated": _savegif}
+    func = mapping.get(flag)
+    if not func:
+        raise Exception(f"Unknow flag: {flag}")
+    return func(file, data)
+
+
+def saves(filepathPrefix, images):
+    counts = len(images)
+    n = len(str(counts))
+    iterable = (
+        (f"{filepathPrefix}_{str(index).zfill(n)}.png", image)
+        for index, image in enumerate(images)
+    )
+    asyncio.starmap(_save, iterable, thread=True)
+    return filepathPrefix
+
+
+def saveall(filepathPrefix, data):
+    image3D = convert.to3D(data)
+    layers = len(image3D)
+    n = len(str(layers))
+    results = [
+        asyncio.run(
+            _save,
+            args=(f"{filepathPrefix}_{str(index).zfill(n)}.png", image),
+            thread=True,
+        )
+        for index, image in enumerate(image3D)
+    ]
+    results.append(
+        asyncio.run(_saveflat, args=(f"{filepathPrefix}.png", image3D), thread=True)
+    )
+    results.append(
+        asyncio.run(_savegif, args=(f"{filepathPrefix}.gif", image3D), thread=True)
+    )
+    asyncio.wait(results)
+    return filepathPrefix
+
+
+def resize(data, size):
+    import cv2  # pylint: disable=import-outside-toplevel
+    return cv2.resize(data, size)  # pylint: disable-msg=e1101
