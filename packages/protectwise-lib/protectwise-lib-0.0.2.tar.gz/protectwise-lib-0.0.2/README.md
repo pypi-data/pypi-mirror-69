249a9f31a0726fa1a1ff54dd037fedd9a15743c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:57 +0000

    Add package protectwise-lib/0.0.2/protectwise-lib-0.0.2.tar.gz

---
+++ packages/protectwise-lib/protectwise-lib-0.0.2.tar.gz/protectwise-lib-0.0.2/README.md
@@ -0,0 +1,5 @@
+# protectwise-cli
+A python cli tool for interacting with protectwise api. 
+
+
+WIP, this tool is not ready for production but is a WIP. 
---
+++ packages/protectwise-lib/protectwise-lib-0.0.2.tar.gz/protectwise-lib-0.0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="protectwise-lib", # Replace with your own username
+    version="0.0.2",
+    author="5A4B48",
+    author_email="pw@cybersecintel.ca",
+    description="A python Library for interacting with Protectwise API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/5A4B48/protectwise-lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.1',
+)
+
