9cc1e454e86608649c546451416126816fc9927c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:07 +0000

    Add package lemberg-ai/0.0.0/lemberg_ai-0.0.0-py3-none-any.whl

---
+++ packages/lemberg-ai/lemberg_ai-0.0.0-py3-none-any.whl/lemberg_ai/__init__.py
@@ -0,0 +1,9 @@
+# This is free and unencumbered software released into the public domain.
+
+"""Lemberg.AI Software Development Kit (SDK) for Python."""
+
+import sys
+
+assert sys.version_info >= (3, 6), "Lemberg.AI SDK requires Python 3.6+"
+
+__all__ = []
