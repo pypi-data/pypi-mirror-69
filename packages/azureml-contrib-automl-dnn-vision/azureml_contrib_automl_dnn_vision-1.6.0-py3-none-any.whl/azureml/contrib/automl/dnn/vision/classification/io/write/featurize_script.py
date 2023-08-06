a53d737ae867de4b14af064cab0184f725c5a5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:23 +0000

    Add package azureml-contrib-automl-dnn-vision/1.6.0/azureml_contrib_automl_dnn_vision-1.6.0-py3-none-any.whl

---
+++ packages/azureml-contrib-automl-dnn-vision/azureml_contrib_automl_dnn_vision-1.6.0-py3-none-any.whl/azureml/contrib/automl/dnn/vision/classification/io/write/featurize_script.py
@@ -0,0 +1,28 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Featurize images from model produced by another run."""
+
+from azureml.contrib.automl.dnn.vision.classification.inference.score import featurize
+import argparse
+
+
+def main():
+    """Wrapper method to execute script only when called and not when imported."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--run_id', help='run id of the experiment that generated the model')
+    parser.add_argument('--experiment_name', help='experiment that ran the run which generated the model')
+    parser.add_argument('--output_file', help='path to output file')
+    parser.add_argument('--root_dir', help='path to root dir for files listed in image_list_file')
+    parser.add_argument('--image_list_file', help='image files list')
+
+    args = parser.parse_args()
+
+    featurize(args.run_id, experiment_name=args.experiment_name, output_file=args.output_file,
+              root_dir=args.root_dir, image_list_file=args.image_list_file)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
