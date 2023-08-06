0fac82bb776085a2b466ddf4738a83e0801a2ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:54 +0000

    Add package leon-distributions/1.0/leon_distributions-1.0.tar.gz

---
+++ packages/leon-distributions/leon_distributions-1.0.tar.gz/leon_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='leon_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['leon_distributions'],
+      author='Panayiotis Leontiou',
+      zip_safe=False)
