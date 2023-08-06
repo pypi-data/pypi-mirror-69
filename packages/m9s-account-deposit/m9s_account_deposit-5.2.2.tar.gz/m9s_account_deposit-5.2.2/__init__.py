b1f72408f4257d5ecbf42c0673aab5cc1455fe0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:56 +0000

    Add package m9s-account-deposit/5.2.2/m9s_account_deposit-5.2.2.tar.gz

---
+++ packages/m9s-account-deposit/m9s_account_deposit-5.2.2.tar.gz/m9s_account_deposit-5.2.2/__init__.py
@@ -0,0 +1,25 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+from . import account
+from . import configuration
+from . import invoice
+from . import party
+
+
+def register():
+    Pool.register(
+        account.AccountTypeTemplate,
+        account.AccountType,
+        configuration.Configuration,
+        configuration.ConfigurationDefaultAccount,
+        configuration.ConfigurationDepositSettlementMethod,
+        invoice.Invoice,
+        invoice.InvoiceLine,
+        invoice.DepositRecallStart,
+        party.Party,
+        module='account_deposit', type_='model')
+    Pool.register(
+        invoice.DepositRecall,
+        party.PartyErase,
+        module='account_deposit', type_='wizard')
