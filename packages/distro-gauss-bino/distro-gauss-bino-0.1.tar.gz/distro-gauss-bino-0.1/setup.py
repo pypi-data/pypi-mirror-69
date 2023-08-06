a4bd54fe08b7465bff6cc0fb918fa9df1fbe8b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:59 +0000

    Add package distro-gauss-bino/0.1/distro-gauss-bino-0.1.tar.gz

---
+++ packages/distro-gauss-bino/distro-gauss-bino-0.1.tar.gz/distro-gauss-bino-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distro-gauss-bino',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distro-gauss-bino'],
+      zip_safe=False)
