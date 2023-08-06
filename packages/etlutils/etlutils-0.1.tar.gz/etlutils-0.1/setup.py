df2bf97e3cbfff47406fea45e4b48ba2ed6b7881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:13 +0000

    Add package etlutils/0.1/etlutils-0.1.tar.gz

---
+++ packages/etlutils/etlutils-0.1.tar.gz/etlutils-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='etlutils',
+      version='0.1',
+      description='utilities functions for etl projects',
+      long_description='a collection of utilities functions useful for ETL projects',
+      long_description_content_type='text/plain',
+      author='Kevin Goldsmith',
+      author_email='github@kevingoldsmith.com',
+      url='https://github.com/kevingoldsmith/etlutils',
+      license='MIT',
+      keywords='development etl extract transform load',
+      project_urls={
+            'Source': 'https://github.com/kevingoldsmith/etlutils'
+      },
+      classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Developers',
+            'Natural Language :: English',
+            'Topic :: Utilities',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9'
+      ],
+      install_requires=['python-dateutil>=2.8.1'],
+      python_requires='>=3.6',
+      packages=['etlutils'])
