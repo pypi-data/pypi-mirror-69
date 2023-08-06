9c954f8810535273509e2c95b1c2fa3bd53d3be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:20 +0000

    Add package gux-dsnd-distributions/1.0/gux_dsnd_distributions-1.0.tar.gz

---
+++ packages/gux-dsnd-distributions/gux_dsnd_distributions-1.0.tar.gz/gux_dsnd_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gux_dsnd_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gux_dsnd_distributions'],
+      author='Gabriel Bezerra',
+      author_email='gabrielsrbezerra@gmail.com',
+      zip_safe=False)
