db6dd392a9b3f1ece9f8394d30950d7879142e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:02 +0000

    Add package kodexa/2.0.16/kodexa-2.0.16-py3-none-any.whl

---
+++ packages/kodexa/kodexa-2.0.16-py3-none-any.whl/kodexa/mixins/navigation.py
@@ -0,0 +1,21 @@
+class NavigationMixin:
+
+    @staticmethod
+    def get_name():
+        return "navigation"
+
+    @staticmethod
+    def get_renderer(document):
+        return None
+
+    @staticmethod
+    def to_text(self):
+        return None
+
+    @staticmethod
+    def get_dependencies():
+        return ['core']
+
+    @staticmethod
+    def apply_to(node):
+        pass
