86bb220a7d23801c0adb287152cc49d1b984f0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:03 +0000

    Add package MPlearn-distributions/0.1/MPlearn_distributions-0.1.tar.gz

---
+++ packages/MPlearn-distributions/MPlearn_distributions-0.1.tar.gz/MPlearn_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='MPlearn_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['MPlearn_distributions'],
+      zip_safe=False)
