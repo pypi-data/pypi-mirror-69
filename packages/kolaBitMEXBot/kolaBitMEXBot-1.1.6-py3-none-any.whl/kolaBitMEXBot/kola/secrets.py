33f2bfc336ce6117c523d900adfdf6b442241801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:46 +0000

    Add package kolaBitMEXBot/1.1.6/kolaBitMEXBot-1.1.6-py3-none-any.whl

---
+++ packages/kolaBitMEXBot/kolaBitMEXBot-1.1.6-py3-none-any.whl/kolaBitMEXBot/kola/secrets.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+File with secrets.
+
+Should find a better way to store them.
+"""
+
+LIVE_KEY = "zIKTHISISARANDOMKEYNHII3"
+LIVE_SECRET = "HUMOI9OkK89aIoXDAND THIS IS A SECRET0KAthnauwKj0"
+TEST_KEY = "THEn_XATESTgXOcfKEYbuttz"
+TEST_SECRET = "ANDjmJ3tbACz12VERYnzJS7LONGrPKI3r4uSECRETMU2C4HO"
