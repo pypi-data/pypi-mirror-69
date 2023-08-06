c452c2e09f63491e814eccb99a2bb86b86c5469b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:16 +0000

    Add package ems-gcp-toolkit/0.1.61/ems-gcp-toolkit-0.1.61.tar.gz

---
+++ packages/ems-gcp-toolkit/ems-gcp-toolkit-0.1.61.tar.gz/ems-gcp-toolkit-0.1.61/bigquery/job/ems_load_job.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from bigquery.job.config.ems_load_job_config import EmsLoadJobConfig
+from bigquery.job.ems_job import EmsJob
+from bigquery.job.ems_job_state import EmsJobState
+
+
+class EmsLoadJob(EmsJob):
+    def __init__(self,
+                 job_id: str,
+                 load_config: EmsLoadJobConfig,
+                 state: EmsJobState,
+                 error_result: Union[dict, None]):
+        super(EmsLoadJob, self).__init__(job_id, state, error_result)
+
+        self.__load_config = load_config
+
+    @property
+    def load_config(self) -> EmsLoadJobConfig:
+        return self.__load_config
