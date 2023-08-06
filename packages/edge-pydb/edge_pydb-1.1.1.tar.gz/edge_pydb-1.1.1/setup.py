6c38ff3541107bfd2bf366fee614de0254f98993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:22 +0000

    Add package edge-pydb/1.1.1/edge_pydb-1.1.1.tar.gz

---
+++ packages/edge-pydb/edge_pydb-1.1.1.tar.gz/edge_pydb-1.1.1/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as req:
+    requirements = req.read()
+
+# version_num = input("Input the new version number: ")
+version_num  = "1.1.1"
+setuptools.setup(
+    name="edge_pydb",
+    version=version_num,
+    author="Tony Wong",
+    author_email="tonywong94@gmail.com",
+    description="Python based database for CARMA EDGE",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tonywong94/edge_pydb",
+    packages=['edge_pydb'],
+    package_data={
+        'edge_pydb': [
+            'dat_*/*/*.csv',
+            'img_*/*/*.csv',
+            'img_*/*.hdf5',
+            '_config.json'
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    include_package_data=True,
+    install_requires=requirements,
+)
