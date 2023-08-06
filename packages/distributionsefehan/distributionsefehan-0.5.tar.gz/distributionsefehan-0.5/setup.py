d52ace3d18eb05dd9d94f698b05ffcf7f29d768a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:05 +0000

    Add package distributionsefehan/0.5/distributionsefehan-0.5.tar.gz

---
+++ packages/distributionsefehan/distributionsefehan-0.5.tar.gz/distributionsefehan-0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributionsefehan',
+      version='0.5',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author ='Efehan Danisman',
+      author_email = 'efedanisman@gmail.com',
+      zip_safe=False)
