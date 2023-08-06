8d9c380e7d980e1ec1f85166c960ee3c62575dae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:25 +0000

    Add package zigpy-znp/0.0.5/zigpy_znp-0.0.5-py3-none-any.whl

---
+++ packages/zigpy-znp/zigpy_znp-0.0.5-py3-none-any.whl/zigpy_znp/commands/__init__.py
@@ -0,0 +1,36 @@
+from .rpc_error import RPCError
+from .af import AF
+from .app import App
+from .app_config import AppConfig
+from .mac import MAC
+from .sapi import SAPI
+from .sys import SYS
+from .util import Util
+from .zdo import ZDO
+from .zgp import ZGP
+
+ALL_COMMANDS = [
+    RPCError,
+    AF,
+    App,
+    AppConfig,
+    MAC,
+    SAPI,
+    SYS,
+    Util,
+    ZDO,
+    ZGP,
+]
+
+COMMANDS_BY_ID = {}
+
+for cmds in ALL_COMMANDS:
+    for command in cmds:
+        if command.Req is not None:
+            COMMANDS_BY_ID[command.Req.header] = command.Req
+
+        if command.Rsp is not None:
+            COMMANDS_BY_ID[command.Rsp.header] = command.Rsp
+
+        if command.Callback is not None:
+            COMMANDS_BY_ID[command.Callback.header] = command.Callback
