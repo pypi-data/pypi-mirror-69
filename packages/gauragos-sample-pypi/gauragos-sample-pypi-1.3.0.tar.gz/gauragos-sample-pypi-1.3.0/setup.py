9bd9a59db346c6586320881c9ef4399dca9a9201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:37 +0000

    Add package gauragos-sample-pypi/1.3.0/gauragos-sample-pypi-1.3.0.tar.gz

---
+++ packages/gauragos-sample-pypi/gauragos-sample-pypi-1.3.0.tar.gz/gauragos-sample-pypi-1.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+import sys
+
+__version__ = '1.3.0'
+
+
+setup(name='gauragos-sample-pypi',
+      version=__version__,
+      description='desc',
+      license='lic',
+      author='gary',
+      author_email='gouravgoswami48@gmail.com',
+      long_description='long_desc',
+      url='https://github.com/gary1998',
+      keywords='abc',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+      ]
+    )
