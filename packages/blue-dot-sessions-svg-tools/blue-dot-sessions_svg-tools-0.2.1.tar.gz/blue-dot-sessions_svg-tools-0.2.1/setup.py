7124670cdfa1c94957cff26925debfd767279f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:52 +0000

    Add package blue-dot-sessions-svg-tools/0.2.1/blue-dot-sessions_svg-tools-0.2.1.tar.gz

---
+++ packages/blue-dot-sessions-svg-tools/blue-dot-sessions_svg-tools-0.2.1.tar.gz/blue-dot-sessions_svg-tools-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['svg_tools']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'blue-dot-sessions-svg-tools',
+    'version': '0.2.1',
+    'description': 'SVG tools for Blue Dot Sessions',
+    'long_description': None,
+    'author': 'Dean Shaff',
+    'author_email': 'dean.shaff@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
