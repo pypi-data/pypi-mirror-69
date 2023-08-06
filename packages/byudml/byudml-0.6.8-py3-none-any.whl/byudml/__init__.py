f1e2ad961546a2fa6e6655247472828340658dda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:01 +0000

    Add package byudml/0.6.8/byudml-0.6.8-py3-none-any.whl

---
+++ packages/byudml/byudml-0.6.8-py3-none-any.whl/byudml/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.6.8'
+__metafeature_version__ = '0.4.5'
+__metafeature_path__ = 'd3m.primitives.metalearning.metafeature_extractor.BYU'
+__imputer_version__ = '0.2.1'
+__imputer_path__ = 'd3m.primitives.data_preprocessing.random_sampling_imputer.BYU'
