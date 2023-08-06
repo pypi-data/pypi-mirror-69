a1c943f5b8c1db05ff31c0e0ec80530fc1aecea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:15 +0000

    Add package vidit-distribution/0.3/vidit_distribution-0.3.tar.gz

---
+++ packages/vidit-distribution/vidit_distribution-0.3.tar.gz/vidit_distribution-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vidit_distribution',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Vidit Kumar Jain',
+      author_email= 'viditjain0025@gmail.com',
+      zip_safe=False)
