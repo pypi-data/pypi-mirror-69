adb9040776100aa60c25d223904c71a855110337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:44 +0000

    Add package cuenca/0.2.0.dev1/cuenca-0.2.0.dev1-py3-none-any.whl

---
+++ packages/cuenca/cuenca-0.2.0.dev1-py3-none-any.whl/cuenca/resources/deposits.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+from typing import ClassVar, Optional, cast
+
+from ..types import DepositNetwork
+from .accounts import Account
+from .base import Cacheable, Transaction
+from .resources import retrieve_uri
+
+
+class Deposit(Transaction, Cacheable):
+    _resource: ClassVar = 'deposits'
+
+    source_uri: str
+    network: DepositNetwork
+    tracking_key: Optional[str]  # clave rastreo if network is SPEI
+
+    @property  # type: ignore
+    @lru_cache()
+    def source(self) -> Account:
+        return cast(Account, retrieve_uri(self.source_uri))
