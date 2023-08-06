1d852a0b4924b9028958a84be8afcea1b709fcc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:01 +0000

    Add package distributions-Binomial-Gaussian/1.5/distributions_Binomial_Gaussian-1.5.tar.gz

---
+++ packages/distributions-Binomial-Gaussian/distributions_Binomial_Gaussian-1.5.tar.gz/distributions_Binomial_Gaussian-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_Binomial_Gaussian',
+      version='1.5',
+      description='Gaussian distributions',
+      packages=['distributions_Binomial_Gaussian'],
+      author='Raveena Mathur',
+      author_email='raveenam@usc.edu',
+     zip_safe=False)
