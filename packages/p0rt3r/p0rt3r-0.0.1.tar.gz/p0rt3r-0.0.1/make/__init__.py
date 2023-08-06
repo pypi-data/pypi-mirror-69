f66b786a3c40c6c43f5db070e90d06833173f83d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:19 +0000

    Add package p0rt3r/0.0.1/p0rt3r-0.0.1.tar.gz

---
+++ packages/p0rt3r/p0rt3r-0.0.1.tar.gz/p0rt3r-0.0.1/make/__init__.py
@@ -0,0 +1,1 @@
+from .p0rt3r import makePack
---
+++ packages/p0rt3r/p0rt3r-0.0.1.tar.gz/p0rt3r-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+__project__ = "p0rt3r"
+__version__ = "0.0.1" 
+__description__ = "Simple, efficient, and relatively quick port scanner"
+__packages__ = ["make"]
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+)
