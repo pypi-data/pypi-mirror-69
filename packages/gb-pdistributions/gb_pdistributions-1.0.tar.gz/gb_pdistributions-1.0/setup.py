355478817e1b1ce1c567fdeef158c39719c218e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:44 +0000

    Add package gb-pdistributions/1.0/gb_pdistributions-1.0.tar.gz

---
+++ packages/gb-pdistributions/gb_pdistributions-1.0.tar.gz/gb_pdistributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gb_pdistributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_pdistributions'],
+      auther='R Kritika Kaveri',
+      email='rkkaveri.kk@gmail.com',
+      zip_safe=False)
