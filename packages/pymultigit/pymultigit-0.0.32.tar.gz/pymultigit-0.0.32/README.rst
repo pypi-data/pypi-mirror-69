a7a86ed30edd9dcb2f543b51e781d210835533df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:01 +0000

    Add package pymultigit/0.0.32/pymultigit-0.0.32.tar.gz

---
+++ packages/pymultigit/pymultigit-0.0.32.tar.gz/pymultigit-0.0.32/README.rst
@@ -0,0 +1,5 @@
+=======================
+*pymultigit* project by Mark Veltzer
+=======================
+
+version: (0, 0, 32)
---
+++ packages/pymultigit/pymultigit-0.0.32.tar.gz/pymultigit-0.0.32/setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+"""
+The documentation can be found at:
+http://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
+setuptools.setup(
+    # the first three fields are a must according to the documentation
+    name='pymultigit',
+    version='0.0.32',
+    packages=[
+        'pymultigit',
+        'pymultigit.endpoints',
+    ],
+    # from here all is optional
+    description='pymultigit is a command to help you deal with multiple git repositories',
+    long_description='pymultigit is a command to help you deal with multiple git repositories',
+    author='Mark Veltzer',
+    author_email='mark.veltzer@gmail.com',
+    maintainer='Mark Veltzer',
+    maintainer_email='mark.veltzer@gmail.com',
+    keywords=[
+        'git',
+        'python',
+        'repositories',
+        'multiple',
+    ],
+    url='https://veltzer.github.io/pymultigit',
+    download_url='https://github.com/veltzer/pymultigit',
+    license='MIT',
+    platforms=[
+        'python3',
+    ],
+    install_requires=[
+        'gitpython',
+        'pyfakeuse',
+        'pytconf',
+        'pylogconf',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    data_files=[
+    ],
+    entry_points={'console_scripts': [
+        'pymultigit=pymultigit.endpoints.main:main',
+    ]},
+    python_requires='>=3.5',
+)
