569f1ac81cc30b278361f53918efa8dd0db23051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:27 +0000

    Add package n-vy-distributions/0.1/n_vy_distributions-0.1.tar.gz

---
+++ packages/n-vy-distributions/n_vy_distributions-0.1.tar.gz/n_vy_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='n_vy_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['n_vy_distributions'],
+      zip_safe=False)
