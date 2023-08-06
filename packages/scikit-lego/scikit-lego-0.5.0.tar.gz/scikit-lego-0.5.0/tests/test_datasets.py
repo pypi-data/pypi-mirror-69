a1df85e6161dc46ee2f98f0d74ef7ae2db56b95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:17 +0000

    Add package scikit-lego/0.5.0/scikit-lego-0.5.0.tar.gz

---
+++ packages/scikit-lego/scikit-lego-0.5.0.tar.gz/scikit-lego-0.5.0/tests/test_datasets.py
@@ -0,0 +1,47 @@
+import pytest
+from sklego.datasets import load_chicken, load_abalone, make_simpleseries, load_hearts
+
+
+def test_chickweight1():
+    X, y = load_chicken(return_X_y=True)
+    assert X.shape == (578, 3)
+    assert y.shape[0] == 578
+
+
+def test_chickweight2():
+    df = load_chicken(give_pandas=True)
+    assert df.shape == (578, 4)
+
+
+def test_abalone1():
+    X, y = load_abalone(return_X_y=True)
+    assert X.shape == (4177, 8)
+    assert y.shape[0] == 4177
+
+
+def test_abalone2():
+    df = load_abalone(give_pandas=True)
+    assert df.shape == (4177, 9)
+
+
+def test_simpleseries_constant_season():
+    df = (
+        make_simpleseries(
+            n_samples=365 * 2,
+            give_pandas=True,
+            start_date="2018-01-01",
+            trend=0,
+            noise=0,
+            season_trend=0,
+        )
+        .assign(month=lambda d: d["date"].dt.month)
+        .assign(year=lambda d: d["date"].dt.year)
+    )
+    agg = df.groupby(["year", "month"]).mean().reset_index()
+    assert agg.loc[lambda d: d["month"] == 1].var().month == pytest.approx(
+        0.0, abs=0.01
+    )
+
+def test_load_hearts():
+    df = load_hearts(give_pandas=True)
+    assert df.shape == (303, 14)
