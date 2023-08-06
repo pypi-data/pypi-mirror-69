d715bd21a22741d22177aafbcf6c88004ffad281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:16 +0000

    Add package roseng/0.5/roseng-0.5.tar.gz

---
+++ packages/roseng/roseng-0.5.tar.gz/roseng-0.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='roseng',
+      version='0.5',
+      description='ROS Engine',
+      author='Dirk Zhou',
+      author_email='dontbmh2011@gmail.com',
+      license='MIT',
+      packages=['roseng'],
+      python_requires='>=3.6',
+      zip_safe=False)
