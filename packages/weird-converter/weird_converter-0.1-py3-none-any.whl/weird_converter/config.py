fa9ce6862a65f7769ccf34d1ecf9014ce81aeb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:15 +0000

    Add package weird-converter/0.1/weird_converter-0.1-py3-none-any.whl

---
+++ packages/weird-converter/weird_converter-0.1-py3-none-any.whl/weird_converter/config.py
@@ -0,0 +1,9 @@
+EXTENSION_IMAGE: str = 'png'
+EXTENSION_AUDIO: str = 'wav'
+
+TEST_FOLDER_DATA: str = 'data'
+TEST_FOLDER_EXAMPLES: str = 'examples'
+TEST_FOLDER_AUDIO: str = 'from_audio'
+TEST_FILE_AUDIO: str = 'baby_elephant.wav'
+TEST_FILE_AUDIO_LIST: list = [TEST_FILE_AUDIO, 'cantina_short.wav', 'fanfare.wav', 'silent.wav']
+TEST_FILE_IMAGE: str = f'image.{EXTENSION_IMAGE}'
