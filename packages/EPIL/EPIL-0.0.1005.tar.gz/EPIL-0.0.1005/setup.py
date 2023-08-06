12fca8a0270e18140db5bf6e4e8f1fcaf9dbb561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:49 +0000

    Add package EPIL/0.0.1005/EPIL-0.0.1005.tar.gz

---
+++ packages/EPIL/EPIL-0.0.1005.tar.gz/EPIL-0.0.1005/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+ldesc = 'Module for work with PIL (replace colors, change color and more...)'
+desc  = 'Module for work with PIL (replace colors, change color and more...)'
+
+setup (
+
+    name='EPIL',
+    version='0.0.1005',
+    description=desc,
+    long_description=ldesc,
+    packages=['EPIL'],
+    author_email='kolyaleonof@yandex.ru'
+
+)
