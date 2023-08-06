b1d5fcdad3c0e2eba6383208ef4310940c633f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:11 +0000

    Add package Frank-distributions/2.7/Frank_distributions-2.7.tar.gz

---
+++ packages/Frank-distributions/Frank_distributions-2.7.tar.gz/Frank_distributions-2.7/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Frank_distributions',
+      version='2.7',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Frank',
+#       author_email = 
+      zip_safe=False)
