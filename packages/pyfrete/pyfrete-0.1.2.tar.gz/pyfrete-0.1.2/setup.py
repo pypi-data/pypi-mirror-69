1c24bf91b11f76f9d306740629fafac180329eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:10 +0000

    Add package pyfrete/0.1.2/pyfrete-0.1.2.tar.gz

---
+++ packages/pyfrete/pyfrete-0.1.2.tar.gz/pyfrete-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='pyfrete',
+    version='0.1.2',    
+    description='API for Brazillian Shipping Methods in Python',
+    url='https://github.com/felipevisu/pyfrete',
+    author='Felipe Faria',
+    author_email='felipevisu@gmail.com',
+    license='BSD 2-clause',
+    packages=['pyfrete'],
+    install_requires=['zeep>=3', 'zipp>=3'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+
+    python_requires='>=3.6',
+)
