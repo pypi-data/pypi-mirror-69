2051d33c754e85f6afdb3e616bf91c80532cf1bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:24 +0000

    Add package ngkel-distributions/0.1/ngkel_distributions-0.1.tar.gz

---
+++ packages/ngkel-distributions/ngkel_distributions-0.1.tar.gz/ngkel_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ngkel_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ngkel_distributions'],
+      authoer = 'Ng Ka Lok',
+      author_email = 'ngkel@connect.hku.hk',
+      zip_safe=False)
