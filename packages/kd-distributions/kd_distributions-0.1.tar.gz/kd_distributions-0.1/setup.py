e564e75ad7a95ccde8c6ac6e63dbda73db08b84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:49 +0000

    Add package kd-distributions/0.1/kd_distributions-0.1.tar.gz

---
+++ packages/kd-distributions/kd_distributions-0.1.tar.gz/kd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kd_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['kd_distributions'],
+      author = 'Kaustubh Dixit',
+      author_email = 'kdkishu335@gmail.com',
+      zip_safe=False)
