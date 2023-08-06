31414945f75864389ed305261a7937de58c9a106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:07 +0000

    Add package Naun/0.0.6/Naun-0.0.6.tar.gz

---
+++ packages/Naun/Naun-0.0.6.tar.gz/Naun-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("readme.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name = 'Naun',
+    version = "0.0.6",
+    author = "Andreas Evensen",
+    author_email = "Andreas.evensen11@gmail.com",
+    description = "This package implements video, and picture recognition using dlib and face_recognition.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    py_modules=["Nuan"],
+    package_dir={'':'src'},
+    packages = find_packages(),
+    Lisence = "Mit",
+    install_requires = ["dlib", "face_recognition"],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License"
+    ],
+    keywords = "Facial Detection"
+    
+)
