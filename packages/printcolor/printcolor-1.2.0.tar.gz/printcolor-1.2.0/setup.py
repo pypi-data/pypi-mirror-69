2d7361cc9da10a5eb53d898d7db54471e440a6d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:20 +0000

    Add package printcolor/1.2.0/printcolor-1.2.0.tar.gz

---
+++ packages/printcolor/printcolor-1.2.0.tar.gz/printcolor-1.2.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import print_function
+from setuptools import setup, find_packages
+
+with open("README.md",'r') as f:
+    long_description = f.read()
+
+setup(name = "printcolor",
+    version = "1.2.0",
+    description = "print colorful string",
+    author = "zack",
+    author_email = "514838728@qq.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/dustingo/PrintColor.git",
+    packages = find_packages(),
+    install_requires = [],
+    py_modules = ["printcolor","printcolor2"],
+    platforms = ["linux"],
+)
