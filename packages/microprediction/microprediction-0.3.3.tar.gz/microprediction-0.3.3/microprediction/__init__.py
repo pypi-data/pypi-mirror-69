48b55f649a6aee32df653e5c36eebef1bcdec5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:00 +0000

    Add package microprediction/0.3.3/microprediction-0.3.3.tar.gz

---
+++ packages/microprediction/microprediction-0.3.3.tar.gz/microprediction-0.3.3/microprediction/__init__.py
@@ -0,0 +1,5 @@
+from microprediction.conventions import MicroConventions, new_key
+from microprediction.reader import MicroReader
+from microprediction.writer import MicroWriter
+from microprediction.crawler import MicroCrawler
+from microprediction.supporter import donate
