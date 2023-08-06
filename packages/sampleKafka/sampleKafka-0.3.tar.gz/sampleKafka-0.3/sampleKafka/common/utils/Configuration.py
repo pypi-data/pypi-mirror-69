e383462735b43119126689abfe24ca6f2202ceda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:03 +0000

    Add package sampleKafka/0.3/sampleKafka-0.3.tar.gz

---
+++ packages/sampleKafka/sampleKafka-0.3.tar.gz/sampleKafka-0.3/sampleKafka/common/utils/Configuration.py
@@ -0,0 +1,10 @@
+class Configuration:
+    def __init__(self):
+        print("configuration")
+
+
+
+
+
+
+
---
+++ packages/sampleKafka/sampleKafka-0.3.tar.gz/sampleKafka-0.3/sampleKafka/producer/producer2.py
@@ -0,0 +1,7 @@
+from .producer1 import producer1
+from ..common.utils.Configuration import Configuration
+class producer2:
+    def __init__(self):
+        p1 = producer1()
+        c = Configuration()
+        print("Producer2")
