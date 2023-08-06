974a50acb496a1afa47af7a082c575bcd7f982df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:23 +0000

    Add package allosaurus/0.0.6/allosaurus-0.0.6.tar.gz

---
+++ packages/allosaurus/allosaurus-0.0.6.tar.gz/allosaurus-0.0.6/allosaurus/am/factory.py
@@ -0,0 +1,27 @@
+from allosaurus.am.allosaurus_torch import AllosaurusTorchModel
+from allosaurus.am.utils import *
+import json
+from argparse import Namespace
+
+def read_am(model_path, inference_config):
+    """
+    load pretrained acoustic model
+
+    :param model_path: path to the
+    :return:
+    """
+
+    am_config = Namespace(**json.load(open(str(model_path / 'am_config.json'))))
+
+    assert am_config.model == 'allosaurus', "This project only support allosaurus model"
+
+    model = AllosaurusTorchModel(am_config)
+
+    # load weights
+    torch_load(model, str(model_path / 'model.pt'))
+
+    # move to device
+    if inference_config.device_id >= 0:
+        model = model.cuda(inference_config.device_id)
+
+    return model
