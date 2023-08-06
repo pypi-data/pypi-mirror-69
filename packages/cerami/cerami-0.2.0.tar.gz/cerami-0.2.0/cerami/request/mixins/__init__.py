04d8de8536a2b906cc45ddf88dfa51a408f1385a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:21 +0000

    Add package cerami/0.2.0/cerami-0.2.0.tar.gz

---
+++ packages/cerami/cerami-0.2.0.tar.gz/cerami-0.2.0/cerami/request/mixins/__init__.py
@@ -0,0 +1,6 @@
+from .base_request import BaseRequest
+from .filterable import Filterable
+from .keyable import Keyable
+from .returnable import Returnable
+from .projectable import Projectable
+from .limitable import Limitable
