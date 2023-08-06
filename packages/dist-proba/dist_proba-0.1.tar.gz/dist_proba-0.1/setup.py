7755fc084daf6a34c2ffdc4e3be8325f99ff14aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:58 +0000

    Add package dist-proba/0.1/dist_proba-0.1.tar.gz

---
+++ packages/dist-proba/dist_proba-0.1.tar.gz/dist_proba-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_proba',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_proba'],
+      author= 'Christophe Berset',
+      author_email = 'bersetchristophe@gmail.com',
+      zip_safe=False)
