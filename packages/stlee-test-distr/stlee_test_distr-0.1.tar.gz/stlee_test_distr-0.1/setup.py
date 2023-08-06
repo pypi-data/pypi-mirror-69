0c5d09cb1ca6e52998a74e2c34bb6f74ad08b654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:45 +0000

    Add package stlee-test-distr/0.1/stlee_test_distr-0.1.tar.gz

---
+++ packages/stlee-test-distr/stlee_test_distr-0.1.tar.gz/stlee_test_distr-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stlee_test_distr',
+      version='0.1',
+      description='Gaussian and Binomial distr.',
+      packages=['stlee_test_distr'],
+      author = 'stlee',
+      author_email = 'myemail@testmail.com',
+      zip_safe=False)
