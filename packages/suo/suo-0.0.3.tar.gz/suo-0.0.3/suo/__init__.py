26d30ce9b8ae47077e5c2edfbaabe0b19546ece0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:41 +0000

    Add package suo/0.0.3/suo-0.0.3.tar.gz

---
+++ packages/suo/suo-0.0.3.tar.gz/suo-0.0.3/suo/__init__.py
@@ -0,0 +1,13 @@
+from . import shell
+from . import version
+from .utils import suo
+from .utils import kuo
+
+__version__ = version.VERSION
+
+__all__ = [
+    "shell",
+    "version",
+    "suo",
+    "kuo"
+]
