d0688efff4711c850d3f4cee50780640024108cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:22 +0000

    Add package gcloud-rest-datastore/6.1.0/gcloud-rest-datastore-6.1.0.tar.gz

---
+++ packages/gcloud-rest-datastore/gcloud-rest-datastore-6.1.0.tar.gz/gcloud-rest-datastore-6.1.0/gcloud/rest/datastore/array.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import super
+from builtins import str
+from future import standard_library
+standard_library.install_aliases()
+try:
+    from collections.abc import Sequence
+except ImportError:
+    from collections import Sequence
+from typing import Any
+from typing import Dict
+from typing import List
+
+from gcloud.rest.datastore import value
+
+
+# https://cloud.google.com/datastore/docs/reference/data/rest/v1/projects/runQuery#ArrayValue
+class Array(Sequence):
+    def __init__(self, items           )        :
+        super(Sequence, self).__init__()  # pylint: disable=bad-super-call
+        self.items = items
+
+    def __eq__(self, other     )        :
+        if not isinstance(other, Array):
+            return False
+        return self.items == other.items
+
+    def __repr__(self)       :
+        return str(self.to_repr())
+
+    def __getitem__(self, index     )       :
+        return self.items[index]
+
+    def __len__(self)       :
+        return len(self.items)
+
+    @classmethod
+    def from_repr(cls, data                )           :
+        return cls([value.Value.from_repr(x) for x in data['values']])
+
+    def to_repr(self)                  :
+        return {'values': [x.to_repr() for x in self]}
---
+++ packages/gcloud-rest-datastore/gcloud-rest-datastore-6.1.0.tar.gz/gcloud-rest-datastore-6.1.0/tests/unit/property_order_test.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import str
+from future import standard_library
+standard_library.install_aliases()
+from builtins import object
+import pytest
+from gcloud.rest.datastore import Direction
+from gcloud.rest.datastore import PropertyOrder
+
+
+class TestPropertyOrder(object):
+    @staticmethod
+    def test_order_defaults_to_ascending():
+        assert PropertyOrder('prop_name').direction == Direction.ASCENDING
+
+    @staticmethod
+    def test_order_from_repr(property_order):
+        original_order = property_order
+        data = {
+            'property': {
+                'name': original_order.prop
+            },
+            'direction': original_order.direction
+        }
+
+        output_order = PropertyOrder.from_repr(data)
+
+        assert output_order == original_order
+
+    @staticmethod
+    def test_order_to_repr():
+        property_name = 'my_prop'
+        direction = Direction.DESCENDING
+        order = PropertyOrder(property_name, direction)
+
+        r = order.to_repr()
+
+        assert r['property']['name'] == property_name
+        assert r['direction'] == direction.value
+
+    @staticmethod
+    def test_repr_returns_to_repr_as_string(property_order):
+        assert repr(property_order) == str(property_order.to_repr())
+
+    @staticmethod
+    @pytest.fixture()
+    def property_order()                 :
+        return PropertyOrder(prop='prop_name', direction=Direction.DESCENDING)
