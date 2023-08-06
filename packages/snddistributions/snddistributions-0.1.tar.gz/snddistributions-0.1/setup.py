625b2c9e0a031dc212c45db5179dcb5bb320ae36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:38 +0000

    Add package snddistributions/0.1/snddistributions-0.1.tar.gz

---
+++ packages/snddistributions/snddistributions-0.1.tar.gz/snddistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='snddistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='kushagra',
+      author_email='kushagrarastogiies@gmail.com',
+      zip_safe=False)
