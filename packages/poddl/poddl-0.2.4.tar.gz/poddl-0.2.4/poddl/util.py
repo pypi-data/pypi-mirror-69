73bfe800f1c0e5a7420d5034f0b1c950f3cdcc51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:51 +0000

    Add package poddl/0.2.4/poddl-0.2.4.tar.gz

---
+++ packages/poddl/poddl-0.2.4.tar.gz/poddl-0.2.4/poddl/util.py
@@ -0,0 +1,22 @@
+import poddl
+import argparse
+import logging
+
+
+def main():
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter('%(message)s'))
+    handler.setLevel(logging.INFO)
+    logger = logging.getLogger('poddl')
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser(description='A basic RSS podcast downloading script')
+    parser.add_argument('--url', help='The RSS feed URL', required=True, type=str)
+    parser.add_argument('--summary', help='Show a summary of available episodes', action='store_true')
+    parser.add_argument('--destination', help='Directory to save podcast files to', default='~/Downloads/poddl',
+                        type=str)
+    parser.add_argument('--limit', help="Limit the number of items retrieved", type=int, default=-1)
+    args = parser.parse_args()
+
+    poddl.get(args.url, args.destination, limit=args.limit, summary=args.summary)
