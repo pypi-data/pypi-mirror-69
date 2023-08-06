d1f6ef6c3c1ec283008cdd0f234bf34ffbbabb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:55 +0000

    Add package text-actions-girvel/0.0.1/text-actions-girvel-0.0.1.tar.gz

---
+++ packages/text-actions-girvel/text-actions-girvel-0.0.1.tar.gz/text-actions-girvel-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="text-actions-girvel",
+    version="0.0.1",
+    author="girvel",
+    author_email="widauka@yandex.ru",
+    description="Package for managing UI commands in text package in bash syntax",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/girvel/text_actions",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
