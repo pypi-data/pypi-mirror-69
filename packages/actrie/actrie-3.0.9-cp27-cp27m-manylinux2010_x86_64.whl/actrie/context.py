566ae757af4a52fe4603839dfa1b1d78a369c9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:36 +0000

    Add package actrie/3.0.9/actrie-3.0.9-cp27-cp27m-manylinux2010_x86_64.whl

---
+++ packages/actrie/actrie-3.0.9-cp27-cp27m-manylinux2010_x86_64.whl/actrie/context.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+
+from collections import Iterator
+
+from . import _actrie
+from .util import convert2pass
+
+
+class Context(Iterator):
+    def __init__(self, matcher, content, return_byte_pos=False):
+        self._matcher = matcher
+        self._content = convert2pass(content)
+        self._return_byte_pos = return_byte_pos
+        self._context = _actrie.AllocContext(self._matcher._matcher)
+        self._uninitialized = not _actrie.ResetContext(self._context, self._content, self._return_byte_pos)
+
+    def __del__(self):
+        _actrie.FreeContext(self._context)
+
+    def reset(self, content=None, return_byte_pos=None):
+        if content is not None:
+            self._content = convert2pass(content)
+        if return_byte_pos is not None:
+            self._return_byte_pos = return_byte_pos
+        self._uninitialized = not _actrie.ResetContext(self._context, self._content, self._return_byte_pos)
+
+    def __next__(self):
+        if self._uninitialized:
+            raise StopIteration()
+        matched = _actrie.Next(self._context)
+        if matched is None:
+            raise StopIteration()
+        return matched
+
+    def next(self):
+        return self.__next__()
+
+
+class PrefixContext(Context):
+    def __next__(self):
+        if self._uninitialized:
+            raise StopIteration()
+        matched = _actrie.NextPrefix(self._context)
+        if matched is None:
+            raise StopIteration()
+        return matched
