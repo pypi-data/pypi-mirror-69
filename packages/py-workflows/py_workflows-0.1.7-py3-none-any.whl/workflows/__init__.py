a80d2453cf927e62abfa8e21c7348ad7e93eac97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:52 +0000

    Add package py-workflows/0.1.7/py_workflows-0.1.7-py3-none-any.whl

---
+++ packages/py-workflows/py_workflows-0.1.7-py3-none-any.whl/workflows/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from workflows.file_task import FileTask
+from workflows.run import run_tasks
+from workflows.task import Task
+
+
+__all__ = ["FileTask", "run_tasks", "Task"]
+__version__ = "0.1.7"
