5067366e0fde684c975e45d42abf231c8bf6447e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:12 +0000

    Add package pytest-func-cov/0.2.2/pytest_func_cov-0.2.2.tar.gz

---
+++ packages/pytest-func-cov/pytest_func_cov-0.2.2.tar.gz/pytest_func_cov-0.2.2/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="pytest_func_cov",
+    packages=["pytest_func_cov"],
+    version="0.2.2",
+    license="MIT",
+    description="Pytest plugin for measuring function coverage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Radu Ghitescu",
+    author_email="radu.ghitescu@gmail.com",
+    url="https://github.com/radug0314/pytest_func_cov",
+    install_requires=["pytest>=5"],
+    entry_points={"pytest11": ["pytest_func_cov = pytest_func_cov.plugin"]},
+    classifiers=[
+        "Framework :: Pytest",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+    ],
+    python_requires=">=3.6",
+    zip_safe=False,
+)
