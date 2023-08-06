fe00dd9a90fccacb730f684540f6c04528325dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:29 +0000

    Add package distributions-999/0.1/distributions_999-0.1.tar.gz

---
+++ packages/distributions-999/distributions_999-0.1.tar.gz/distributions_999-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_999',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_999'],
+      author = 'Muhsin Sahin',
+      author_email = 'sahinmuhsin2018@gmail.com',
+      zip_safe=False)
