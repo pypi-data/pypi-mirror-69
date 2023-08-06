ae5756d3542606d956aa5e29b006bcafb2e98faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:17 +0000

    Add package novelWriter/0.6.3/novelWriter-0.6.3-py3-none-any.whl

---
+++ packages/novelWriter/novelWriter-0.6.3-py3-none-any.whl/nw/gui/dialogs/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from nw.gui.dialogs.about import GuiAbout
+from nw.gui.dialogs.configeditor import GuiConfigEditor
+from nw.gui.dialogs.docmerge import GuiDocMerge
+from nw.gui.dialogs.docsplit import GuiDocSplit
+from nw.gui.dialogs.itemeditor import GuiItemEditor
+from nw.gui.dialogs.projectsettings import GuiProjectSettings
+from nw.gui.dialogs.projectload import GuiProjectLoad
+from nw.gui.dialogs.sessionlog import GuiSessionLogView
+
+__all__ = [
+    "GuiAbout",
+    "GuiConfigEditor",
+    "GuiDocMerge",
+    "GuiDocSplit",
+    "GuiItemEditor",
+    "GuiProjectSettings",
+    "GuiProjectLoad",
+    "GuiSessionLogView",
+]
