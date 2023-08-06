7332d3392050b40d8a4bb28e02d69e8b7e31c65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:27 +0000

    Add package gsrest/0.1.3/gsrest-0.1.3-py3-none-any.whl

---
+++ packages/gsrest/gsrest-0.1.3-py3-none-any.whl/gsrest/helper/logger.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 Instituto Tecnológico de Canarias, S.A.
+#
+# This file is part of GsRest
+# (see https://github.com/esuarezsantana/gsrest).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+"""Metaclass for logging.
+"""
+
+import logging
+
+
+class MetaLogger(type):
+    """Metaclass for logging.
+
+    See https://stackoverflow.com/a/50731615
+
+    """
+    def __init__(cls, *args):
+        super().__init__(*args)
+        # Explicit name mangling
+        logger_attribute_name = '_' + cls.__name__ + '__logger'
+
+        # Logger name derived accounting for inheritance for the bonus marks
+        logger_name = '.'.join([c.__name__ for c in cls.mro()[-2::-1]])
+
+        setattr(cls, logger_attribute_name, logging.getLogger(logger_name))
