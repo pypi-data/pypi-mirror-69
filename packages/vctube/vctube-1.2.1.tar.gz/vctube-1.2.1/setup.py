cd071ddb04b605163bb723fca3e49a64cb72f456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:24 +0000

    Add package vctube/1.2.1/vctube-1.2.1.tar.gz

---
+++ packages/vctube/vctube-1.2.1.tar.gz/vctube-1.2.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vctube", # Replace with your own username
+    version="1.2.1",
+    author="Seugnhun Jeong",
+    author_email="zldzmfoq12@naver.com",
+    description="A pakage for crawling and processing audio, caption from Youtube",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zldzmfoq12/aud-crawler",
+    download_url="https://github.com/zldzmfoq12/aud-crawler/archive/1.2.tar.gz",
+    keywords=['aud_crawler'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
