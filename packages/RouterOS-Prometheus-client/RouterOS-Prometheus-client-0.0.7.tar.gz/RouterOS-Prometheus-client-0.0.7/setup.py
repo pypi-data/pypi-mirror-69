7a3ac26fdf54077c8808838186d0801a4918cfb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:13 +0000

    Add package RouterOS-Prometheus-client/0.0.7/RouterOS-Prometheus-client-0.0.7.tar.gz

---
+++ packages/RouterOS-Prometheus-client/RouterOS-Prometheus-client-0.0.7.tar.gz/RouterOS-Prometheus-client-0.0.7/setup.py
@@ -0,0 +1,36 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def get_long_description():
+    with open('README.md') as readme_file:
+        return readme_file.read()
+
+
+setup(
+    name='RouterOS-Prometheus-client',
+    version='0.0.7',
+    license='MIT',
+    url='https://github.com/vanche93/RouterOS-Prometheus-client',
+    author='Ivan Burashev',
+    author_email='vanche93@yandex.ru',
+    description='Python Prometheus client to RouterBoard devices produced by MikroTik.',
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    packages=['routeros_prometheus_client'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+    install_requires=['RouterOS-api', 'prometheus_client'],
+    entry_points={
+        'console_scripts': [
+            'routeros-prometheus-client = routeros_prometheus_client.__main__:main',
+        ]
+    },
+)
