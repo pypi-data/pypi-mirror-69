ec69045ed3a77cc3fd18a8a4893707a54ae485e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:45 +0000

    Add package gbgd-distributions/1.5/gbgd_distributions-1.5.tar.gz

---
+++ packages/gbgd-distributions/gbgd_distributions-1.5.tar.gz/gbgd_distributions-1.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gbgd_distributions',
+      version='1.5',
+      description='Gaussian and Binomial Distributions',
+      author='Olaotan Oladapo',
+      author_email='horlight@gmail.com',
+      packages=['gbgd_distributions'],
+      zip_safe=False)
