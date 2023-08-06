47f4002dd6792333ccc0834d0a007a10c6379ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:51 +0000

    Add package sudippdf/1.0/sudippdf-1.0.tar.gz

---
+++ packages/sudippdf/sudippdf-1.0.tar.gz/sudippdf-1.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="sudippdf",
+    version=1.0,
+    long_description=Path("README.md").read_text(),
+    packages=setuptools.find_packages(
+        exclude=["test", "data"]
+    ),
+
+
+)
