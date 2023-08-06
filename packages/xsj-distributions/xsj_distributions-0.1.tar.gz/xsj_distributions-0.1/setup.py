1a25c6ac75f8b124e112b5335f361a56381cba9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:28 +0000

    Add package xsj-distributions/0.1/xsj_distributions-0.1.tar.gz

---
+++ packages/xsj-distributions/xsj_distributions-0.1.tar.gz/xsj_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='xsj_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['xsj_distributions'],
+      zip_safe=False)
