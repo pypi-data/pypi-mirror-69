61a8860b9780ceb69cffc1aa80dae6d852166d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:36 +0000

    Add package distributions-elnurvl/0.1/distributions_elnurvl-0.1.tar.gz

---
+++ packages/distributions-elnurvl/distributions_elnurvl-0.1.tar.gz/distributions_elnurvl-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_elnurvl',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_elnurvl'],
+      author='Elnur Hajiyev',
+      author_email='elnurvl@gmail.com',
+      zip_safe=False)
