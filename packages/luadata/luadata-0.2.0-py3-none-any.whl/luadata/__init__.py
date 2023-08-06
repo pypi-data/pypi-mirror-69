369d9d250cacccf5ff5985be44ea270277f6933d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:14 +0000

    Add package luadata/0.2.0/luadata-0.2.0-py3-none-any.whl

---
+++ packages/luadata/luadata-0.2.0-py3-none-any.whl/luadata/__init__.py
@@ -0,0 +1,12 @@
+
+from luadata.io.write import serialize
+from luadata.io.write import write
+from luadata.io.write import const
+from luadata.io.read import unserialize
+from luadata.io.read import read
+
+name = "luadata"
+
+__doc__ = """
+a Python package that can serialize Python list & dictionary to Lua table.
+"""
