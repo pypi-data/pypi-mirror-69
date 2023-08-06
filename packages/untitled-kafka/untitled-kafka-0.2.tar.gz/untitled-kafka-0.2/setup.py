3705f27db6008b14025fac156147a2cec9cdf143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:40 +0000

    Add package untitled-kafka/0.2/untitled-kafka-0.2.tar.gz

---
+++ packages/untitled-kafka/untitled-kafka-0.2.tar.gz/untitled-kafka-0.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='untitled-kafka',  # How you named your package folder (MyLib)
+    packages=['untitled-kafka', 'untitled-kafka.common', 'untitled-kafka.consumer', 'untitled-kafka.producer'],
+    version='0.2',  # Start with a small number and increase it with every change you make
+    license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description='Sample pip package',  # Give a short description about your library
+    author='KHALIDH AHAMED',  # Type in your name
+    author_email='khalidh.ahamed@mrcooper.com',  # Type in your E-Mail
+    keywords=['SAMPLE', 'FORMAT', 'PIP'],  # Keywords that define your package best
+    install_requires=[],  # I get to this in a second
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'Programming Language :: Python :: 3.5'
+    ],
+)
