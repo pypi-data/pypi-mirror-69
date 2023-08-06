d14137c2f63158e9b7aad6d5f9d85cdce56d8a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package super-csv/0.9.9/super-csv-0.9.9.tar.gz

---
+++ packages/super-csv/super-csv-0.9.9.tar.gz/super-csv-0.9.9/super_csv/apps.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+super_csv Django application initialization.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from django.apps import AppConfig
+
+
+class SuperCSVConfig(AppConfig):
+    """
+    Configuration for the super_csv Django application.
+    """
+
+    name = 'super_csv'
+    plugin_app = {
+        u'settings_config': {
+            u'lms.djangoapp': {
+                u'common': {'relative_path': u'common_settings'},
+            },
+        },
+
+    }
+
+    def ready(self):
+        from . import signals  # pylint: disable=unused-import
