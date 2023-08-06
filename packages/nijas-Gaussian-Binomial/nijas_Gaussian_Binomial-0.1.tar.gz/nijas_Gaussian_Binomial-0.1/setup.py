0ea997f0c92bc424937eb48d81807b6c30de3844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:36 +0000

    Add package nijas-Gaussian-Binomial/0.1/nijas_Gaussian_Binomial-0.1.tar.gz

---
+++ packages/nijas-Gaussian-Binomial/nijas_Gaussian_Binomial-0.1.tar.gz/nijas_Gaussian_Binomial-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='nijas_Gaussian_Binomial',
+      version='0.1',
+      description='Gaussian and Binomialdistributions',
+      packages=['nijas_Gaussian_Binomial'],
+      zip_safe=False)
