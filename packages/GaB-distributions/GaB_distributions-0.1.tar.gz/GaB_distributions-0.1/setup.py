aa62baaa0b87841f101d5cf1957b7448d6147f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:50 +0000

    Add package GaB-distributions/0.1/GaB_distributions-0.1.tar.gz

---
+++ packages/GaB-distributions/GaB_distributions-0.1.tar.gz/GaB_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='GaB_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author = 'Khondoker',
+      packages=['GaB_distributions'],
+      zip_safe=False)
