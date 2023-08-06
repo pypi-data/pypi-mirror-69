d9b2e9576d33b4a10c8d823a75938183dc209244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:43 +0000

    Add package distributions-NM-may2020/0.1/distributions_NM_may2020-0.1.tar.gz

---
+++ packages/distributions-NM-may2020/distributions_NM_may2020-0.1.tar.gz/distributions_NM_may2020-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_NM_may2020',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_NM_may2020'],
+      author = 'Nuria Malet Quintar',
+      author_email = 'maletq.ds@gmail.com',
+      zip_safe=False)
