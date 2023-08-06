25a3f2c55840373c5697ccc312376a61d60c4baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:02 +0000

    Add package instapvapi/0.0.6/instapvapi-0.0.6-py3-none-any.whl

---
+++ packages/instapvapi/instapvapi-0.0.6-py3-none-any.whl/instapv/exceptions.py
@@ -0,0 +1,21 @@
+class SentryBlockException(Exception):
+    pass
+
+class AccountTypeException(Exception):
+    pass
+
+class UserNotFoundException(Exception):
+    pass
+
+class InvalidCredentialsException(Exception):
+    pass
+
+class RateLimitException(Exception):
+    pass
+
+class AccessDeniedException(Exception):
+    pass
+
+class ChallengeRequiredException(Exception):
+    pass
+
