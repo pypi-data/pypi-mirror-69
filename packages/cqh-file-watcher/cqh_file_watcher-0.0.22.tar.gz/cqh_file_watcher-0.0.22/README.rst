3ccdc8e586449dc945e063eedf625bf728c42dc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:01 +0000

    Add package cqh-file-watcher/0.0.22/cqh_file_watcher-0.0.22.tar.gz

---
+++ packages/cqh-file-watcher/cqh_file_watcher-0.0.22.tar.gz/cqh_file_watcher-0.0.22/README.rst
@@ -0,0 +1,29 @@
+cqh_file_watcher
+=============================================
+
+something like `File-Watcher` for vscode
+
+
+Usage
+-------------------------------------------------
+
+
+``cqh-file-watcher -c ***.conf``
+
+conf example
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+.. code-block::
+
+    {"command_list":[
+        {
+            "pattern": "*.py",
+            "command": "sudo supervisorctl restart redis"
+        },
+        {
+            "command": "echo things changed"
+        }
+    ]
+    "directory": "/home/vagrant/code/code1"
+    }
---
+++ packages/cqh-file-watcher/cqh_file_watcher-0.0.22.tar.gz/cqh_file_watcher-0.0.22/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+import os
+name = 'cqh_file_watcher'
+_dir = os.path.dirname(os.path.abspath(__file__))
+
+init_path = os.path.join(_dir, name, '__init__.py')
+
+def read_version():
+    content = open(init_path).readlines()[0]
+    quote_index = content.index('"')
+    version = content[quote_index+1:-1]
+    return version
+
+
+
+
+setuptools.setup(
+    name=name,  # Replace with your own username
+    version=read_version(),
+    author="chenqinghe",
+    author_email="1832866299@qq.com",
+    description="tools like vscode file-watcher but for command only",
+    long_description=long_description,
+    url="https://github.com/chen19901225/cqh_file_watcher",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pyinotify"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cqh_file_watcher = cqh_file_watcher.run:main",
+        ],
+    },
+    python_requires='>=3.6',
+)
