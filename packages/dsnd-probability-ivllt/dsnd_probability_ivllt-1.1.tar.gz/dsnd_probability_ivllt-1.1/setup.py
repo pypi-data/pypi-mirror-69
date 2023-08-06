b712e53181f02a44fbd0ccac99d253c53c9f5c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:39 +0000

    Add package dsnd-probability-ivllt/1.1/dsnd_probability_ivllt-1.1.tar.gz

---
+++ packages/dsnd-probability-ivllt/dsnd_probability_ivllt-1.1.tar.gz/dsnd_probability_ivllt-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_ivllt',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_ivllt'],
+      author = 'Ivan Lau',
+      author_email = 'testing@hotmail.com',
+      zip_safe=False)
