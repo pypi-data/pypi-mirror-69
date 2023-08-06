dd0229dfec4d98d778bbd4f58c581ad135f3a65a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:43 +0000

    Add package visguy-probability/1.0/visguy_probability-1.0.tar.gz

---
+++ packages/visguy-probability/visguy_probability-1.0.tar.gz/visguy_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='visguy_probability',
+      version='1.0',
+      description='Gaussian distributions by visguy',
+      packages=['visguy_probability'],
+      author='Amir Smajevic',
+      author_email='visocicaguy@gmail.com',
+      zip_safe=False)
