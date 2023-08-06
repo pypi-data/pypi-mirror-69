734edf1d7a9e2ef1817c4104beba01a06a407a15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:09 +0000

    Add package fmpa/0.2/fmpa-0.2.tar.gz

---
+++ packages/fmpa/fmpa-0.2.tar.gz/fmpa-0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'fmpa',         
+  packages = ['fmpa'],   
+  version = '0.2',      
+  license='MIT',        
+  description = 'wrapper for the financial modelling prep api',   
+  url = 'https://github.com/raphaelfeikert/fmpa',   
+  download_url = 'https://github.com/raphaelfeikert/fmpa/archive/v_02.tar.gz',    
+  install_requires=[            
+          'requests',
+          'numpy',
+          'pandas'         
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
