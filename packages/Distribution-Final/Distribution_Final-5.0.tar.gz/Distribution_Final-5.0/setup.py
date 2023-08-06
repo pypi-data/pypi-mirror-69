eb08359ff5079f4e90a55d8f01cc93f544c9314c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:30 +0000

    Add package Distribution-Final/5.0/Distribution_Final-5.0.tar.gz

---
+++ packages/Distribution-Final/Distribution_Final-5.0.tar.gz/Distribution_Final-5.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Distribution_Final',
+      version='5.0',
+      description='Gaussian and Binomial distributions',
+      packages=['Distribution_Final'],
+      zip_safe=False)
