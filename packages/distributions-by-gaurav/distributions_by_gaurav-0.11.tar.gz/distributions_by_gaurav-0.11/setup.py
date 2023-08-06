12ff23ca41c590b8844b67f762f86bc62494987c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:03 +0000

    Add package distributions-by-gaurav/0.11/distributions_by_gaurav-0.11.tar.gz

---
+++ packages/distributions-by-gaurav/distributions_by_gaurav-0.11.tar.gz/distributions_by_gaurav-0.11/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_by_gaurav',
+      version='0.11',
+      description='Gaussian distributions, Binomial distributions',
+      packages=['distributions_by_gaurav'],
+      zip_safe=False)
