171cfec2fec01c8bb481a743816467d8f88b3323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:41 +0000

    Add package labs-cdk/0.0.6/labs_cdk-0.0.6-py3-none-any.whl

---
+++ packages/labs-cdk/labs_cdk-0.0.6-py3-none-any.whl/labs_cdk/services/ecr/__init__.py
@@ -0,0 +1,14 @@
+import typing
+
+from aws_cdk.aws_ecr import Repository, LifecycleRule, TagStatus
+from aws_cdk.core import Construct
+
+
+class ContainerRepository(Construct):
+    def __init__(self, scope: Construct, id: str, *, repository_name: typing.Optional[str] = None):
+        super().__init__(scope, id)
+        remove_untagged_images_rule = LifecycleRule(description='Remove untagged images', max_image_count=0,
+                                                    rule_priority=1,
+                                                    tag_status=TagStatus.UNTAGGED)
+        Repository(scope, f'${id}-repository', image_scan_on_push=True, repository_name=repository_name,
+                   lifecycle_rules=[remove_untagged_images_rule])
