d03a58df32723a8505a8188d1bbac22f551297a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:09 +0000

    Add package utils4py/0.0.23/utils4py-0.0.23.tar.gz

---
+++ packages/utils4py/utils4py-0.0.23.tar.gz/utils4py-0.0.23/utils4py/flask_utils.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from datetime import date, datetime
+
+from flask import jsonify, request
+from flask.json import JSONEncoder
+from flask_wtf import FlaskForm
+
+from .utils import get_items
+
+
+class CustomJSONEncoder(JSONEncoder):
+    """
+    JSON Encoder format
+    """
+
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime):
+                return obj.strftime("%Y-%m-%d %H:%M:%S")
+            elif isinstance(obj, date):
+                return obj.strftime("%Y-%m-%d")
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+
+        return JSONEncoder.default(self, obj)
+
+
+def response(code=0, data={}, msg="", http_code=200, **kwargs):
+    """
+    return flask json response
+    """
+    rtn = {"code": code}
+    if data:
+        rtn["data"] = data
+
+    if msg:
+        rtn["msg"] = msg
+
+    if kwargs:
+        rtn = rtn.update(kwargs)
+
+    return jsonify(rtn), http_code
+
+
+def get_json(force=True, silent=True, cache=True):
+    """
+    wrap flask request get_json
+    """
+    return request.get_json(force=force, silent=silent, cache=cache)
+
+
+class Form(FlaskForm):
+    def get_error_message(self):
+        """
+        return first error message
+        """
+        messages = get_items(self.errors)
+        return None if not messages else messages[0]
+
+
+def validate(validate_form=None, upload=False):
+    """
+    form validate decorator
+    """
+
+    def form_check(fn):
+        def wrapper(*args, **kwargs):
+            form = validate_form(
+                data=request.args
+                if request.method == "GET"
+                else (get_json() if not upload else request.form)
+            )
+
+            if not form.validate():
+                return response(code=90001, msg=form.get_error_message())
+            kwargs.update({"form": form})
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    return form_check
