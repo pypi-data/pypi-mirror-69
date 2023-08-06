682f36b59bb8ab4116d3ab577b032f0c2378ab8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:46 +0000

    Add package ageng-distributions/0.2/ageng-distributions-0.2.tar.gz

---
+++ packages/ageng-distributions/ageng-distributions-0.2.tar.gz/ageng-distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ageng-distributions',
+      version='0.2',
+      description='Gaussian distributions from udacity',
+      packages=['distributions'],
+      author='Widya Ageng',
+      author_email='ageng.setyatutuko@gmail.com',
+      zip_safe=False)
