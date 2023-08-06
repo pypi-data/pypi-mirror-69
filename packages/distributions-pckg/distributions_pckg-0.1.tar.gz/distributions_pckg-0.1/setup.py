fab3e1df415d234b30b66f44ab56cc610a34e25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:12 +0000

    Add package distributions-pckg/0.1/distributions_pckg-0.1.tar.gz

---
+++ packages/distributions-pckg/distributions_pckg-0.1.tar.gz/distributions_pckg-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_pckg',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_pckg'],
+      author = 'Andrew Paster',
+      author_email = 'bour.walae@gmail.com',
+      zip_safe=False)
