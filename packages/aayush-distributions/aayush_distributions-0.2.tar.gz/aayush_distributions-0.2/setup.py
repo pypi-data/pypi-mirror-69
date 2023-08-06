e9913bf323e08a1b1522992fd2c148bd9c2f42fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:46 +0000

    Add package aayush-distributions/0.2/aayush_distributions-0.2.tar.gz

---
+++ packages/aayush-distributions/aayush_distributions-0.2.tar.gz/aayush_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='aayush_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['aayush_distributions'],
+      zip_safe=False)
