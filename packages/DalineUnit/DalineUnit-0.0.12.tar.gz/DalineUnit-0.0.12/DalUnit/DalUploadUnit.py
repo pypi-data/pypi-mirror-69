46e1e4579795148c5b8f81578c37e28747d425c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:27 +0000

    Add package DalineUnit/0.0.12/DalineUnit-0.0.12.tar.gz

---
+++ packages/DalineUnit/DalineUnit-0.0.12.tar.gz/DalineUnit-0.0.12/DalUnit/DalUploadUnit.py
@@ -0,0 +1,61 @@
+import os
+import click
+import json
+
+runscript = '''
+import click
+from concurrent import futures
+import grpc
+import puwrapper_pb2
+import puwrapper_pb2_grpc
+from %s import %s
+
+@click.command()
+@click.option('--wport', default=0, help='java wrapper grpc server port')
+@click.option('--port', default=0, help='python grpc server port')
+def serve(wport,port):
+    if wport == 0:
+        print('Wport should be set!')
+        return
+    if port == 0:
+        print('Port should be set!')
+        return
+    pcu = %s() 
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    puwrapper_pb2_grpc.add_PURequestServicer_to_server(
+        %s(), server)
+    server.add_insecure_port("[::]:{}".format(port))
+    server.start()
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
+'''
+
+@click.command()
+@click.option('--folder', help='target folder')
+def run(folder):
+    #read in unit info
+    dicInfo={}
+    if folder is None:
+        print('Folder should be set, use --help see help!')
+        return
+    infoF = folder+'\\info.json'
+    file_info = open(infoF, 'r')
+    # content = file_info.read()    
+    # print(file_info)
+    dicInfo = json.load(file_info)
+    file_info.close()    
+    if 'name' not in dicInfo:
+        print('Info.json format wrong, no name field found!')
+        return    
+    #write runscript
+    name = dicInfo['name']
+    scriptF = folder+'\\run.py'
+    file_script = open(scriptF, 'w')
+    file_script.write(runscript%(name,name,name,name))
+    file_script.close( )
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/DalineUnit/DalineUnit-0.0.12.tar.gz/DalineUnit-0.0.12/DalUnit/pycPackage.py
@@ -0,0 +1,13 @@
+import py_compile
+import shutil
+
+py_compile.compile('DalBaseUnit.py', r'..\DalineUnit\DalBaseUnit.pyc')
+py_compile.compile('OracleOper.py', r'..\DalineUnit\OracleOper.pyc')
+py_compile.compile('puwrapper_pb2.py', r'..\DalineUnit\puwrapper_pb2.pyc')
+py_compile.compile('puwrapper_pb2_grpc.py', r'..\DalineUnit\puwrapper_pb2_grpc.pyc')
+py_compile.compile('OracleOper.py', r'..\DalineUnit\OracleOper.pyc')
+py_compile.compile('Template.py', r'..\DalineUnit\Template.pyc')
+py_compile.compile('DalDecorate.py', r'..\DalineUnit\DalDecorate.pyc')
+py_compile.compile('DalGenScript.py', r'..\DalineUnit\DalGenScript.pyc')
+py_compile.compile('DesignFile.py', r'..\DalineUnit\DesignFile.pyc')
+py_compile.compile('GenTestData.py', r'..\DalineUnit\GenTestData.pyc')
