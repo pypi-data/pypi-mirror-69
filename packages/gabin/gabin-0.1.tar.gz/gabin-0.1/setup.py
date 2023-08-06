3a852f07f5d3147168cd87de1000a476622d3abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:58 +0000

    Add package gabin/0.1/gabin-0.1.tar.gz

---
+++ packages/gabin/gabin-0.1.tar.gz/gabin-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gabin',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gabin'],
+      author= 'Lakshya Bansal',
+      author_email= 'bansallakshya94128@gmail.com',  
+      zip_safe=False)
