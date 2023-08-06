b978e6771260739c615a94a663bb013161ea7728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:22 +0000

    Add package pop-distributions/0.1/pop_distributions-0.1.tar.gz

---
+++ packages/pop-distributions/pop_distributions-0.1.tar.gz/pop_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pop_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['pop_distributions'],
+      author = 'Nagaveda Reddy',
+      author_email = 'gajjalanagaveda@karunya.edu.in',
+      zip_safe=False)
