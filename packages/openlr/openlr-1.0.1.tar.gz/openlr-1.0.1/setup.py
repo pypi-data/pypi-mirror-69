76619659434dfd5a2f01ae6ecfbe6129ee0fe3e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:47 +0000

    Add package openlr/1.0.1/openlr-1.0.1.tar.gz

---
+++ packages/openlr/openlr-1.0.1.tar.gz/openlr-1.0.1/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import sys
+from setuptools import setup, find_packages
+
+install_requires = []
+if sys.version_info[0] == 2:
+    install_requires = ["enum34", "typing"]
+
+with open("README.md", "rb") as f:
+    readme = f.read().decode("utf-8")
+
+about = {}
+with open("openlr/_version.py", "rb") as f:
+    exec(f.read(), about)
+
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
+    license=about["__license__"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Operating System :: OS Independent",
+    ],
+    packages=["openlr"],
+    install_requires=install_requires,
+    test_suite="tests",
+)
