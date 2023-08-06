fe6c5de1576055c9739b69c831f7e10464bd2aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:21 +0000

    Add package poismf/0.2.3/poismf-0.2.3.tar.gz

---
+++ packages/poismf/poismf-0.2.3.tar.gz/poismf-0.2.3/setup.py
@@ -0,0 +1,55 @@
+try:
+    from setuptools import setup
+    from setuptools import Extension
+except:
+    from distutils.core import setup
+    from distutils.extension import Extension
+import numpy
+from sys import platform
+import os
+from findblas.distutils import build_ext_with_blas
+from sys import platform
+
+
+## https://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used
+class build_ext_subclass( build_ext_with_blas ):
+    def build_extensions(self):
+        compiler = self.compiler.compiler_type
+        if compiler == 'msvc': # visual studio is 'special'
+            for e in self.extensions:
+                e.extra_compile_args += ['/O2', '/openmp']
+        else:
+            for e in self.extensions:
+                e.extra_compile_args += ['-O2', '-fopenmp', '-march=native', '-std=c99']
+                e.extra_link_args += ['-fopenmp']
+                # e.extra_compile_args += ["-fsanitize=address", "-static-libasan", "-ggdb"]
+                # e.extra_link_args += ["-fsanitize=address", "-static-libasan"]
+                # e.extra_compile_args += ["-ggdb"]
+        build_ext_with_blas.build_extensions(self)
+
+
+setup(
+    name  = "poismf",
+    packages = ["poismf"],
+    author = 'David Cortes',
+    author_email = 'david.cortes.rivera@gmail.com',
+    url = 'https://github.com/david-cortes/poismf',
+    version = '0.2.3',
+    install_requires = ['numpy', 'pandas>=0.24', 'cython', 'findblas'],
+    description = 'Fast and memory-efficient Poisson factorization for sparse count matrices',
+    cmdclass = {'build_ext': build_ext_subclass},
+    ext_modules = [
+        Extension("poismf.c_funs_double",
+            sources=["poismf/cfuns_double.pyx",
+                     "src/poismf.c", "src/topN.c", "src/pred.c",
+                     "src/nonnegcg.c", "src/tnc.c"],
+            include_dirs=[numpy.get_include(), "src/"],
+            define_macros = [("_FOR_PYTHON", None)]),
+        Extension("poismf.c_funs_float",
+            sources=["poismf/cfuns_float.pyx",
+                     "src/poismf.c", "src/topN.c", "src/pred.c",
+                     "src/nonnegcg.c", "src/tnc.c"],
+            include_dirs=[numpy.get_include(), "src/"],
+            define_macros = [("_FOR_PYTHON", None), ("USE_FLOAT", None)])
+        ]
+    )
