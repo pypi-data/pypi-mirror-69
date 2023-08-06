29ef40cb4d5cda3a1677ca32d316a9b1466a597d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:57 +0000

    Add package RocStat/0.1/RocStat-0.1.tar.gz

---
+++ packages/RocStat/RocStat-0.1.tar.gz/RocStat-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='RocStat',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['RocStat'],
+      author= 'Allan Ruan',
+      author_email = 'allanruan1@gmail.com',
+      zip_safe=False)
