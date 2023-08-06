b635924ebf266151524953621f579a3a462d4383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:35 +0000

    Add package naagin/0.1/naagin-0.1.tar.gz

---
+++ packages/naagin/naagin-0.1.tar.gz/naagin-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='naagin',
+      version='0.1',
+      description='Gaussian and Binomial distribution from Souvik Sankar Mitra',
+      packages=['naagin'],
+      author = 'Souvik Sankar Mitra',
+      author_email = 'souviksankar2013@gmail.com',
+      zip_safe=False)
