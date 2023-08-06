6b6756d27b9de1b37afe79931010c7bdecb18424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:35 +0000

    Add package input_get/1.0/input_get-1.0.tar.gz

---
+++ packages/input_get/input_get-1.0.tar.gz/input_get-1.0/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='input_get',
+      version='1.0',
+      description='控制台输入显示为星号',
+      author='zhaobin',
+      author_eamil='zhaobin.lqy@live.cn',
+      py_modules=['input_get.py'])
