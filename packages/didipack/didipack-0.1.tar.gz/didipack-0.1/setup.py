119a8c6e0f3113f63b44f6757e2b57b504000b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:07:56 +0000

    Add package didipack/0.1/didipack-0.1.tar.gz

---
+++ packages/didipack/didipack-0.1.tar.gz/didipack-0.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+setup(
+  name = 'didipack',
+  packages = ['didipack'],
+  version = '0.1',
+  license='MIT',
+  description = 'Usfull time saver for data-science and finance with python',
+  author = 'Antoine Didisheim',
+  author_email = 'antoinedidisheim@gmail.com',
+  url = 'https://github.com/AntoineDidisheim',
+  download_url = 'https://github.com/AntoineDidisheim/didipack/archive/v01.tar.gz',    # I explain this later on
+  keywords = ['LaTex', 'Finance'],
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+          'statsmodels'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
