ac5167811d9f68d5d85f9e3ff258a7128f68ad7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:51 +0000

    Add package boto3-fixtures/0.0.4/boto3_fixtures-0.0.4-py2.py3-none-any.whl

---
+++ packages/boto3-fixtures/boto3_fixtures-0.0.4-py2.py3-none-any.whl/boto3_fixtures/__init__.py
@@ -0,0 +1,12 @@
+"""
+boto3-fixtures
+~~~~~~~~~
+
+Simple test fixtures for your local AWS cloud stack of choice
+"""
+
+# flake8: noqa
+
+# from boto3_fixtures import awslambda, contrib, dynamodb, kinesis, s3, sqs, utils
+from boto3_fixtures._version import __version__
+from boto3_fixtures.service import Service
