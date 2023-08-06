788d3eda46a457cf88b3a2e0c4351c342ad6f95a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:20 +0000

    Add package markdown-hiko/0.2.3/markdown_hiko-0.2.3-py3-none-any.whl

---
+++ packages/markdown-hiko/markdown_hiko-0.2.3-py3-none-any.whl/src/service/image_path_service.py
@@ -0,0 +1,23 @@
+import os
+
+from src.model.directory_path import DirectoryPath
+from src.model.file_path import FilePath
+
+
+class ImagePathService:
+    """Service class of image path"""
+
+    def __init__(
+        self, image_path: FilePath, project_root_path: DirectoryPath, markdown_file_directory_path: DirectoryPath
+    ):
+        self.__image_path = image_path
+        self.__project_root_path = project_root_path
+        self.__markdown_file_directory_path = markdown_file_directory_path
+
+    def get_absolute_path(self) -> FilePath:
+        """
+        :return: Absolute path of the image from the root directory
+        """
+        if self.__image_path.is_absolute_path():
+            return FilePath(self.__project_root_path.value + self.__image_path.value)
+        return FilePath(os.path.abspath(self.__markdown_file_directory_path.value + "/" + self.__image_path.value))
