0dcd18d8c86c5ff6dbf08eebde37efa655b00028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:56 +0000

    Add package mmxtest/1.0.7/mmxtest-1.0.7.tar.gz

---
+++ packages/mmxtest/mmxtest-1.0.7.tar.gz/mmxtest-1.0.7/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+ 
+setup(name='mmxtest',
+  version='1.0.7',
+  description='A print test for PyPI',
+  author='mmx',
+  author_email='mingxing157157@163.com',
+  url='https://www.python.org/',
+  packages=['mmxtest'],
+  )
