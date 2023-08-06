439628a3670987a22dda270c18cc4e2fff6517da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:52 +0000

    Add package distributions-space/0.1/distributions-space-0.1.tar.gz

---
+++ packages/distributions-space/distributions-space-0.1.tar.gz/distributions-space-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions-space',
+      version='0.1',
+      description='Statistical Distributions',
+      packages=['distributions-space'],
+      zip_safe=False)
