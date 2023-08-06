bcefc58aa7bfc2da55d8163cc16a311cc9cfa132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:36 +0000

    Add package corgy-erp/0.3.4/corgy-erp-0.3.4.tar.gz

---
+++ packages/corgy-erp/corgy-erp-0.3.4.tar.gz/corgy-erp-0.3.4/corgy_mdm/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from material.frontend.views import ModelViewSet
+from material import Layout, Fieldset, Row
+from django.utils.translation import ugettext_lazy as _
+from . import models
+
+# Create your views here.
+
+class PersonModelViewSet(ModelViewSet):
+    model = models.PersonModel
+
+    layout = Layout(
+        Fieldset(
+            _('Personal details'),
+            Row(
+                'gender',
+                'name_prefix'
+            ),
+            Row(
+                'name_prefix',
+                'first_name',
+                'middle_name',
+                'last_name'
+            ),
+        ),
+        Fieldset(
+            _('Birth data'),
+            Row(
+                'birthdate',
+                'birthplace',
+            ),
+            'name_of_mother',
+        ),
+        Fieldset(
+            _('Contact information'),
+            'permanent_address',
+            'temporary_address',
+            Row('email', 'phone_number'),
+        ),
+    )
+
+class OrganizationModelViewSet(ModelViewSet):
+    model = models.OrganizationModel
+
