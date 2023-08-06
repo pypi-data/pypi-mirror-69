d1a524c1fd3e7bb0fce1e3255f75bff2c069ce25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:41 +0000

    Add package aiozyre/1.1.4/aiozyre-1.1.4.tar.gz

---
+++ packages/aiozyre/aiozyre-1.1.4.tar.gz/aiozyre-1.1.4/setup.py
@@ -0,0 +1,48 @@
+
+from setuptools import setup, Extension
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='aiozyre',
+    version='1.1.4',
+    description='asyncio-friendly Python bindings for Zyre',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Elijah Shaw-Rutschman',
+    author_email='elijahr+aiozyre@gmail.com',
+    packages=['aiozyre'],
+    package_dir={
+      'aiozyre': 'src/aiozyre',
+    },
+    package_data={'aiozyre': [
+        '*.pyx',
+        '*.pxd',
+    ]},
+    ext_modules=[
+        Extension('aiozyre.nodeactor', sources=['src/aiozyre/nodeactor.pyx'], libraries=['czmq', 'zyre']),
+        Extension('aiozyre.signals', sources=['src/aiozyre/signals.pyx']),
+        Extension('aiozyre.util', sources=['src/aiozyre/util.pyx'], libraries=['czmq', 'zyre']),
+    ],
+    setup_requires=['cython'],
+    extras_require={
+        'dev': [
+            'blessed',
+            'aioconsole',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Networking',
+        'Framework :: AsyncIO',
+    ],
+)
