fe36d7345151dfa77afa75f9a8cc01ad3e1f6512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:47 +0000

    Add package ujimaru-markov-model/0.1.2/ujimaru-markov-model-0.1.2.tar.gz

---
+++ packages/ujimaru-markov-model/ujimaru-markov-model-0.1.2.tar.gz/ujimaru-markov-model-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ujimaru_markov_model']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['markovify>=0.8.0,<0.9.0']
+
+setup_kwargs = {
+    'name': 'ujimaru-markov-model',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'hppRC',
+    'author_email': 'hpp.ricecake@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
