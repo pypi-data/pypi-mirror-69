1332b5cc5973615315e411258c1136faf28c6bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:03 +0000

    Add package distributions-aman/1.2/distributions_aman-1.2.tar.gz

---
+++ packages/distributions-aman/distributions_aman-1.2.tar.gz/distributions_aman-1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_aman',
+      version='1.2',
+      description='Gaussian distributions',
+      packages=['distributions_aman'],
+      zip_safe=False)
