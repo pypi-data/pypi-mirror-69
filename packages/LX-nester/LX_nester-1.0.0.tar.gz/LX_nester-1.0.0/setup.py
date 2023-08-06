5527fdb8610c9d2024225474293bc97888022f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:05 +0000

    Add package LX-nester/1.0.0/LX_nester-1.0.0.tar.gz

---
+++ packages/LX-nester/LX_nester-1.0.0.tar.gz/LX_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='LX_nester',
+	version='1.0.0',
+	py_modules=['LX_nester'],
+	author='captain',
+	author_email='469237854@qq.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+	)
