c8fc38aaa613b36a2970b4cbe518c7b4f297aa59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:41 +0000

    Add package gbinom-distributions/1.1/gbinom_distributions-1.1.tar.gz

---
+++ packages/gbinom-distributions/gbinom_distributions-1.1.tar.gz/gbinom_distributions-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gbinom_distributions',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gbinom_distributions'],
+      author = 'Hamid Adesokan',
+      author_email = 'aadehamid@gmail.com',
+      zip_safe=False)
+
