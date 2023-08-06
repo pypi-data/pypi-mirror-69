081de6ff5798a028be408344ebd60d0ff1be848a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:41 +0000

    Add package pydori/0.1.1/pydori-0.1.1.tar.gz

---
+++ packages/pydori/pydori-0.1.1.tar.gz/pydori-0.1.1/pydori/__init__.py
@@ -0,0 +1,3 @@
+from pydori.base_objects import *
+from pydori.util import bandori_loader
+from pydori.BandoriApi import BandoriApi
---
+++ packages/pydori/pydori-0.1.1.tar.gz/pydori-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+  
+setup(
+  name = 'pydori',         
+  packages = ['pydori'],   
+  version = '0.1.1',      
+  license='MIT',        
+  description = 'A python package to interact with the bandori.party and bandori.ga public APIs',
+  long_description=long_description,
+  long_description_content_type='text/markdown',  
+  author = 'William Tang',                   
+  author_email = 'williaamt0@gmail.com',      
+  url = 'https://github.com/WiIIiamTang/pydori',  
+  download_url = 'https://github.com/WiIIiamTang/pydori/archive/v0.1.1.tar.gz',  
+  keywords = ['API', 'Bandori', 'Bang Dream'],   
+  install_requires=['requests'],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+  ],
+)
