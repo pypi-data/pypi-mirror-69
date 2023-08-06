f607c5bcc1b9975c6573df95e970e78aab2227b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:31 +0000

    Add package petrelic/0.1.2/petrelic-0.1.2-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/petrelic/petrelic-0.1.2-cp36-cp36m-manylinux1_x86_64.whl/petrelic/multiplicative/pairing.py
@@ -0,0 +1,388 @@
+r"""
+This module provides a Python wrapper around RELIC's pairings using a
+multiplicative interface: operations in
+:py:obj:`petrelic.multiplicative.pairings.G1`,
+:py:obj:`petrelic.multiplicative.pairings.G2`, and
+:py:obj:`petrelic.multiplicative.pairings.GT` are all written
+multiplicatively.
+
+Let's see how we can use this interface to implement the Boney-Lynn-Shacham
+signature scheme for type III pairings. First we generate a private key:
+
+>>> sk = G1.order().random()
+
+which is a random integer modulo the group order. Note that for this setting,
+all three groups have the same order. Next, we generate the corresponding public
+key:
+
+>>> pk = (G1.generator() ** sk, G2.generator() ** sk)
+
+(For security in the type III setting, the first component is a necessary part
+of the public key. It is not actually used in the scheme.) To sign a message
+`m` we first hash it to the curve G1 using :py:meth:`G1.hash_to_point` and then
+raise it to the power of the signing key `sk` to obtain a signature:
+
+>>> m = b"Some message"
+>>> signature = G1.hash_to_point(m) ** sk
+
+Finally, we can use the pairing operator to verify the signature:
+
+>>> signature.pair(G2.generator()) == G1.hash_to_point(m).pair(pk[1])
+True
+
+Indeed, the pairing operator is bilinear. For example:
+
+>>> a, b = 13, 29
+>>> A = G1.generator() ** a
+>>> B = G2.generator() ** b
+>>> A.pair(B) == G1.generator().pair(G2.generator()) ** (a * b)
+True
+
+"""
+
+from petrelic.bindings import _FFI, _C
+from petrelic.bn import Bn, force_Bn_other
+from petrelic.native.pairing import NoAffineCoordinateForECPoint
+
+import petrelic.native.pairing as native
+
+
+class BilinearGroupPair:
+    """
+    A bilinear group pair.
+
+    Contains two origin groups G1, G2 and the image group GT.
+    """
+
+    def __init__(self):
+        """Initialise a bilinear group pair."""
+        self.GT = GT()
+        self.G1 = G1()
+        self.G2 = G2()
+
+    def groups(self):
+        """
+        Returns the three groups in the following order :  G1, G2, GT.
+        """
+        return self.G1, self.G2, self.GT
+
+
+class G1(native._G1Base):
+    """G1 group."""
+
+    @classmethod
+    def _element_type(cls):
+        return G1Element
+
+    #
+    # Replicated functions, fixing doc strings
+    #
+
+    @classmethod
+    def order(cls):
+        """Return the order of the group as a Bn large integer.
+
+        Example:
+            >>> generator = G1.generator()
+            >>> neutral = G1.neutral_element()
+            >>> order = G1.order()
+            >>> generator ** order == neutral
+            True
+        """
+        return super().order()
+
+    @classmethod
+    def generator(cls):
+        """Return generator of the group.
+
+        Example:
+            >>> generator = G1.generator()
+            >>> neutral = G1.neutral_element()
+            >>> generator * neutral == generator
+            True
+        """
+        return super().generator()
+
+    @classmethod
+    def neutral_element(cls):
+        """Return the neutral element of the group G1.
+
+        In this case, the point at infinity.
+
+        Example:
+            >>> generator = G1.generator()
+            >>> neutral = G1.neutral_element()
+            >>> generator * neutral == generator
+            True
+        """
+        return super().neutral_element()
+
+    #
+    # Interface specific methods
+    #
+
+    @classmethod
+    def prod(cls, elems):
+        """Efficient product of a number of elements
+
+        In the current implementation this function is not optimized.
+
+        Example:
+            >>> elems = [ G1.generator() ** x for x in [10, 25, 13]]
+            >>> G1.prod(elems) ==  G1.generator() ** (10 + 25 + 13)
+            True
+        """
+        res = cls.neutral_element()
+        for el in elems:
+            res *= el
+        return res
+
+    @classmethod
+    def wprod(cls, weights, elems):
+        """Efficient weighted product of a number of elements
+
+        In the current implementation this function is not optimized.
+
+        Example:
+            >>> weights = [1, 2, 3]
+            >>> elems = [ G1.generator() ** x for x in [10, 25, 13]]
+            >>> G1.wprod(weights, elems) ==  G1.generator() ** (1 * 10 + 2 * 25 + 3 * 13)
+            True
+        """
+        res = cls.neutral_element()
+        for w, el in zip(weights, elems):
+            res *= el ** w
+
+        return res
+
+    #
+    # Aliases
+    #
+
+    @classmethod
+    def unity(cls):
+        """The unity element
+
+        Alias for :py:meth:`G1.neutral_element`
+        """
+        return cls.neutral_element()
+
+
+class G1Element(native._G1ElementBase):
+    """Element of the G1 group."""
+
+    group = G1
+
+    def pair(self, other):
+        """Pair element with another element in G2
+
+        Computes the bilinear pairing between self and another element in
+        :py:obj:`petrelic.multiplicative.pairing.G2`.
+
+        Examples:
+             >>> g1, g2 = G1.generator(), G2.generator()
+             >>> a, b = 10, 50
+             >>> A, B = g1 ** a, g2 ** b
+             >>> A.pair(B) == g1.pair(g2) ** (a * b)
+             True
+             >>> A.pair(g2) == g1.pair(g2 ** a)
+             True
+             >>> A.pair(g2) == g1.pair(g2) ** a
+             True
+        """
+        if not type(other) == G2Element:
+            raise TypeError("Second parameter should be of type G2Element is {}".format(type(other)))
+
+        res = GTElement()
+        _C.pc_map(res.pt, self.pt, other.pt)
+        return res
+
+    #
+    # Unary operators
+    #
+
+    def square(self):
+        return native.G1Element.double(self)
+
+    def isquare(self):
+        return native.G1Element.idouble(self)
+
+    #
+    # Binary operators
+    #
+
+    def __mul__(self, other):
+        return native.G1Element.__add__(self, other)
+
+    def __imul__(self, other):
+        return native.G1Element.__iadd__(self, other)
+
+    def __truediv__(self, other):
+        return native.G1Element.__sub__(self, other)
+
+    def __itruediv__(self, other):
+        return native.G1Element.__isub__(self, other)
+
+    def __pow__(self, other):
+        return native.G1Element.__mul__(self, other)
+
+    def __ipow__(self, other):
+        return native.G1Element.__imul__(self, other)
+
+    # Copy documentation from native.GTElement
+    square.__doc__ = native.GTElement.square.__doc__.replace("GT", "G1")
+    isquare.__doc__ = native.GTElement.isquare.__doc__.replace("GT", "G1")
+
+    __mul__.__doc__ = native.GTElement.__mul__.__doc__.replace("GT", "G1")
+    __imul__.__doc__ = native.GTElement.__imul__.__doc__.replace("GT", "G1")
+
+    __truediv__.__doc__ = native.GTElement.__truediv__.__doc__.replace("GT", "G1")
+    __itruediv__.__doc__ = native.GTElement.__itruediv__.__doc__.replace("GT", "G1")
+
+    __pow__.__doc__ = native.GTElement.__pow__.__doc__.replace("GT", "G1")
+    __ipow__.__doc__ = native.GTElement.__ipow__.__doc__.replace("GT", "G1")
+
+    #
+    # Aliases
+    #
+
+    mul = __mul__
+    imul = __imul__
+    div = __truediv__
+    idiv = __itruediv__
+    __floordiv__ = __truediv__
+    __ifloordiv__ = __itruediv__
+    pow = __pow__
+    ipow = __ipow__
+
+
+class G2(native._G2Base):
+
+    @classmethod
+    def _element_type(cls):
+        return G2Element
+
+    @classmethod
+    def prod(cls, elems):
+        """Efficient product of a number of elements
+
+        In the current implementation this function is not optimized.
+
+        Example:
+            >>> elems = [ G2.generator() ** x for x in [10, 25, 13]]
+            >>> G2.prod(elems) ==  G2.generator() ** (10 + 25 + 13)
+            True
+        """
+        res = cls.neutral_element()
+        for el in elems:
+            res *= el
+        return res
+
+    @classmethod
+    def wprod(cls, weights, elems):
+        """Efficient weighted product of a number of elements
+
+        In the current implementation this function is not optimized.
+
+        Example:
+            >>> weights = [1, 2, 3]
+            >>> elems = [ G2.generator() ** x for x in [10, 25, 13]]
+            >>> G2.wprod(weights, elems) ==  G2.generator() ** (1 * 10 + 2 * 25 + 3 * 13)
+            True
+        """
+        res = cls.neutral_element()
+        for w, el in zip(weights, elems):
+            res *= el ** w
+
+        return res
+
+    #
+    # Aliases
+    #
+
+    @classmethod
+    def unity(cls):
+        """The unity element
+
+        Alias for :py:meth:`G2.neutral_element`
+        """
+        return cls.neutral_element()
+
+
+class G2Element(native._G2ElementBase):
+    """Element of the G2 group."""
+
+    group = G2
+
+    #
+    # Unary operators
+    #
+
+    def square(self):
+        return native.G2Element.double(self)
+
+    def isquare(self):
+        return native.G2Element.idouble(self)
+
+    #
+    # Binary operators
+    #
+
+    def __mul__(self, other):
+        return native.G2Element.__add__(self, other)
+
+    def __imul__(self, other):
+        return native.G2Element.__iadd__(self, other)
+
+    def __truediv__(self, other):
+        return native.G2Element.__sub__(self, other)
+
+    def __itruediv__(self, other):
+        return native.G2Element.__isub__(self, other)
+
+    def __pow__(self, other):
+        return native.G2Element.__mul__(self, other)
+
+    def __ipow__(self, other):
+        return native.G2Element.__imul__(self, other)
+
+    # Copy documentation from native.GTElement
+    square.__doc__ = native.GTElement.square.__doc__.replace("GT", "G2")
+    isquare.__doc__ = native.GTElement.isquare.__doc__.replace("GT", "G2")
+
+    __mul__.__doc__ = native.GTElement.__mul__.__doc__.replace("GT", "G2")
+    __imul__.__doc__ = native.GTElement.__imul__.__doc__.replace("GT", "G2")
+
+    __truediv__.__doc__ = native.GTElement.__truediv__.__doc__.replace("GT", "G2")
+    __itruediv__.__doc__ = native.GTElement.__itruediv__.__doc__.replace("GT", "G2")
+
+    __pow__.__doc__ = native.GTElement.__pow__.__doc__.replace("GT", "G2")
+    __ipow__.__doc__ = native.GTElement.__ipow__.__doc__.replace("GT", "G2")
+
+    #
+    # Aliases
+    #
+
+    mul = __mul__
+    imul = __imul__
+    div = __truediv__
+    idiv = __itruediv__
+    __floordiv__ = __truediv__
+    __ifloordiv__ = __itruediv__
+    pow = __pow__
+    ipow = __ipow__
+
+
+class GT(native.GT):
+    """GT group."""
+
+    @classmethod
+    def _element_type(cls):
+        return GTElement
+
+
+class GTElement(native.GTElement):
+    """GT element."""
+
+    group = GT
+
---
+++ packages/petrelic/petrelic-0.1.2-cp36-cp36m-manylinux1_x86_64.whl/petrelic/petlib/pairing.py
@@ -0,0 +1,258 @@
+r"""
+This module provides a Python wrapper around RELIC's pairings using petlib's
+interface: operations in :py:obj:`petrelic.native.pairings.G1` and
+:py:obj:`petrelic.native.pairings.G2` are written additively, whereas operations in
+:py:obj:`petrelic.native.pairings.GT` are written multiplicatively.
+
+"""
+from petrelic.bindings import _FFI, _C
+from petrelic.bn import Bn, force_Bn_other
+from petrelic.native.pairing import NoAffineCoordinateForECPoint
+
+import petrelic.native.pairing as native
+
+
+class BilinearGroupPair:
+    """
+    A bilinear group pair.
+
+    Contains two origin groups G1, G2 and the image group GT. The underlying
+    ``bplib.bp.BpGroup`` object is also embedded.
+    """
+
+    def __init__(self):
+        self.GT = GTGroup()
+        self.G1 = G1Group()
+        self.G2 = G2Group()
+
+    def groups(self):
+        """
+        Returns the three groups in the following order :  G1, G2, GT.
+        """
+        return self.G1, self.G2, self.GT
+
+
+class G1Group(native.G1):
+    """G1 group"""
+    
+    @classmethod
+    def _element_type(cls):
+        return G1Elem
+
+    def check_point(self, pt):
+        """Ensures the point is on the curve.
+
+        Example:
+            >>> G = G1Group()
+            >>> G.check_point(G.generator())
+            True
+            >>> G.check_point(G.infinite())
+            True
+        """
+        return type(pt) == self._element_type() and pt.is_valid()
+
+    @classmethod
+    def infinite(cls):
+        """The point at infinity.
+
+        Alias for :py:meth:`G1.neutral_element`
+        """
+        return cls.neutral_element()
+    # Not implemented: list_curves(), parameters(), get_points_from_x
+
+
+class G1Elem(native.G1Element):
+    """Element of the G1 group"""
+
+    group = G1Group
+
+    pt_add = native.G1Element.__add__
+    pt_add_inplace = native.G1Element.__iadd__
+    pt_sub = native.G1Element.__sub__
+    pt_double = native.G1Element.double
+    pt_double_inplace = native.G1Element.idouble
+    pt_neg = native.G1Element.__neg__
+    pt_neg_inplace = native.G1Element.iinverse
+    pt_mul = native.G1Element.__mul__
+    pt_mul_inplace = native.G1Element.__imul__
+    pt_eq = native.G1Element.__eq__
+    is_infinite = native.G1Element.is_neutral_element
+    export = native.G1Element.to_binary
+    get_affine = native.G1Element.get_affine_coordinates
+
+    def pair(self, other):
+        """Computes bilinear pairing with self and otherwise
+
+        Examples:
+            >>> G1 = G1Group()
+            >>> G2 = G2Group()
+            >>> GT = GTGroup()
+            >>> G1.generator().pair(G2.generator()) == GT.generator()
+            True
+
+            >>> p = 100 * G1.generator()
+            >>> q = 200 * G2.generator()
+            >>> p.pair(q) == GT.generator() ** 20000
+            True
+        """
+        if not type(other) == G2Elem:
+            raise TypeError("Second parameter should be of type G2Elem is {}".format(type(other)))
+
+        res = GTElem()
+        _C.pc_map(res.pt, self.pt, other.pt)
+        return res
+
+
+    def _get_coords(self):
+        x, y, z = Bn(), Bn(), Bn()
+        _C.fp_prime_back(x.bn, self.pt[0].x)
+        _C.fp_prime_back(y.bn, self.pt[0].y)
+        _C.fp_prime_back(z.bn, self.pt[0].z)
+        return (x, y, z)
+
+
+    @classmethod
+    def from_binary(cls, sbin, group=None):
+        return super().from_binary(sbin)
+
+
+    def __repr__(self):
+        """ Representation of G1Point
+
+        Examples:
+            >>> G1Group().infinite()
+            G1Elem(0, 0, 0)
+            >>> G1Group().generator()
+            G1Elem(3685416753713387016781088315183077757961620795782546409894578378688607592378376318836054947676345821548104185464507, 1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569, 1)
+        """
+        x, y, z = self._get_coords()
+        return "G1Elem({}, {}, {})".format(x, y, z)
+
+
+class G2Group(native.G2):
+    """G2 group"""
+
+    @classmethod
+    def _element_type(cls):
+        return G2Elem
+
+
+    def check_point(self, pt):
+        """Ensures the point is on the curve.
+
+        Example:
+            >>> G = G2Group()
+            >>> G.check_point(G.generator())
+            True
+            >>> G.check_point(G.infinite())
+            True
+        """
+        return type(pt) == self._element_type() and pt.is_valid()
+
+
+    @classmethod
+    def infinite(cls):
+        """The point at infinity.
+
+        Alias for :py:meth:`G2.neutral_element`
+        """
+        return cls.neutral_element()
+
+
+class G2Elem(native.G2Element):
+    """Element of the G2 group"""
+
+    group = G1Group
+
+    pt_add = native.G2Element.__add__
+    pt_add_inplace = native.G2Element.__iadd__
+    pt_sub = native.G2Element.__sub__
+    pt_double = native.G2Element.double
+    pt_double_inplace = native.G2Element.idouble
+    pt_neg = native.G2Element.__neg__
+    pt_neg_inplace = native.G2Element.iinverse
+    pt_mul = native.G2Element.__mul__
+    pt_mul_inplace = native.G2Element.__imul__
+    pt_eq = native.G2Element.__eq__
+    is_infinite = native.G2Element.is_neutral_element
+    export = native.G2Element.to_binary
+
+    @classmethod
+    def from_binary(cls, sbin, group=None):
+        return super().from_binary(sbin)
+
+    def __repr__(self):
+        """ Representation of G2Point
+        Examples:
+            >>> G2Group().infinite()
+            G2Elem(00)
+            >>> G2Group().generator()
+            G2Elem(02024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb813e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e)
+        """
+        pt_hex = self.to_binary().hex()
+        return "G2Elem({})".format(pt_hex)
+
+
+class GTGroup(native.GT):
+    """GT group"""
+
+    @classmethod
+    def _element_type(cls):
+        return GTElem
+
+    def check_elem(self, pt):
+        """Ensures the element is an element of the group
+
+        Example:
+            >>> G = GTGroup()
+            >>> G.check_elem(G.generator())
+            True
+            >>> G.check_elem(G.unity())
+            True
+        """
+
+        return type(pt) == self._element_type() and pt.is_valid()
+
+    # Not implemented: list_curves(), parameters(), get_points_from_x
+
+
+class GTElem(native.GTElement):
+    """GT element"""
+
+    group = GTGroup
+
+    mul_inplace = native.GTElement.__imul__
+    inv = native.GTElement.inverse
+    inv_inplace = native.GTElement.iinverse
+    sqr = native.GTElement.square
+    sqr_inplace = native.GTElement.isquare
+    exp = native.GTElement.__pow__
+    exp_inplace = native.GTElement.__ipow__
+    export = native.GTElement.to_binary
+
+    @classmethod
+    def from_binary(cls, sbin, group=None):
+        """Create an element from a byte sequence.
+
+        It accepts (but ignores) group as extra argument.
+
+        Example:
+            >>> G = GTGroup()
+            >>> byte_string = G.generator().export()                # Export EC point as byte string
+            >>> GTElem.from_binary(byte_string, G) == G.generator()    # Import EC point from binary string
+            True
+            >>> GTElem.from_binary(byte_string) == G.generator()    # Import EC point from binary string
+            True
+        """
+
+        return super().from_binary(sbin)
+
+
+    def __repr__(self):
+        """ Representation of GTPoint
+        Examples:
+            >>> GTGroup().generator()
+            GTElem(1368bb445c7c2d209703f239689ce34c0378a68e72a6b3b216da0e22a5031b54ddff57309396b38c881c4c849ec23e87193502b86edb8857c273fa075a50512937e0794e1e65a7617c90d8bd66065b1fffe51d7a579973b1315021ec3c19934f01b2f522473d171391125ba84dc4007cfbf2f8da752f7c74185203fcca589ac719c34dffbbaad8431dad1c1fb597aaa5018107154f25a764bd3c79937a45b84546da634b8f6be14a8061e55cceba478b23f7dacaa35c8ca78beae9624045b4b619f26337d205fb469cd6bd15c3d5a04dc88784fbb3d0b2dbdea54d43b2b73f2cbb12d58386a8703e0f948226e47ee89d06fba23eb7c5af0d9f80940ca771b6ffd5857baaf222eb95a7d2809d61bfe02e1bfd1b68ff02f0b8102ae1c2d5d5ab1a04c581234d086a9902249b64728ffd21a189e87935a954051c7cdba7b3872629a4fafc05066245cb9108f0242d0fe3ef0f41e58663bf08cf068672cbd01a7ec73baca4d72ca93544deff686bfd6df543d48eaa24afe47e1efde449383b676631)
+        """
+        pt_hex = self.to_binary().hex()
+        return "GTElem({})".format(pt_hex)
