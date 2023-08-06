2a165ed451df1073706fc8d9b33bea9848be5e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package super-csv/0.9.9/super_csv-0.9.9-py2.py3-none-any.whl

---
+++ packages/super-csv/super_csv-0.9.9-py2.py3-none-any.whl/super_csv/admin.py
@@ -0,0 +1,15 @@
+"""
+Django admin for CSV Operations
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+from django.contrib import admin
+
+from .models import CSVOperation
+
+
+@admin.register(CSVOperation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'class_name', 'unique_id', 'created')
+    readonly_fields = ('created', 'modified')
+    raw_id_fields = ('user', )
