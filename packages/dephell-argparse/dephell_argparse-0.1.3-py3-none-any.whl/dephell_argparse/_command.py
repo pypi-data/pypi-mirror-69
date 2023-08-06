89ec2d9b09a948bb3c75742dddfa4bce2c1bb1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:56 +0000

    Add package dephell-argparse/0.1.3/dephell_argparse-0.1.3-py3-none-any.whl

---
+++ packages/dephell-argparse/dephell_argparse-0.1.3-py3-none-any.whl/dephell_argparse/_command.py
@@ -0,0 +1,111 @@
+# built-in
+from collections import Counter, defaultdict
+from typing import DefaultDict, FrozenSet, Iterable, List, Optional, Tuple
+
+# app
+from ._cached_property import cached_property
+
+
+class Command:
+    def __init__(self, argv: Iterable[str], commands: Iterable[str]):
+        self._argv = tuple(argv)
+        self.commands = {c.lower() for c in commands}
+
+    @cached_property
+    def groups(self) -> FrozenSet[str]:
+        groups = set()
+        for command in self.commands:
+            group, _, _ = command.rpartition(' ')
+            if group:
+                groups.add(group)
+        return frozenset(groups)
+
+    @cached_property
+    def words(self) -> int:
+        return len((self.match or '').split())
+
+    @property
+    def argv(self) -> Tuple[str, ...]:
+        return self._argv[self.words:]
+
+    @property
+    def group(self) -> Optional[str]:
+        group = self._argv[0]
+        if group in self.groups:
+            return group
+        return None
+
+    @staticmethod
+    def _similar(cmd1: str, cmd2: str, threshold: int = 1) -> bool:
+        given = Counter(cmd1)
+        guess = Counter(cmd2)
+        counter_diff = (given - guess) + (guess - given)
+        diff = sum(counter_diff.values())
+        return diff <= threshold
+
+    @cached_property
+    def match(self) -> Optional[str]:
+        if not self._argv:
+            return None
+
+        for size, direction in ((2, 1), (1, 1), (2, -1)):
+            command_name = ' '.join(self._argv[:size][::direction])
+            if command_name in self.commands:
+                return command_name
+
+        # specified the only one word from command
+        commands_by_parts = defaultdict(list)  # type: DefaultDict[str, List[str]]
+        for command_name in self.commands:
+            for part in command_name.split():
+                commands_by_parts[part].append(command_name)
+        command_names = commands_by_parts[self._argv[0]]
+        if len(command_names) == 1:
+            return command_names[0]
+
+        # typo in command name
+        for size in (1, 2):
+            command_name = ' '.join(self._argv[:size])
+            for command_guess in self.commands:
+                if self._similar(command_name, command_guess):
+                    return command_guess
+
+        return None
+
+    @cached_property
+    def guesses(self) -> FrozenSet[str]:
+        guesses = set()
+
+        if self.group:
+            for command in self.commands:
+                if command.startswith(self.group + ' '):
+                    guesses.add(command)
+            return frozenset(guesses)
+
+        # typed only one word from two words
+        for name in self.argv[:2]:
+            for command_name in self.commands:
+                _, _, subcommand_name = command_name.rpartition(' ')
+                if name == subcommand_name:
+                    guesses.add(command_name)
+        if guesses:
+            return frozenset(guesses)
+
+        # typed fully but with too many mistakes
+        for size in 1, 2:
+            name = ' '.join(self._argv[:size])
+            for command_name in self.commands:
+                if self._similar(name, command_name, threshold=3):
+                    guesses.add(command_name)
+        if guesses:
+            return frozenset(guesses)
+
+        # typed only one word from two, and it contains typos
+        for name in self.argv[:2]:
+            for command_name in self.commands:
+                for part in command_name.split():
+                    if self._similar(name, part):
+                        guesses.add(command_name)
+        if guesses:
+            return frozenset(guesses)
+
+        return frozenset()
---
+++ packages/dephell-argparse/dephell_argparse-0.1.3-py3-none-any.whl/dephell_argparse/_handler.py
@@ -0,0 +1,120 @@
+# built-in
+import os
+import re
+import sys
+from argparse import ArgumentParser, Namespace
+from inspect import getdoc
+from logging import getLogger
+from textwrap import dedent
+from typing import IO, Iterable, Optional
+
+# app
+from ._cached_property import cached_property
+
+
+REX_WORD = re.compile(r'([a-z\d])([A-Z])')
+
+
+class CommandHandler:
+    logger = getLogger('dephell_argparse')
+    stream = sys.stdout
+    argv = None
+
+    def __init__(self, *, handler=None, argv: Iterable[str] = None, **kwargs):
+        for key, value in kwargs.items():
+            if value is not None:
+                setattr(self, key, value)
+        self.handler = handler
+        if argv is not None:
+            self.argv = tuple(argv)
+
+    def copy(self, **kwargs) -> 'CommandHandler':
+        params = vars(self).copy()
+        params.update(kwargs)
+        return type(self)(**params)
+
+    @classmethod
+    def build_parser(cls, parser: ArgumentParser) -> ArgumentParser:
+        return parser
+
+    def __call__(self) -> bool:
+        if self.handler is not None:
+            return self.handler(self.args)
+        raise NotImplementedError
+
+    # helpers for handler
+
+    def print(self, *args, sep: str = ' ', end: str = '\n',
+              stream: IO = None, flush: bool = False) -> None:
+        print(*args, sep=sep, end=end, file=stream or self.stream, flush=flush)
+
+    # defaults
+
+    @cached_property
+    def parser(self) -> ArgumentParser:
+        from ._parser import Parser
+
+        parser = Parser(
+            prog=self.prog + ' ' + self.name,
+            usage=self.usage,
+            description=self.description,
+            url=self.url,
+            epilog=self.epilog,
+        )
+        return self.build_parser(parser=parser)
+
+    @cached_property
+    def name(self) -> str:
+        if self.handler is not None:
+            raw = self.handler.__name__
+            result = raw.split('_')
+        else:
+            raw = type(self).__name__
+            result = REX_WORD.sub(r'\1 \2', raw).split()
+        result = [word.strip() for word in result]
+        result = [word.lower() for word in result if word]
+        if result[-1] == 'handler':
+            result = result[:-1]
+        if result[-1] == 'command':
+            result = result[:-1]
+        return ' '.join(result)
+
+    @cached_property
+    def prog(self) -> str:
+        return os.path.basename(sys.argv[0])
+
+    @cached_property
+    def url(self) -> Optional[str]:
+        return None
+
+    @cached_property
+    def epilog(self) -> Optional[str]:
+        return self.url
+
+    @cached_property
+    def usage(self) -> Optional[str]:
+        return None
+
+    @cached_property
+    def description(self) -> str:
+        doc = self._get_docstring()
+        return dedent(doc).strip()
+
+    def _get_docstring(self) -> str:
+        doc = getdoc(type(self))
+        if doc is not None:
+            return doc
+        if self.handler is None:
+            return ''
+        doc = getdoc(self.handler)
+        if doc is not None:
+            return doc
+        return ''
+
+    @cached_property
+    def summary(self) -> str:
+        return self.parser.description.split('\n')[0]
+
+    @cached_property
+    def args(self) -> Namespace:
+        return self.parser.parse_args(self.argv)
