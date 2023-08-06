6045fed5b9e46bb08ad110b47ea3bcb12021eabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:23 +0000

    Add package iot-facerecognition-client/1.1.0/iot-facerecognition-client-1.1.0.tar.gz

---
+++ packages/iot-facerecognition-client/iot-facerecognition-client-1.1.0.tar.gz/iot-facerecognition-client-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='iot-facerecognition-client',
+    version='1.1.0',
+    py_modules=['iot_facerecognition_client'],
+    install_requires=['opencv-python', 'websocket-client'],
+    entry_points={
+        'console_scripts': [
+            'iot-facerecognition-client = iot_facerecognition_client:main'
+        ]},
+    url='https://github.com/fuzun/iot-facerecognition',
+    license='GPLv3',
+    author='fuzun',
+    author_email='fuzun54@outlook.com',
+    description='python client for iot-facerecognition-server'
+)
