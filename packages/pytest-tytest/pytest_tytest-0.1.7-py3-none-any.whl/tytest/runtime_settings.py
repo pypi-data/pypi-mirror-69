eff48ed397870c68d1c1e55b30e380afc8122a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:42 +0000

    Add package pytest-tytest/0.1.7/pytest_tytest-0.1.7-py3-none-any.whl

---
+++ packages/pytest-tytest/pytest_tytest-0.1.7-py3-none-any.whl/tytest/runtime_settings.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+class Config:
+    pass
+
+
+class Settings:
+    XRAY_HOST = ''
+    XRAY_CLIENT_ID = ''
+    XRAY_CLIENT_SECRET = ''
+    XRAY_PLAN_KEY = ''
+    XRAY_FAIL_SILENTLY = False
+    XRAY_TOKEN = ''
+    JIRA_HOST = ''
+    JIRA_USER = ''
+    JIRA_PASSWORD = ''
+    JIRA_AUTH = []
+    RUN_CONFIG = ''
+
+
+class Stats:
+    START_TIME = None
+    END_TIME = None
+
+
+class TestExecutionResult:
+    xray_keys = {}
+    functions = {}
