8e420944058cc6aefd1c59c4da5553fd7ddc5f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:35 +0000

    Add package pwlf/2.0.2/pwlf-2.0.2.tar.gz

---
+++ packages/pwlf/pwlf-2.0.2.tar.gz/pwlf-2.0.2/setup.py
@@ -0,0 +1,22 @@
+import io
+from distutils.core import setup
+
+setup(
+    name='pwlf',
+    version='2.0.2',
+    author='Charles Jekel',
+    author_email='cjekel@gmail.com',
+    packages=['pwlf'],
+    url='https://github.com/cjekel/piecewise_linear_fit_py',
+    license='MIT License',
+    description='fit piecewise linear functions to data',
+    long_description=io.open('README.rst', encoding="utf-8").read(),
+    # long_description_content_type='text/markdown',
+    platforms=['any'],
+    install_requires=[
+        "numpy >= 1.14.0",
+        "scipy >= 1.2.0",
+        "pyDOE >= 0.3.8",
+        'importlib-metadata ~= 1.0 ; python_version < "3.8"',
+    ],
+)
