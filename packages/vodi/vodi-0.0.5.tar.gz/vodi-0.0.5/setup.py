7fc2c8bd97183590022dd51d25bbfdc821862b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:50 +0000

    Add package vodi/0.0.5/vodi-0.0.5.tar.gz

---
+++ packages/vodi/vodi-0.0.5.tar.gz/vodi-0.0.5/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="vodi", # Replace with your own username
+    version="0.0.5",
+    author="Andrea Caligiuri",
+    author_email="andrea.caligiuri@vodafone.com",
+    description="a vodafone digital automation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    py_modules=['vodi'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+            'click',
+            'atlassian-python-api'
+        ],
+    entry_points='''
+        [console_scripts]
+        vodi=vodi:cli
+    ''',
+
+)
