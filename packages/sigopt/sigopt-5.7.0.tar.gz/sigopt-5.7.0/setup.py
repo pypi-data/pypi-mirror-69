e596e8eef91ba9d75b37c76681f581eaf51f0898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:35 +0000

    Add package sigopt/5.7.0/sigopt-5.7.0.tar.gz

---
+++ packages/sigopt/sigopt-5.7.0.tar.gz/sigopt-5.7.0/setup.py
@@ -0,0 +1,49 @@
+from codecs import open
+import os
+import sys
+import warnings
+
+from setuptools import setup
+
+if sys.version_info < (2, 7):
+  warnings.warn(
+    'Python 2.6 is no longer supported.'
+    ' Please upgrade to Python 2.7 or use version 2 of the sigopt-python client.',
+    DeprecationWarning
+  )
+
+# keep this in sync with requirements.txt
+install_requires = ['requests>=2.11.1']
+
+# NOTE(patrick): We can't `import sigopt.version` directly, because that
+# will cause us to execute `sigopt/__init__.py`, which may transitively import
+# packages that may not have been installed yet. So jump straight to sigopt/version.py
+# and execute that directly, which should be simple enough that it doesn't import anything
+# Learned from https://github.com/stripe/stripe-python (MIT licensed)
+version_contents = {}
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'sigopt', 'version.py'), encoding='utf-8') as f:
+  exec(f.read(), version_contents)
+VERSION = version_contents['VERSION']
+
+setup(
+  name='sigopt',
+  version=VERSION,
+  description='SigOpt Python API Client',
+  author='SigOpt',
+  author_email='support@sigopt.com',
+  url='https://sigopt.com/',
+  packages=['sigopt', 'sigopt.examples', 'sigopt.vendored'],
+  install_requires=install_requires,
+  extras_require={
+    'dev': 'numpy>=1.16.4',
+  },
+  classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+  ],
+)
---
+++ packages/sigopt/sigopt-5.7.0.tar.gz/sigopt-5.7.0/test/test_interface.py
@@ -0,0 +1,65 @@
+import os
+import pytest
+import mock
+
+from sigopt.interface import Connection
+from sigopt.requestor import DEFAULT_HTTP_TIMEOUT
+from sigopt.resource import ApiResource
+
+class TestInterface(object):
+  def test_create(self):
+    conn = Connection(client_token='client_token')
+    assert conn.impl.api_url == 'https://api.sigopt.com'
+    assert conn.impl.requestor.verify_ssl_certs is None
+    assert conn.impl.requestor.session is None
+    assert conn.impl.requestor.proxies is None
+    assert conn.impl.requestor.timeout == DEFAULT_HTTP_TIMEOUT
+    assert isinstance(conn.clients, ApiResource)
+    assert isinstance(conn.experiments, ApiResource)
+
+  def test_create_uses_session_if_provided(self):
+    session = mock.Mock()
+    conn = Connection(client_token='client_token', session=session)
+    assert conn.impl.requestor.session is session
+
+    response = mock.Mock()
+    session.request.return_value = response
+    response.status_code = 200
+    response.text = '{}'
+    session.request.assert_not_called()
+    conn.experiments().fetch()
+    session.request.assert_called_once()
+
+  def test_environment_variable(self):
+    with mock.patch.dict(os.environ, {'SIGOPT_API_TOKEN': 'client_token'}):
+      Connection()
+
+  def test_api_url(self):
+    conn = Connection('client_token')
+    conn.set_api_url('https://api-test.sigopt.com')
+    assert conn.impl.api_url == 'https://api-test.sigopt.com'
+
+  def test_api_url_env(self):
+    with mock.patch.dict(os.environ, {'SIGOPT_API_URL': 'https://api-env.sigopt.com'}):
+      conn = Connection('client_token')
+      assert conn.impl.api_url == 'https://api-env.sigopt.com'
+
+  def test_verify(self):
+    conn = Connection('client_token')
+    conn.set_verify_ssl_certs(False)
+    assert conn.impl.requestor.verify_ssl_certs is False
+
+  def test_proxies(self):
+    conn = Connection('client_token')
+    conn.set_proxies({'http': 'http://127.0.0.1:6543'})
+    assert conn.impl.requestor.proxies['http'] == 'http://127.0.0.1:6543'
+
+  def test_error(self):
+    with mock.patch.dict(os.environ, {'SIGOPT_API_TOKEN': ''}):
+      with pytest.raises(ValueError):
+        Connection()
+
+  def test_timeout(self):
+    conn = Connection('client_token')
+    conn.set_timeout(30)
+    assert conn.impl.requestor.timeout == 30
