97225ca7544374ee712d175bf6ccbae159efb2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:29 +0000

    Add package gaussian-binomial-nmsami/0.1/gaussian-binomial-nmsami-0.1.tar.gz

---
+++ packages/gaussian-binomial-nmsami/gaussian-binomial-nmsami-0.1.tar.gz/gaussian-binomial-nmsami-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian-binomial-nmsami',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian-binomial-nmsami'],
+      author='Naeem Mohammad Sami',
+      year='2020',
+      zip_safe=False)
