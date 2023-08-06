38cf8f03059aa5c62ed5d09eac7153f1c6ed42e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:27 +0000

    Add package django-prefab-endpoints/0.0.2/django-prefab-endpoints-0.0.2.tar.gz

---
+++ packages/django-prefab-endpoints/django-prefab-endpoints-0.0.2.tar.gz/django-prefab-endpoints-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-prefab-endpoints", # Replace with your own username
+    version="0.0.2",
+    author="John Synnott",
+    author_email="johnsynn@gmail.com",
+    description="Quick and basic endpoints for Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/johnsynnott/django-prefab-endpoints",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
