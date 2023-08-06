994b6b2d27022eb0a7f67221cbe41a7755b330ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:10 +0000

    Add package distribution-calculator/0.1/distribution_calculator-0.1.tar.gz

---
+++ packages/distribution-calculator/distribution_calculator-0.1.tar.gz/distribution_calculator-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distribution_calculator',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distribution_calculator'],
+      author = 'Sumeet Sarode',
+      zip_safe=False)
