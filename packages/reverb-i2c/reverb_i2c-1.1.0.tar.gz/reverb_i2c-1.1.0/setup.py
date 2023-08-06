92dee34969ef50edb36208f9c94dcd5484d5b939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:01 +0000

    Add package reverb-i2c/1.1.0/reverb_i2c-1.1.0.tar.gz

---
+++ packages/reverb-i2c/reverb_i2c-1.1.0.tar.gz/reverb_i2c-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="reverb_i2c",
+    version="1.1.0",
+    license="GPL",
+    packages=find_packages(),
+    author="tjreverb",
+    author_email="tjreverb@gmail.com",
+    description="A fake i2c library for testing purposes",
+    long_description=long_description,
+    url="https://github.com/TJREVERB/reverb_i2c",
+    python_requires='>=3.6',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Other Audience",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+    ]
+)
