82b42e743abdd0a5b8a7ca171139e27e1782ed91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:14 +0000

    Add package sid-probability/0.1/sid_probability-0.1.tar.gz

---
+++ packages/sid-probability/sid_probability-0.1.tar.gz/sid_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sid_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sid_probability'],
+      author = 'Siddhartha Shrestha',
+      author_email = 'shrestha.siddhartha06@outlook.com',
+      zip_safe = False)
