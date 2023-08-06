4679caae40ac7ce8e42e7d0a06e7cc25049493ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:47 +0000

    Add package ammolite/0.2.0/ammolite-0.2.0-py3-none-any.whl

---
+++ packages/ammolite/ammolite-0.2.0-py3-none-any.whl/ammolite/__init__.py
@@ -0,0 +1,16 @@
+# This source code is part of the Biotite package and is distributed
+# under the 3-Clause BSD License. Please see 'LICENSE.rst' for further
+# information.
+
+"""
+Transfer structure data from Biotite to PyMOL.
+"""
+
+__version__ = "0.2.0"
+__name__ = "ammolite"
+__author__ = "Patrick Kunzmann"
+
+from .convert import *
+from .display import *
+from .object import *
+from .startup import *
