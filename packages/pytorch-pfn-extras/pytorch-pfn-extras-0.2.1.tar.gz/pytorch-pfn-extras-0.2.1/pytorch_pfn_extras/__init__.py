302fc1e5f90f5d9bfaad193591e1cf33a228bfa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:31 +0000

    Add package pytorch-pfn-extras/0.2.1/pytorch-pfn-extras-0.2.1.tar.gz

---
+++ packages/pytorch-pfn-extras/pytorch-pfn-extras-0.2.1.tar.gz/pytorch-pfn-extras-0.2.1/pytorch_pfn_extras/__init__.py
@@ -0,0 +1,6 @@
+from pytorch_pfn_extras import config  # NOQA
+from pytorch_pfn_extras import nn  # NOQA
+from pytorch_pfn_extras import reporting  # NOQA
+from pytorch_pfn_extras import training  # NOQA
+from pytorch_pfn_extras import writing  # NOQA
+from pytorch_pfn_extras._version import __version__  # NOQA
