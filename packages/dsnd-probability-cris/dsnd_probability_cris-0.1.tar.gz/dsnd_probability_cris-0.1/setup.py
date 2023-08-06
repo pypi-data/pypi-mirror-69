71872cc1517fd112a1ddb9ba849f64fe2151ecef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:18 +0000

    Add package dsnd-probability-cris/0.1/dsnd_probability_cris-0.1.tar.gz

---
+++ packages/dsnd-probability-cris/dsnd_probability_cris-0.1.tar.gz/dsnd_probability_cris-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_cris',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_cris'],
+      author = "Cristyan Gil",
+      zip_safe=False)
