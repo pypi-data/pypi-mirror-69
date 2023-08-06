1a41a6bea511b17797124263235514aefa15b93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:50 +0000

    Add package sdt-probability/0.1/sdt_probability-0.1.tar.gz

---
+++ packages/sdt-probability/sdt_probability-0.1.tar.gz/sdt_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sdt_probability',
+      version='0.1',
+      description='Gaussian,General and Binomial distributions',
+      packages=['sdt_probability'],
+      author='Soubhik Das',
+      author_email='info@soubhikdas.me',
+      zip_safe=False)
