2a15f497ca9a017454af2ac711ed903228c56a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:22 +0000

    Add package bng-dist/1.0/bng_dist-1.0.tar.gz

---
+++ packages/bng-dist/bng_dist-1.0.tar.gz/bng_dist-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bng_dist',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['bng_dist'],
+      author='Hardik Trivedi',
+      author_email = 'htrivedicali@gmail.com',
+      zip_safe=False)
