1086564acb26b9a5ce4cbba8af490e76280037c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:19 +0000

    Add package plumbing/2.7.9/plumbing-2.7.9.tar.gz

---
+++ packages/plumbing/plumbing-2.7.9.tar.gz/plumbing-2.7.9/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'plumbing',
+    version          = '2.7.9',
+    description      = 'Helps with plumbing-type programing in python.',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+    license          = 'MIT',
+    url              = 'http://github.com/xapple/plumbing/',
+    author           = 'Lucas Sinclair',
+    author_email     = 'lucas.sinclair@me.com',
+    packages         = find_packages(),
+    install_requires = ['autopaths', 'six', 'pandas', 'numpy', 'matplotlib',
+                        'retry'],
+    include_package_data = True,
+)
