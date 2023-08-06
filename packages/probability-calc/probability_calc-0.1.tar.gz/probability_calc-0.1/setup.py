457797f72d031689212b570d4daffdca22329cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:32 +0000

    Add package probability-calc/0.1/probability_calc-0.1.tar.gz

---
+++ packages/probability-calc/probability_calc-0.1.tar.gz/probability_calc-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_calc',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author= 'Sayantan Kumar',
+      author_email= 'sayantan.jr15@gmail.com',
+      zip_safe=False)
