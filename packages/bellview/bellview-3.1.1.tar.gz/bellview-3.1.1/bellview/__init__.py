0ecb0e0f90a9bb3a322a9edb4d89d695cb10dad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:32 +0000

    Add package bellview/3.1.1/bellview-3.1.1.tar.gz

---
+++ packages/bellview/bellview-3.1.1.tar.gz/bellview-3.1.1/bellview/__init__.py
@@ -0,0 +1,18 @@
+######################################################################
+### Bell View
+### Copyright (c) 2020 Jonathan Wilson
+### Please refer to help/licence.txt for further information
+### File: __init__.py
+### Description: Bell View Application
+### Last Modified: 24 May 2020
+######################################################################
+
+'''\
+Bell View is an interactive method explorer for church bell ringers
+which uses a Graphical User Interface (GUI). It works out the rows of
+a plain course or touch, displays it the screen, and audibly rings it
+using a set of bell sample files. Bell View makes use of the Change
+Ringing Engine (crengine) as an API'''
+
+from .bvapp import BellViewApp
+
