404e2a7ae5f652e432639d72471fe807959b4656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:48 +0000

    Add package recline/2020.5/recline-2020.5-py3-none-any.whl

---
+++ packages/recline/recline-2020.5-py3-none-any.whl/recline/arg_types/positional.py
@@ -0,0 +1,40 @@
+"""
+A Positional type allows the CLI command writer to specify an argument that the
+user will provide positionally. That is, instead of the user typing
+"-arg_name argvalue", they can just type "argvalue". For example:
+
+@recline.command
+def ls(path: Positional = ".") -> None:
+    # If the user provided "ls my_dir", then path will be set to "my_dir". If the
+    # user just said "ls", then path will be set to ".".
+"""
+
+from recline.arg_types.recline_type import ReclineType
+from recline.arg_types.recline_type_error import ReclineTypeError
+
+
+class Positional(ReclineType):
+    """The Positional type allows the user to not have to specify the arugment name"""
+
+    @staticmethod
+    def define(completer=None, data_type=str):
+        """Allows a Positional type to be defined with its own completer function"""
+
+        class _Positional(Positional):
+            def __init__(self):
+                if completer:
+                    self.completer = completer
+                self.data_type = data_type
+                super().__init__()
+
+            def validate(self, arg: str):
+                if arg is None or arg == "":
+                    raise ReclineTypeError("Value cannot be empty")
+                try:
+                    return self.data_type(arg)
+                except Exception:
+                    raise ReclineTypeError(
+                        "Cannot parse %s as a valid %s" % (arg, data_type.__name__)
+                    )
+
+        return _Positional
