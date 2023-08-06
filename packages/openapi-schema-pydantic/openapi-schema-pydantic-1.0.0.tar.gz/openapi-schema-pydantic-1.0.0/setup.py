7f469bd92612739466623ff8729e33c7fb485baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:00 +0000

    Add package openapi-schema-pydantic/1.0.0/openapi-schema-pydantic-1.0.0.tar.gz

---
+++ packages/openapi-schema-pydantic/openapi-schema-pydantic-1.0.0.tar.gz/openapi-schema-pydantic-1.0.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="openapi-schema-pydantic",
+    version="1.0.0",
+    author="Kuimono",
+    description="OpenAPI (v3) specification schema as pydantic class",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kuimono/openapi-schema-pydantic",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=["pydantic>=1.5.1"],
+    tests_require=["pytest"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
