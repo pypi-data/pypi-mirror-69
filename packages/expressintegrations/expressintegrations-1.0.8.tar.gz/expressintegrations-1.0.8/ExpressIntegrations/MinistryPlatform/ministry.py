a02d739ff7b43cf67b066a941bf77a83c99030f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:58 +0000

    Add package expressintegrations/1.0.8/expressintegrations-1.0.8.tar.gz

---
+++ packages/expressintegrations/expressintegrations-1.0.8.tar.gz/expressintegrations-1.0.8/ExpressIntegrations/MinistryPlatform/ministry.py
@@ -0,0 +1,54 @@
+import json
+
+from ..HTTP.Requests import *
+from ..Utils import Utils
+from urllib.parse import urlencode
+
+
+class ministry:
+
+  def __init__(self, site, env, access_token):
+    self.headers = {"Content-Type": "application/json", "Accept": "application/json"}
+    self.headers['Authorization'] = f"Bearer {access_token}"
+    self.base_url = site
+
+  def create_record(self, table, record):
+    return self.create_records(table, [record])
+
+  def create_records(self, table, records):
+    post_url = f"{self.base_url}tables/{table}"
+    result = post(post_url, self.headers, json.dumps(records))
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to retrieve record. Result: {result}")
+    return result
+
+  def get_record_by_id(self, table, record_id):
+    post_url = f"{self.base_url}tables/{table}/{record_id}"
+    result = get(post_url, self.headers)
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to retrieve record. Result: {result}")
+    return result
+
+  def search_records(self, table, search_string=None):
+    post_url = f"{self.base_url}tables/{table}"
+    if search_string is not None:
+      sql_filter = {'filter': search_string}
+      post_url = f"{post_url}?${urlencode(sql_filter)}"
+    result = get(post_url, self.headers)
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to retrieve records. Result: {result}")
+    return result
+
+  def update_records(self, table, records):
+    post_url = f"{self.base_url}tables/{table}"
+    result = put(post_url, self.headers, json.dumps(records))
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to retrieve record. Result: {result}")
+    return result
+
+  def delete_record(self, table, record_id):
+    post_url = f"{self.base_url}tables/{table}/{record_id}"
+    result = delete(post_url, self.headers)
+    if not Utils.is_success(result['status_code']):
+      raise Exception(f"Failed to delete record. Result: {result}")
+    return result
