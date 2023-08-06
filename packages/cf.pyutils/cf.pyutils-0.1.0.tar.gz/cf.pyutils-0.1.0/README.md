7d6c5a5d09c2a27bea1a0f6e1cd2d67b721ef8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:37 +0000

    Add package cf.pyutils/0.1.0/cf.pyutils-0.1.0.tar.gz

---
+++ packages/cf.pyutils/cf.pyutils-0.1.0.tar.gz/cf.pyutils-0.1.0/README.md
@@ -0,0 +1,3 @@
+# PyUtils
+
+Some utilities tools and commonly used function across multiple python projects
---
+++ packages/cf.pyutils/cf.pyutils-0.1.0.tar.gz/cf.pyutils-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import os
+
+# if os.environ.get('CI_COMMIT_TAG'):
+#     version = os.environ['CI_COMMIT_TAG']
+# else:
+#     version = os.environ['CI_JOB_ID']
+
+setup(
+    name="cf.pyutils",
+    include_package_data=True,
+    install_requires=[],
+    version="0.1.0",
+    url="https://gitlab.com/MatthieuJacquemet/pyutils",
+    author="Matthieu Jacquemet",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
