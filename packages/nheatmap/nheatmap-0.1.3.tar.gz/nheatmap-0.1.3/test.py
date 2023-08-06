3fcf9607a6df6cec88f759ca3ce747964e110cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:06 +0000

    Add package nheatmap/0.1.3/nheatmap-0.1.3.tar.gz

---
+++ packages/nheatmap/nheatmap-0.1.3.tar.gz/nheatmap-0.1.3/test.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import sklearn.decomposition, sklearn.cluster
+from nheatmap import nhm, scripts
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+df = scripts.simulate_data(nrows=60)
+nrows, ncols = np.shape(df)
+pc = sklearn.decomposition.PCA().fit(df)
+dfr = pd.DataFrame(pc.transform(df)[:, 0], index=['sample '+str(x) for x in np.arange(1, nrows+1)],
+        columns=['PC1'])
+dfr['cell cluster'] = sklearn.cluster.KMeans(n_clusters=3).fit_predict(df).astype(str)
+dfc = pd.DataFrame(pc.components_[0], index=['gene '+str(x) for x in
+    np.arange(1, ncols+1)], columns=['PC score'])
+dfc['gene cluster'] = sklearn.cluster.KMeans(n_clusters=20).fit_predict(df.T).astype(str)
+dfc['PC score 2'] = pc.components_[1]
+cmaps={'cell cluster':'Paired', 'PC1':'RdYlGn', 'gene cluster':'inferno',
+        'PC score':'gist_heat', 'PC score 2':'rainbow'}
+dfsmall = df.iloc[:20, :10]
+dfrsmall = dfr.iloc[:20]
+dfcsmall = dfr.iloc[:10]
+
+g = nhm(data=df, dfr=dfr, dfc=dfc, figsize=(10, 10),
+        cmaps=cmaps, linewidths=0, showxticks=False, showyticks=False)
+g.hcluster()
+fig, plots = g.run()
+fig.savefig('./examples/example2.png', bbox_inches='tight')
+
