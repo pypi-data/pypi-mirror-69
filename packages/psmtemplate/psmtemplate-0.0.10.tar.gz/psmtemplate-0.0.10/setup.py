a299f65b2f3586056c1a911315386e2436cf4626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:32 +0000

    Add package psmtemplate/0.0.10/psmtemplate-0.0.10.tar.gz

---
+++ packages/psmtemplate/psmtemplate-0.0.10.tar.gz/psmtemplate-0.0.10/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="psmtemplate",
+    version="0.0.10",
+    author="Jianan Jiang",
+    author_email="jianan.jiang@psm.com.au",
+    description="PSM Templates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=["reportlab","matplotlib","pdfrw","pillow","importlib_resources ; python_version<'3.7'"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    zip_safe = False,
+    include_package_data = True
+)
