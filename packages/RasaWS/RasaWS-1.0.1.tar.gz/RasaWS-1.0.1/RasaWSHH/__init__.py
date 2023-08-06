4040ce71c3253ed4de483ccc69eb49518f526599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:07 +0000

    Add package RasaWS/1.0.1/RasaWS-1.0.1.tar.gz

---
+++ packages/RasaWS/RasaWS-1.0.1.tar.gz/RasaWS-1.0.1/RasaWSHH/__init__.py
@@ -0,0 +1,8 @@
+import logging
+
+from packaging_tutorial.RasaWSHH import version
+
+# define the version before the other imports since these need it
+__version__ = version.__version__
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
