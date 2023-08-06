08e4a17aa71db56553d6b2ca651dcbcc43dfa62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:34 +0000

    Add package devgossip-v2/2.0.1/devgossip_v2-2.0.1.tar.gz

---
+++ packages/devgossip-v2/devgossip_v2-2.0.1.tar.gz/devgossip_v2-2.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['devgossip', 'readme']
+install_requires = \
+['dotenv>=0.0.5,<0.0.6',
+ 'getpass>=1.2,<2.0',
+ 'json>=0.1.0,<0.2.0',
+ 'pusher>=3.0.0,<4.0.0',
+ 'pysher>=1.0.6,<2.0.0',
+ 'termcolor>=1.1.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['devgossip_v2 = devgossip:main']}
+
+setup_kwargs = {
+    'name': 'devgossip-v2',
+    'version': '2.0.1',
+    'description': 'Chat forum for interaction between developers',
+    'long_description': None,
+    'author': 'Mark Okhakumhe',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
