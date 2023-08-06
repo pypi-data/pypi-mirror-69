8a7ec5760ba2be1458e2a1fff6c5a4b7c6eec305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:44 +0000

    Add package metapi/0.7.9/metapi-0.7.9.tar.gz

---
+++ packages/metapi/metapi-0.7.9.tar.gz/metapi-0.7.9/metapi/__about__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+__version__ = '0.7.9'
+__author__ = "Jie Zhu"
---
+++ packages/metapi/metapi-0.7.9.tar.gz/metapi-0.7.9/metapi/sampler.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import glob
+import gzip
+import os
+import sys
+import time
+import pandas as pd
+from Bio import bgzf
+
+
+def parse_samples(config):
+    samples_df = pd.read_csv(config["params"]["samples"], sep="\s+").set_index(
+        "id", drop=False
+    )
+    return samples_df
+
+
+def parse_bins(bins_dir):
+    bin_list = []
+    for bin_ in glob.glob(bins_dir + "/*/*bin*fa"):
+        bin_dict = dict()
+        bin_dict["path"] = bin_.strip()
+        bin_dict["id"] = os.path.basename(bin_).rstrip(".fa")
+        bin_list.append(bin_dict)
+    bins = pd.DataFrame(bin_list).set_index("id", drop=False)
+    return bins
+
+
+def get_reads(sample_df, wildcards, col):
+    return sample_df.loc[[wildcards.sample], col].dropna().tolist()
+
+
+def get_sample_id(sample_df, wildcards, col):
+    return sample_df.loc[wildcards.sample, [col]].dropna()[0]
+
+
+def get_sample_id_(sample_df, wildcards, col):
+    return sample_df.loc[wildcards.sample_, [col]].dropna()[0]
+
+
+def get_bin_id(bin_df, wildcards, col):
+    return bin_df.loc[wildcards.bin, [col]].dropna()[0]
