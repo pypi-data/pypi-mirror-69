ed3c4de99bb026ca4870fa5dbdcf36450828883f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:19 +0000

    Add package covidata/0.0.4/covidata-0.0.4.tar.gz

---
+++ packages/covidata/covidata-0.0.4.tar.gz/covidata-0.0.4/README.md
@@ -0,0 +1,36 @@
+# Covidata
+
+Easily access NASA, ESA and JAXA data
+
+## About
+
+You can use this API to access and see statistics about NASA, JAXA and ESA datasets. Check the available datasets and how to use them bellow.
+
+## Available Datasets
+### ESA
+    mortality and natality
+    coronavirus cases worldwide
+
+### JAXA
+    greenhouse gases concentration
+
+### NASA
+    landslides
+    atmospherical data
+
+## Main Functions
+### Constructors
+    Initialize an object with the required data
+
+### Describe
+    Generete statistics about all the features in the dataset, such as mean, standard deviation and quartiles.
+
+## Examples
+```python
+from covidata.nasa import Atmospheric
+
+AG = Atmospheric(subset=100, verbose=True)
+
+print(AG.summarize())
+print(AG.head())
+```
---
+++ packages/covidata/covidata-0.0.4.tar.gz/covidata-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="covidata",
+    version="0.0.4",
+    author="Control-space",
+    author_email="author@example.com",
+    description="Easily access NASA, ESA and JAXA data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lusmoura/Nasa-space-apps",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['pandas', 
+                    'requests',
+                    'beautifulsoup4',
+                    ' lxml']
+)
