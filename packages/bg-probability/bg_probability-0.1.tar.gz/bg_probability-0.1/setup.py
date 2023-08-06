ec45e4e84faea43e415b94dcc7341a852f0a2996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:54 +0000

    Add package bg-probability/0.1/bg_probability-0.1.tar.gz

---
+++ packages/bg-probability/bg_probability-0.1.tar.gz/bg_probability-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='bg_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['bg_probability'],
+      author = 'Anuranjan Pandey',
+      author_email = 'anuranjanpandey5@gmail.com',
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
