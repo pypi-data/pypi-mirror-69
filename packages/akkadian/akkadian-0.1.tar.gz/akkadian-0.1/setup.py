01725fc6c57f3d198ec4945f3331d1a7878c1bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:00 +0000

    Add package akkadian/0.1/akkadian-0.1.tar.gz

---
+++ packages/akkadian/akkadian-0.1.tar.gz/akkadian-0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="akkadian",
+    version="0.1",
+    author="Ariel Elazary, Gai Gutherz",
+    author_email="am.elazary@gmail.com, gaigutherz@gmail.com",
+    description="Translating Akkadian signs to transliteration using NLP algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaigutherz/Translating-Akkadian-using-NLP",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='==3.7.1',
+)
