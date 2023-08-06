5e6e0fef8c17fc6604f73d4cf07717c06f66feec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:05 +0000

    Add package e3nn/0.0.0/e3nn-0.0.0-py3-none-any.whl

---
+++ packages/e3nn/e3nn-0.0.0-py3-none-any.whl/e3nn/non_linearities/s2.py
@@ -0,0 +1,74 @@
+# pylint: disable=invalid-name, arguments-differ, missing-docstring, line-too-long, no-member
+import torch
+
+from e3nn import o3, rs, s2grid
+
+
+class S2Activation(torch.nn.Module):
+    def __init__(self, Rs, act, res, normalization='component', lmax_out=None, random_rot=False):
+        '''
+        map to the sphere, apply the non linearity point wise and project back
+        the signal on the sphere is a quasiregular representation of O3
+        and we can apply a pointwise operation on these representations
+
+        :param Rs: input representation of the form [(1, l, p0 * u^l) for l in [0, ..., lmax]]
+        :param act: activation function
+        :param res: resolution of the grid on the sphere (the higher the more accurate)
+        :param normalization: either 'norm' or 'component'
+        :param lmax_out: maximum l of the output
+        :param random_rot: rotate randomly the grid
+        '''
+        super().__init__()
+
+        Rs = rs.simplify(Rs)
+        _, _, p0 = Rs[0]
+        _, lmax, _ = Rs[-1]
+        assert all(mul == 1 for mul, _, _ in Rs)
+        assert [l for _, l, _ in Rs] == [l for l in range(lmax + 1)]
+        if all(p == p0 for _, l, p in Rs):
+            u = 1
+        elif all(p == p0 * (-1) ** l for _, l, p in Rs):
+            u = -1
+        else:
+            assert False, "the parity of the input is not well defined"
+        self.Rs_in = Rs
+        # the input transforms as : A_l ---> p0 * u^l * A_l
+        # the sphere signal transforms as : f(r) ---> p0 * f(u * r)
+        if lmax_out is None:
+            lmax_out = lmax
+
+        if p0 == +1 or p0 == 0:
+            self.Rs_out = [(1, l, p0 * u ** l) for l in range(lmax_out + 1)]
+        if p0 == -1:
+            x = torch.linspace(0, 10, 256)
+            a1, a2 = act(x), act(-x)
+            if (a1 - a2).abs().max() < a1.abs().max() * 1e-10:
+                # p_act = 1
+                self.Rs_out = [(1, l, u ** l) for l in range(lmax_out + 1)]
+            elif (a1 + a2).abs().max() < a1.abs().max() * 1e-10:
+                # p_act = -1
+                self.Rs_out = [(1, l, -u ** l) for l in range(lmax_out + 1)]
+            else:
+                # p_act = 0
+                raise ValueError("warning! the parity is violated")
+
+        self.to_s2 = s2grid.ToS2Grid(lmax, res, normalization=normalization)
+        self.from_s2 = s2grid.FromS2Grid(res, lmax_out, normalization=normalization, lmax_in=lmax)
+        self.act = act
+        self.random_rot = random_rot
+
+    def forward(self, features):
+        '''
+        :param features: [..., l * m]
+        '''
+        if self.random_rot:
+            abc = o3.rand_angles()
+            features = torch.einsum('ij,...j->...i', rs.rep(self.Rs_in, *abc), features)
+
+        features = self.to_s2(features)  # [..., beta, alpha]
+        features = self.act(features)
+        features = self.from_s2(features)
+
+        if self.random_rot:
+            features = torch.einsum('ij,...j->...i', rs.rep(self.Rs_out, *abc).T, features)
+        return features
---
+++ packages/e3nn/e3nn-0.0.0-py3-none-any.whl/e3nn/tensor_product.py
@@ -0,0 +1,88 @@
+# pylint: disable=invalid-name, arguments-differ, missing-docstring, line-too-long, no-member, redefined-builtin
+from functools import partial
+
+import torch
+
+from e3nn import o3, rs
+from e3nn.linear_mod import KernelLinear
+from e3nn.util.sparse import get_sparse_buffer, register_sparse_buffer
+
+
+class LearnableTensorSquare(torch.nn.Module):
+    def __init__(self, Rs_in, Rs_out, allow_change_output=False):
+        super().__init__()
+
+        self.Rs_in = rs.simplify(Rs_in)
+        self.Rs_out = rs.simplify(Rs_out)
+
+        ls = [l for _, l, _ in self.Rs_out]
+        selection_rule = partial(o3.selection_rule, lfilter=lambda l: l in ls)
+
+        Rs_ts, T = rs.tensor_square(self.Rs_in, selection_rule)
+        register_sparse_buffer(self, 'T', T)  # [out, in1 * in2]
+
+        ls = [l for _, l, _ in Rs_ts]
+        if allow_change_output:
+            self.Rs_out = [(mul, l, p) for mul, l, p in self.Rs_out if l in ls]
+        else:
+            assert all(l in ls for _, l, _ in self.Rs_out)
+
+        self.kernel = KernelLinear(Rs_ts, self.Rs_out)  # [out, in, w]
+
+    def __repr__(self):
+        return "{name} ({Rs_in} -> {Rs_out})".format(
+            name=self.__class__.__name__,
+            Rs_in=rs.format_Rs(self.Rs_in),
+            Rs_out=rs.format_Rs(self.Rs_out),
+        )
+
+    def forward(self, features):
+        '''
+        :param features: [..., channels]
+        '''
+        *size, n = features.size()
+        features = features.reshape(-1, n)
+
+        T = get_sparse_buffer(self, 'T')  # [out, in1 * in2]
+        kernel = (T.t() @ self.kernel().T).T.reshape(rs.dim(self.Rs_out), rs.dim(self.Rs_in), rs.dim(self.Rs_in))  # [out, in1, in2]
+        features = torch.einsum('kij,zi,zj->zk', kernel, features, features)
+        return features.reshape(*size, -1)
+
+
+class LearnableTensorProduct(torch.nn.Module):
+    def __init__(self, Rs_in1, Rs_in2, Rs_out, allow_change_output=False):
+        super().__init__()
+
+        self.Rs_in1 = rs.simplify(Rs_in1)
+        self.Rs_in2 = rs.simplify(Rs_in2)
+        self.Rs_out = rs.simplify(Rs_out)
+
+        ls = [l for _, l, _ in self.Rs_out]
+        selection_rule = partial(o3.selection_rule, lfilter=lambda l: l in ls)
+
+        Rs_ts, T = rs.tensor_product(self.Rs_in1, self.Rs_in2, selection_rule)
+        register_sparse_buffer(self, 'T', T)  # [out, in1 * in2]
+
+        ls = [l for _, l, _ in Rs_ts]
+        if allow_change_output:
+            self.Rs_out = [(mul, l, p) for mul, l, p in self.Rs_out if l in ls]
+        else:
+            assert all(l in ls for _, l, _ in self.Rs_out)
+
+        self.kernel = KernelLinear(Rs_ts, self.Rs_out)  # [out, in, w]
+
+    def forward(self, features_1, features_2):
+        """
+        :return:         tensor [..., channel]
+        """
+        *size, n = features_1.size()
+        features_1 = features_1.reshape(-1, n)
+        assert n == rs.dim(self.Rs_in1)
+        *size2, n = features_2.size()
+        features_2 = features_2.reshape(-1, n)
+        assert size == size2
+
+        T = get_sparse_buffer(self, 'T')  # [out, in1 * in2]
+        kernel = (T.t() @ self.kernel().T).T.reshape(rs.dim(self.Rs_out), rs.dim(self.Rs_in1), rs.dim(self.Rs_in2))  # [out, in1, in2]
+        features = torch.einsum('kij,zi,zj->zk', kernel, features_1, features_2)
+        return features.reshape(*size, -1)
