584be548767557ee5403cd5e6d6e413e23acba8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:53 +0000

    Add package binormal-distributions/0.1/binormal_distributions-0.1.tar.gz

---
+++ packages/binormal-distributions/binormal_distributions-0.1.tar.gz/binormal_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='binormal_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['binormal_distributions'],
+      author = 'Tharun Ragulan',
+      zip_safe=False)
