db7df097d73e934f099904e261cacf97f76a3f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:08 +0000

    Add package mjyMath/1.1/mjyMath-1.1.tar.gz

---
+++ packages/mjyMath/mjyMath-1.1.tar.gz/mjyMath-1.1/mjyMath/demo.py
@@ -0,0 +1,9 @@
+"""本方法用于计算两个数的加法"""
+def add(a,b):
+    """
+    计算两个数字的加法
+    :param a: 数字1
+    :param b: 数字2
+    :return: 结果
+    """
+    return a+b
---
+++ packages/mjyMath/mjyMath-1.1.tar.gz/mjyMath-1.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='mjyMath', # 对外我们模块的名字
+    version='1.1', # 版本号
+    description='本方法用于计算阶乘', #描述
+    author='jym', # 作者
+    author_email='861825232@qq.com',
+    py_modules=['mjyMath.demo'] # 要发布的模块
+)
