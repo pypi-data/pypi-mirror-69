ef577a2b1520e46fa3e15bb7c6ed1a394e5c8998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:11 +0000

    Add package opentoken/1.2.0/opentoken-1.2.0.tar.gz

---
+++ packages/opentoken/opentoken-1.2.0.tar.gz/opentoken-1.2.0/opentoken/_ciphersuite.py
@@ -0,0 +1,62 @@
+"""CipherSuite helper module
+"""
+
+from Crypto.Protocol.KDF import PBKDF2
+
+from . import _utils
+
+CIPHERS = [
+    {
+        "id": 0,
+        "name": None,
+        "cipher": None,
+        "key_size": 0,
+        "mode": None,
+        "padding": None,
+        "iv_length": 0
+    },
+    {
+        "id": 1,
+        "name": "aes-256-cbc",
+        "cipher": "AES",
+        "key_size": 256,
+        "mode": "CBC",
+        "padding": "PKCS 5",
+        "iv_length": 16
+    },
+    {
+        "id": 2,
+        "name": "aes-128-cbc",
+        "cipher": "AES",
+        "key_size": 128,
+        "mode": "CBC",
+        "padding": "PKCS 5",
+        "iv_length": 16
+    },
+    {
+        "id": 3,
+        "name": "3des",
+        "cipher": "3DES",
+        "key_size": 168,
+        "mode": "CBC",
+        "padding": "PKCS 5",
+        "iv_length": 8
+    }
+]
+
+
+def generate_key(password, cipher_suite_id, salt=None):
+    password = _utils.validate_password(password)
+    cipher_suite_id = _utils.validate_cipher_suite_id(cipher_suite_id)
+
+    if cipher_suite_id == 0:
+        return None
+
+    salt = salt or bytearray([0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0])
+    cipher_suite = CIPHERS[cipher_suite_id]
+
+    return PBKDF2(
+        password,
+        salt,
+        dkLen=cipher_suite["key_size"] // 8,
+    )
