e98dcb9856ec5ad6b77219100f1339ecff6059c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:32 +0000

    Add package cqh-file-watcher/0.0.21/cqh_file_watcher-0.0.21.tar.gz

---
+++ packages/cqh-file-watcher/cqh_file_watcher-0.0.21.tar.gz/cqh_file_watcher-0.0.21/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="cqh_file_watcher",  # Replace with your own username
+    version="0.0.21",
+    author="chenqinghe",
+    author_email="1832866299@qq.com",
+    description="tools like vscode file-watcher but for command only",
+    long_description=long_description,
+    url="https://github.com/chen19901225/cqh_file_watcher",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pyinotify"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ],
+    entry_points={
+        "console_scripts": [
+            "cqh_file_watcher = cqh_file_watcher.run:main",
+        ],
+    },
+    python_requires='>=3.6',
+)
