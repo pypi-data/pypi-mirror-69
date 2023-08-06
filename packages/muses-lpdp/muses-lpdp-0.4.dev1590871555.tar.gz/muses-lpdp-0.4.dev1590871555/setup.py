c5a7d6ef5c40f63a6bfbd85568a455abe7b227d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:16 +0000

    Add package muses-lpdp/0.4.dev1590871555/muses-lpdp-0.4.dev1590871555.tar.gz

---
+++ packages/muses-lpdp/muses-lpdp-0.4.dev1590871555.tar.gz/muses-lpdp-0.4.dev1590871555/setup.py
@@ -0,0 +1,33 @@
+from glob import glob
+
+from setuptools import setup
+from setuptools.command.install import install
+from os import getenv, getcwd
+
+
+with open('requirements.txt', 'r') as f:
+    install_reqs = [
+        s for s in [
+            line.split('#', 1)[0].strip(' \t\n') for line in f
+        ] if s != ''
+    ]
+
+
+class InstallWrapper(install):
+    media_path = "media"
+    static_path = "static"
+
+    def run(self):
+        super().run()
+        self.static_path = getenv("STATIC_PATH", f"{getcwd()}/{self.static_path}")
+
+
+setup(
+    install_requires=install_reqs,
+    include_package_data=True,
+    setup_requires=['wheel'],
+    data_files=[
+        ('static', glob('static/**/*.*', recursive=True)),
+        ('media', glob('media/**/*.*', recursive=True)),
+    ]
+)
