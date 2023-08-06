0cd47d35447a62d428abeeed8010d9fe242478e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:30 +0000

    Add package priyagaussbinom/0.2/priyagaussbinom-0.2.tar.gz

---
+++ packages/priyagaussbinom/priyagaussbinom-0.2.tar.gz/priyagaussbinom-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='priyagaussbinom',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['priyagaussbinom'],
+      zip_safe=False)
