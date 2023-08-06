acba23646759e659a4135fbc04bb0e5f75980ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:59 +0000

    Add package deepworm-algorithm/0.1.4/deepworm_algorithm-0.1.4.tar.gz

---
+++ packages/deepworm-algorithm/deepworm_algorithm-0.1.4.tar.gz/deepworm_algorithm-0.1.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import setuptools
+
+setuptools.setup(
+    name='deepworm_algorithm',
+    version='0.1.4',
+    author='JohnsonNie',
+    author_email='pubrcv@163.com',
+    url='http://www.deepdiy.net/',
+    description='A set of algorithms for analysing worm image',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'thinning-py3',
+        'opencv-python',
+        'matplotlib',
+        'scikit-image',
+        'networkx',
+        'scipy',
+        'numpy', 
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
