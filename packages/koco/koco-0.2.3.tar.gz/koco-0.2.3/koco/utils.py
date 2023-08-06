8b9e6636ce00777120443ec10e6e6bfaf28c7b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:09 +0000

    Add package koco/0.2.3/koco-0.2.3.tar.gz

---
+++ packages/koco/koco-0.2.3.tar.gz/koco-0.2.3/koco/utils.py
@@ -0,0 +1,24 @@
+import os
+
+DOWNLOAD_DIR = f'{os.path.expanduser("~")}/.kocohub'
+
+
+def dataset_to_module_name(dataset):
+    return dataset.replace('-', '_')
+
+
+def exist_dataset(dataset):
+    return os.path.exists(f'{DOWNLOAD_DIR}/{dataset}-master')
+
+
+def exist_dir(dirpath):
+    return os.path.exists(dirpath)
+
+
+def make_dirs(dirpath):
+    if not exist_dir(dirpath):
+        os.makedirs(dirpath)
+
+
+def read_lines(path):
+    return [line.rstrip('\n') for line in open(path)]
