2ba6ea5e5f39c16d0fb3fb1d2b18f5ff57fa2833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:37 +0000

    Add package Eel/0.12.4/Eel-0.12.4.tar.gz

---
+++ packages/Eel/Eel-0.12.4.tar.gz/Eel-0.12.4/setup.py
@@ -0,0 +1,23 @@
+from io import open
+from setuptools import setup
+
+with open('README.md') as read_me:
+    long_description = read_me.read()
+
+setup(
+    name='Eel',
+    version='0.12.4',
+    author='Chris Knott',
+    author_email='chrisknott@hotmail.co.uk',
+    url='https://github.com/samuelhwilliams/Eel',
+    packages=['eel'],
+    package_data={
+        'eel': ['eel.js'],
+    },
+    install_requires=['bottle', 'bottle-websocket', 'future', 'whichcraft'],
+    python_requires='>=2.6',
+    description='For little HTML GUI applications, with easy Python/JS interop',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['gui', 'html', 'javascript', 'electron'],
+)
