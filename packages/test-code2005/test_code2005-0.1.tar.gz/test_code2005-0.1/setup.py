805d77d9658357806a595d483cc125e15d827980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:24 +0000

    Add package test-code2005/0.1/test_code2005-0.1.tar.gz

---
+++ packages/test-code2005/test_code2005-0.1.tar.gz/test_code2005-0.1/setup.py
@@ -0,0 +1,35 @@
+
+from distutils.core import setup
+setup(
+  name = 'test_code2005',         # How you named your package folder (MyLib)
+  packages = ['test_code2005'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT License',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = ' pandas_ui helps you wrangle & explore your data and create custom visualizations without digging through StackOverflow. All inside your Jupyter Notebook or JupyterLab ( alternative to Bamboolib ).',   # Give a short description about your library
+  author = 'Arunn Thangavel',                   # Type in your name
+  author_email = 'arunnbabainfo@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/arunnbaba/',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/arunnbaba/test_code2005/archive/v12.0.zip',
+  keywords = ['Pands_ui', 'bamboolib', 'ui for pandas'],
+  include_package_data=True,
+  install_requires=[            # I get to this in a second
+          'future',
+          'ipywidgets',
+          'ipython',
+          'pandas',
+          'qgrid',
+          'traitlets',
+          'pandas-profiling',
+          'bokeh',
+          'plotly',
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.7'      #Specify which pyhton versions that you want to support
+    
+  ],
+)
---
+++ packages/test-code2005/test_code2005-0.1.tar.gz/test_code2005-0.1/test_code2005/__init__.py
@@ -0,0 +1,2 @@
+# Inside of __init__.py
+from test_code2005.funct1 import pandas_ui, get_df, get_pivotdf, get_meltdf
