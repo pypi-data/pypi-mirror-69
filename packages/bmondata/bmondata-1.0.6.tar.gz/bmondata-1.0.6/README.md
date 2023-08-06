13900a8181003f7a1853222de44ce4c33528560b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:08 +0000

    Add package bmondata/1.0.6/bmondata-1.0.6.tar.gz

---
+++ packages/bmondata/bmondata-1.0.6.tar.gz/bmondata-1.0.6/README.md
@@ -0,0 +1,19 @@
+# bmondata
+Allows easy access to sensor data residing on a [BMON Server](https://github.com/alanmitchell/bmon).
+
+This package provides a wrapper around the the BMON API, version 2.  It allows retrieval
+of key data stored in the BMON database, including sensor readings, sensor metadata, building
+information, and organization information.  It is not comprehensive as some BMON information is
+not available.  It also allows for storage of new sensor readings in a BMON
+sensor reading database.
+
+Usage examples are [shown here](http://web.analysisnorth.com.s3-us-west-2.amazonaws.com/bmondata/usage_examples.html).
+The docstring for each method gives additional information.
+
+Install with pip:
+
+    pip install --upgrade bmondata
+
+This package is only compatible with BMON Servers running the BMON software version
+equal to or greater than:  Mon Jul 29 16:06:14 2019 -0800 (shown on the footer of the 
+BMON Graphs/Reports page.)
---
+++ packages/bmondata/bmondata-1.0.6.tar.gz/bmondata-1.0.6/setup.py
@@ -0,0 +1,33 @@
+# See: https://packaging.python.org/tutorials/packaging-projects/
+# for how to install this package on PyPi.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# read version number from package
+import bmondata
+ver = bmondata.__version__
+
+setuptools.setup(
+    name="bmondata",
+    version=ver,
+    author="Alan Mitchell",
+    author_email="tabb99@gmail.com",
+    description="Allows retrieval and storage of data on BMON Servers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alanmitchell/bmondata",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests>=2.18.4',
+        'pandas>=0.23.2'
+    ]
+)
