609fb9c5360e45630f005d84c5aa112c782dc5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:48 +0000

    Add package peek-plugin-diagram/2.3.3/peek-plugin-diagram-2.3.3.tar.gz

---
+++ packages/peek-plugin-diagram/peek-plugin-diagram-2.3.3.tar.gz/peek-plugin-diagram-2.3.3/peek_plugin_diagram/plugin-module/lookups/DispLevel.ts
@@ -0,0 +1,19 @@
+import {addTupleType, Tuple} from "@synerty/vortexjs";
+import {diagramTuplePrefix} from "@peek/peek_plugin_diagram/_private";
+
+
+@addTupleType
+export class DispLevel extends Tuple {
+    public static readonly tupleName = diagramTuplePrefix + "DispLevel";
+
+    id: number;
+    name: string;
+    order: number;
+    minZoom: number;
+    maxZoom: number;
+    coordSetId: number;
+
+    constructor() {
+        super(DispLevel.tupleName)
+    }
+}
