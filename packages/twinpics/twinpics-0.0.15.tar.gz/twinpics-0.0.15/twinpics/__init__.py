6721ec858a3ea6c9257bfdb5ec36ae12e7e23092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:56 +0000

    Add package twinpics/0.0.15/twinpics-0.0.15.tar.gz

---
+++ packages/twinpics/twinpics-0.0.15.tar.gz/twinpics-0.0.15/twinpics/__init__.py
@@ -0,0 +1,42 @@
+# Copyright 2020 Alberto Martín Mateos and Niloufar Shoeibi
+# See LICENSE for details.
+
+__author__ = 'Alberto Martín Mateos and Niloufar Shoeibi'
+__version__ = '0.0.1'
+
+from .api import load_file
+from .api import merge_dataframes
+from .api import profile_connections
+from .api import building_node_list
+from .api import building_DiGraph
+from .api import self_loop_iteration_nodes
+from .api import DiGraph_visualization
+from .api import in_degree_centrality
+from .api import out_degree_centrality
+from .api import degree_centrality
+from .api import in_degree
+from .api import out_degree
+from .api import degree
+from .api import self_loops
+from .api import graph_features
+from .api import user_timeline_extraction
+from .api import metadata_extraction
+from .api import text_tweets_features_extraction
+from .api import time_twitter_account
+from .api import time_series_tw
+from .api import advanced_features
+from .api import trend_tweets_features
+from .api import filter_possible_irregular_profiles
+from .api import evaluate_NLP_profiles
+from .api import general_pieChart
+from .api import sentiental_analysis_features
+from .api import distribution_sentiment_tweets
+from .api import tokenize_tweets
+from .api import keywords_score
+from .api import hashtag_score
+from .api import terrorist_keywords_belonging
+from .api import pie_chart_keywords_terrorist
+from .api import terrorist_hashtags_belonging
+from .api import pie_chart_hashtags_terrorist
+from .api import NLP_analysis
+from .api import more_tweets_required
