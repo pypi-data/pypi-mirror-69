4257f3cd130980592544f653d7e2e8491023538c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:37 +0000

    Add package pysin/1.0.2/pysin-1.0.2.tar.gz

---
+++ packages/pysin/pysin-1.0.2.tar.gz/pysin-1.0.2/setup.py
@@ -0,0 +1,48 @@
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pysin",
+    packages=find_packages('pysin'),
+    version="1.0.2",
+    license="Apache License 2.0",
+    description="PySin is a toolbox for text retrieval in unstructured documents datasets. It contains both a multi-type text extractor and a search engine. To test them, you can use the medical prescriptions generator that is also provided.",
+    author="Jean-Baptiste Laval",
+    author_email="contact@arkhn.com",
+    url="https://github.com/arkhn/PySin",
+    download_url="https://github.com/arkhn/PySin/archive/1.0.2.tar.gz",
+    keywords=[
+        "arkhn",
+        "text retrieval",
+        "search engine",
+        "text extraction",
+        "dataset generator",
+        "medical",
+    ],
+    install_requires=[
+        'pandas==1.0.3',
+        'fpdf==1.7.2',
+        'Faker==4.1.0',
+        'Unidecode==1.1.1',
+        'docx2txt==0.8',
+        'path.py==12.4.0',
+        'pdftotext==2.1.4',
+        'striprtf==0.0.10',
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
