e2e8766a6945e14d8985c1b07f401e9c0eb9e6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:01 +0000

    Add package myrabbit/0.1.1/myrabbit-0.1.1-py3-none-any.whl

---
+++ packages/myrabbit/myrabbit-0.1.1-py3-none-any.whl/myrabbit/__init__.py
@@ -0,0 +1,6 @@
+from .events import EventBus
+from .events import EventWithMessage
+from .events import EventBusAdapter
+from .service import Service
+from .runner import run_services
+from .runner import run_services_threaded
