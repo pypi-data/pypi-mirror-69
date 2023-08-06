fb8d02c7690b869a10922a95b7ea1aced8dc8328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:30 +0000

    Add package razorflow/0.0.1/razorflow-0.0.1.tar.gz

---
+++ packages/razorflow/razorflow-0.0.1.tar.gz/razorflow-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='razorflow',
+    version='0.0.1',
+    author='Razorthink',
+    author_email='sandeep.kumar@razorthink.com',
+    description='Razor Dev Tools for Razorthink AI',
+    url='https://razorthink.com',
+    packages=find_packages(),
+    python_requires='>=3.7'
+)
