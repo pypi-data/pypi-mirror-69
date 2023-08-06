173f8104fe4bf3a0d4fe626481f8cb717e3e25fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:58 +0000

    Add package RoboMentor-Client/1.0.31/RoboMentor_Client-1.0.31.tar.gz

---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.0.31.tar.gz/RoboMentor_Client-1.0.31/robomentor_client/robot/robot.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+
+
+class Robot(object):
+
+    @staticmethod
+    def start():
+        pass
+
+    @staticmethod
+    def close():
+        pass
+
+    @staticmethod
+    def message(message):
+        pass
