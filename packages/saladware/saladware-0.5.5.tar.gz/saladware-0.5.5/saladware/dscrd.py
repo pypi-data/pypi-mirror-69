7d6d5b9a3304aa9f61c0361d8602dc2b0f0ba9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:30 +0000

    Add package saladware/0.5.5/saladware-0.5.5.tar.gz

---
+++ packages/saladware/saladware-0.5.5.tar.gz/saladware-0.5.5/saladware/dscrd.py
@@ -0,0 +1,8 @@
+def standart(title, desc):
+    embed = discord.Embed(title = title, description = desc, color = 0xffaaff, timestamp = ntx.message.created_at)
+    embed.set_footer(text = ntx.author.name, icon_url = ntx.author.avatar_url)
+    return embed
+
+def init(ctx):
+    global ntx
+    ntx = ctx
