e02e9601d7116bd4e1df32d3e673bff32495eba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:18 +0000

    Add package winvers/0.1.6/winvers-0.1.6.tar.gz

---
+++ packages/winvers/winvers-0.1.6.tar.gz/winvers-0.1.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name = 'winvers',
+    packages = ['winvers'],
+    version = '0.1.6',
+    description = 'Definitive Windows version checker for Python',
+    long_description=open("README.md").read(),
+    long_description_content_type='text/markdown',
+    author = 'Daniele Giudice',
+    license = 'GPL v3',
+    python_requires='>=2.7, <4',
+    url = 'https://github.com/MagicGreenDragon/winver',
+    keywords = ['windows', 'version', 'check'],
+    classifiers = (
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        ),
+)
