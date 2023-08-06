9d552e92f754f8236c9afa054c13f1fdcdd97806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:10 +0000

    Add package covid-surge/0.0.39/covid_surge-0.0.39-py3-none-any.whl

---
+++ packages/covid-surge/covid_surge-0.0.39-py3-none-any.whl/covid_surge/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This file is part of the COVID-surge application.
+# https://github/dpploy/covid-surge
+"""covid-surge import to simplify usage."""
+
+from .src.covid_surge_main import Surge
