cc0da6abfac3a3ed9de70c48aa44b9ab1731842a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:42 +0000

    Add package lg-distributions/0.1/lg_distributions-0.1.tar.gz

---
+++ packages/lg-distributions/lg_distributions-0.1.tar.gz/lg_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lg_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['lg_distributions'],
+      author = 'lenz',
+      email = 'lbgarcia4@up.edu.ph',
+      zip_safe=False)
