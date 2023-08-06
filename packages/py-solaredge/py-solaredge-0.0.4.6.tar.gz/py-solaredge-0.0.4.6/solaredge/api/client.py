2411112494fe370fff639d4ffaafadca55d8b2c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:56 +0000

    Add package py-solaredge/0.0.4.6/py-solaredge-0.0.4.6.tar.gz

---
+++ packages/py-solaredge/py-solaredge-0.0.4.6.tar.gz/py-solaredge-0.0.4.6/solaredge/api/client.py
@@ -0,0 +1,15 @@
+from .error import Error, RequestError, IdentifierError
+from .sites import Sites
+
+
+class Client(object):
+    def __init__(self):
+        self.api_key = None
+        self.sites = Sites(self)
+        self.client = Client(self)
+
+    def set_api_key(self, api_key):
+        self.api_key = api_key
+
+    def get_api_key(self):
+        return self.api_key
