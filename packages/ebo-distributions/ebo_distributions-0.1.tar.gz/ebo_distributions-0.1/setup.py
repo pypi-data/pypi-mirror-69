1abd8d57c52d5f868b1cce09cabcbac169334293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:12 +0000

    Add package ebo-distributions/0.1/ebo_distributions-0.1.tar.gz

---
+++ packages/ebo-distributions/ebo_distributions-0.1.tar.gz/ebo_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ebo_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ebo_distributions'],
+      author='Robert Ebo Armah-Sekum', 
+      author_email = 'rarmahse@alumni.cmu.edu',
+      zip_safe=False)
