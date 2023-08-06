13de4da77b17c2912603ae9da9a878edd1002245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:11 +0000

    Add package mvassconvert/0.0.1/mvassconvert-0.0.1.tar.gz

---
+++ packages/mvassconvert/mvassconvert-0.0.1.tar.gz/mvassconvert-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mvassconvert",
+    version="0.0.1",
+    author="Matthew Vasseur",
+    author_email="mvasseur22@students.hopkins.edu",
+    description="A package that allows the user to convert 1000+ different units",
+    long_description=long_description,
+    long_description_content_type="text",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    package_data={
+        "": ["*.txt", "*.rst"],
+        }
+)
