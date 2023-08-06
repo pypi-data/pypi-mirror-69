98ebfeea3bfe3d02ee67f84b026e2d87d250529e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:58 +0000

    Add package aws-ssm-secrets-cli/0.0.4/aws_ssm_secrets_cli-0.0.4-py3-none-any.whl

---
+++ packages/aws-ssm-secrets-cli/aws_ssm_secrets_cli-0.0.4-py3-none-any.whl/aws_secrets/resolvers/set_parameter_resolver.py
@@ -0,0 +1,38 @@
+import yaml
+import boto3
+import base64
+from botocore.exceptions import ClientError
+from aws_secrets.miscellaneous import kms
+from aws_secrets.miscellaneous.session import session
+
+
+class SetParameterResolver():
+    def __call__(self, args):
+        with open(args.env_file, 'r') as env:
+            yaml_data = yaml.safe_load(env.read())
+
+        print(f'Putting parameter {args.name} value with {args.value}')
+
+        if not 'parameters' in yaml_data:
+            yaml_data['parameters'] = []
+
+        parameter = next(
+            (param for param in yaml_data['parameters'] if param['name'] == args.name), None)
+
+        if parameter is None:
+            parameter = {
+                'name': args.name,
+                'type': args.type
+            }
+            yaml_data['parameters'].append(parameter)
+
+        if parameter['type'] == 'SecureString':
+            kms_arn = str(yaml_data['kms']['arn'])
+
+            encrypted_value = kms.encrypt(session(), args.value, kms_arn)
+            parameter['value'] = encrypted_value.decode('utf-8')
+        else:
+            parameter['value'] = args.value
+
+        with open(args.env_file, 'w') as outfile:
+            yaml.safe_dump(yaml_data, outfile)
---
+++ packages/aws-ssm-secrets-cli/aws_ssm_secrets_cli-0.0.4-py3-none-any.whl/aws_secrets/resolvers/set_secret_resolver.py
@@ -0,0 +1,38 @@
+import yaml
+import boto3
+import base64
+from botocore.exceptions import ClientError
+from aws_secrets.miscellaneous import kms
+from aws_secrets.miscellaneous.session import session
+
+
+class SetSecretResolver():
+    def __call__(self, args):
+        with open(args.env_file, 'r') as env:
+            yaml_data = yaml.safe_load(env.read())
+
+        print(f'Putting secret {args.name} value with {args.value}')
+
+        if not 'secrets' in yaml_data:
+            yaml_data['secrets'] = []
+
+        secret = next(
+            (secret for secret in yaml_data['secrets'] if secret['name'] == args.name), None)
+
+        if secret is None:
+            if args.type is None:
+                raise ValueError('-t or --type is required to this operation')
+
+            secret = {
+                'name': args.name,
+                'type': args.type
+            }
+            yaml_data['secrets'].append(secret)
+
+        kms_arn = str(yaml_data['kms']['arn'])
+
+        encrypted_value = kms.encrypt(session(), args.value, kms_arn)
+        secret['value'] = encrypted_value.decode('utf-8')
+
+        with open(args.env_file, 'w') as outfile:
+            yaml.safe_dump(yaml_data, outfile)
