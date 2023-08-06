8486c50bd218fcdeb39d1f45609eaec3bb8cc662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:29 +0000

    Add package sirivasv-distributions/0.1/sirivasv_distributions-0.1.tar.gz

---
+++ packages/sirivasv-distributions/sirivasv_distributions-0.1.tar.gz/sirivasv_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sirivasv_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['sirivasv_distributions'],
+      author='Saul Ivan Rivas Vega',
+      author_email='saul.ivan.rivas.vega@gmail.com',
+      zip_safe=False)
