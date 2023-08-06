4410c2ec6daf5763c5ac2f12223fae27a0c0797e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:44 +0000

    Add package Essential-Probability-Functions/0.1/Essential_Probability_Functions-0.1.tar.gz

---
+++ packages/Essential-Probability-Functions/Essential_Probability_Functions-0.1.tar.gz/Essential_Probability_Functions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Essential_Probability_Functions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['Essential_Probability_Functions'],
+      author = 'Muhammed Essam',
+      author_email = 'muhammed.essam00@gmail.com',
+      zip_safe=False)
