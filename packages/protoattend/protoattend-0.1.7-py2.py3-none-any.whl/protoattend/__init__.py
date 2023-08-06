1e32f277dd8c26c67203bbd08b25d27def6a2f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:13 +0000

    Add package protoattend/0.1.7/protoattend-0.1.7-py2.py3-none-any.whl

---
+++ packages/protoattend/protoattend-0.1.7-py2.py3-none-any.whl/protoattend/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for protoattend."""
+# fmt: off
+__author__ = """Laksh Aithani"""
+__email__ = 'lakshaithanii@gmail.com'
+__version__ = '0.1.7'
+# fmt: on
+
+
+from protoattend.module import ProtoAttendModule
+from protoattend.protoattend import ProtoAttend
