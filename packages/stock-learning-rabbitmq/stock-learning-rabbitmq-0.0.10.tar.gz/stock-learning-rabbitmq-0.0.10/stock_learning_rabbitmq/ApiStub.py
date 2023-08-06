e825a786ce462c17698ca98f19c68ae024fbaeda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:15 +0000

    Add package stock-learning-rabbitmq/0.0.10/stock-learning-rabbitmq-0.0.10.tar.gz

---
+++ packages/stock-learning-rabbitmq/stock-learning-rabbitmq-0.0.10.tar.gz/stock-learning-rabbitmq-0.0.10/stock_learning_rabbitmq/ApiStub.py
@@ -0,0 +1,14 @@
+from stock_learning_rabbitmq.QueueNameConstants import API_QUEUE_NAME
+from stock_learning_rabbitmq.Stub import Stub
+
+
+class ApiStub(Stub):
+    
+    def __init__(self, server):
+        super().__init__(server, 'stock-learning-api')
+
+    def infomoney_ibovespa_initial_load(self, content):
+        self._send('infomoney-ibovespa-initial-load', content)
+
+    def infomoney_ibovespa_live_update(self, content):
+        self._send('infomoney-ibovespa-live-update', content)
