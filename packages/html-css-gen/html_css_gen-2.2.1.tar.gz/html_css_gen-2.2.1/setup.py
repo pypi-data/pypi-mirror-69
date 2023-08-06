96ef1e952e5a6bfe15cc4987c2e4fe89e2d0d101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:38 +0000

    Add package html-css-gen/2.2.1/html_css_gen-2.2.1.tar.gz

---
+++ packages/html-css-gen/html_css_gen-2.2.1.tar.gz/html_css_gen-2.2.1/setup.py
@@ -0,0 +1,21 @@
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='html_css_gen',
+    version='2.2.1',
+    author="guangyong",
+    author_email="guangyong@live.com",
+    description="snippet to merge html/css files",
+    long_description=long_description,
+    url="https://bitbucket.org/guangyong/html_css_gen/",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
