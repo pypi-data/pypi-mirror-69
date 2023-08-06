88be7159b1205b844143d1ac64d2a8b5a0b605c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:37 +0000

    Add package tilecloud/1.4.2.post20200529/tilecloud-1.4.2.post20200529.tar.gz

---
+++ packages/tilecloud/tilecloud-1.4.2.post20200529.tar.gz/tilecloud-1.4.2.post20200529/setup.py
@@ -0,0 +1,45 @@
+import os
+from glob import glob
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, "README.rst")).read()
+
+install_requires = [
+    "boto3",
+    "bottle",
+    "Pillow",
+    "pyproj",
+    "requests>=1.0",
+    "redis>=2",
+]
+
+setup(
+    name="tilecloud",
+    version="1.4.2",
+    description="Tools for managing tiles",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
+    author="Tom Payne",
+    author_email="twpayne@gmail.com",
+    url="http://github.com/camptocamp/tilecloud",
+    license="BSD",
+    packages=find_packages(exclude=["tiles", "tilecloud.tests"]),
+    zip_safe=True,
+    install_requires=install_requires,
+    test_suite="tilecloud.tests",
+    scripts=glob("tc-*"),
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    long_description=README,
+)
