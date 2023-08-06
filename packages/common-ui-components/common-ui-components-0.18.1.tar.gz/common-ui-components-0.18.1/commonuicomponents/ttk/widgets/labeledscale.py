c8a0b420d8d264c831623c0654a0a7ffbf3d2ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:38 +0000

    Add package common-ui-components/0.18.1/common-ui-components-0.18.1.tar.gz

---
+++ packages/common-ui-components/common-ui-components-0.18.1.tar.gz/common-ui-components-0.18.1/commonuicomponents/ttk/widgets/labeledscale.py
@@ -0,0 +1,80 @@
+from commonutils import StaticUtils
+from tkinter import E, IntVar, W
+from tkinter.ttk import Frame, Label, Scale
+from .smartwidget import SmartWidget
+
+class LabeledScale(SmartWidget):
+   def __init__(self, master = None, **kw):
+      self.__multiplyValue = kw.pop("multiplyValue", False)
+      
+      twoRows = kw.pop("twoRows", False)
+      value = kw.pop("value", None)
+      
+      kw["columns"] = 1 if twoRows else 3
+      kw["rows"] = 1
+      kw["hasValueBuffer"] = True
+      kw["value"] = IntVar()
+      
+      self.__frame = Frame(master) if twoRows else None
+      
+      super().__init__(master, **kw)
+      
+      if self.__frame:
+         master = self.__frame
+      
+      # = Caption = #
+      self.__caption = Label(master, text = kw["text"])
+      
+      # = Value = #
+      self.__step = kw.get("step", 1)
+      
+      from_, to = [v // self.__step for v in kw["range"]]
+      
+      value = StaticUtils.getOrSetIfAbsent(self._smartWidgetValueBuffer, 0, from_ if value == None else value)
+      
+      self.__value = Label(master, anchor = E, text = value * self.__step, width = max(StaticUtils.getPlaces([x * self.__step for x in (from_, to)])))
+      
+      # = Scale = #
+      self.getRawValue().set(value)
+      
+      self.__scale = Scale(
+         master,
+         command = self.onChanged,
+         from_ = from_,
+         length = 400,
+         to = to,
+         variable = self.getRawValue())
+   
+   def getValue(self):
+      return super().getValue() * (self.__step if self.__multiplyValue else 1)
+   
+   def grid(self, **kw):
+      kw = StaticUtils.mergeJson(kw, self._smartWidgetGrid, True)
+      
+      if self.__frame:
+         self.__frame.columnconfigure(1, weight = 1)
+         self.__frame.grid(sticky = W + E, **kw)
+         
+         self.__caption.grid(column = 0, row = 0, sticky = W)
+         self.__value.grid(column = 1, padx = (20, 0), row = 0, sticky = E)
+         self.__scale.grid(column = 0, columnspan = 2, pady = (20, 0), row = 1, sticky = W + E)
+      
+      else:
+         padxRight = kw["padx"][1] # TODO It can also be a scalar!
+         kw["padx"][1] = 0 # TODO It can also be a scalar!
+         self.__caption.grid(sticky = E, **kw)
+         
+         kw["column"] += 1
+         kw["padx"] = [20, 0]
+         self.__value.grid(sticky = W + E, **kw)
+         
+         kw["column"] += 1
+         kw["padx"][1] = padxRight
+         self.__scale.grid(**kw)
+   
+   def onChanged(self, _):
+      value = self.getRawValue().get()
+      
+      self.__value.configure(text = value * self.__step)
+      
+      StaticUtils.setSafely(self._smartWidgetValueBuffer, 0, value)
---
+++ packages/common-ui-components/common-ui-components-0.18.1.tar.gz/common-ui-components-0.18.1/commonuicomponents/ttk/widgets/smartwidget.py
@@ -0,0 +1,90 @@
+from commonutils import StaticUtils
+from tkinter import BooleanVar, IntVar, StringVar # _setVariable()
+
+class SmartWidget:
+   __FONT = None
+   
+   def __init__(self, master = None, **kw):
+      self._smartWidgetGrid = kw.pop("grid")
+      
+      # Can be reset in children.
+      self._smartWidgetStyle = StaticUtils.mergeJson(*map(lambda styleName: SmartWidget._STYLE_INSTANCE.configure(styleName) or dict(), [self.__class__.STYLE, kw.get("style", "")]), True)
+      
+      self.__columns = kw.pop("columns", 1)
+      self.__rows = kw.pop("rows", 1)
+      self.__smartWidgetName = kw.pop("name", None)
+      self.__value = kw.pop("value", None)
+      
+      parentBuffer = kw.pop("parentBuffer")
+      parentBufferIndex = kw.pop("parentBufferIndex")
+      
+      if kw.pop("hasValueBuffer", False):
+         self._smartWidgetValueBuffer = StaticUtils.getOrSetIfAbsent(parentBuffer, parentBufferIndex, [])
+      
+      if self.__class__._TKINTER_BASE:
+         from tkinter.ttk import Widget
+         
+         if issubclass(self.__class__._TKINTER_BASE, Widget) and "style" not in kw:
+            kw["style"] = self.__class__.STYLE
+         
+         self.__class__._TKINTER_BASE.__init__(self, master, **kw)
+   
+   def getRawValue(self):
+      return self.__value
+   
+   def getValue(self):
+      return self.__value.get()
+   
+   def grid(self, **kw):
+      self.__class__._TKINTER_BASE.grid(self, **StaticUtils.mergeJson(kw, self._smartWidgetGrid, True))
+   
+   @property
+   def column(self):
+      return self._smartWidgetGrid["column"]
+   
+   @property
+   def columns(self):
+      return self.__columns
+   
+   @property
+   def hasValueBuffer(self):
+      return hasattr(self, "_smartWidgetValueBuffer")
+   
+   @property
+   def row(self):
+      return self._smartWidgetGrid["row"]
+   
+   @property
+   def rows(self):
+      return self.__rows
+   
+   @property
+   def smartWidgetName(self):
+      return self.__smartWidgetName
+   
+   def _initValueAndTraceAdd(self):
+      self.__value.set(StaticUtils.getOrSetIfAbsent(self._smartWidgetValueBuffer, 0, self.__value.get()))
+         
+      self.__value.trace_add("write", lambda *_: StaticUtils.setSafely(self._smartWidgetValueBuffer, 0, self.__value.get()))
+   
+   @staticmethod
+   def setFont(font):
+      SmartWidget.__FONT = font
+   
+   @staticmethod
+   def _setFont(kw):
+      if "font" not in kw and SmartWidget.__FONT:
+         kw["font"] = SmartWidget.__FONT
+   
+   @staticmethod
+   def _setVariable(kw, defaultTypeName, defaultValueKey = "value", setHasValueBuffer = True, variableKey = None):
+      value = eval(f"{kw.pop('valueType', defaultTypeName)}()")
+      value.set(kw.get(defaultValueKey, value.get()))
+      
+      kw["value"] = value
+      
+      if variableKey:
+         kw[variableKey] = value
+      
+      if setHasValueBuffer:
+         kw["hasValueBuffer"] = True
