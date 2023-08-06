ed61631967669e9b958101b888121b31519c8109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:09 +0000

    Add package gpumonitor/0.1.1/gpumonitor-0.1.1-py3-none-any.whl

---
+++ packages/gpumonitor/gpumonitor-0.1.1-py3-none-any.whl/gpumonitor/callbacks/tf.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+from gpumonitor.monitor import GPUStatMonitor
+
+
+class TFGpuMonitorCallback(tf.keras.callbacks.Callback):
+    def __init__(self, delay):
+        super(TFGpuMonitorCallback, self).__init__()
+        self.delay = delay
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.monitor = GPUStatMonitor(self.delay)
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.monitor.stop()
+        print("")
+        self.monitor.display_average_stats_per_gpu()
