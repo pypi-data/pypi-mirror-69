b512ba378478e29ffbb41baa2aac98ab611247e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:07 +0000

    Add package PyBinglate/0.2.71/PyBinglate-0.2.71.tar.gz

---
+++ packages/PyBinglate/PyBinglate-0.2.71.tar.gz/PyBinglate-0.2.71/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="PyBinglate",
+    version="0.2.71",
+    author="Wirtos_new",
+    author_email="Wirtos.new@gmail.com",
+    description="Bing translator client for python",
+    url="https://github.com/Wirtos/PyBinglate",
+    packages=setuptools.find_packages(),
+    project_urls={
+        "Source Code": "https://github.com/Wirtos/PyBinglate",
+    },
+    install_requires=['requests'],
+    keywords="bing translator translation microsoft",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
