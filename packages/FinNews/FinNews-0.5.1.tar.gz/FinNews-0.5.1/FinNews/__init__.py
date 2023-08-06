fb15e4da134ca329cf3d31dcb09a78e3133db55e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:35 +0000

    Add package FinNews/0.5.1/FinNews-0.5.1.tar.gz

---
+++ packages/FinNews/FinNews-0.5.1.tar.gz/FinNews-0.5.1/FinNews/__init__.py
@@ -0,0 +1,9 @@
+from FinNews.feed import Feed
+from FinNews.cnbc import CNBC
+from FinNews.seeking_alpha import SeekingAlpha
+
+name = "FinNews"
+
+__version__ = "0.5.1"
+
+__all__ = ['Feed', 'CNBC', 'SeekingAlpha']
