918d58765b372215e1ab90b508bb2a9434908abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:55 +0000

    Add package dsnd-probbbb/0.2/dsnd_probbbb-0.2.tar.gz

---
+++ packages/dsnd-probbbb/dsnd_probbbb-0.2.tar.gz/dsnd_probbbb-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dsnd_probbbb',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['dsnd_probbbb'],
+      author = 'Bamigbade Opeyemi',
+      author_email = 'bamigbadeopeyemi@gmail.com',
+      zip_safe=False)
