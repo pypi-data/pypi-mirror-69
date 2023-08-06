cdb0cb66b57cc1accaec67ccdacd45b2d466f3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:11 +0000

    Add package iredis/1.7.2/iredis-1.7.2.tar.gz

---
+++ packages/iredis/iredis-1.7.2.tar.gz/iredis-1.7.2/tests/cli_tests/test_command_input.py
@@ -0,0 +1,20 @@
+def test_wrong_select_db_index(cli):
+    cli.sendline("select 1")
+    cli.expect(["OK", "127.0.0.1"])
+
+    cli.sendline("select 128")
+    cli.expect(["DB index is out of range", "127.0.0.1:6379[1]>"])
+
+    cli.sendline("select abc")
+    cli.expect(["invalid DB index", "127.0.0.1:6379[1]>"])
+
+    cli.sendline("select 15")
+    cli.expect("OK")
+
+
+def test_set_command_with_shash(clean_redis, cli):
+    cli.sendline("set a \\hello\\")  # legal redis command
+    cli.expect("OK")
+
+    cli.sendline("get a")
+    cli.expect(r"hello")
