222ec7014df6630345588fddff8084305d314384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:24 +0000

    Add package scprint/1.0.0b3/scprint-1.0.0b3.tar.gz

---
+++ packages/scprint/scprint-1.0.0b3.tar.gz/scprint-1.0.0b3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scprint",
+    version="1.0.0b3",
+    author="Daniel Gill",
+    author_email="DanG_@outlook.com",
+    description="None",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DanGill/scprint",
+    py_modules=["scprint", "colorsToHex"],
+    packages=setuptools.find_packages(),
+    keywords="None",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='~=3.3',
+)
