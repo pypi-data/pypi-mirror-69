e14d199569c9917c633145f83e70038fe32e7447
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:21 +0000

    Add package odoo12-addon-facto/12.0.0.0.1/odoo12_addon_facto-12.0.0.0.1-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-facto/odoo12_addon_facto-12.0.0.0.1-py2.py3-none-any.whl/odoo/addons/facto/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Odoo Facto customizations",
+    'version': '12.0.0.0.1',
+    'depends': ['account_payment_partner'],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': """
+    Odoo Facto customizations.
+    """,
+    "license": "AGPL-3",
+    'data': [
+    ],
+}
