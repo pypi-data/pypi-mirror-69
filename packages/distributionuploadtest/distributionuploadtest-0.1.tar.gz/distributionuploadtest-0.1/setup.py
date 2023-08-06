94c105dce73d7b32f74c510723c0877618c51019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:34 +0000

    Add package distributionuploadtest/0.1/distributionuploadtest-0.1.tar.gz

---
+++ packages/distributionuploadtest/distributionuploadtest-0.1.tar.gz/distributionuploadtest-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionuploadtest',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionuploadtest'],
+      zip_safe=False)
