64c9740c0f8ead9a2567f7ef90523592cd5ea667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:51 +0000

    Add package SpaRcle/0.0.7/SpaRcle-0.0.7.tar.gz

---
+++ packages/SpaRcle/SpaRcle-0.0.7.tar.gz/SpaRcle-0.0.7/setup.py
@@ -0,0 +1,19 @@
+import sys 
+from setuptools import setup, find_packages
+
+setup(name='SpaRcle',
+      version='0.0.7',
+      #url='https://github.com/Monika0000/SpaRcle-2.0',
+      license='MIT',
+      author='Monika',
+      author_email='rotaru5craft@gmail.com',
+      description='Искусственный интеллект. Искорка :)',
+      packages=['SpaRcle'],
+      #packages=find_packages(exclude=['tests']),
+      long_description="",
+      #entry_points={
+      #  "console_scripts": [
+      #      "SpaRcle=SpaRcle.__init__:main",
+      #  ]
+      #},
+      zip_safe=False)
