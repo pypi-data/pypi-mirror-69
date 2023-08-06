6fa14c4892d4be9fc1a54c13ad8f0ad1b462f6f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:36 +0000

    Add package odeopt/0.0.0/odeopt-0.0.0.tar.gz

---
+++ packages/odeopt/odeopt-0.0.0.tar.gz/odeopt-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='odeopt',
+    version='0.0.0',
+    description='Optimization based solve for ODE parameter inference problem.',
+    url='https://github.com/ihmeuw-msca/ODEOPT',
+    license='MIT',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'pytest',
+        'xspline'
+    ],
+    zip_safe=False,
+)
