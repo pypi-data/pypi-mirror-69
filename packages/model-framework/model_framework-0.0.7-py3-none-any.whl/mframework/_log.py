de1771ddcdd64dc7e26caf22c07b03e2488a18cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:03 +0000

    Add package model-framework/0.0.7/model_framework-0.0.7-py3-none-any.whl

---
+++ packages/model-framework/model_framework-0.0.7-py3-none-any.whl/mframework/_log.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------
+# Copyright 2019-2020 Airinnova AB and the Model-Framework authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------
+
+# Author: Aaron Dettmann
+
+"""
+Logger for debug purposes
+"""
+
+import logging
+
+from . import MODULE_NAME
+
+FORMAT = f"%(levelname)s | {MODULE_NAME} | %(message)s"
+logging.basicConfig(level=logging.ERROR, format=FORMAT)
+logger = logging.getLogger(MODULE_NAME)
+
+def disable_logger():
+    logger.setLevel(logging.ERROR)
+
+
+def enable_logger():
+    logger.setLevel(logging.DEBUG)
