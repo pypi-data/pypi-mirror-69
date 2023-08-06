3fca38507f27dd1c1c6b242929e691fbe7126c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:20 +0000

    Add package entropy/0.1.1/entropy-0.1.1.tar.gz

---
+++ packages/entropy/entropy-0.1.1.tar.gz/entropy-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+setup(
+    name='entropy',
+    version='0.1.1',
+    description='entropy is a command line friend that helps you reduce entropy in your life',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Gyanendra Mishra',
+    author_email='anomaly.the@gmail.com',
+    license='MIT',
+    keywords=['entropy', 'productivity', 'journaling'],
+    url='https://github.com/h4ck3rk3y/entropy',
+    install_requires=[
+        'colorama>=0.4.3',
+        'docopt>=0.6.2',
+        'setuptools>=42.0.2'
+    ],
+    packages=['entropy'],
+    entry_points={
+        'console_scripts': [
+            'entropy = entropy.entropy:main'
+        ]
+    })
