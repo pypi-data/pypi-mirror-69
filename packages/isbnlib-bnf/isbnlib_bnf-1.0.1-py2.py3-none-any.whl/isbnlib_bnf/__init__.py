378d3d1c6de416a93d1ec5ee6ad9789bb9af1841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:33 +0000

    Add package isbnlib-bnf/1.0.1/isbnlib_bnf-1.0.1-py2.py3-none-any.whl

---
+++ packages/isbnlib-bnf/isbnlib_bnf-1.0.1-py2.py3-none-any.whl/isbnlib_bnf/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+from ._bnf import query
+
+__version__ = '1.0.1'
+
+__plugin_main_language__ = 'fr'
+__plugin_service_name__ = 'bnf'
+__plugin_type__ = 'metadata'
