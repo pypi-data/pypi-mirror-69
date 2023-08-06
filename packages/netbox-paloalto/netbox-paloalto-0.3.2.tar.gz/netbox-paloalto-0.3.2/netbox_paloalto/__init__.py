bac20fb8cb65b71a964c2dc0cfd3b315de130e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:23 +0000

    Add package netbox-paloalto/0.3.2/netbox-paloalto-0.3.2.tar.gz

---
+++ packages/netbox-paloalto/netbox-paloalto-0.3.2.tar.gz/netbox-paloalto-0.3.2/netbox_paloalto/__init__.py
@@ -0,0 +1,25 @@
+try:
+    from extras.plugins import PluginConfig
+except ImportError:
+    # Dummy for when importing outside of netbox
+    class PluginConfig:
+        pass
+
+
+VERSION = "0.3.2"
+
+
+class PaloaltoConfig(PluginConfig):
+    name = "netbox_paloalto"
+    verbose_name = "Palo Alto firewall rules"
+    description = "A plugin for listing associated firewall rules to a NetBox object"
+    version = VERSION
+    author = "Martin Rødvand"
+    author_email = "martin@rodvand.net"
+    base_url = "paloalto"
+    required_settings = []
+    default_settings = {}
+    caching_config = {"*": None}
+
+
+config = PaloaltoConfig
---
+++ packages/netbox-paloalto/netbox-paloalto-0.3.2.tar.gz/netbox-paloalto-0.3.2/netbox_paloalto/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.FirewallRulesView.as_view(), name="firewall_input"),
+    path("<str:name>/", views.FirewallRulesView.as_view(), name="firewall_rule"),
+]
