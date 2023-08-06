1e8c5de39f603f717a823fa612aa3c75f16e3487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:58 +0000

    Add package avni/0.0.1a0/avni-0.0.1a0.tar.gz

---
+++ packages/avni/avni-0.0.1a0.tar.gz/avni-0.0.1a0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='avni',
+    version='0.0.1a',
+    description='',
+    long_description='',
+    url='https://www.rem3d.org',
+    author='Pritwiraj Moulik',
+    author_email='pritwiraj.moulik@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
