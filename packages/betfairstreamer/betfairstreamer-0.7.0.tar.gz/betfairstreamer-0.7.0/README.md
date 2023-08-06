23dc324d882bd362715e66a4663e0174d58027bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:51 +0000

    Add package betfairstreamer/0.7.0/betfairstreamer-0.7.0.tar.gz

---
+++ packages/betfairstreamer/betfairstreamer-0.7.0.tar.gz/betfairstreamer-0.7.0/README.md
@@ -0,0 +1,37 @@
+# Betfairstreamer
+
+What this library provides
+
+* Run single or multiple streams simultaneously.
+* Market cache and order cache, these provide abstractions over the betfairstream.
+* Using numpy arrays to slicing markets selections.
+* Async streaming (Optional).
+* Parse historical data.
+
+## Installation
+
+```
+pip install betfairstreamer==0.7.0
+```
+
+## Example
+
+Jupyter Notebook available in examples folder
+
+## Demo
+![](stream.gif)
+
+
+## Benchmark
+```Benchmark
+Setup: Two processes, one sending betfair stream messages , one receiving.
+
+Hardware: I7 8550U, 16GB ram
+
+Results: 
+ * Using a market cache it can read around ~90k messages/second (no decoding bytes -> dict), with decoding ~25k
+ * Without market cache (reading and splitting byte messages. >> 100 Mb/s
+
+```
+
+       
---
+++ packages/betfairstreamer/betfairstreamer-0.7.0.tar.gz/betfairstreamer-0.7.0/betfairstreamer/stream/betfair_connection_pool.py
@@ -0,0 +1,85 @@
+from __future__ import annotations
+
+import os
+import select
+import socket
+from typing import Dict, Generator, List, Union
+
+import attr
+import zmq
+
+from betfairlightweight import APIClient
+from betfairstreamer.models.betfair_api import BetfairMarketSubscriptionMessage, BetfairOrderSubscriptionMessage
+from betfairstreamer.stream.betfair_connection import BetfairConnection
+from betfairstreamer.stream.protocols import Connection
+
+
+@attr.s(auto_attribs=True)
+class BetfairConnectionPool:
+    poller: zmq.Poller = attr.ib(factory=zmq.Poller)
+    connections: Dict[Union[int, zmq.Socket], Connection] = attr.ib(factory=dict)
+
+    def add_connection(self, connection: Connection) -> None:
+        self.poller.register(connection.get_socket(), select.POLLIN)
+
+        if isinstance(connection.get_socket(), socket.socket):
+            self.connections[connection.get_socket().fileno()] = connection
+
+        if isinstance(connection.get_socket(), zmq.Socket):
+            self.connections[connection.get_socket()] = connection
+
+        # print(self.connections)
+
+    def read(self) -> Generator[bytes, None, None]:
+        while True:
+            events = self.poller.poll()
+
+            for fd, e in events:
+                for m in self.connections[fd].read():
+                    yield m
+
+    def close(self) -> None:
+        for k, c in self.connections.items():
+            c.get_socket().shutdown(socket.SHUT_RDWR)
+            c.get_socket().close()
+
+    @classmethod
+    def create_connection_pool(
+        cls,
+        subscription_messages: List[Union[BetfairMarketSubscriptionMessage, BetfairOrderSubscriptionMessage]],
+        session_token: str,
+        app_key: str,
+    ) -> BetfairConnectionPool:
+        connection_pool = cls()
+
+        for subscription_message in subscription_messages:
+            connection_pool.add_connection(
+                BetfairConnection.create_connection(subscription_message, session_token, app_key)
+            )
+
+        return connection_pool
+
+
+def create_connection_pool(
+    subscription_messages: List[Union[BetfairOrderSubscriptionMessage, BetfairMarketSubscriptionMessage]]
+) -> BetfairConnectionPool:
+    username, password, app_key, cert_path = (
+        os.environ["USERNAME"],
+        os.environ["PASSWORD"],
+        os.environ["APP_KEY"],
+        os.environ["CERT_PATH"],
+    )
+
+    cert_path = os.path.abspath(cert_path)
+
+    trading: APIClient = APIClient(username, password, app_key, certs=cert_path, locale=os.environ["LOCALE"])
+
+    trading.login()
+
+    app_key, session_token = trading.app_key, trading.session_token
+
+    connection_pool = BetfairConnectionPool.create_connection_pool(
+        subscription_messages=subscription_messages, session_token=session_token, app_key=app_key,
+    )
+
+    return connection_pool
