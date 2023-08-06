8b981fe320c9eadc486d6470ab340f9ed71afa77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:11 +0000

    Add package nltkPackage/0.0.28/nltkPackage-0.0.28.tar.gz

---
+++ packages/nltkPackage/nltkPackage-0.0.28.tar.gz/nltkPackage-0.0.28/nlp/load_model_from_package.py
@@ -0,0 +1,10 @@
+import mlflow
+import mlflow.spark
+import mlflow.pyfunc
+from os import listdir
+
+def read_pyspark_model(path):
+    # newv = "nltkTransformer-0.0.12/nlp/"
+    # return mlflow.spark.load_model(site.getsitepackages()[0] + "/nlp/")
+    print(listdir("."))
+    # return mlflow.spark.load_model(".")
