1bbf487d62eae8555869d0a8b99cf7c10376c288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:22 +0000

    Add package etnawrapper/2.7.0/etnawrapper-2.7.0.tar.gz

---
+++ packages/etnawrapper/etnawrapper-2.7.0.tar.gz/etnawrapper-2.7.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+__version__ = "2.7.0"
+URL = "https://github.com/massard-t/etnawrapper/archive/{}.tar.gz".format(__version__)
+
+setup(
+    name="etnawrapper",
+    packages=["etnawrapper"],
+    install_requires=['requests'],
+    version=__version__,
+    description="API wrapper for ETNA' APIs",
+    author="Theo 'Bob' Massard",
+    author_email="massar_t@etna-alternance.net",
+    url="https://github.com/tbobm/etnawrapper",
+    download_url=URL,
+    keywords=["school", "wrapper", "APIs"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
