4e823c09a9de1f93e0000cb3be7ac163abb681f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:58 +0000

    Add package timethese/0.0.5/timethese-0.0.5.tar.gz

---
+++ packages/timethese/timethese-0.0.5.tar.gz/timethese-0.0.5/CHANGELOG.rst
@@ -0,0 +1,23 @@
+
+Changelog
+=========
+
+0.0.5 (2020-05-31)
+------------------
+
+* Added better documentations
+* Now using NumPy documentation format for function def doc
+* Fixed typos in pyproject.toml
+
+0.0.4 (2020-05-30)
+------------------
+
+* Fixed code to be compatible with python 3.5
+* Fixed travis stuff
+* Added decorators to time specific functions for pandas.DataFrame.pipe arguments
+
+
+0.0.3 (2020-05-27)
+------------------
+
+* First release on PyPI.
---
+++ packages/timethese/timethese-0.0.5.tar.gz/timethese-0.0.5/docs/conf.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+]
+source_suffix = ".rst"
+master_doc = "index"
+project = "TimeThese"
+year = "2020"
+author = "Joachim W. Bargsten"
+copyright = "{0}, {1}".format(year, author)
+version = release = "0.0.5"
+
+pygments_style = "trac"
+templates_path = ["."]
+extlinks = {
+    "issue": ("https://github.com/jwbargsten/python-timethese/issues/%s", "#"),
+    "pr": ("https://github.com/jwbargsten/python-timethese/pull/%s", "PR #"),
+}
+
+html_use_smartypants = True
+html_last_updated_fmt = "%b %d, %Y"
+html_split_index = False
+html_sidebars = {
+    "**": ["searchbox.html", "globaltoc.html", "sourcelink.html"],
+}
+html_short_title = "%s-%s" % (project, version)
+
+napoleon_use_ivar = True
+napoleon_use_rtype = False
+napoleon_use_param = False
