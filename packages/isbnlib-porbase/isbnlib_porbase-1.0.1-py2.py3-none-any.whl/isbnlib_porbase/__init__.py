91c1580bd1a2996fc23c8b89cf150407c88a64de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:38 +0000

    Add package isbnlib-porbase/1.0.1/isbnlib_porbase-1.0.1-py2.py3-none-any.whl

---
+++ packages/isbnlib-porbase/isbnlib_porbase-1.0.1-py2.py3-none-any.whl/isbnlib_porbase/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+from ._porbase import query
+
+__version__ = '1.0.1'
+
+__plugin_author__ = 'xlcnd'
+__plugin_description__ = 'Metadata for Portuguese books from porbase.pt'
+__plugin_homepage__ = 'github.com/xlcnd/isbnlib-porbase'
+__plugin_service_language__ = 'pt'
+__plugin_service_name__ = 'porbase'
+__plugin_type__ = 'metadata'
