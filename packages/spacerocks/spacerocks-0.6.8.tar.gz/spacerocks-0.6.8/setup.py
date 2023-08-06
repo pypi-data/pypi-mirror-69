75089d82fd0672219617bf2e1776b588b6e55ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:25 +0000

    Add package spacerocks/0.6.8/spacerocks-0.6.8.tar.gz

---
+++ packages/spacerocks/spacerocks-0.6.8.tar.gz/spacerocks-0.6.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+   name='spacerocks',
+   version='0.6.8',
+   description='Calculate solar system ephemerides from orbital elements.',
+   author='Kevin Napier',
+   author_email='kjnapier@umich.edu',
+   url="https://github.com/kjnapes/spacerocks",
+   packages=['spacerocks'],
+   # I'm not sure package_data is working.
+   package_data={'spacerocks': ['data/observatories.csv']},
+   install_requires=['healpy', 'numpy', 'matplotlib', 'scipy', 'skyfield',
+                     'astropy', 'numba', 'pandas'],
+   include_package_data=True
+)
