9744b697e4d6022e523d5e1ce80d8fb27d8e8fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:14 +0000

    Add package html-sanitizer/1.9.1/html_sanitizer-1.9.1-py2.py3-none-any.whl

---
+++ packages/html-sanitizer/html_sanitizer-1.9.1-py2.py3-none-any.whl/html_sanitizer/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+
+VERSION = (1, 9, 1)
+__version__ = ".".join(map(str, VERSION))
+
+
+try:
+    from .sanitizer import *  # noqa
+except ImportError:  # pragma: no cover
+    pass
