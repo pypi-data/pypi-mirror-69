248d18f1a43c5887a24735b56d8c5de709fefe4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:50 +0000

    Add package wfmess/0.0.3/wfmess-0.0.3.tar.gz

---
+++ packages/wfmess/wfmess-0.0.3.tar.gz/wfmess-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wfmess",
+    version="0.0.3",
+    author="Neil Vaytet",
+    author_email="neil.vaytet@esss.se",
+    description="Wave-Frame Multiplication data reduction for ESS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nvaytet/wfmess",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "h5py",
+        "scipy"
+    ],
+)
