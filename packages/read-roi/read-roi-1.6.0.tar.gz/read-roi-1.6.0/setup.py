8cec9be515706299c429175c03adf24ac5a76f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:54 +0000

    Add package read-roi/1.6.0/read-roi-1.6.0.tar.gz

---
+++ packages/read-roi/read-roi-1.6.0.tar.gz/read-roi-1.6.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='read-roi',
+      version='1.6.0',
+      author='Hadrien Mary',
+      author_email='hadrien.mary@gmail.com',
+      url='https://github.com/hadim/read-roi/',
+      description='Read ROI files .zip or .roi generated with ImageJ.',
+      long_description_content_type='text/markdown',
+      packages=find_packages(),
+      classifiers=[
+              'Development Status :: 5 - Production/Stable',
+              'Intended Audience :: Developers',
+              'Natural Language :: English',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 3',
+              ])
