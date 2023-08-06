7007d28583f37d6b340b9c4506e132124b9c1f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:23 +0000

    Add package create-machine-utils/1.2.4/create-machine-utils-1.2.4.tar.gz

---
+++ packages/create-machine-utils/create-machine-utils-1.2.4.tar.gz/create-machine-utils-1.2.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='create-machine-utils',
+    version='1.2.4',
+    packages=find_packages(),
+    url='https://github.com/Minion3665/MiniUtils',
+    license='GNU General Public License V3',
+    author='Minion3665',
+    author_email='nathan@clicksminuteper.net',
+    description='The MiniUtils module to make making discord bots easier',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
