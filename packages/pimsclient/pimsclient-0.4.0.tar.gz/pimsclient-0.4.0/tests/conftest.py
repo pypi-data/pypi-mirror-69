d4000f43cb3d22b4b847ac27c4842c5959afad75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:27 +0000

    Add package pimsclient/0.4.0/pimsclient-0.4.0.tar.gz

---
+++ packages/pimsclient/pimsclient-0.4.0.tar.gz/pimsclient-0.4.0/tests/conftest.py
@@ -0,0 +1,30 @@
+"""Fixtures shared between pytest modules
+"""
+
+import pytest
+
+from pimsclient.server import PIMSSession
+from tests.factories import RequestsMock
+
+
+@pytest.fixture()
+def mock_pims_session(monkeypatch):
+    """PIMS session that does not hit any server and can return arbitrary responses
+
+    Returns
+    -------
+    PIMSSession
+
+    """
+    mock = RequestsMock()
+    session = PIMSSession(session=mock, base_url="https://testserver.test")
+    return session
+
+
+@pytest.fixture()
+def mock_requests(monkeypatch):
+    """Replace requests lib in pimsclient.server with a mock requests lib"""
+
+    mock = RequestsMock
+    monkeypatch.setattr('pimsclient.server.requests', mock)
+    return mock
