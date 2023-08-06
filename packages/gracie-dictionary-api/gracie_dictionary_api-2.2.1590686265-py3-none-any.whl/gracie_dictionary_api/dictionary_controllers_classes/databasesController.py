7c95fd1c1bc3d93c2b40e192777dc63188e4f253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:10 +0000

    Add package gracie-dictionary-api/2.2.1590686265/gracie_dictionary_api-2.2.1590686265-py3-none-any.whl

---
+++ packages/gracie-dictionary-api/gracie_dictionary_api-2.2.1590686265-py3-none-any.whl/gracie_dictionary_api/dictionary_controllers_classes/databasesController.py
@@ -0,0 +1,176 @@
+from gracie_dictionary_api import GracieBaseAPI
+
+
+class databasesController(GracieBaseAPI):
+    """Databases."""
+
+    _controller_name = "databasesController"
+
+    def archive(self, classifierId, **kwargs):
+        """
+
+        Args:
+            classifierId: (string): classifierId
+            languageId: (string): languageId
+
+        Consumes:
+            application/json
+
+        Returns:
+            application/json
+        """
+
+        all_api_parameters = {'classifierId': {'name': 'classifierId', 'required': True, 'in': 'query'}, 'languageId': {'name': 'languageId', 'required': False, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/databases/archive'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def backup(self, **kwargs):
+        """
+
+        Args:
+            incremental: (boolean): incremental
+            removeFolder: (boolean): removeFolder
+            zip: (boolean): zip
+
+        Consumes:
+            application/json
+
+        Returns:
+            application/json;charset=UTF-8
+        """
+
+        all_api_parameters = {'incremental': {'name': 'incremental', 'required': False, 'in': 'query'}, 'removeFolder': {'name': 'removeFolder', 'required': False, 'in': 'query'}, 'zip': {'name': 'zip', 'required': False, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/databases/backup'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def export(self, **kwargs):
+        """
+
+        Args:
+            databaseId: (string): Id is some of { country, topic, skillSet, skill, clusterSet, clusterGroup, cluster, profile }. If empty then export all databases.
+            password: (string): password
+
+        Consumes:
+            application/json
+
+        Returns:
+            application/json;charset=UTF-8
+        """
+
+        all_api_parameters = {'databaseId': {'name': 'databaseId', 'required': False, 'in': 'query'}, 'password': {'name': 'password', 'required': False, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/databases/export'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def import(self, file, **kwargs):
+        """
+
+        Args:
+            conflictsResolving: (string): conflictsResolving
+            file: (file): file
+            password: (string): password
+
+        Consumes:
+            multipart/form-data
+
+        Returns:
+            application/json;charset=UTF-8
+        """
+
+        all_api_parameters = {'conflictsResolving': {'name': 'conflictsResolving', 'required': False, 'in': 'query'}, 'file': {'name': 'file', 'required': True, 'in': 'formData'}, 'password': {'name': 'password', 'required': False, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/databases/import'
+        actions = ['post']
+        consumes = ['multipart/form-data']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def updateBinaries(self):
+        """"""
+
+        all_api_parameters = {}
+        parameters_names_map = {}
+        api = '/databases/updateBinaries'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def updateDictionaries(self, **kwargs):
+        """
+
+        Args:
+            dictionaryId: (string): dictionaryId
+            forceUpdateUnchanged: (boolean): forceUpdateUnchanged
+            recalculateDocumentsVectors: (boolean): recalculateDocumentsVectors
+
+        Consumes:
+            application/json
+
+        Returns:
+            application/json;charset=UTF-8
+        """
+
+        all_api_parameters = {'dictionaryId': {'name': 'dictionaryId', 'required': False, 'in': 'query'}, 'forceUpdateUnchanged': {'name': 'forceUpdateUnchanged', 'required': False, 'in': 'query'}, 'recalculateDocumentsVectors': {'name': 'recalculateDocumentsVectors', 'required': False, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/databases/updateDictionaries'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def updateDoc2vec(self):
+        """"""
+
+        all_api_parameters = {}
+        parameters_names_map = {}
+        api = '/databases/updateDoc2vec'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def updateIdfModel(self):
+        """"""
+
+        all_api_parameters = {}
+        parameters_names_map = {}
+        api = '/databases/updateIdfModel'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
+
+    def updateSkillsets(self, **kwargs):
+        """
+
+        Args:
+            forceUpdateUnchanged: (boolean): forceUpdateUnchanged
+            recalculateDocumentsVectors: (boolean): recalculateDocumentsVectors
+            skillId: (string): skillId
+
+        Consumes:
+            application/json
+
+        Returns:
+            application/json;charset=UTF-8
+        """
+
+        all_api_parameters = {'forceUpdateUnchanged': {'name': 'forceUpdateUnchanged', 'required': False, 'in': 'query'}, 'recalculateDocumentsVectors': {'name': 'recalculateDocumentsVectors', 'required': False, 'in': 'query'}, 'skillId': {'name': 'skillId', 'required': False, 'in': 'query'}}
+        parameters_names_map = {}
+        api = '/databases/updateSkillsets'
+        actions = ['post']
+        consumes = ['application/json']
+        params, data = self._format_params_for_api(locals(), all_api_parameters, parameters_names_map)
+        return self._process_api(self._controller_name, api, actions, params, data, consumes)
