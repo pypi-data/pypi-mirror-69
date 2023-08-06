36d8d93a431bda6dfa9379241a7cfe112bafcc77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:21 +0000

    Add package gb-gaussian-binomial-distribution/0.1/gb_gaussian_binomial_distribution-0.1.tar.gz

---
+++ packages/gb-gaussian-binomial-distribution/gb_gaussian_binomial_distribution-0.1.tar.gz/gb_gaussian_binomial_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gb_gaussian_binomial_distribution',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gb_gaussian_binomial_distribution'],
+      author='Gianluca Bianco',
+      author_email='ciao@gianlucabianco.dev',
+      zip_safe=False)
