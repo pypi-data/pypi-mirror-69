b9886b28e27c815775c2d17a495cded1b808087e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:11 +0000

    Add package echo360/2.0.1/echo360-2.0.1.tar.gz

---
+++ packages/echo360/echo360-2.0.1.tar.gz/echo360-2.0.1/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="echo360",
+    version="2.0.1",
+    description="Commandline tool for automated downloads of echo360 videos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://cs.tinyiu.com/echo360",
+    project_urls={
+        "Documentation": "https://github.com/soraxas/echo360/blob/master/README.md",
+        "Code": "https://github.com/soraxas/echo360",
+        "Issue tracker": "https://github.com/soraxas/echo360/issues",
+    },
+    python_requires='>=2.7',
+    install_requires=required,
+    platforms='linux, macos, windows',
+    entry_points={
+    'console_scripts': [
+        'echo360-download=echo360.main:main',
+        ],
+        },
+    package_data={'echo360': ['*.py']},
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Unix Shell",
+        "Topic :: System :: Shells",
+        "Topic :: System :: System Shells",
+        "Topic :: Terminals",
+        "Topic :: System :: Networking",
+        "License :: OSI Approved :: MIT License"
+    ],
+    license="MIT",
+    author="soraxas",
+    author_email="oscar@tinyiu.com"
+)
