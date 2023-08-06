982a94fbfbfba9ed15ee556d13e8d2b428f8028f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:40 +0000

    Add package tsmt-distributions/0.1/tsmt_distributions-0.1.tar.gz

---
+++ packages/tsmt-distributions/tsmt_distributions-0.1.tar.gz/tsmt_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tsmt_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['tsmt_distributions'],
+      author = 'Taslim Mahbub',
+      author_email = 'taslimmahbub@hotmail.com',
+      zip_safe=False)
