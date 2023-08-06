58862cd7d429bab3f2590dbd63bd524a3c70b1ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:17 +0000

    Add package stat-distribs/0.1/stat_distribs-0.1.tar.gz

---
+++ packages/stat-distribs/stat_distribs-0.1.tar.gz/stat_distribs-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='stat_distribs',
+      version='0.1',
+      description='Statistical distributions',
+      packages=['stat_distribs'],
+      author='Hafiz Adewuyi',
+      author_email='myemail@example.com',
+      zip_safe=False)
