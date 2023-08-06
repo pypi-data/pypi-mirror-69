990068b2176fa9c77d92b49e452717f9c88f0b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:27 +0000

    Add package trajectorytools/0.3a1/trajectorytools-0.3a1.tar.gz

---
+++ packages/trajectorytools/trajectorytools-0.3a1.tar.gz/trajectorytools-0.3a1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(name='trajectorytools',
+      version='0.3-alpha.1',
+      description='A tool to study 2D trajectories',
+      long_description=long_description,
+      url='http://github.com/fjhheras/trajectorytools',
+      author='Francisco J.H. Heras',
+      author_email='fjhheras@gmail.com',
+      license='GPL',
+
+      packages=['trajectorytools'],
+      package_data={'trajectorytools': ['data/*.npy']},
+      include_package_data=True,
+      zip_safe=False,
+
+      classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities'
+      ],
+
+      )
+
