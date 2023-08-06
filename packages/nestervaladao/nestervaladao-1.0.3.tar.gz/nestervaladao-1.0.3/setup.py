641aac2154413932f6b5ea6082b7cc291072ad41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:22 +0000

    Add package nestervaladao/1.0.3/nestervaladao-1.0.3.tar.gz

---
+++ packages/nestervaladao/nestervaladao-1.0.3.tar.gz/nestervaladao-1.0.3/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+from setuptools import setup
+
+setup(name = 'nestervaladao',
+      version = '1.0.3',
+      author='Fernando Matias Valadao',
+      author_email='fernando.valadao@gmail.com',
+      url='https://github.com/fernandovaladao',
+      packages=setuptools.find_packages(),
+      description='A simple printer of nested lists')
