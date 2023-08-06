7d19a9f9abb1bb903999e0c4a9231902c232e8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:37 +0000

    Add package plk345-distributions/2.6/plk345_distributions-2.6.tar.gz

---
+++ packages/plk345-distributions/plk345_distributions-2.6.tar.gz/plk345_distributions-2.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='plk345_distributions',
+      version='2.6',
+      description='Gaussian distributions',
+      packages=['plk345_distributions'],
+      author = 'Irving Chow',
+      author_email = '1624000827@qq.com',
+      zip_safe=False)
