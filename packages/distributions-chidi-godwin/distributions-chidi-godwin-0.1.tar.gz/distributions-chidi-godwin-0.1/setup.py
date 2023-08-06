2c9cf7b6b33649d9f6c54967e89eabff0de6633c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:10 +0000

    Add package distributions-chidi-godwin/0.1/distributions-chidi-godwin-0.1.tar.gz

---
+++ packages/distributions-chidi-godwin/distributions-chidi-godwin-0.1.tar.gz/distributions-chidi-godwin-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions-chidi-godwin',
+      version='0.01',
+      author="Nwachukwu Chidiebere",
+      description='Gaussian distributions',
+      packages=['distributions-chidi-godwin'],
+      zip_safe=False)
