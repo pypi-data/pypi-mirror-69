99f5e6c6f96cdb5799cc434c324fad723f37db1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:13 +0000

    Add package tc-admin/2.5.1/tc-admin-2.5.1.tar.gz

---
+++ packages/tc-admin/tc-admin-2.5.1.tar.gz/tc-admin-2.5.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name="tc-admin",
+    version="2.5.1",
+    description="Administration of Taskcluster runtime configuration",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Dustin Mitchell",
+    author_email="dustin@mozilla.com",
+    url="https://github.com/taskcluster/tc-admin",
+    packages=find_packages("."),
+    install_requires=[
+        "taskcluster~=30.0.1",
+        "click~=7.0",
+        "blessings~=1.7",
+        "attrs~=19.3.0",
+        "memoized==0.3",  # no semver..
+        "sortedcontainers~=2.1.0",
+        "aiohttp~=3.6.2",
+        "pytest~=5.2",
+        "pyyaml~=4.2b1",
+        "patiencediff==0.1.0",
+    ],
+    setup_requires=["pytest-runner", "flake8"],
+    tests_require=["pytest", "pytest-mock", "pytest-asyncio~=0.10.0", "flake8", "asyncmock"],
+    classifiers=("Programming Language :: Python :: 3",),
+    entry_points={
+        "console_scripts": [
+            "tc-admin = tcadmin.boot:boot",
+        ]},
+)
---
+++ packages/tc-admin/tc-admin-2.5.1.tar.gz/tc-admin-2.5.1/tcadmin/resources/util.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+from ..util.json import pretty_json
+
+# utilities for resource attr.ib's
+DESCRIPTION_PREFIX = """\
+*DO NOT EDIT* - This resource is configured automatically.
+
+"""
+
+
+def description_converter(value):
+    """Prepend *DO NOT EDIT* and a short explainer to the given value"""
+    if not value.startswith("*DO NOT EDIT*"):
+        value = DESCRIPTION_PREFIX + value
+    return value
+
+
+def scopes_converter(value):
+    """Ensure that scopes are always sorted and immutable (a tuple)"""
+    return tuple(sorted(value))
+
+
+def list_formatter(id, value):
+    """Format as a list of bulleted strings"""
+    return "\n".join("- " + scope for scope in value)
+
+
+def json_formatter(id, value):
+    """Format as a pretty-printed JSON string"""
+    return pretty_json(value)
