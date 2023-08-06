1e77667b3d51e1a671900679f2c88455c7ac7aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:10 +0000

    Add package brotli-middleware/0.1.3/brotli_middleware-0.1.3.tar.gz

---
+++ packages/brotli-middleware/brotli_middleware-0.1.3.tar.gz/brotli_middleware-0.1.3/setup.py
@@ -0,0 +1,30 @@
+"""A compression AGSI middleware using brotli.
+
+Built using starlette under the hood, it can be used as a drop in replacement to
+GZipMiddleware for Starlette or FastAPI.
+"""
+
+from setuptools import setup  # type: ignore
+
+setup(
+    name="brotli_middleware",
+    version="0.1.3",
+    url="",
+    license="",
+    author="Diogo B Freitas",
+    author_email="somnium@riseup.net",
+    description="A compression AGSI middleware using brotli",
+    long_description=__doc__,
+    packages=["brotli_middleware"],
+    python_requires=">=3.5",
+    include_package_data=True,
+    install_requires=["starlette>=0.13.4"],
+    dependency_links=["git+https://github.com/google/brotli"],
+    platforms="any",
+    zip_safe=False,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+)
