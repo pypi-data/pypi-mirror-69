4c033cd38ba8f39a793aeec7333ae365d02269b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:13 +0000

    Add package itarC/0.1.0/itarC-0.1.0.tar.gz

---
+++ packages/itarC/itarC-0.1.0.tar.gz/itarC-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+        
+
+setup(
+    name ="itarC",
+    version = "0.1.0",
+    description="A python package to register or view fake job alerts",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/startng/forward4-itarj-adekams",
+    author="Adenike Salau",
+    author_email="kifademi@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords = "fake job alerts",
+    packages=find_packages(),
+    package_data = {'itarj_console': ['*.txt']},
+    include_package_data = True,
+    install_requires = ["requests"],
+    entry_points = {
+        "console_scripts": [
+            "itarj-console=itarj_console.itarj:main",
+        ],
+    }   
+)
+
