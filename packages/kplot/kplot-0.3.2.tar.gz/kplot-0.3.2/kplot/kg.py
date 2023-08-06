0669ac66cc83fab8977ef8c2063a85cdd4431079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:58 +0000

    Add package kplot/0.3.2/kplot-0.3.2.tar.gz

---
+++ packages/kplot/kplot-0.3.2.tar.gz/kplot-0.3.2/kplot/kg.py
@@ -0,0 +1,3 @@
+import kplot.lib
+
+kplot.lib.loadtheme("kg")
---
+++ packages/kplot/kplot-0.3.2.tar.gz/kplot-0.3.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kplot",
+    version="0.3.2",
+    author="Keyan Gootkin",
+    author_email="KeyanGootkin@gmail.com",
+    description="A small package for making matplotlib plots pretty <3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KeyanGootkin/kplot",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={"": ["**styles/**.mplstyle"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
