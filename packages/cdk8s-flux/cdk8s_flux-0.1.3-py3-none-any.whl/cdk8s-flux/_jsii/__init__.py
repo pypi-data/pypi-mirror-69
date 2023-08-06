b7579bbc3837dfdd4fe2b6b7dd2ca623c7ecdbf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:59 +0000

    Add package cdk8s-flux/0.1.3/cdk8s_flux-0.1.3-py3-none-any.whl

---
+++ packages/cdk8s-flux/cdk8s_flux-0.1.3-py3-none-any.whl/cdk8s-flux/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import cdk8s
+import constructs
+
+__jsii_assembly__ = jsii.JSIIAssembly.load("cdk8s-flux", "0.1.3", __name__[0:-6], "cdk8s-flux@0.1.3.jsii.tgz")
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
