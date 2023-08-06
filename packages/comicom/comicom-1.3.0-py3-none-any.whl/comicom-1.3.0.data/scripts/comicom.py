3812ede7f4e0a234e231985f4266205edd9d7a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:05 +0000

    Add package comicom/1.3.0/comicom-1.3.0-py3-none-any.whl

---
+++ packages/comicom/comicom-1.3.0-py3-none-any.whl/comicom-1.3.0.data/scripts/comicom.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import tkinter
+
+from comiccompiler import compiler
+from comiccompiler import comgui
+from comiccompiler import arguments
+
+
+# execute only if run as a script
+if __name__ == "__main__":
+    args = arguments.parse()
+
+    # Trigger the actual program....
+    if args.gui:
+        window = tkinter.Tk()
+        comgui = comgui.MainWindow(window)
+        window.mainloop()
+    else:
+        compiler.run(args)
+
+
+def run(parseable_args):
+    compiler.run(arguments.parse(parseable_args))
