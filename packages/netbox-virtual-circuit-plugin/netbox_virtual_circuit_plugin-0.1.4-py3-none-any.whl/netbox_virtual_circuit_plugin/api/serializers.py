e8edb4486f70492e005b3b3810ff633a19152ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:45 +0000

    Add package netbox-virtual-circuit-plugin/0.1.4/netbox_virtual_circuit_plugin-0.1.4-py3-none-any.whl

---
+++ packages/netbox-virtual-circuit-plugin/netbox_virtual_circuit_plugin-0.1.4-py3-none-any.whl/netbox_virtual_circuit_plugin/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from netbox_virtual_circuit_plugin.models import VirtualCircuit, VirtualCircuitVLAN
+
+
+class VCVLANSerializer(ModelSerializer):
+
+    class Meta:
+        model = VirtualCircuitVLAN
+        fields = ['vlan']
+
+class VirtualCircuitSerializer(ModelSerializer):
+    vlans = VCVLANSerializer(many=True)
+
+    class Meta:
+        model = VirtualCircuit
+        fields = ['vcid', 'name', 'status', 'context', 'vlans']
+
+    def create(self, validated_data):
+        vlans_data = validated_data.pop('vlans')
+        virtual_circuit = VirtualCircuit.objects.create(**validated_data)
+        for vlan in vlans_data:
+            VirtualCircuitVLAN.objects.create(virtual_circuit=virtual_circuit, **vlan)
+        return virtual_circuit
+
+
+class VirtualCircuitVLANSerializer(ModelSerializer):
+
+    class Meta:
+        model = VirtualCircuitVLAN
+        fields = ['id', 'virtual_circuit', 'vlan']
