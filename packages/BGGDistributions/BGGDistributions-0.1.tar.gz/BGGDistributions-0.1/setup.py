65f4dcb45e29210e2ed21e4a20b0a272aedfb688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:07:58 +0000

    Add package BGGDistributions/0.1/BGGDistributions-0.1.tar.gz

---
+++ packages/BGGDistributions/BGGDistributions-0.1.tar.gz/BGGDistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='BGGDistributions',
+      version='0.1',
+      description='Gaussian, Binomail, and General distributions',
+      packages=['BGGDistributions'],
+      zip_safe=False)
