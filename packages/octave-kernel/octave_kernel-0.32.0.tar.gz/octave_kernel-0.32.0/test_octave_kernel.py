7610f5140a17ce30ffa1bc9754650d3017c256f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:50 +0000

    Add package octave-kernel/0.32.0/octave_kernel-0.32.0.tar.gz

---
+++ packages/octave-kernel/octave_kernel-0.32.0.tar.gz/octave_kernel-0.32.0/test_octave_kernel.py
@@ -0,0 +1,31 @@
+"""Example use of jupyter_kernel_test, with tests for IPython."""
+
+import sys
+import unittest
+import jupyter_kernel_test as jkt
+
+
+class OctaveKernelTests(jkt.KernelTests):
+    kernel_name = "octave"
+
+    language_name = "octave"
+
+    code_hello_world = "disp('hello, world')"
+
+    code_display_data = [
+        {'code': '%plot -f png\nplot([1,2,3])', 'mime': 'image/png'},
+        {'code': '%plot -f svg\nplot([1,2,3])', 'mime': 'image/svg+xml'}
+    ] if sys.platform == 'darwin' else []
+
+    completion_samples = [
+        {
+            'text': 'acos',
+            'matches': {'acos', 'acosd', 'acosh'},
+        },
+    ]
+
+    code_page_something = "ones?"
+
+
+if __name__ == '__main__':
+    unittest.main()
