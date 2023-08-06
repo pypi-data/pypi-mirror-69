95c9fe3564436030a692d88713b3441a634d793d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:41 +0000

    Add package nbnhhsh/1.0/nbnhhsh-1.0.tar.gz

---
+++ packages/nbnhhsh/nbnhhsh-1.0.tar.gz/nbnhhsh-1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nbnhhsh",
+    version="1.0",
+    author="purofle",
+    author_email="3272912942@qq.com",
+    description="nbnhhsh的python版本",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/purofle/nbnhhsh-python",
+    entry_points={
+        'console_scripts': [
+            'nbnhhsh=nbnhhsh.cmd:main',
+
+        ],
+
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
