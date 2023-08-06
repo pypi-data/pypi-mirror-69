5358e7637cd6ba9ac6720e769622348b4d43b35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:22 +0000

    Add package vdistributions/1.0/vdistributions-1.0.tar.gz

---
+++ packages/vdistributions/vdistributions-1.0.tar.gz/vdistributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='vdistributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['vdistributions'],
+      author = 'Vishakha Goyal',
+      zip_safe=False)
