1b4d5cf054b22c5726628565eea3d2e5a23f3ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:39 +0000

    Add package prob-dist-min/0.1/prob_dist_min-0.1.tar.gz

---
+++ packages/prob-dist-min/prob_dist_min-0.1.tar.gz/prob_dist_min-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_dist_min',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
