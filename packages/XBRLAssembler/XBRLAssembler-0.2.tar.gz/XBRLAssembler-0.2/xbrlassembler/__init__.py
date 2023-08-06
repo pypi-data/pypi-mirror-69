93b035cb1deef42df1b4de407962cf255d423b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:23 +0000

    Add package XBRLAssembler/0.2/XBRLAssembler-0.2.tar.gz

---
+++ packages/XBRLAssembler/XBRLAssembler-0.2.tar.gz/XBRLAssembler-0.2/xbrlassembler/__init__.py
@@ -0,0 +1,3 @@
+from xbrlassembler.assembler import XBRLAssembler
+from xbrlassembler.enums import XBRLType, FinancialStatement
+from xbrlassembler.error import XBRLError, XBRLCellsError, XBRLLabelError, XBRLSchemaError
