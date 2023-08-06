aedb858a5199b6793a885cec123fa8e40f617c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:16 +0000

    Add package trim21-flexget-plugins/0.0.1a2/trim21-flexget-plugins-0.0.1a2.tar.gz

---
+++ packages/trim21-flexget-plugins/trim21-flexget-plugins-0.0.1a2.tar.gz/trim21-flexget-plugins-0.0.1a2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['trim21_flexget_plugins', 'trim21_flexget_plugins.modify']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'FlexGet.plugins': ['magnet_add_dn = '
+                     'trim21_flexget_plugins.modify.magnet_add_dn.PluginMagnetAddDownloadName']}
+
+setup_kwargs = {
+    'name': 'trim21-flexget-plugins',
+    'version': '0.0.1a2',
+    'description': 'A set of plugins for flexget',
+    'long_description': '',
+    'author': 'Trim21',
+    'author_email': 'i@trim21.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Trim21/flexget-plugins',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
