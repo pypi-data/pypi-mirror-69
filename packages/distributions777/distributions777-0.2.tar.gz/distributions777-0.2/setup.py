056b2caab4fc84a1656066f793edbc344108de07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package distributions777/0.2/distributions777-0.2.tar.gz

---
+++ packages/distributions777/distributions777-0.2.tar.gz/distributions777-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions777',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions777'],
+      zip_safe=False)
