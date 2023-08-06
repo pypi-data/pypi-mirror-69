9dbfb71b9547f7cffbf1034eae9fa7e258b7fc7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:35 +0000

    Add package ddlp-distributions/0.2/ddlp_distributions-0.2.tar.gz

---
+++ packages/ddlp-distributions/ddlp_distributions-0.2.tar.gz/ddlp_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ddlp_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['ddlp_distributions'],
+      zip_safe=False)
