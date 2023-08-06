7e0d6a41701e0b5fb603f47de7c03915ce885479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:40 +0000

    Add package snd-probability/0.1/snd_probability-0.1.tar.gz

---
+++ packages/snd-probability/snd_probability-0.1.tar.gz/snd_probability-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='snd_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['snd_probability'],
+      author = 'Divyesh Mangroliya',
+      zip_safe=False)
