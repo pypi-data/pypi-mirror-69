5bd3c24d012beafd618fcde606e82d4d08bb330d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:53 +0000

    Add package basic-probability/0.2/basic_probability-0.2.tar.gz

---
+++ packages/basic-probability/basic_probability-0.2.tar.gz/basic_probability-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='basic_probability',
+      version='0.2',
+      description='Library for basic operations on Binomial and Gaussian distributions',
+      packages=['basic_probability'],
+      zip_safe=False)
