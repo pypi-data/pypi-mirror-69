8f5a56b89ddef7ee6de990d2c17e49cd3bf82fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:14 +0000

    Add package jina/0.1.14/jina-0.1.14.tar.gz

---
+++ packages/jina/jina-0.1.14.tar.gz/jina-0.1.14/jina/clients/python/request.py
@@ -0,0 +1,64 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+import ctypes
+import random
+from typing import Iterator, Union
+
+from ...enums import ClientInputType, ClientMode
+from ...helper import batch_iterator
+from ...proto import jina_pb2
+
+
+def _generate(data: Union[Iterator[bytes], Iterator['jina_pb2.Document'], Iterator[str]], batch_size: int = 0,
+              first_doc_id: int = 0, first_request_id: int = 0,
+              random_doc_id: bool = False, mode: ClientMode = ClientMode.INDEX, top_k: int = 50,
+              input_type: ClientInputType = ClientInputType.RAW_BYTES,
+              *args, **kwargs) -> Iterator['jina_pb2.Message']:
+    if isinstance(mode, str):
+        mode = ClientMode.from_string(mode)
+
+    for pi in batch_iterator(data, batch_size):
+        req = jina_pb2.Request()
+        req.request_id = first_request_id
+
+        if mode == ClientMode.SEARCH:
+            if top_k <= 0:
+                raise ValueError('"top_k: %d" is not a valid number' % top_k)
+            else:
+                req.search.top_k = top_k
+
+        for _raw in pi:
+            d = getattr(req, str(mode).lower()).docs.add()
+            if input_type == ClientInputType.PROTOBUF:
+                d.CopyFrom(_raw)
+            elif input_type == ClientInputType.DATA_URI:
+                d.data_uri = _raw
+            elif input_type == ClientInputType.RAW_BYTES:
+                if isinstance(_raw, str):
+                    _raw = _raw.encode()  # auto-fix for str
+                d.raw_bytes = _raw
+            d.doc_id = first_doc_id if not random_doc_id else random.randint(0, ctypes.c_uint(-1).value)
+            d.weight = 1.0
+            first_doc_id += 1
+        yield req
+        first_request_id += 1
+
+
+def index(*args, **kwargs):
+    """Generate indexing request"""
+    yield from _generate(*args, **kwargs)
+
+
+def train(*args, **kwargs):
+    """Generate training request """
+    yield from _generate(*args, **kwargs)
+    req = jina_pb2.Request()
+    req.request_id = 1
+    req.train.flush = True
+    yield req
+
+
+def search(*args, **kwargs):
+    """Generate search request """
+    yield from _generate(*args, **kwargs)
---
+++ packages/jina/jina-0.1.14.tar.gz/jina-0.1.14/jina/executors/crafters/image/io.py
@@ -0,0 +1,47 @@
+__copyright__ = "Copyright (c) 2020 Jina AI Limited. All rights reserved."
+__license__ = "Apache-2.0"
+
+import io
+from typing import Dict, List
+
+import numpy as np
+
+from .. import BaseSegmenter
+
+
+class ImageReader(BaseSegmenter):
+    """
+    :class:`ImageReader` loads the image from the given file path and save the `ndarray` of the image in the Chunk.
+    """
+
+    def __init__(self, channel_axis: int = -1, from_bytes: bool = False, *args, **kwargs):
+        """
+        :class:`ImageReader` load an image file and craft into image matrix.
+
+        :param channel_axis: the axis id of the color channel, ``-1`` indicates the color channel info at the last axis
+        :param from_bytes: if set to true, then load image directly from raw_bytes, otherwise it assumes raw_bytes as file path
+        """
+        super().__init__(*args, **kwargs)
+        self.channel_axis = channel_axis
+        self.from_bytes = from_bytes
+
+    def craft(self, raw_bytes: bytes, doc_id: int, *args, **kwargs) -> List[Dict]:
+        """
+        Read the image from the given file path that specified in `raw_bytes` and save the `ndarray` of the image in
+            the `blob` of the chunk.
+
+        :param raw_bytes: the image file path in raw bytes
+        :param doc_id: the id of the Document
+
+        """
+        from PIL import Image
+        if self.from_bytes:
+            raw_img = Image.open(io.BytesIO(raw_bytes))
+        else:
+            raw_img = Image.open(raw_bytes.decode())
+        if raw_img.mode != 'RGB':
+            raw_img = raw_img.convert('RGB')
+        img = np.array(raw_img).astype('float32')
+        if self.channel_axis != -1:
+            img = np.moveaxis(img, -1, self.channel_axis)
+        return [dict(doc_id=doc_id, offset=0, weight=1., blob=img), ]
