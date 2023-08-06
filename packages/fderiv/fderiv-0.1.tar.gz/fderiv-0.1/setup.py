ad2dcaf3c20db13bd5c0dd78bb4e4de8056cc50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:51 +0000

    Add package fderiv/0.1/fderiv-0.1.tar.gz

---
+++ packages/fderiv/fderiv-0.1.tar.gz/fderiv-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fderiv',
+      version='0.1',
+      description='Five point derivatives',
+      packages=['fderiv'],
+      author='Sudhanva Lalit',
+      author_email='sudsid@gmail.com',
+      zip_safe=False)
