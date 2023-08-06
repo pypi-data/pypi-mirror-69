ed3a64a61570fc2e1c2db140441e48d5ca3e9005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:58 +0000

    Add package mysqlcp/1.1.0/mysqlcp-1.1.0.tar.gz

---
+++ packages/mysqlcp/mysqlcp-1.1.0.tar.gz/mysqlcp-1.1.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mysqlcp",
+    version="1.1.0",
+    author="AlaBouali",
+    author_email="trap.leader.123@gmail.com",
+    description="Simple, secure and advanced module to perform safe MySQL queries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlaBouali/mysqlcp",
+    python_requires=">=2.7",
+    install_requires=['pymysql'],
+    packages=["mysqlcp"],
+    license="MIT License",
+    entry_points={
+       'console_scripts': [
+           'xmysql = mysqlcp.__main__:run',
+       ],
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License ",
+    ],
+)
