60cb759a59b58ad271a6fbe7e53507d0b84a352a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:47 +0000

    Add package distributions-cardos/0.1/distributions_cardos-0.1.tar.gz

---
+++ packages/distributions-cardos/distributions_cardos-0.1.tar.gz/distributions_cardos-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_cardos',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_cardos'],
+      zip_safe=False)
