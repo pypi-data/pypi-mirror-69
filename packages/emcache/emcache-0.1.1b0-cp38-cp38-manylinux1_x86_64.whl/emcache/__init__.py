0a435761a4f820f0fba22ed5db0c50b8e772b103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:14 +0000

    Add package emcache/0.1.1b0/emcache-0.1.1b0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/emcache/emcache-0.1.1b0-cp38-cp38-manylinux1_x86_64.whl/emcache/__init__.py
@@ -0,0 +1,23 @@
+from .base_client import Client, Item
+from .client import create_client
+from .client_errors import NotStoredStorageCommandError, StorageCommandError
+from .cluster import MemcachedHostAddress
+from .default_values import (
+    DEFAULT_CONNECTION_TIMEOUT,
+    DEFAULT_MAX_CONNECTIONS,
+    DEFAULT_PURGE_UNUSED_CONNECTIONS_AFTER,
+    DEFAULT_TIMEOUT,
+)
+
+__all__ = (
+    "Client",
+    "create_client",
+    "DEFAULT_TIMEOUT",
+    "DEFAULT_CONNECTION_TIMEOUT",
+    "DEFAULT_MAX_CONNECTIONS",
+    "DEFAULT_PURGE_UNUSED_CONNECTIONS_AFTER",
+    "Item",
+    "MemcachedHostAddress",
+    "NotStoredStorageCommandError",
+    "StorageCommandError",
+)
