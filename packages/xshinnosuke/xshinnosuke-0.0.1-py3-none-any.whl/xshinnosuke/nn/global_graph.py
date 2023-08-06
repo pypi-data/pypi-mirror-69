4a19f4b6209dfe3eb78065227bfe6ce66a42cc27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:51 +0000

    Add package xshinnosuke/0.0.1/xshinnosuke-0.0.1-py3-none-any.whl

---
+++ packages/xshinnosuke/xshinnosuke-0.0.1-py3-none-any.whl/xshinnosuke/nn/global_graph.py
@@ -0,0 +1,23 @@
+from ..utils.toolkit import topological_sort
+
+
+inputs = None
+outputs = None
+graph = None
+
+
+def build_graph():
+    global graph
+    graph = topological_sort(inputs, outputs, mode='forward')
+    return graph
+
+
+def reset_graph():
+    global graph
+    global inputs
+    global outputs
+    inputs = None
+    outputs = None
+    for g in graph:
+        g.in_bounds.clear()
+        g.out_bounds.clear()
