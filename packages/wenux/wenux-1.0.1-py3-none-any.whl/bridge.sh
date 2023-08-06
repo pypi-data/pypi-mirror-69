7acc9681a15e995ab1f8b8e889de7b8d5d10b2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:51 +0000

    Add package wenux/1.0.1/wenux-1.0.1-py3-none-any.whl

---
+++ packages/wenux/wenux-1.0.1-py3-none-any.whl/bridge.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo $1
+if [[ $1 == "setup" ]]; then
+    #cleanup
+    brctl show | grep -q "test_bridge"
+
+    if [[ $? -eq 0 ]]; then
+        echo "Previous setup detected, performing cleanup"
+        ifconfig test_bridge down
+        brctl delbr test_bridge
+    fi
+
+    set -e
+
+    echo "Configuring bridge on interfaces:" $@
+
+    brctl addbr test_bridge
+    brctl addif test_bridge $2
+    brctl addif test_bridge $3
+
+    ifconfig $2 0.0.0.0
+    ifconfig $3 0.0.0.0
+
+    ifconfig test_bridge up
+    ifconfig test_bridge 192.168.100.1 netmask 255.255.255.0 up
+
+    echo "Finished configuring test_bridge"
+elif [[ $1 == "teardown" ]]; then
+    set -e
+
+    echo "Tearing down the bridge used for testing"
+    ifconfig test_bridge down
+    brctl delbr test_bridge
+else
+    echo "Incorrect parameters"
+fi
