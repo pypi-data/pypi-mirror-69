cfa00b8072899560ee822551ba97085d8cd69658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:53 +0000

    Add package mdict-py/0.1a0/mdict-py-0.1a0.tar.gz

---
+++ packages/mdict-py/mdict-py-0.1a0.tar.gz/mdict-py-0.1a0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='mdict-py',
+   version='0.1a0',
+   description='Python API for mdict IO (MDX and MDD).',
+   author='oxdc',
+   author_email='projaias@outlook.com',
+   url='https://github.com/oxdc/mdict.py',
+   packages=[
+      'mdict',
+      'mdict.core'
+   ],
+   install_requires=[]
+)
