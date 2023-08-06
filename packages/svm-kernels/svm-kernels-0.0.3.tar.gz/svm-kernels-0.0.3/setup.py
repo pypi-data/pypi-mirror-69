d893696813114fd3809ddbe1286fb32426e852d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:56 +0000

    Add package svm-kernels/0.0.3/svm-kernels-0.0.3.tar.gz

---
+++ packages/svm-kernels/svm-kernels-0.0.3.tar.gz/svm-kernels-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="svm-kernels", # Replace with your own username
+    version="0.0.3",
+    author="Alberto Benayas",
+    author_email="benayas1@gmail.com",
+    description="Orthogonal Polynomial Kernels for SVM",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/benayas1/svm-kernels",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'numpy',
+          'scikit-learn',
+      ],
+    python_requires='>=3.6',
+)
