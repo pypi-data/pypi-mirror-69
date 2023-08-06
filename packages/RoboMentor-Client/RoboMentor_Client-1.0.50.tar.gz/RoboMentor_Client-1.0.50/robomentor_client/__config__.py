df1a281ca238a1be81a4ce85864975df4de815ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:42 +0000

    Add package RoboMentor-Client/1.0.50/RoboMentor_Client-1.0.50.tar.gz

---
+++ packages/RoboMentor-Client/RoboMentor_Client-1.0.50.tar.gz/RoboMentor_Client-1.0.50/robomentor_client/__config__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+RoboMentor_Client: Python library and framework for RoboMentor_Client.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2020 by RoboMentor.
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "1.0.50"
+__apiUrl__ = "https://api.robomentor.cn"
+__messageService__ = "47.111.96.253"
