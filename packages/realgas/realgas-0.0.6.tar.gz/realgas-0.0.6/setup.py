7dd4e6d99686de17ee0e7901d7bb54b5b452e51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:57 +0000

    Add package realgas/0.0.6/realgas-0.0.6.tar.gz

---
+++ packages/realgas/realgas-0.0.6.tar.gz/realgas-0.0.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="realgas",
+    version="0.0.6",
+    author="Robert F. De Jaco",
+    author_email="dejac001@umn.edu",
+    description="Gas-Phase Thermodynamics in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dejac001/GasThermo",
+    packages=['realgas'],
+    package_data={'realgas': [
+        'cp_ig.csv', 'critical_constants.csv'
+    ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["matplotlib==3.2.1", "numpy==1.18.4", "scipy==1.4.1", 'chem-util==0.0.4']
+)
