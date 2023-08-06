29ad54bf72314343bed08bfdbcf41cd96b4f636e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:45 +0000

    Add package mmxtest/1.0.3/mmxtest-1.0.3.tar.gz

---
+++ packages/mmxtest/mmxtest-1.0.3.tar.gz/mmxtest-1.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+ 
+setup(name='mmxtest',
+  version='1.0.3',
+  description='A print test for PyPI',
+  author='mmx',
+  author_email='mingxing157157@163.com',
+  url='https://www.python.org/',
+  license='MIT',
+  keywords='ga nn',
+  project_urls={
+   'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+   'Funding': 'https://donate.pypi.org',
+   'Source': 'https://github.com/pypa/sampleproject/',
+   'Tracker': 'https://github.com/pypa/sampleproject/issues',
+  },
+  )
