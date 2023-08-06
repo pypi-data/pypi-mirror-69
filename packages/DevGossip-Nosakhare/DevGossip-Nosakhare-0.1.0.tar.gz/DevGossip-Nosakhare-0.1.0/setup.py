59a6bd84a0b369583e896dd2b134a56c88999639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:48 +0000

    Add package DevGossip-Nosakhare/0.1.0/DevGossip-Nosakhare-0.1.0.tar.gz

---
+++ packages/DevGossip-Nosakhare/DevGossip-Nosakhare-0.1.0.tar.gz/DevGossip-Nosakhare-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as description:
+    long_description = description.read()
+
+setuptools.setup(
+    name="DevGossip-Nosakhare", # Replace with your own username
+    version="0.1.0",
+    author="Nosakhare Edokpayi",
+    author_email="co.nosakhare@gmail.com",
+    description="A package of a light weight chat application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nosa-khare/CLI-DevGossip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
