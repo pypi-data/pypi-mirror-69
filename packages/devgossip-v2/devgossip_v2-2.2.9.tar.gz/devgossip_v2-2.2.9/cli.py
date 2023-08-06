1174d0592f4b42ead62841d33d9c9ecbd68f2d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:22 +0000

    Add package devgossip-v2/2.2.9/devgossip_v2-2.2.9.tar.gz

---
+++ packages/devgossip-v2/devgossip_v2-2.2.9.tar.gz/devgossip_v2-2.2.9/cli.py
@@ -0,0 +1,18 @@
+from termcolor import colored
+from pusher import Pusher
+import pysher
+from dotenv import load_dotenv
+import os
+import json
+import random
+import string
+import re
+
+
+from utils import welcome
+
+load_dotenv(dotenv_path='.env')
+
+                
+if __name__ == "__main__":
+    welcome().main()
