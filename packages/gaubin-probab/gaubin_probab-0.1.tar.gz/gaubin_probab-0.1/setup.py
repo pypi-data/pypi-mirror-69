fe42c6eee96c197dc9217ee80da93657ffe69096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:19 +0000

    Add package gaubin-probab/0.1/gaubin_probab-0.1.tar.gz

---
+++ packages/gaubin-probab/gaubin_probab-0.1.tar.gz/gaubin_probab-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaubin_probab',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaubin_probab'],
+      author='Ashish Kumar',
+      author_email = 'luck.ashish@gmail.com',
+      zip_safe=False)
