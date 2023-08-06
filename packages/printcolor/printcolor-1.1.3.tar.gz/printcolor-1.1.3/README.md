c2c19fb1d7e78f74890c7c5093398a4ad594b301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:14 +0000

    Add package printcolor/1.1.3/printcolor-1.1.3.tar.gz

---
+++ packages/printcolor/printcolor-1.1.3.tar.gz/printcolor-1.1.3/README.md
@@ -0,0 +1,46 @@
+##### printcolor
+
+- Usage
+```python
+#pytho3.x
+import printcolor
+pc = printcolor.PrintColor()
+pc.color(string,show="flash",front="green",back="yellow")
+#pytho2.x
+import printcolor
+pc = printcolor.PrintColor()
+pc.color("lash","green","yellow",string)
+```
+- color
+```text
+显示样式:
+    hlt :  高亮
+    underline : 下划线
+    flash : 闪烁
+字体/背景颜色：
+    black : 黑色
+    red:    红色
+    green:  绿色
+    yellow: 黄色
+    blue:   蓝色
+    carmine:胭脂红
+    cyan:   青色
+    white:  白色
+```
+```
+- color
+```text
+显示样式:
+    hlt :  高亮
+    underline : 下划线
+    flash : 闪烁
+字体/背景颜色：
+    black : 黑色
+    red:    红色
+    green:  绿色
+    yellow: 黄色
+    blue:   蓝色
+    carmine:胭脂红
+    cyan:   青色
+    white:  白色
+```
---
+++ packages/printcolor/printcolor-1.1.3.tar.gz/printcolor-1.1.3/printcolor2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+class PrintColor:
+    def __init__(self):
+        self.__show_type = {"default":0, "hlt":1, "underline":4, "flash": 5}
+        self.__front_type = {"black":30, "red": 31, "green":32, "yellow":33, "blue":34, "carmine":35, "cyan":36, "white":37}
+        self.__back_type = {"black":40, "red": 41, "green":42, "yellow":43, "blue":44, "carmine":45, "cyan":46, "white":47}
+    def color(self,show, front, back, *args):
+        if back == '':
+            try:
+                show = self.__show_type[show]
+                front = self.__front_type[front]
+                print ("\033[%d;%dm%s \033[0m" % (show,front,' '.join(args)))
+            except KeyError as e:
+                print("Please confirm your prameters")
+        else:
+            try:
+                show = self.__show_type[show]
+                front = self.__front_type[front]
+                back = self.__back_type[back]
+                print ("\033[%d;%d;%dm%s \033[0m" % (show,front,back,' '.join(args)))
+            except KeyError as e:
+                print("Please confirm your prameters")
