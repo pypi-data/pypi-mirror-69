d7cc079b04e96a910ec63149041b8f18089b6501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:03 +0000

    Add package bnd-package/0.1/bnd_package-0.1.tar.gz

---
+++ packages/bnd-package/bnd_package-0.1.tar.gz/bnd_package-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='bnd_package',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['bnd_package'],
+      author='Gabriel Chiong',
+      author_email='my_name109@hotmail.com',
+    zip_safe=False)
