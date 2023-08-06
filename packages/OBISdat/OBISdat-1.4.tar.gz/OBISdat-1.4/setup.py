f3fbd214f02f29993941a5a20bd660e1b36287ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:22 +0000

    Add package OBISdat/1.4/OBISdat-1.4.tar.gz

---
+++ packages/OBISdat/OBISdat-1.4.tar.gz/OBISdat-1.4/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import setuptools
+from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+setup(name="OBISdat",
+      version='1.4',
+      long_description = readme,
+      long_description_content_type='text/markdown',
+      author='Ulises Rosas',
+      author_email='ulisesfrosasp@gmail.com',
+      url='https://github.com/Ulises-Rosas/OBISdat',
+      packages = ['OBISdat'],
+      package_dir = {'OBISdat': 'src'},
+      entry_points = {
+           'console_scripts': [
+                 'obis = OBISdat.obis:main'
+            ]
+      },
+      classifiers=[
+             'Programming Language :: Python :: 3',
+             'License :: OSI Approved :: MIT License'
+             ]
+      )
