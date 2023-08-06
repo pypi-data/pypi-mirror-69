f4d81b83f3ef9921762191d3332f49a0a4cf297b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:57 +0000

    Add package prob-function88ruisanchez/0.2/prob_function88ruisanchez-0.2.tar.gz

---
+++ packages/prob-function88ruisanchez/prob_function88ruisanchez-0.2.tar.gz/prob_function88ruisanchez-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_function88ruisanchez',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['prob_function88ruisanchez'],
+      zip_safe=False)
