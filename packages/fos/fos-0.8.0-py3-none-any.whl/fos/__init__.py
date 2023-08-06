4c6c65745c6f6099f7761d8a52762d6499db8fcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:29 +0000

    Add package fos/0.8.0/fos-0.8.0-py3-none-any.whl

---
+++ packages/fos/fos-0.8.0-py3-none-any.whl/fos/__init__.py
@@ -0,0 +1,8 @@
+'''
+Fos is a deeplearning framework that is a thin wrapper around Pytorch and reduces
+the amount of boilerplate code required when training a model. It also comes
+with additional features that provide insight into the performance of your model.
+'''
+from .core import *
+
+__version__ = '0.8.0'
