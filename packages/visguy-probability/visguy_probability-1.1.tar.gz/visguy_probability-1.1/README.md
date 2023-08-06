72c743fe048cb44ee5caf8754f8c05508e2130b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:58 +0000

    Add package visguy-probability/1.1/visguy_probability-1.1.tar.gz

---
+++ packages/visguy-probability/visguy_probability-1.1.tar.gz/visguy_probability-1.1/README.md
@@ -0,0 +1,3 @@
+# visguy-probability package
+
+This is a project generated within my time spend for the 'AWS Machine Learning Foundations Course' from Udacity
---
+++ packages/visguy-probability/visguy_probability-1.1.tar.gz/visguy_probability-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='visguy_probability',
+      version='1.1',
+      description='Gaussian distributions by visguy',
+      packages=['visguy_probability'],
+      author='Amir Smajevic',
+      author_email='visocicaguy@gmail.com',
+      zip_safe=False)
