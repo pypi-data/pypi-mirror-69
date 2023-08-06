dffbb4644bc8e828f843fa684dd7debfe6557f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:20 +0000

    Add package dnd-probability/1.2/dnd_probability-1.2.tar.gz

---
+++ packages/dnd-probability/dnd_probability-1.2.tar.gz/dnd_probability-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dnd_probability',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['dnd_probability'],
+      author = "Jeremiah Kamama",
+      zip_safe=False)
