bb427551f7f1b69a924180240e52f0c74fe39634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:24 +0000

    Add package geoip2-data/1.0.0/geoip2_data-1.0.0.tar.gz

---
+++ packages/geoip2-data/geoip2_data-1.0.0.tar.gz/geoip2_data-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='geoip2_data',
+    version='1.0.0',
+    description=('Show how to structure a Python project.'),
+    author='SoundOn',
+    author_email='dev@soundon.fm',
+    url='https://github.com/SoundOn/geoip2-data',
+    license='MIT',
+    packages=[],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'],
+    )
