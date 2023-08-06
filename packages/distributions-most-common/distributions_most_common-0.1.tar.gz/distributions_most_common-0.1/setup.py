9c617f9c5e6291dad4a3e1d7df35d24ade15f5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:17 +0000

    Add package distributions-most-common/0.1/distributions_most_common-0.1.tar.gz

---
+++ packages/distributions-most-common/distributions_most_common-0.1.tar.gz/distributions_most_common-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_most_common',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_most_common'],
+      zip_safe=False)
