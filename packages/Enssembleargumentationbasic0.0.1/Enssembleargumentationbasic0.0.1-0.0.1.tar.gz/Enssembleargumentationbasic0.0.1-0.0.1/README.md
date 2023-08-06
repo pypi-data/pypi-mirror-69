1b6512bce2081d51ca90e46c2574335db745b643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:21 +0000

    Add package Enssembleargumentationbasic0.0.1/0.0.1/Enssembleargumentationbasic0.0.1-0.0.1.tar.gz

---
+++ packages/Enssembleargumentationbasic0.0.1/Enssembleargumentationbasic0.0.1-0.0.1.tar.gz/Enssembleargumentationbasic0.0.1-0.0.1/README.md
@@ -0,0 +1,14 @@
+<h1>BasicEnsembleArgumentation</h1>
+
+<p>The BasicEnsembleArgumentation library provides methods to generate reliable results. A data set is classified by several models (classifiers). Different models produce different outputs and the produced outputs are combined by an argumentation approach. The argumentation approach produces a final output through a decision making process. The whole process follows an ensemble architecture. </p>  
+<h1>User Manual</h1>
+
+<p>Install Basicenssembleargumentation: </p>
+
+<code>pip install Basicenssembleargumentation</code>
+
+<p>Import the library: </p>
+
+<code>from EnsArg import argumentation</code>
+
+<p>Import necessary pacakages: </p>
---
+++ packages/Enssembleargumentationbasic0.0.1/Enssembleargumentationbasic0.0.1-0.0.1.tar.gz/Enssembleargumentationbasic0.0.1-0.0.1/setup.py
@@ -0,0 +1,45 @@
+import sys
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+if sys.version_info < (3, 8):
+    print('EnsArg requires at least Python 3.5 to run reliably.')
+
+install_requires = [
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'sklearn'
+    
+]
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='Enssembleargumentationbasic0.0.1',
+    version='0.0.1',
+    description='A basic argumentation process where the outcomes from different models can argue for generating a final output.',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Istiak Ahmed',
+    author_email='istiak.uiu.bd@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keyword='ensembleargumentation',
+     project_urls={
+        "Code": "https://github.com/Istiak1992/BasicEnsembleArgumentation/",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8"
+   
+        
+
+)
