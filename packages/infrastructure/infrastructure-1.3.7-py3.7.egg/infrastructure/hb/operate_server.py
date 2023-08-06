0cb098d2cadb68774b5ffc0c425047263326ab77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:01 +0000

    Add package infrastructure/1.3.7/infrastructure-1.3.7-py3.7.egg

---
+++ packages/infrastructure/infrastructure-1.3.7-py3.7.egg/infrastructure/hb/operate_server.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# @Author: yongfanmao
+# @Date:   2020-05-25 17:10:22
+# @E-mail: maoyongfan@163.com
+# @Last Modified by:   yongfanmao
+# @Last Modified time: 2020-05-27 11:13:18
+import time
+
+def checkServerStatus(self,remoteServer,service_name,status,coverageLog):
+	command = "cd /workspace/carkey/{service_name}/latest;./init.script status".format(service_name=service_name)
+	result = remoteServer.exec_command(command=command)
+	time.sleep(3)
+	print(result)
+	if status in result[0]:
+		record = coverageLog(data=
+				{
+					"operationType": "检查服务器状态是否符合预期",
+					"message": str(result),
+					"remark": status,
+					"typeInfo": "覆盖率桥接",
+					"status":1
+				})					
+		record.is_valid(raise_exception=True)
+		record.save()
+		return True
+	else:
+		return False
+class OperateServer(object):
+	def __init__(self,remoteServer):
+		"""
+			操作远程服务器
+		"""
+		self.remoteServer = remoteServer
+
+	def restartServer(self,service_name,coverageLog):
+		stopCommandList = ["sudo su - deploy\n",
+		"sudo su\n",
+		"cd /workspace/carkey/{service_name}/latest\n".format(service_name=service_name),
+		"sudo ./init.script stop\n"]
+		# while self.checkServerStatus(service_name,'stopped',coverageLog):
+		stopResult = self.remoteServer.exec_command(command=restartCommandList,
+			func=checkServerStatus,remoteServer=self.remoteServer,
+			service_name=service_name,status="stopped",coverageLog=coverageLog)
+		record = coverageLog(data=
+				{
+					"operationType": "统计静态代码停止服务器结果",
+					"message": stopResult,
+					"typeInfo": "覆盖率桥接",
+					"status":1
+				})					
+		record.is_valid(raise_exception=True)
+		record.save()
+
+		
+		time.sleep(5)
+		startCommandList = ["sudo su - deploy\n",
+		"sudo su\n",
+		"cd /workspace/carkey/{service_name}/latest\n".format(service_name=service_name),
+		"sudo ./init.script start\n"]
+		#while self.checkServerStatus(service_name,'running',coverageLog):
+		restartResult = self.remoteServer.exec_command(command=startCommandList,
+			func=checkServerStatus,remoteServer=self.remoteServer,
+		service_name=service_name,status="running",coverageLog=coverageLog)		
+		return restartResult
+
+	def addJacocoArg(self,service_name,port):
+		commandList = ["sudo su - deploy\n",
+		"cd /workspace/carkey/{service_name}/latest\n".format(service_name=service_name),
+		"""sudo sed -i -e '1i START_OPTS="$START_OPTS -javaagent:/usr/local/jacoco/jacocoagent.jar=includes=*,output=tcpserver,address=*,port={port},append=true"' init.script\n""".format(
+			port=port)]
+		result = self.remoteServer.exec_command(command=commandList)
+		return result
+
+	def __del__(self):
+		self.remoteServer.close()
+
+
