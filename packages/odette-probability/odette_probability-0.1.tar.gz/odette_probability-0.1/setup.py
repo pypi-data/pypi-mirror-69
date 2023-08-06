aa833f7f3cadab74bffa2976c356529e5b6a2ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:41 +0000

    Add package odette-probability/0.1/odette_probability-0.1.tar.gz

---
+++ packages/odette-probability/odette_probability-0.1.tar.gz/odette_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='odette_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['odette_probability'],
+      author='Felix Odete',
+      author_email='felix.odette@gmail.com',
+      zip_safe=False)
