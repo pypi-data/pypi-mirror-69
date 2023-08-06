7ac9339a7ec98302aee38f19473a9c9796f2731b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:50 +0000

    Add package Silence/0.2.0/Silence-0.2.0-py3-none-any.whl

---
+++ packages/Silence/Silence-0.2.0-py3-none-any.whl/silence/settings/default_settings.py
@@ -0,0 +1,25 @@
+###############################################################################
+# Default settings, which can be overriden by each individual project.
+###############################################################################
+
+DEBUG_ENABLED = False
+HTTP_PORT = 8080
+API_PREFIX = ""
+
+RUN_API = True
+RUN_WEB = True
+
+DB_CONN = {
+    "host": "localhost",
+    "port": 3306,
+    "username": "default_username",
+    "password": "default_password",
+    "database": "default_database",
+}
+
+SECRET_KEY = "These are generated automatically for each project."
+
+SQL_SCRIPTS = []
+
+PROJECT_TEMPLATE_NAME = "silence-app"
+PROJECT_TEMPLATE_REPO = "https://github.com/agu-borrego/Silence-project-template.git"
