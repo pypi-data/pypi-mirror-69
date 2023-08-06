ad9659386d0221dac789565857b8eca198d73022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:48 +0000

    Add package pyzabbix/0.8.2/pyzabbix-0.8.2.tar.gz

---
+++ packages/pyzabbix/pyzabbix-0.8.2.tar.gz/pyzabbix-0.8.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+with open("README.markdown", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyzabbix",
+    version="0.8.2",
+    install_requires=[
+        "requests>=1.0",
+    ],
+    description="Zabbix API Python interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Luke Cyca",
+    author_email="me@lukecyca.com",
+    license="LGPL",
+    keywords="zabbix monitoring api",
+    url="http://github.com/lukecyca/pyzabbix",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+	"Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+	"Programming Language :: Python :: 3.6",
+	"Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Natural Language :: English",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Networking :: Monitoring",
+        "Topic :: System :: Systems Administration",
+    ],
+    packages=["pyzabbix"],
+    tests_require=[
+        "httpretty<0.8.7",
+    ],
+)
