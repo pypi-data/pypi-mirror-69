54b17f9c3cb89d69d51623a0b093743b8f8fd61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:46 +0000

    Add package slixmpp/1.5.2/slixmpp-1.5.2.tar.gz

---
+++ packages/slixmpp/slixmpp-1.5.2.tar.gz/slixmpp-1.5.2/tests/test_overall.py
@@ -0,0 +1,26 @@
+import os
+import re
+import sys
+import unittest
+import tabnanny
+import compileall
+
+class TestOverall(unittest.TestCase):
+
+    """
+    Test overall package health by compiling and checking
+    code style.
+    """
+
+    def testModules(self):
+        """Testing all modules by compiling them"""
+        src = '.%sslixmpp' % os.sep
+        rx = re.compile('/[.]svn|.*26.*')
+        self.assertTrue(compileall.compile_dir(src, rx=rx, quiet=True))
+
+    def testTabNanny(self):
+        """Testing that indentation is consistent"""
+        self.assertFalse(tabnanny.check('..%sslixmpp' % os.sep))
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestOverall)
