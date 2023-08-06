5b7725f36985b6bfa2e68e71d7a597626a00d898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:46 +0000

    Add package distributions-gaussian-binomial-nd/0.1/distributions_gaussian_binomial_nd-0.1.tar.gz

---
+++ packages/distributions-gaussian-binomial-nd/distributions_gaussian_binomial_nd-0.1.tar.gz/distributions_gaussian_binomial_nd-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_gaussian_binomial_nd',
+      version='0.1',
+      description='Gaussian and Bionomial distributions',
+      packages=['distributions_gaussian_binomial_nd'],
+      author = 'Nam Duong',
+      author_email = 'khanhnamdn91@gmail.com',
+      zip_safe=False)
