331b50dcc7f965b0a8b4cad3eb0244b49aa6d3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:09 +0000

    Add package ald-distributions/0.1/ald_distributions-0.1.tar.gz

---
+++ packages/ald-distributions/ald_distributions-0.1.tar.gz/ald_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ald_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ald_distributions'],
+      zip_safe=False)
