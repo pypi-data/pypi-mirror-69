b14fb41608c317936373a2ad2b44731cd5823566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:44 +0000

    Add package manganelo/1.4.4/manganelo-1.4.4.tar.gz

---
+++ packages/manganelo/manganelo-1.4.4.tar.gz/manganelo-1.4.4/manganelo/api/__init__.py
@@ -0,0 +1,10 @@
+
+from .searchmanga import SearchManga
+from .mangainfo import MangaInfo
+from .downloadchapter import DownloadChapter
+
+__ALL__ = (
+    "SearchManga",
+    "MangaInfo",
+    "DownloadChapter"
+)
