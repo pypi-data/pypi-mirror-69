26d9f1cf5e4e2d5d03e8ab1f8a0c12d2374618ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:17 +0000

    Add package distributions-dataset/1.1/distributions_dataset-1.1.tar.gz

---
+++ packages/distributions-dataset/distributions_dataset-1.1.tar.gz/distributions_dataset-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_dataset',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['distributions_dataset'],
+      author = 'pratik_sheth',
+      author_email = 'psheth04@nyit.edu',
+      zip_safe=False)
