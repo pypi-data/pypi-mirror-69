0e0124e78de5efa4c4ff836b6b7a66b1631f33dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:05 +0000

    Add package PyCronExpression/0.0.5/PyCronExpression-0.0.5-py3-none-any.whl

---
+++ packages/PyCronExpression/PyCronExpression-0.0.5-py3-none-any.whl/py_cron_expression/validations.py
@@ -0,0 +1,45 @@
+"""
+A simple data validation
+@Since: 02 May, 2020
+"""
+from datetime import datetime
+import pytz
+from tzlocal import get_localzone
+
+GET_TIMEZONE = get_localzone()
+
+
+def date_validation(func):
+    """
+    :param func:
+    :return:
+    """
+
+    def wrapper(*args, **kwargs):
+        """
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        try:
+            time = kwargs['time']
+            timezone = kwargs.get('timezone', GET_TIMEZONE.zone)
+            cancel = kwargs.get('cancel', None)
+            if isinstance(time, int):
+                string = datetime.fromtimestamp(time, tz=pytz.timezone(timezone)).strftime('%Y-%m-%dT%H:%M:%S')
+                if cancel != 'second':
+                    kwargs['cancel'] = False
+                kwargs['time'] = string
+                return func(*args, **kwargs)
+            elif isinstance(time, datetime):
+                string = (time.astimezone(tz=pytz.timezone(timezone))).strftime('%Y-%m-%dT%H:%M:%S')
+                if cancel != "second":
+                    kwargs['cancel'] = False
+                kwargs['time'] = string
+                return func(*args, **kwargs)
+            else:
+                raise ValueError(f"Invalid data type {time}")
+        except KeyError as key:
+            raise KeyError(f"Key name should {key}")
+
+    return wrapper
