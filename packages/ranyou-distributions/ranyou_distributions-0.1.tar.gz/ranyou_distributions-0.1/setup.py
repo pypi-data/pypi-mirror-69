5239aa62d8f8f2c4530b67a84a8f2c1c585811f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:08 +0000

    Add package ranyou-distributions/0.1/ranyou_distributions-0.1.tar.gz

---
+++ packages/ranyou-distributions/ranyou_distributions-0.1.tar.gz/ranyou_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ranyou_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ranyou_distributions'],
+      author = 'Ran You', 
+      author_email = 'iamranyou@gmail.com', 
+      zip_safe=False)
