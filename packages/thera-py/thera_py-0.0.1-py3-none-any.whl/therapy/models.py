0844e2ff795ca15329fd8d034fc0691cf1262c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:42 +0000

    Add package thera-py/0.0.1/thera_py-0.0.1-py3-none-any.whl

---
+++ packages/thera-py/thera_py-0.0.1-py3-none-any.whl/therapy/models.py
@@ -0,0 +1,35 @@
+"""data models for representing VICC normalized therapy records."""
+from typing import List
+from pydantic import BaseModel
+
+
+class Therapy(BaseModel):
+    """A procedure or substance used in the treatment of a disease."""
+
+    label: str
+    concept_identifier: str
+    aliases: List[str]
+    other_identifiers: List[str]
+
+
+# class Attribute(BaseModel):
+#     """Wikidata statement describing an item."""
+#
+#     property: str
+#     property_identifier: str
+#     value: str
+#     value_identifier: str
+
+
+class Drug(Therapy):
+    """A pharmacologic substance used to treat a medical condition."""
+
+    antineoplastic: bool = False
+
+
+class DrugGroup(Therapy):
+    """A grouping of drugs based on shared common attributes."""
+
+    description: str
+    type_identifier: str
+    drugs: List[Drug]
