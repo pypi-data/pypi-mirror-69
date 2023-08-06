ee78fadec45acad31737b834c71f65e40f4ab36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:41 +0000

    Add package django-flatpages-multilanguage/0.1.0/django-flatpages-multilanguage-0.1.0.tar.gz

---
+++ packages/django-flatpages-multilanguage/django-flatpages-multilanguage-0.1.0.tar.gz/django-flatpages-multilanguage-0.1.0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(
+    name='django-flatpages-multilanguage',
+    version='0.1.0',
+    description='Translatable flatpages',
+    long_description=open('README.rst').read(),
+    author='Pragmatic Mates',
+    author_email='info@pragmaticmates.com',
+    maintainer='Pragmatic Mates',
+    maintainer_email='info@pragmaticmates.com',
+    url='https://github.com/PragmaticMates/django-flatpages-i18n',
+    packages=[
+        'flatpages_i18n',
+        'flatpages_i18n.templatetags',
+        'flatpages_i18n.migrations'
+    ],
+    include_package_data=True,
+    install_requires=('future', 'django_modeltranslation', 'django', 'django_mptt'),
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License'],
+    license='BSD License',
+    keywords = "django flatpages translation i18n wysiwyg",
+)
