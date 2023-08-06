07df7e5a21941de416c5732861296feab2b16df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:02 +0000

    Add package biochemia.analysis/0.0.0/biochemia.analysis-0.0.0.tar.gz

---
+++ packages/biochemia.analysis/biochemia.analysis-0.0.0.tar.gz/biochemia.analysis-0.0.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="biochemia.analysis",
+    author='Genadij Razdorov',
+    author_email='genadijrazdorov@gmail.com',
+    url='https://biochemia.bitbucket.io/'
+)
