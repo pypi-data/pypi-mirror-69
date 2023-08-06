8f2b185dee529d21a6638ecc987b0e639ee3842e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:19 +0000

    Add package hashable-df/0.0.4/hashable_df-0.0.4-py2.py3-none-any.whl

---
+++ packages/hashable-df/hashable_df-0.0.4-py2.py3-none-any.whl/hashable_df/__init__.py
@@ -0,0 +1,12 @@
+import collections
+import numpy as np
+from autohash import AutoHash
+
+
+def hashable_df(df):
+    return df.apply(np.vectorize(
+                    lambda value: value if isinstance(value, collections.abc.Hashable)
+                                        else AutoHash(value)))
+
+
+__version__ = '0.0.4'
