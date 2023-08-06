ab45674a6463033068d0a01f80d50c44293dd607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:36 +0000

    Add package veazy/0.0.1/veazy-0.0.1.tar.gz

---
+++ packages/veazy/veazy-0.0.1.tar.gz/veazy-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='veazy',
+	version='0.0.1',
+    author='Xomnia14',
+	author_email='jan.scholten@xomnia.com',
+	description='Easy way to visualise your python package',
+	log_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://gitlab.com/janscholten/veazy',
+	packages=setuptools.find_packages(),
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    tests_require=['pytest'],
+)
+
