66c8a0faa28dccf0f7fc8d6518378c1ee078d803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:52 +0000

    Add package rapo/0.2.5/rapo-0.2.5.tar.gz

---
+++ packages/rapo/rapo-0.2.5.tar.gz/rapo-0.2.5/rapo/__init__.py
@@ -0,0 +1,17 @@
+"""Python Revenue Assurance Process Optimizer."""
+
+from .control import Control
+from .scheduler import Scheduler
+
+
+__author__ = 'Timur Faradzhov'
+__copyright__ = 'Copyright 2020, The RAPO project'
+__credits__ = ['Timur Faradzhov']
+
+__license__ = 'MIT'
+__version__ = '0.2.5'
+__maintainer__ = 'Timur Faradzhov'
+__email__ = 'timurfaradzhov@gmail.com'
+__status__ = 'Development'
+
+__all__ = [Control, Scheduler]
