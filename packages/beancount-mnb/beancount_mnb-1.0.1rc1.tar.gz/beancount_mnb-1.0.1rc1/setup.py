2984411d6ca902e63c45654ad8e0ff5a71786ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:51 +0000

    Add package beancount-mnb/1.0.1rc1/beancount_mnb-1.0.1rc1.tar.gz

---
+++ packages/beancount-mnb/beancount_mnb-1.0.1rc1.tar.gz/beancount_mnb-1.0.1rc1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="beancount_mnb",
+    version="1.0.1-rc1",
+    description="Az MNB hivatalos középárfolyamainak letöltését megvalósító beancount plugin.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="beancount importer mnb currency",
+    url="https://github.com/belidzs/beancount_mnb",
+    author="Balázs Keresztury",
+    license="MIT",
+    packages=["beancount_mnb"],
+    install_requires=[
+        "beancount>=2.2.1",
+        "zeep>=3.2.0",
+        "pytz>=2018.9",
+        "lxml>=4.3.2"
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Plugins",
+        "Natural Language :: Hungarian",
+        "Topic :: Office/Business :: Financial :: Accounting"
+    ],
+    zip_safe=False)
