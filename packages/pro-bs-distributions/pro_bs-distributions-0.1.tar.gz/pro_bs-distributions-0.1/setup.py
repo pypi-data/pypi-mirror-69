7e2692840f1bcd2aadc30a8f7b9a56d6fa879201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:49 +0000

    Add package pro-bs-distributions/0.1/pro_bs-distributions-0.1.tar.gz

---
+++ packages/pro-bs-distributions/pro_bs-distributions-0.1.tar.gz/pro_bs-distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup (name='pro_bs-distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['pro_bs-distributions'],
+      author = 'Rupesh Somana',
+      author_email = 'somanarupesh32@gmail.com',
+      zip_safe=False)
