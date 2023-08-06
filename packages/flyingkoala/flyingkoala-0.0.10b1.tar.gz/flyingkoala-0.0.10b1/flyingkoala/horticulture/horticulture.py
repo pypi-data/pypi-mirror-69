59f599c5c7b058b8cdba0821f48eb5110bd2af8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:21 +0000

    Add package flyingkoala/0.0.10b1/flyingkoala-0.0.10b1.tar.gz

---
+++ packages/flyingkoala/flyingkoala-0.0.10b1.tar.gz/flyingkoala-0.0.10b1/flyingkoala/horticulture/horticulture.py
@@ -0,0 +1,107 @@
+import xlwings as xw
+import numpy as np
+import pandas as pd
+
+from flyingkoala import flyingkoala as fk
+
+
+@xw.func
+@xw.arg('model_name', doc='Name, as a string, of the model which will be evaluated. The Excel cell name / named range')
+@xw.arg('T_min', np.array, doc='Daily minimum temperature')
+@xw.arg('T_max', np.array, doc='Daily maximum temperature')
+@xw.ret(index=False, header=False)
+def DegreeDayByName(model_name, T_min, T_max):
+    """Function to assemble a dataframe for calculating Degree Day.
+
+    :param model: Text name (string) of the model which will be evaluated. The Excel cell name / named range
+    :param T_min: Daily minimum temperature
+    :param T_max: Daily maximum temperature
+    """
+
+    if model_name not in fk.koala_models.keys():
+        model = None
+        wb = xw.books.active
+        for name in wb.names:
+            if model_name == name.name:
+                model = xw.Range(model_name)
+                fk.generate_model_graph(model)
+
+        if model is None:
+            return 'Model "%s" has not been loaded into cache, if named range exists check spelling.' % model_name
+
+    inputs_for_DegreeDay = pd.DataFrame({'T_min': np.array([T_min]), 'T_max': np.array([T_max])})
+    return fk.evaluate_koala_model(model_name, inputs_for_DegreeDay)
+
+
+@xw.func
+@xw.arg('model_name', doc='Name, as a string, of the model which will be evaluated. The Excel cell name / named range')
+@xw.arg('T_min', np.array, doc='Daily minimum temperature')
+@xw.arg('T_max', np.array, doc='Daily maximum temperature')
+@xw.ret(index=False, header=False, expand='down')
+def DegreeDayDynamicArrayByName(model_name, T_min, T_max):
+    """Function to assemble a dataframe for calculating Degree Day using dynamic arrays.
+
+    :param model: Text name (string) of the model which will be evaluated. The Excel cell name / named range
+    :param T_min: Daily minimum temperature
+    :param T_max: Daily maximum temperature
+    """
+
+    if model_name not in fk.koala_models.keys():
+        model = None
+        wb = xw.books.active
+        for name in wb.names:
+            if model_name == name.name:
+                model = xw.Range(model_name)
+                fk.generate_model_graph(model)
+
+        if model is None:
+            return 'Model "%s" has not been loaded into cache, if named range exists check spelling.' % model_name
+
+    inputs_for_DegreeDay = pd.DataFrame({'T_min': T_min, 'T_max': T_max})
+    return fk.evaluate_koala_model(model_name, inputs_for_DegreeDay)
+
+
+@xw.func
+@xw.arg('model_name', xw.Range, doc='Name, as a string or named range, of the model which will be evaluated.')
+@xw.arg('T_min', np.array, doc='Daily minimum temperature')
+@xw.arg('T_max', np.array, doc='Daily maximum temperature')
+@xw.ret(index=False, header=False)
+def DegreeDay(model_name, T_min, T_max):
+    """Function to assemble a dataframe for calculating Degree Day. Takes either a name of a model or the named range itself.
+
+    :param model: Named Range of the model which will be evaluated. The Excel cell name / named range
+    :param T_min: Daily minimum temperature
+    :param T_max: Daily maximum temperature
+    """
+    try:
+        prepared_model_name = fk.load_model(model_name)
+
+        # format the equation terms, evalueate the equation and return the result.
+        inputs_for_DegreeDay = pd.DataFrame({'T_min': np.array([T_min]), 'T_max': np.array([T_max])})
+        return fk.evaluate_koala_model(prepared_model_name, inputs_for_DegreeDay)
+
+    except Exception as err:
+        return str(err)
+
+
+@xw.func
+@xw.arg('model_name', xw.Range, doc='Name, as a string or named range, of the model which will be evaluated.')
+@xw.arg('T_min', np.array, doc='Daily minimum temperature')
+@xw.arg('T_max', np.array, doc='Daily maximum temperature')
+@xw.ret(index=False, header=False, expand='down')
+def DegreeDayDynamicArray(model_name, T_min, T_max):
+    """Function to assemble a dataframe for calculating Degree Day using dynamic arrays. Takes either a name of a model or the named range itself.
+
+    :param model: Named Range of the model which will be evaluated. The Excel cell name / named range
+    :param T_min: Daily minimum temperature
+    :param T_max: Daily maximum temperature
+    """
+    try:
+        prepared_model_name = fk.load_model(model_name)
+
+        # format the equation terms, evalueate the equation and return the result.
+        inputs_for_DegreeDay = pd.DataFrame({'T_min': T_min, 'T_max': T_max})
+        return fk.evaluate_koala_model(prepared_model_name, inputs_for_DegreeDay)
+
+    except Exception as err:
+            return str(err)
---
+++ packages/flyingkoala/flyingkoala-0.0.10b1.tar.gz/flyingkoala-0.0.10b1/setup.py
@@ -0,0 +1,72 @@
+"""
+FlyingKoala provides the ability to dynamically define Python calculations from Excel formulas enabling you to replace the Excel calculation engine with Python with a genuine minimum of Python code.
+
+The main benefit of replacing existing formulas with FyingKoala is that you get faster calculation, less need to get your hands dirty and can unit test your formulas. It speeds up scenario analysis and model development
+
+Two users;
+Those who want to easily add python functions to excel
+Those who want to speed up their existing crazy big models
+
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flyingkoala",
+    version="0.0.10b1",
+    author="Bradley van Ree",
+    author_email="flyingkoala@bradbase.net",
+    description="Integration of xlwings and xlcalculator with MS Excel plug-in",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['xls',
+        'excel',
+        'spreadsheet',
+        'workbook',
+        'vba',
+        'macro',
+        'data analysis',
+        'analysis'
+        'reading excel',
+        'excel formula',
+        'excel formulas',
+        'excel equations',
+        'excel equation',
+        'formula',
+        'formulas',
+        'equation',
+        'equations',
+        'pandas',
+        'harvest',
+        'timeseries',
+        'time series',
+        'energy',
+        'accounting',
+        'horticulture',
+        'research',
+        'visualization',
+        'scenario analysis',
+        'modelling',
+        'model',
+        'unit testing',
+        'testing',
+        'audit'],
+    url="https://github.com/bradbase/flyingkoala",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+    ],
+    install_requires=[
+            'xlwings >= 0.15.8',
+            'numpy >= 1.15.0',
+            'pandas >= 0.25.0',
+            'xlfunctions <= 0.0.3b0',
+            'xlcalculator <= 0.0.5b0',
+        ]
+)
