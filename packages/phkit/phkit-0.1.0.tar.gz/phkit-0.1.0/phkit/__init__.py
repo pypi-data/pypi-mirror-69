4077315f02191473cac1f62b178aeab26ba44475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:11 +0000

    Add package phkit/0.1.0/phkit-0.1.0.tar.gz

---
+++ packages/phkit/phkit-0.1.0.tar.gz/phkit-0.1.0/phkit/__init__.py
@@ -0,0 +1,65 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+# author: kuangdd
+# date: 2020/2/17
+"""
+## phkit
+phoneme toolkit: 音素相关的文本处理工具箱。
+
+### 安装
+
+```
+pip install -U phkit
+```
+
+### v0.1.0
+- Add English phonemes and text_to_sequence.
+- Add simple say number for Chinese.
+
+todo:
+文本正则化处理
+数字读法
+字符读法
+常见规则读法
+
+
+文本转拼音
+pypinyin
+国标和alnum转换
+
+anything转音素
+字符
+英文
+汉字
+OOV
+
+进阶:
+分词
+命名实体识别
+依存句法分析
+"""
+
+__version__ = "0.1.0"
+
+version_doc = """
+### 版本
+v{}
+""".format(__version__)
+
+from .symbol import __doc__ as doc_symbol
+from .sequence import __doc__ as doc_sequence
+from .pinyin import __doc__ as doc_pinyin
+from .phoneme import __doc__ as doc_phoneme
+from .number import __doc__ as doc_number
+from .convert import __doc__ as doc_convert
+from .english import __doc__ as doc_english
+
+from .convert import fan2jian, jian2fan, quan2ban, ban2quan
+from .number import say_digit, say_decimal, say_number
+from .pinyin import text2pinyin, split_pinyin
+from .sequence import text2sequence, text2phoneme, pinyin2phoneme, phoneme2sequence, sequence2phoneme
+from .sequence import symbol_chinese, ph2id_dict, id2ph_dict
+from .english import text_to_sequence as english_text_to_sequence, sequence_to_text as english_sequence_to_text
+
+if __name__ == "__main__":
+    print(__file__)
