7d94cf79f736e2ac4a696a6581c56ec09d88d22d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:38 +0000

    Add package pl-face-recognition/0.0.1/pl_face_recognition-0.0.1.tar.gz

---
+++ packages/pl-face-recognition/pl_face_recognition-0.0.1.tar.gz/pl_face_recognition-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pl_face_recognition",
+    version="0.0.1",
+    author="Platanus",
+    author_email="andres.cadiz@platan.us",
+    description="A package for out of the box face recognition learning",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/platanus/face-recognition",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
