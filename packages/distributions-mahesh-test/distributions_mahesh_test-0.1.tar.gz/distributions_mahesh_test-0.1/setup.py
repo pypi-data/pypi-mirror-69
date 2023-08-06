b0f19ded764dd75de99e8309be7d806118ae9483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:53 +0000

    Add package distributions-mahesh-test/0.1/distributions_mahesh_test-0.1.tar.gz

---
+++ packages/distributions-mahesh-test/distributions_mahesh_test-0.1.tar.gz/distributions_mahesh_test-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_mahesh_test',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_mahesh_test'],
+      author="Mahesh Sonavane",
+      author_email="mahesh.sonavane14@hotmail.com",
+      zip_safe=False)
