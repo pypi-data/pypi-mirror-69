5edc8e90bb7ce3c7d4f7119de962ee78f1b49acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:17 +0000

    Add package Viditkj-guassian1/0.3/Viditkj_guassian1-0.3.tar.gz

---
+++ packages/Viditkj-guassian1/Viditkj_guassian1-0.3.tar.gz/Viditkj_guassian1-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Viditkj_guassian1',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Vidit Kumar Jain',
+      author_email= 'viditkj25@gmail.com',
+      zip_safe=False)
