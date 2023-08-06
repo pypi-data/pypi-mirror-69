f401f26176d500f0e6037fd7804512d1ea59cdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:27 +0000

    Add package stytra/0.8.33/stytra-0.8.33-py2.py3-none-any.whl

---
+++ packages/stytra/stytra-0.8.33-py2.py3-none-any.whl/stytra/__init__.py
@@ -0,0 +1,6 @@
+__author__ = """Vilim Stih & Luigi Petrucco @portugueslab"""
+__version__ = "0.8.33"
+
+from stytra.core import Stytra
+from stytra.stimulation import Protocol
+from stytra.metadata import AnimalMetadata, GeneralMetadata
