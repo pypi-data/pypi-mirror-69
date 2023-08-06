26244c3de566c1cadd48fb6c412ef2fabe7a6cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:41 +0000

    Add package developmental-index/1.0/developmental_index-1.0.tar.gz

---
+++ packages/developmental-index/developmental_index-1.0.tar.gz/developmental_index-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'developmental_index',
+    version = '1.0',
+    author = 'Ben Devlin',
+    author_email = 'bad36@duke.edu',
+    packages = find_packages('numpy', 'pandas', 'scipy')
+
+)
