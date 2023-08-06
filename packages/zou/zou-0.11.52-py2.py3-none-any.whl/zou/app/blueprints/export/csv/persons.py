29a506cd3835530c90b3cf6c887383e3d5fb0bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:09 +0000

    Add package zou/0.11.52/zou-0.11.52-py2.py3-none-any.whl

---
+++ packages/zou/zou-0.11.52-py2.py3-none-any.whl/zou/app/blueprints/export/csv/persons.py
@@ -0,0 +1,24 @@
+from zou.app.blueprints.export.csv.base import BaseCsvExport
+
+from zou.app.models.person import Person
+
+
+class PersonsCsvExport(BaseCsvExport):
+    def __init__(self):
+        BaseCsvExport.__init__(self, Person)
+        self.file_name = "people_export"
+
+    def build_headers(self):
+        return ["Last Name", "First Name", "Email", "Phone", "Role"]
+
+    def build_query(self):
+        return self.model.query.order_by(Person.last_name, Person.first_name)
+
+    def build_row(self, person):
+        return [
+            person.last_name,
+            person.first_name,
+            person.email,
+            person.phone,
+            person.role,
+        ]
