6badccee10c5dab3b3559200e1e40667e0defc04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:49 +0000

    Add package abhi-probability/0.1/abhi_probability-0.1.tar.gz

---
+++ packages/abhi-probability/abhi_probability-0.1.tar.gz/abhi_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='abhi_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['abhi_probability'],
+      author = 'Abhi G',
+      author_email = 'abhi@yahoo.com',
+      zip_safe=False)
