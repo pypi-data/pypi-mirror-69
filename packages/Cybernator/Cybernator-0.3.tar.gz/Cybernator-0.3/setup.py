a562822648cf78c4335b8ceb453c9bd02403c1b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:57 +0000

    Add package Cybernator/0.3/Cybernator-0.3.tar.gz

---
+++ packages/Cybernator/Cybernator-0.3.tar.gz/Cybernator-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Cybernator',
+      version='0.3',
+      description='Paginator',
+      packages=['Cybernator'],
+      author_email='rucybernetic@yandex.ru',
+      zip_safe=False)
