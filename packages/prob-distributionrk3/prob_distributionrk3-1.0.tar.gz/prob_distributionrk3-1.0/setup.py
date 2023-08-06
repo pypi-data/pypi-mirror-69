fa8cf3952e5a3bf29b352bf5d55d521231b60bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:13 +0000

    Add package prob-distributionrk3/1.0/prob_distributionrk3-1.0.tar.gz

---
+++ packages/prob-distributionrk3/prob_distributionrk3-1.0.tar.gz/prob_distributionrk3-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_distributionrk3',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['prob_distributionrk3'],
+      zip_safe=False)
