88906c874e68b31ed0bd0a50ce739c4dbc88641d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:20 +0000

    Add package pyweathercli/0.0.4/pyweathercli-0.0.4.tar.gz

---
+++ packages/pyweathercli/pyweathercli-0.0.4.tar.gz/pyweathercli-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+ 
+with open("README.md", "r", encoding="utf8") as fh:
+    long_desc = fh.read()
+
+setup(
+    name="pyweathercli",
+    version="0.0.4",
+    author="Gavyn Stanley",
+    description="A cli using click for weather",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/A-Trash-Coder/pyweathercli",
+    packages=find_packages(),
+    package_data={'pyweathercli': ['*.json']},
+    install_requires=["click", "pyfiglet", "pyowm", "colorama"],
+    entry_points={
+        "console_scripts":["weather=pyweathercli.cli:weather"]
+    }
+)
