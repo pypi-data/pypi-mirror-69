55119851db8bbd97d75d333f581a5055e686312a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:21 +0000

    Add package python-webex-bot/0.76/python_webex_bot-0.76.tar.gz

---
+++ packages/python-webex-bot/python_webex_bot-0.76.tar.gz/python_webex_bot-0.76/setup.py
@@ -0,0 +1,59 @@
+import setuptools
+
+required = None
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_webex_bot",
+    version="0.76",
+    scripts=["python_webex_bot"],
+    author="Paul-weqe",
+    author_email="paul1tw1@gmail.com",
+    description="Enable incoming message information to be seen",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Paul-weqe/python_webex_bot",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "atomicwrites",
+        "attrs",
+        "backcall",
+        "certifi",
+        "chardet",
+        "Click",
+        "colorama",
+        "coverage",
+        "decorator",
+        "Flask",
+        "idna",
+        "itsdangerous",
+        "jedi",
+        "Jinja2",
+        "MarkupSafe",
+        "more-itertools",
+        "parso",
+        "pickleshare",
+        "pluggy",
+        "prompt-toolkit",
+        "py",
+        "Pygments",
+        "pytz",
+        "requests",
+        "six",
+        "traitlets",
+        "urllib3",
+        "virtualenv",
+        "wcwidth",
+        "Werkzeug"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
