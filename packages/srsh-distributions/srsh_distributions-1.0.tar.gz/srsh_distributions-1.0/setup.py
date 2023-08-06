3cbf508620390fa72ef1f7c47fab83cdd715bec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:12 +0000

    Add package srsh-distributions/1.0/srsh_distributions-1.0.tar.gz

---
+++ packages/srsh-distributions/srsh_distributions-1.0.tar.gz/srsh_distributions-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+    
+setup(name='srsh_distributions',
+      version='1.0',
+      description='Gaussian distribution and Binomial distribution',
+      packages=['srsh_distributions'],
+      author='Srinivas Shekar',
+      author_email='srinivasshekar10@gmail.com',
+      zip_safe=False)
