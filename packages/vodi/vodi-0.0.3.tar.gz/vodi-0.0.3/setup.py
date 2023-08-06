3ada07f57213106c80ab98f14254188c022e9ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:41 +0000

    Add package vodi/0.0.3/vodi-0.0.3.tar.gz

---
+++ packages/vodi/vodi-0.0.3.tar.gz/vodi-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="vodi", # Replace with your own username
+    version="0.0.3",
+    author="Andrea Caligiuri",
+    author_email="andrea.caligiuri@vodafone.com",
+    description="a vodafone digital automation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=find_packages(),
+    scripts=['bin/vodi'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    package_dir={'vodi': 'vodi'},
+    install_requires=[
+            'click',
+            'atlassian-python-api'
+        ]
+
+)
---
+++ packages/vodi/vodi-0.0.3.tar.gz/vodi-0.0.3/vodi/__init__.py
@@ -0,0 +1,4 @@
+import atlassian
+import click
+
+__all__= ["EnvCommand", "Environment", "Environments", "LoginCommand", "ProjectCommand", "Projects", "Release", "ReleaseCommand", "vodi"]
