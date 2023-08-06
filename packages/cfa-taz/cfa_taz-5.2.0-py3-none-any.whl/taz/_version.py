79b7ea27580d16f64bca7bfd27ec266d7474a886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:50 +0000

    Add package cfa-taz/5.2.0/cfa_taz-5.2.0-py3-none-any.whl

---
+++ packages/cfa-taz/cfa_taz-5.2.0-py3-none-any.whl/taz/_version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------
+# hermes: _version.py
+#
+# Copyright (C) 2018-2020, Christophe Fauchard
+# -----------------------------------------------------------------
+
+__version_info__ = (5, 2, 0)
+
+__version__ = ".".join(map(str, __version_info__))
