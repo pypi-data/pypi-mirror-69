a343d8a26884a9a5be65d30d3ffb9a12181671aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:38 +0000

    Add package dnd-stats/0.1/dnd_stats-0.1.tar.gz

---
+++ packages/dnd-stats/dnd_stats-0.1.tar.gz/dnd_stats-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dnd_stats',
+      version='0.1',
+      description='Probability Distributions and Statistics Methods',
+      packages=['dnd_stats'],
+      author='Divy Pandya',
+      zip_safe=False)
