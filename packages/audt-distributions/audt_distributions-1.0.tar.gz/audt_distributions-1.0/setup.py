a9d769a7c0b9a66d5ee36a9aa91b93c52837bf13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:28:56 +0000

    Add package audt-distributions/1.0/audt_distributions-1.0.tar.gz

---
+++ packages/audt-distributions/audt_distributions-1.0.tar.gz/audt_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='audt_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['audt_distributions'],
+      author='AudST',
+      author_email='contactme@gmail.com',
+      zip_safe=False)
