b1670f5ffc84eb926df4177c02923ef99f8c2051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:34 +0000

    Add package savimonty-distributions/0.1/savimonty_distributions-0.1.tar.gz

---
+++ packages/savimonty-distributions/savimonty_distributions-0.1.tar.gz/savimonty_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='savimonty_distributions',
+      version='0.1',
+      description='Probability Distributions',
+      packages=['savimonty_distributions'],
+      zip_safe=False)
