17193a7e5eb40a3238f42bbf755be6f9ac1d8c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:57 +0000

    Add package BiGa-distributions/1.2/BiGa_distributions-1.2.tar.gz

---
+++ packages/BiGa-distributions/BiGa_distributions-1.2.tar.gz/BiGa_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='BiGa_distributions',
+      version='1.2',
+      description='Binomial and Gaussian distributions',
+      packages=['BiGa_distributions'],
+      author= 'Farha Haider' ,
+      author_email='farhahaider2017@gmail.com',
+    zip_safe=False)
