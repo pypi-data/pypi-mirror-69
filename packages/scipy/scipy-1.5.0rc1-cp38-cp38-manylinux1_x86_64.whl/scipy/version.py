d1b22e7b14a0838d32c489ebed570a41c65b645f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:20 +0000

    Add package scipy/1.5.0rc1/scipy-1.5.0rc1-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/scipy/scipy-1.5.0rc1-cp38-cp38-manylinux1_x86_64.whl/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.5.0rc1'
+version = '1.5.0rc1'
+full_version = '1.5.0rc1'
+git_revision = 'ff72ef8d8ef64e9a125d3fca0dec1c0cb5b6c38d'
+release = True
+
+if not release:
+    version = full_version
