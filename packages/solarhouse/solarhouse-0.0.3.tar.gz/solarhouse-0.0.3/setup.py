6a778598a81846bc54c66db47403ed0407a50d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:15 +0000

    Add package solarhouse/0.0.3/solarhouse-0.0.3.tar.gz

---
+++ packages/solarhouse/solarhouse-0.0.3.tar.gz/solarhouse-0.0.3/setup.py
@@ -0,0 +1,41 @@
+import os
+import setuptools
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="solarhouse",  # Replace with your own username
+    version="0.0.3",
+    author="Yaric Pisarev",
+    author_email="yaricp@gmail.com",
+    description="package for calculate solar power what you can get\
+                on surfaces of building by solar heat collectors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yaricp/py-solarhouse",
+    package_dir={'solarhouse': 'src/solarhouse'},
+    packages=['solarhouse'],
+    install_requires=[
+        'numpy',
+        'scipy',
+        'trimesh',
+        'pvlib',
+        'pandas',
+        'matplotlib',
+        'mpld3',
+        'shapely',
+        'jinja2',
+        'netCDF4',
+        'siphon',
+        'tables',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
