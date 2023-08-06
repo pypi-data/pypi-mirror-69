6f68f937abf6f305dfaa7de4a64756f90192f781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:58 +0000

    Add package heaps/0.0.1/heaps-0.0.1.tar.gz

---
+++ packages/heaps/heaps-0.0.1.tar.gz/heaps-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='heaps',
+    packages=[],
+    version='0.0.1',
+    license='MIT',
+    description='Better Heaps for python',
+    author='Ankit Kathuria',
+    author_email='ankitkathuria534@gmail.com',
+    url='https://github.com/gekco/pyheap',
+    keywords=['Heap', 'python', 'ordered heaps', 'easy', 'priority queue'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
