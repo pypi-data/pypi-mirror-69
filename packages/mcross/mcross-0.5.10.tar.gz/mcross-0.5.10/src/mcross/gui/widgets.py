e73011630b519a95228686f865edc32b95912f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:41 +0000

    Add package mcross/0.5.10/mcross-0.5.10.tar.gz

---
+++ packages/mcross/mcross-0.5.10.tar.gz/mcross-0.5.10/src/mcross/gui/widgets.py
@@ -0,0 +1,36 @@
+from idlelib.redirector import WidgetRedirector
+from tkinter import Text, ttk
+
+
+# Can't just use a Text widget with state='disabled' because that would
+# also disable Ctrl+C / Ctrl+V.
+# Further low-level reading:
+# https://wiki.tcl-lang.org/page/Read%2Donly+text+widget
+class ReadOnlyText(Text):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.redirector = WidgetRedirector(self)
+        self.insert = self.redirector.register("insert", lambda *args, **kw: "break")
+        self.delete = self.redirector.register("delete", lambda *args, **kw: "break")
+
+        self.bind("<Control-a>", self._on_ctrl_a)
+
+    def _on_ctrl_a(self, ev):
+        self.tag_add("sel", "1.0", "end")
+        return "break"
+
+
+class McEntry(ttk.Entry):
+    """
+    Entry widget with reasonable defaults
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.bind("<Control-a>", self.select_all)
+
+    def select_all(self, ev=None):
+        self.select_range(0, "end")
+        self.icursor("end")
+        return "break"
