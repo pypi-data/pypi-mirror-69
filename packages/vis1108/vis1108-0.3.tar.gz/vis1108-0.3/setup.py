06c90342a986844634281a6f9e328ceea7b181fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:59 +0000

    Add package vis1108/0.3/vis1108-0.3.tar.gz

---
+++ packages/vis1108/vis1108-0.3.tar.gz/vis1108-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vis1108',
+      version='0.3',
+      description='Gaussian and binomial distributions',
+      packages=['vis1108'],
+      author = 'Vishnusai',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
