34a5e0717135e465aa23f415f093df8c11aa4f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:13 +0000

    Add package dist-uploadTest-exercise/0.1/dist_uploadTest_exercise-0.1.tar.gz

---
+++ packages/dist-uploadTest-exercise/dist_uploadTest_exercise-0.1.tar.gz/dist_uploadTest_exercise-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_uploadTest_exercise',
+      version='0.1',
+      description='Gaussian distributions, Binomial distributions',
+      packages=['dist_uploadTest_exercise'],
+      zip_safe=False)
