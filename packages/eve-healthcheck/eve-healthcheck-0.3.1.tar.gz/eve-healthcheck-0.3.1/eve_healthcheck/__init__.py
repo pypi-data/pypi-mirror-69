9d6956338943c4ba4ae660f651976aedc40c4c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:31 +0000

    Add package eve-healthcheck/0.3.1/eve-healthcheck-0.3.1.tar.gz

---
+++ packages/eve-healthcheck/eve-healthcheck-0.3.1.tar.gz/eve-healthcheck-0.3.1/eve_healthcheck/__init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import logging
+from functools import partial
+
+from eve.render import send_response
+from healthcheck import HealthCheck
+
+
+def view_health_func(hc, resource=None):
+    message, status, headers = hc.run()
+    response = (
+        json.loads(message),
+        None,
+        None,
+        status,
+        None,
+    )
+
+    resp = send_response(resource, response)
+    [resp.headers.set(k, v) for k, v in headers.items()]
+    return resp
+
+
+def check_wrapper(app):
+    def database_check():
+        # perform a count
+        for k, v in app.config.get('DOMAIN', {}).items():
+            result = app.data.is_empty(k)
+            assert type(result) == bool, "Database NOK"
+        return True, "Database OK"
+
+    return database_check
+
+
+class EveHealthCheck(object):
+    def __init__(self, app=None, healthcheck_uri='/healthcheck', *argv, **kw):
+        self.logger = logging.getLogger(self.__module__)
+        self.hc = HealthCheck(*argv, **kw)
+
+        if app:
+            self.init_app(app, healthcheck_uri)
+
+    def init_app(self, app, healthcheck_uri='/healthcheck'):
+        self.hc.add_check(check_wrapper(app))
+
+        uri = "{}{}".format(app.api_prefix, healthcheck_uri)
+
+        app.add_url_rule(uri, uri,
+                         view_func=partial(view_health_func, self.hc),
+                         methods=['GET', 'OPTIONS'])
+        for resource, v in app.config.get('DOMAIN', {}).items():
+            uri = "{}/{}{}".format(app.api_prefix, resource, healthcheck_uri)
+            self.logger.debug("Adding uri {}".format(uri))
+            app.add_url_rule(uri, uri,
+                             view_func=partial(view_health_func, self.hc, resource),
+                             methods=['GET', 'OPTIONS'])
---
+++ packages/eve-healthcheck/eve-healthcheck-0.3.1.tar.gz/eve-healthcheck-0.3.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from os.path import abspath, dirname, join
+
+import setuptools
+
+__version__ = "0.3.1"
+__project_name__ = "eve-healthcheck"
+__repo__ = "https://github.com/ateliedocodigo/eve-healthcheck"
+
+
+def read(fname):
+    with open(join(abspath(dirname(__file__)), fname)) as thefile:
+        return thefile.read()
+    # return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setuptools.setup(
+    name=__project_name__,
+    version=__version__,
+    description="Python {} project".format(__project_name__),
+    long_description=read("README.rst"),
+    author="Luis Fernando Gomes",
+    author_email="luiscoms@ateliedocodigo.com.br",
+    url=__repo__,
+    download_url="{}/tarball/{}".format(__repo__, __version__),
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="healthcheck eve_healthcheck",
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    install_requires=read("requirements.txt"),
+)
