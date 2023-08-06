dcd92e7632fc80a9005e323678d266756b923874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:17 +0000

    Add package hahahaha-distributions/0.1/hahahaha_distributions-0.1.tar.gz

---
+++ packages/hahahaha-distributions/hahahaha_distributions-0.1.tar.gz/hahahaha_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hahahaha_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hahahaha_distributions'],
+      zip_safe=False)
