39415797f745611af28870cc1c1d5765eb7ef8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:40 +0000

    Add package compiled-distributions/0.1/compiled_distributions-0.1.tar.gz

---
+++ packages/compiled-distributions/compiled_distributions-0.1.tar.gz/compiled_distributions-0.1/README.md
@@ -0,0 +1,2 @@
+# compiled-distributions package
+
---
+++ packages/compiled-distributions/compiled_distributions-0.1.tar.gz/compiled_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='compiled_distributions',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['compiled_distributions'],
+      author = 'LEE Hong Kai',
+      author_email = 'leehongkai@gmail.com',
+      zip_safe=False)
