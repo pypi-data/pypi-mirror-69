7bb2eea2f287308279f12251649d5caca3fdd8ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:32 +0000

    Add package raylab/0.6.6/raylab-0.6.6.tar.gz

---
+++ packages/raylab/raylab-0.6.6.tar.gz/raylab-0.6.6/raylab/agents/mapo/__init__.py
@@ -0,0 +1,10 @@
+"""Model-Aware Policy Optimization."""
+
+from .mapo import MAPOTrainer
+from .mapo_policy import MAPOTorchPolicy
+
+
+__all__ = [
+    "MAPOTrainer",
+    "MAPOTorchPolicy",
+]
---
+++ packages/raylab/raylab-0.6.6.tar.gz/raylab-0.6.6/raylab/agents/svg/__init__.py
@@ -0,0 +1,8 @@
+"""Learning Continuous Control Policies by Stochastic Value Gradients.
+
+http://papers.nips.cc/paper/5796-learning-continuous-control-policies-by-stochastic-value-gradients
+"""
+
+from .soft_svg import SoftSVGTrainer
+from .svg_inf import SVGInfTrainer
+from .svg_one import SVGOneTrainer
