6c5283bdbeadf5ca37f8832d42339cfc1e3cb436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:05 +0000

    Add package tpDcc-libs-qt/0.0.14/tpDcc_libs_qt-0.0.14-py3.6.egg

---
+++ packages/tpDcc-libs-qt/tpDcc_libs_qt-0.0.14-py3.6.egg/tpDcc/libs/qt/widgets/library/__init__.py
@@ -0,0 +1,16 @@
+order = [
+    'tpDcc.libs.qt.widgets.library.consts',
+    'tpDcc.libs.qt.widgets.library.exceptions',
+    'tpDcc.libs.qt.widgets.library.manager',
+    'tpDcc.libs.qt.widgets.library.utils',
+    'tpDcc.libs.qt.widgets.library.library',
+    'tpDcc.libs.qt.widgets.library.widgets',
+    'tpDcc.libs.qt.widgets.library.mixin',
+    'tpDcc.libs.qt.widgets.library.savewidget',
+    'tpDcc.libs.qt.widgets.library.loadwidget',
+    'tpDcc.libs.qt.widgets.library.items',
+    'tpDcc.libs.qt.widgets.library.listview',
+    'tpDcc.libs.qt.widgets.library.treewidget',
+    'tpDcc.libs.qt.widgets.library.viewer',
+    'tpDcc.libs.qt.widgets.library.window'
+]
