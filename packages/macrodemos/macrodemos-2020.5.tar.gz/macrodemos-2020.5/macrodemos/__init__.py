b8c9e8a7433fe9ca9b06c0209bd4b54004cbc226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:34 +0000

    Add package macrodemos/2020.5/macrodemos-2020.5.tar.gz

---
+++ packages/macrodemos/macrodemos-2020.5.tar.gz/macrodemos-2020.5/macrodemos/__init__.py
@@ -0,0 +1,8 @@
+"""
+macrodemos: A Python module to teach macroeconomics and macroeconometrics
+
+Randall Romero Aguilar
+2016-2020
+"""
+from .demo_ARMA import ARMA_demo
+from .demo_Solow import Solow_demo
---
+++ packages/macrodemos/macrodemos-2020.5.tar.gz/macrodemos-2020.5/setup.py
@@ -0,0 +1,32 @@
+""" The macrodemos package setup.
+Based on setuptools
+
+Randall Romero-Aguilar, 2016-2020
+"""
+
+from setuptools import setup
+from codecs import open
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='macrodemos',
+    version='2020.05',
+    description='Demos to learn macroeconomics and macro-econometrics concepts',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Randall Romero-Aguilar',
+    author_email='randall.romero@outlook.com',
+    url='http://randall-romero.com/code/macrodemos',
+    license='MIT',
+    keywords='time series, ARMA',
+    packages=['macrodemos'],
+    python_requires='>=3.7',
+    install_requires=['pandas', 'numpy', 'plotly', 'dash', 'statsmodels'],
+    include_package_data=False
+)
+
+
+
