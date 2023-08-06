91d3b00938d005320d62890a79421ae2dcd7bfeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:42 +0000

    Add package scikit-mpe/0.2.2/scikit_mpe-0.2.2-py3-none-any.whl

---
+++ packages/scikit-mpe/scikit_mpe-0.2.2-py3-none-any.whl/CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+
+## v0.2.2 (28.05.2020)
+
+- Update documentation: add maze example and low-level api section 
+
+## v0.2.1 (27.05.2020)
+
+- Fix building docs on ReadTheDocs
+
+## v0.2.0 (27.05.2020)
+
+- Refactoring the package with changing some low-level API
+- Add documentation
+
+## v0.1.1
+
+- Fix links
+- Update readme
+
+## v0.1.0
+
+- Initial release
---
+++ packages/scikit-mpe/scikit_mpe-0.2.2-py3-none-any.whl/skmpe/__init__.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from importlib_metadata import version, PackageNotFoundError
+
+from ._base import (
+    InitialInfo,
+    PathInfo,
+    PathInfoResult,
+)
+
+from ._parameters import (
+    TravelTimeOrder,
+    OdeSolverMethod,
+    Parameters,
+    parameters,
+    default_parameters,
+)
+
+from ._exceptions import (
+    MPEError,
+    ComputeTravelTimeError,
+    PathExtractionError,
+    EndPointNotReachedError,
+)
+
+from ._mpe import (
+    PathExtractionResult,
+    MinimalPathExtractor,
+)
+
+from skmpe._api import mpe
+
+
+try:
+    __version__ = version('scikit-mpe')
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = '0.0.0.dev'
+
+
+__all__ = [
+    'InitialInfo',
+    'PathInfo',
+    'PathInfoResult',
+
+    'TravelTimeOrder',
+    'OdeSolverMethod',
+    'Parameters',
+    'parameters',
+    'default_parameters',
+
+    'MPEError',
+    'ComputeTravelTimeError',
+    'PathExtractionError',
+    'EndPointNotReachedError',
+
+    'PathExtractionResult',
+    'MinimalPathExtractor',
+    'mpe',
+]
