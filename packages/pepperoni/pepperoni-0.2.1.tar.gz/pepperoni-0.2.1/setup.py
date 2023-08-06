81190e9c488d1d6e07455d9ca88b91edb1735f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:58 +0000

    Add package pepperoni/0.2.1/pepperoni-0.2.1.tar.gz

---
+++ packages/pepperoni/pepperoni-0.2.1.tar.gz/pepperoni-0.2.1/setup.py
@@ -0,0 +1,39 @@
+"""Setup."""
+
+import setuptools
+import pepperoni
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+install_requires = ['sqlalchemy>=1.3.1']
+
+
+name = 'pepperoni'
+author = pepperoni.__author__
+email = pepperoni.__email__
+version = pepperoni.__version__
+description = 'Developer-friendly Python toolkit.'
+license = pepperoni.__license__
+
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license=license,
+    url='https://github.com/t3eHawk/pepperoni',
+    install_requires=install_requires,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
