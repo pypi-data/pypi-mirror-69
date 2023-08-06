42f1dc6188dfa33fac5d17e6a1c70b18431ca20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:52 +0000

    Add package nexa/1.0/nexa-1.0.tar.gz

---
+++ packages/nexa/nexa-1.0.tar.gz/nexa-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nexa',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['nexa'],
+      author= 'Ashwin Raj',
+      author_email= 'rajashwin812@gmail.com',
+      zip_safe=False)
