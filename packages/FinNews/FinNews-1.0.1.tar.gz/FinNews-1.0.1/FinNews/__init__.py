1da921886e62c844829d6360c1c710560cc37386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:18 +0000

    Add package FinNews/1.0.1/FinNews-1.0.1.tar.gz

---
+++ packages/FinNews/FinNews-1.0.1.tar.gz/FinNews-1.0.1/FinNews/__init__.py
@@ -0,0 +1,8 @@
+from FinNews.feed import Feed
+from FinNews.sources import *
+
+name = "FinNews"
+
+__version__ = "1.0.1"
+
+__all__ = ['Feed', 'Source', 'CNBC', 'SeekingAlpha', 'Investing', 'WSJ', 'Yahoo', 'FT', 'Fortune', 'MarketWatch', 'Zacks', 'Nasdaq', 'Reddit']
