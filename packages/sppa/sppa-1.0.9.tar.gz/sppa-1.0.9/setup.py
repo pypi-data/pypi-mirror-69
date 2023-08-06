345bcde4a72219723addcff49435c8453c9784c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:51 +0000

    Add package sppa/1.0.9/sppa-1.0.9.tar.gz

---
+++ packages/sppa/sppa-1.0.9.tar.gz/sppa-1.0.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='sppa',
+    version='1.0.9',
+    packages=['sppa'],
+    install_requires=['numpy', 'pulp'],
+    url='https://github.com/jamespltan/sppa',
+	download_url='https://github.com/jamespltan/sppa/archive/v1.0.9.tar.gz',
+    license='MIT',
+    author='James Tan',
+    author_email='jamestan@ntu.edu.sg',
+    description='SPPA MINLP solver',
+	classifiers=['Programming Language :: Python :: 3'],
+)
