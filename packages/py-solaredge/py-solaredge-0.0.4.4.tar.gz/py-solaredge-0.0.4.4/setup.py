251917f1318c55f37c6cbc0fcad69d436d1a37d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:57 +0000

    Add package py-solaredge/0.0.4.4/py-solaredge-0.0.4.4.tar.gz

---
+++ packages/py-solaredge/py-solaredge-0.0.4.4.tar.gz/py-solaredge-0.0.4.4/setup.py
@@ -0,0 +1,41 @@
+import os.path
+import re
+
+from setuptools import find_packages, setup
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_long_description():
+    long_description = []
+    with open(os.path.join("README.md")) as f:
+        long_description.append(f.read())
+    return "\n".join(long_description)
+
+setup(
+    name='py-solaredge',
+    version='0.0.4.4',
+    license='BSD',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Python API wrapper for SolarEdge monitoring services',
+    author='Yenthe Van Ginneken',
+    author_email='yenthevg@gmail.com',
+    maintainer='Yenthe Van Ginneken',
+    maintainer_email='yenthevg@gmail.com',
+    keywords=['SolarEdge', 'SolarEdge API', 'SolarEdge Monitoring', 'SolarEdge Monitoring API'],
+    url='https://github.com/Yenthe666/pysolaredge',
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Scientific/Engineering',
+    ],
+)
