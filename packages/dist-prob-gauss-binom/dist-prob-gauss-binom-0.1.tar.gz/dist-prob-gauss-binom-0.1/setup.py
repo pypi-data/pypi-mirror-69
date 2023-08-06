846d84ab8699f019ea906cc102ee549ff65d5275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:29 +0000

    Add package dist-prob-gauss-binom/0.1/dist-prob-gauss-binom-0.1.tar.gz

---
+++ packages/dist-prob-gauss-binom/dist-prob-gauss-binom-0.1.tar.gz/dist-prob-gauss-binom-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist-prob-gauss-binom',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist-prob-gauss-binom'],
+      author = 'Shweta Yadav',
+      author_email = 'datascientist.shweta@gmail.com',
+      zip_safe=False)
