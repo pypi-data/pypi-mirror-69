6c4c153f8238bdf0466f816a291065d2fe4cbd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:38 +0000

    Add package kmeans-bestk-chen/0.0.1/kmeans-bestk-chen-0.0.1.tar.gz

---
+++ packages/kmeans-bestk-chen/kmeans-bestk-chen-0.0.1.tar.gz/kmeans-bestk-chen-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kmeans-bestk-chen", # Replace with your own username
+    version="0.0.1",
+    author="searphYC",
+    author_email="zyc199685@gmail.com",
+    description="A small kmeans package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Seraph-YCZhang/Kmeans-code-assignment",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
