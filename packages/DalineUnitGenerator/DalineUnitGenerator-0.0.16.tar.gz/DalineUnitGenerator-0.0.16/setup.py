92a1b76a31dd14b81dcab0fded26591304b56dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:14 +0000

    Add package DalineUnitGenerator/0.0.16/DalineUnitGenerator-0.0.16.tar.gz

---
+++ packages/DalineUnitGenerator/DalineUnitGenerator-0.0.16.tar.gz/DalineUnitGenerator-0.0.16/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DalineUnitGenerator",
+    version="0.0.16",
+    author="kun.z",
+    author_email="kun.z@daline.com.cn",
+    description="test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DalineWH",
+    packages=['CodePackage'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords='daline PU',
+    install_requires=[
+        'requests',
+    ],
+)
