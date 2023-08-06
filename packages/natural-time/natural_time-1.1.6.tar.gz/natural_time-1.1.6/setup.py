15c41d008b59e2c44d0856e3f86697f7340d4b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:32 +0000

    Add package natural-time/1.1.6/natural_time-1.1.6.tar.gz

---
+++ packages/natural-time/natural_time-1.1.6.tar.gz/natural_time-1.1.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='natural_time',
+    version='1.1.6',
+    description='Tool for understanding dates and times',
+    long_description=long_description,
+    url='https://github.com/DavidAmison/natural_time',
+    author='David Amison',
+    author_email='david.amison20@gmail.com',
+    packages=['natural_time'],
+    install_requires=['python-dateutil', 'pathlib', 'textblob'],
+)
