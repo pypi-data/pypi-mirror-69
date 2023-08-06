70f0d5b8a9c7257a7cd0eb967eaebce30c4d59c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:29 +0000

    Add package manish-package/0.4/manish_package-0.4.tar.gz

---
+++ packages/manish-package/manish_package-0.4.tar.gz/manish_package-0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='manish_package',
+      version='0.4',
+      description='Gaussian distributions',
+      packages=['manish_package'],
+      author = 'Manish Bhurtel',
+      author_email = 'data_scientist@gmail.com',
+      zip_safe=False)
