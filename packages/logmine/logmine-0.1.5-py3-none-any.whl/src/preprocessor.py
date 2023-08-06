70b78105b636f14347b982a1c6d3ee1876728efe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:21 +0000

    Add package logmine/0.1.5/logmine-0.1.5-py3-none-any.whl

---
+++ packages/logmine/logmine-0.1.5-py3-none-any.whl/src/preprocessor.py
@@ -0,0 +1,38 @@
+import re
+from variable import Variable
+
+
+class Preprocessor():
+    def __init__(self, variables=[]):
+        parsed_variables = []
+        for variable in variables:
+            parts = variable.split(':')
+            if len(parts) <= 1:
+                raise Exception('Invalid variable fortmat')
+            name = parts[0]
+            wrapped_regex = ':'.join(parts[1:])
+            regex = wrapped_regex.split('/')[1]
+            parsed_variables.append((name, regex))
+        self.variables = map(
+            lambda (name, regex): (name, re.compile(regex)),
+            parsed_variables
+        )
+
+    def process(self, fields):
+        result = []
+
+        if len(self.variables) == 0:
+            return fields
+
+        for field in fields:
+            matched = False
+            for (name, regex) in self.variables:
+                if re.match(regex, field):
+                    matched = Variable(name, field)
+                    break
+
+            if matched:
+                result.append(matched)
+            else:
+                result.append(field)
+        return result
