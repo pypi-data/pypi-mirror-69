22fe4cd82e8cb0a5cb9bc311511ccc5c6a2f5bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:08 +0000

    Add package farhad-distributions/0.1/farhad_distributions-0.1.tar.gz

---
+++ packages/farhad-distributions/farhad_distributions-0.1.tar.gz/farhad_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='farhad_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['farhad_distributions'],
+      author='Farhad Kamal',
+      author_email='jitubd1971@gmail.com',
+      zip_safe=False)
