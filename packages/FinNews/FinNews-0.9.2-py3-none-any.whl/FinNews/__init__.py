6d22180eb010a543580bd45f7f0c37c26da66a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:36 +0000

    Add package FinNews/0.9.2/FinNews-0.9.2-py3-none-any.whl

---
+++ packages/FinNews/FinNews-0.9.2-py3-none-any.whl/FinNews/__init__.py
@@ -0,0 +1,8 @@
+from FinNews.feed import Feed
+from FinNews.sources import *
+
+name = "FinNews"
+
+__version__ = "0.9.2"
+
+__all__ = ['Feed', 'CNBC', 'SeekingAlpha', 'Investing', 'WSJ', 'Yahoo', 'FT', 'Fortune']
