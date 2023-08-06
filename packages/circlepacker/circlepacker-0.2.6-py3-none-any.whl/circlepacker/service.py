78d91cd9d5bea86a540df36f7b043eb48ba0b6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:54 +0000

    Add package circlepacker/0.2.6/circlepacker-0.2.6-py3-none-any.whl

---
+++ packages/circlepacker/circlepacker-0.2.6-py3-none-any.whl/circlepacker/service.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from circlepacker.routers import health, solves
+
+app = FastAPI()
+
+app.include_router(
+    health.router,
+    prefix="/health",
+    tags=["health"],
+    responses={404: {"description": "Not found"}},
+)
+app.include_router(
+    solves.router,
+    prefix="/solves",
+    tags=["solves"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@app.get("/")
+async def base():
+    return "Circle Packer"
