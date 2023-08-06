bfbd2b1c83fe3d7b0034e228eaba977676c18a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:35 +0000

    Add package prob-stat-dist/0.1/prob_stat_dist-0.1.tar.gz

---
+++ packages/prob-stat-dist/prob_stat_dist-0.1.tar.gz/prob_stat_dist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='prob_stat_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['prob_stat_dist'],
+      zip_safe=False)
