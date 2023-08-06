27c9cb9dd1b97f1d8fd499b0bae6769ebcf21d3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:45 +0000

    Add package distributions-package-oop/0.1/distributions_package_oop-0.1.tar.gz

---
+++ packages/distributions-package-oop/distributions_package_oop-0.1.tar.gz/distributions_package_oop-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_package_oop',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_package_oop'],
+      zip_safe=False,
+     author='Paul Owoicho',
+     author_email = 'paulowoicho@gmail.com')
