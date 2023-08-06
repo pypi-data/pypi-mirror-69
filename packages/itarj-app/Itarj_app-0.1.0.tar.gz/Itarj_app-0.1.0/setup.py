b37eaa68c36198cb273265c979d58fb5ccbdac96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:49 +0000

    Add package itarj-app/0.1.0/Itarj_app-0.1.0.tar.gz

---
+++ packages/itarj-app/Itarj_app-0.1.0.tar.gz/Itarj_app-0.1.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['Fake_job', 'jobs', 'User_Data']
+entry_points = \
+{'console_scripts': ['Itarj_app = Fake_job:choices']}
+
+setup_kwargs = {
+    'name': 'itarj-app',
+    'version': '0.1.0',
+    'description': 'ITARJ CONSOLE: A console app that allows users to register fake job alerts and allows others to view those posts by searching for keywords.',
+    'long_description': 'this is the initial readnme file\n',
+    'author': 'Emmanuel Chijioke aka DREL',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
