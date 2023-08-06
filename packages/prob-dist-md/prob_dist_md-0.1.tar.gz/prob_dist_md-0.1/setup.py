dd262da3b362b1ecd3a49c9c6d97ed9ed27697e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:20 +0000

    Add package prob-dist-md/0.1/prob_dist_md-0.1.tar.gz

---
+++ packages/prob-dist-md/prob_dist_md-0.1.tar.gz/prob_dist_md-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='prob_dist_md',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['prob_dist_md'],
+      author = 'Michael Diggin',
+      zip_safe=False)
